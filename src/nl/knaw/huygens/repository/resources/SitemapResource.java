179a3182f7d55d0cf5dc7b92c7532776274408f1
gijs <gijs@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2012-12-18 12:38:00 +0000

    Add sitemap (still slightly broken)

---
+++ src/nl/knaw/huygens/repository/resources/SitemapResource.java
@@ -0,0 +1,63 @@
+package nl.knaw.huygens.repository.resources;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import nl.knaw.huygens.repository.model.Document;
+import nl.knaw.huygens.repository.model.storage.Storage;
+import nl.knaw.huygens.repository.util.APIDesc;
+import nl.knaw.huygens.repository.util.JAXUtils;
+import nl.knaw.huygens.repository.util.JAXUtils.API;
+
+import com.google.common.collect.Lists;
+
+@Path("api")
+public class SitemapResource {
+  public static class Sitemap extends Document {
+    public List<API> availableAPIs;
+    
+    public Sitemap(Application app) {
+      setType("sitemap");
+      setId(String.format("T%1$010d", System.currentTimeMillis()));
+      List<API> rv = Lists.newArrayList();
+      Set<Class<?>> allResources = JAXUtils.getAllResources(app);
+      for (Class<?> cls : allResources) {
+        List<API> generatedAPIs = JAXUtils.generateAPIs(cls);
+        if (generatedAPIs != null) {
+          rv.addAll(generatedAPIs);
+        }
+      }
+      availableAPIs = rv;
+    }
+
+    @Override
+    public String getDescription() {
+      return "Repository Sitemap";
+    }
+
+    @Override
+    public void fetchAll(Storage storage) {
+      // No-op;
+    }
+  }
+
+  private static Sitemap lastSitemap;
+
+  @GET
+  @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON})
+  @APIDesc("Generates a structured sitemap.")
+  public Sitemap getSitemap(@QueryParam("refresh") boolean shouldRefresh, @Context Application app) {
+    if (shouldRefresh || lastSitemap == null) {
+      lastSitemap = new Sitemap(app);
+    }
+    return lastSitemap;
+  }
+}
