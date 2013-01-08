02b8d93546c163df6b6e7f2bd681162b9bc58153
gijs <gijs@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-01-08 14:51:49 +0000

    Move methods to utility class.

---
+++ src/nl/knaw/huygens/repository/storage/StorageUtils.java
@@ -0,0 +1,33 @@
+package nl.knaw.huygens.repository.storage;
+
+import java.util.List;
+
+import nl.knaw.huygens.repository.model.Document;
+import nl.knaw.huygens.repository.model.storage.StorageIterator;
+
+public class StorageUtils {
+  public static <T extends Document> List<T> readFromIterator(StorageIterator<T> it, int offset, int limit) {
+    if (offset > 0) {
+      it.skip(offset);
+    }
+    return it.getSome(limit);
+  }
+
+  public static <T extends Document> List<T> resolveIterator(StorageIterator<T> it, int limit) {
+    return resolveIterator(it, 0, limit);
+  }
+
+  public static <T extends Document> List<T> resolveIterator(StorageIterator<T> it, int offset, int limit) {
+    if (offset > 0) {
+      it.skip(offset);
+    }
+    List<T> rv = it.getSome(limit);
+    it.close();
+    return rv;
+  }
+
+  public static <T extends Document> List<T> readFromIterator(StorageIterator<T> it, int limit) {
+    return readFromIterator(it, 0, limit);
+  }
+
+}
