35893ff49e986b26fe6638a5c3b76637830cd5a2
gijs <gijs@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-01-08 14:40:16 +0000

    Use annotation to indicate the ID prefix, remove IDMapper which is no longer necessary, make Sitemap its own file.

--- src/nl/knaw/huygens/repository/model/Person.java
+++ src/nl/knaw/huygens/repository/model/Person.java
@@ -2,6 +2,7 @@ package nl.knaw.huygens.repository.model;
 
 import nl.knaw.huygens.repository.model.storage.Storage;
 
+@IDPrefix("PER")
 public class Person extends Document {
   public String name;
   public Datable birthDate;
