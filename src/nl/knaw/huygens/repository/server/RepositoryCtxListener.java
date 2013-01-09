37a695a0a64c02f7e5fe005b49c51947b39921a8
gijs <gijs@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-01-09 12:41:53 +0000

    Constrain static resource regular expression.

--- src/nl/knaw/huygens/repository/server/RepositoryCtxListener.java
+++ src/nl/knaw/huygens/repository/server/RepositoryCtxListener.java
@@ -40,8 +40,7 @@ public class RepositoryCtxListener extends GuiceServletContextListener {
         Map<String, String> params = Maps.newHashMap();
         params.put(PackagesResourceConfig.PROPERTY_PACKAGES, "nl.knaw.huygens.repository.resources;com.fasterxml.jackson.jaxrs.json;nl.knaw.huygens.repository.providers");
         params.put(PackagesResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, "com.sun.jersey.api.container.filter.LoggingFilter");
-        // FIXME: Constrain this regexp please!
-        params.put(ServletContainer.PROPERTY_WEB_PAGE_CONTENT_REGEX, ".*static.*");
+        params.put(ServletContainer.PROPERTY_WEB_PAGE_CONTENT_REGEX, "/static.*");
         params.put(ServletContainer.FEATURE_FILTER_FORWARD_ON_404, "true");
         filter("/*").through(GuiceContainer.class, params);
       }
