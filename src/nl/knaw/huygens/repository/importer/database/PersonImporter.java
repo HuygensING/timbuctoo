15e8493ba2c2fa16d5eef1f4518e7bac236625e1
gijs <gijs@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-14 11:50:10 +0000

    Use Guice in PersonImporter.

--- src/nl/knaw/huygens/repository/importer/database/PersonImporter.java
+++ src/nl/knaw/huygens/repository/importer/database/PersonImporter.java
@@ -1,33 +1,18 @@
 package nl.knaw.huygens.repository.importer.database;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.sql.SQLException;
-
-import org.apache.commons.configuration.ConfigurationException;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 import nl.knaw.huygens.repository.managers.StorageManager;
 import nl.knaw.huygens.repository.model.dwcbia.DWCPerson;
 import nl.knaw.huygens.repository.model.raa.RAAPerson;
-import nl.knaw.huygens.repository.model.util.DocumentTypeRegister;
-import nl.knaw.huygens.repository.pubsub.Hub;
-import nl.knaw.huygens.repository.storage.Storage;
-import nl.knaw.huygens.repository.storage.generic.StorageConfiguration;
-import nl.knaw.huygens.repository.storage.generic.StorageFactory;
-import nl.knaw.huygens.repository.util.Configuration;
+import nl.knaw.huygens.repository.modules.RepositoryBasicModule;
 
 public class PersonImporter {
 
-  public static void main(String[] args) throws SQLException, ConfigurationException, IOException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
-    Configuration conf = new Configuration("config.xml");
-    Hub hub = new Hub();
-    DocumentTypeRegister docTypeRegistry = new DocumentTypeRegister(conf.getSetting("model-packages"));
-    docTypeRegistry.registerPackageFromClass(DWCPerson.class);
-    docTypeRegistry.registerPackageFromClass(RAAPerson.class);
-
-    StorageConfiguration storageConfiguration = new StorageConfiguration(conf);
-    Storage storage = StorageFactory.getInstance(storageConfiguration, docTypeRegistry);
-    StorageManager storageManager = new StorageManager(storageConfiguration, storage, hub, docTypeRegistry);
+  public static void main(String[] args) throws Exception {
+    Injector injector = Guice.createInjector(new RepositoryBasicModule("config.xml"));
+    StorageManager storageManager = injector.getInstance(StorageManager.class);
     storageManager.getStorage().empty();
 
     GenericImporter importer = new GenericImporter();
