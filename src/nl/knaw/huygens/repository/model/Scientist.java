32f4e50dc3783264a8bb7f2b5ba75773103517e6
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-15 12:59:08 +0000

    re #1367: Model updated to the structure Gertjan sugested: Person --> Scientist --> DWCScientist

---
+++ src/nl/knaw/huygens/repository/model/Scientist.java
@@ -0,0 +1,5 @@
+package nl.knaw.huygens.repository.model;
+
+public class Scientist extends Person{
+
+}
---
+++ src/nl/knaw/huygens/repository/model/raa/RAACivilServant.java
@@ -0,0 +1,7 @@
+package nl.knaw.huygens.repository.model.raa;
+
+import nl.knaw.huygens.repository.model.CivilServant;
+
+public class RAACivilServant extends CivilServant{
+
+}
