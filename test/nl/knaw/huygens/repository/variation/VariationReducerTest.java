8b87a62884ea64c7fb79d6a72e125fd2b987d33d
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-18 13:35:04 +0000

    #re 1367: Default values are assigned when the model of the default model package is used.

--- test/nl/knaw/huygens/repository/variation/VariationReducerTest.java
+++ test/nl/knaw/huygens/repository/variation/VariationReducerTest.java
@@ -12,6 +12,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import nl.knaw.huygens.repository.model.util.DocumentTypeRegister;
 import nl.knaw.huygens.repository.storage.mongo.MongoDiff;
+import nl.knaw.huygens.repository.variation.model.GeneralTestDoc;
+import nl.knaw.huygens.repository.variation.model.TestConcreteDoc;
 import nl.knaw.huygens.repository.variation.model.projectb.TestDoc;
 
 public class VariationReducerTest {
@@ -23,7 +25,7 @@ public class VariationReducerTest {
     m = new ObjectMapper();
     reducer = new VariationReducer(new DocumentTypeRegister());
   }
-  
+
   @Test
   public void testReduce() throws IOException {
     String x = "{\"testbasedoc\":{\"name\":[{\"v\":\"a\", \"a\":[\"projectb\"]}]}, \"projectb-testdoc\": {\"blah\": \"stuff\"}}";
@@ -31,20 +33,20 @@ public class VariationReducerTest {
     TestDoc val = reducer.reduce(t, TestDoc.class);
     TestDoc testVal = new TestDoc();
     testVal.name = "a";
-    testVal.blah = "stuff"; 
+    testVal.blah = "stuff";
     assertEquals(null, MongoDiff.diffDocuments(val, testVal));
   }
-  
+
   @Test
   public void testReduceSpecificDataOnly() throws IOException {
     String x = "{\"projectb-testdoc\": {\"blah\": \"stuff\"}}";
     JsonNode t = m.readTree(x);
     TestDoc val = reducer.reduce(t, TestDoc.class);
     TestDoc testVal = new TestDoc();
-    testVal.blah = "stuff"; 
+    testVal.blah = "stuff";
     assertEquals(null, MongoDiff.diffDocuments(val, testVal));
   }
-  
+
   @Test
   public void testReduceCommonDataOnly() throws IOException {
     String x = "{\"testbasedoc\":{\"name\":[{\"v\":\"b\", \"a\":[\"blub\"]}, {\"v\":\"a\", \"a\":[\"projectb\"]}]}}";
@@ -63,37 +65,65 @@ public class VariationReducerTest {
     TestDoc testVal = new TestDoc();
     assertEquals(null, MongoDiff.diffDocuments(val, testVal));
   }
+
+  @Test
+  public void testReduceBaseObject() throws IOException {
+    String x = "{\"testconcretedoc\":{\"name\":[{\"v\":\"name\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"generaltestdoc\":{\"generalTestDocValue\":[{\"v\":\"gtdc\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"projecta-projectageneraltestdoc\": {\"projectAGeneralTestDocValue\": \"stuff\"}}";
+    
+    JsonNode t = m.readTree(x);
+    TestConcreteDoc actual = reducer.reduce(t, TestConcreteDoc.class);
+    TestConcreteDoc expected = new TestConcreteDoc();
+    expected.name = "name";
+    
+    assertEquals(null, MongoDiff.diffDocuments(expected, actual));
+  }
   
+  @Test
+  public void testReduceSharedObject() throws IOException {
+    String x = "{\"testconcretedoc\":{\"name\":[{\"v\":\"name\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"generaltestdoc\":{\"generalTestDocValue\":[{\"v\":\"gtdcv\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"projecta-projectageneraltestdoc\": {\"projectAGeneralTestDocValue\": \"stuff\"}}";
+    
+    JsonNode t = m.readTree(x);
+    GeneralTestDoc actual = reducer.reduce(t, GeneralTestDoc.class);
+    GeneralTestDoc expected = new GeneralTestDoc();
+    expected.name = "name";
+    expected.generalTestDocValue="gtdcv";
+    
+    assertEquals(null, MongoDiff.diffDocuments(expected, actual));
+  }
+
   @Test(expected = VariationException.class)
   public void testReduceVariationNonObject() throws IOException {
     String x = "{\"projectb-testdoc\": \"flups\"}";
     JsonNode t = m.readTree(x);
-    
+
     reducer.reduce(t, TestDoc.class); // This will throw
   }
-  
-  
+
   @Test(expected = VariationException.class)
   public void testReduceMalformedCommonItem() throws IOException {
     String x = "{\"testbasedoc\":{\"name\": 42}}";
     JsonNode t = m.readTree(x);
-    
+
     reducer.reduce(t, TestDoc.class); // This will throw
   }
-  
+
   @Test(expected = VariationException.class)
   public void testReduceMalformedCommonValueArrayItem() throws IOException {
     String x = "{\"testbasedoc\":{\"name\":[42]}}";
     JsonNode t = m.readTree(x);
-    
+
     reducer.reduce(t, TestDoc.class); // This will throw
   }
-  
+
   @Test(expected = VariationException.class)
   public void testReduceMalformedCommonValueArrayItemAgreed() throws IOException {
     String x = "{\"testbasedoc\":{\"name\":[{\"v\":\"b\", \"a\":42}]}}";
     JsonNode t = m.readTree(x);
-    
+
     reducer.reduce(t, TestDoc.class); // This will throw
   }
 }
