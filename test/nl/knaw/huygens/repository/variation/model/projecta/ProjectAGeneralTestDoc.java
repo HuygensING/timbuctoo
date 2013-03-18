dff29648fa93d02779139f117e0f440543a50d61
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-18 12:04:59 +0000

    re #1367: A default value is set when a property is set for the first time. When a property is removed, because only the default value contains the value, the default value is reset.

---
+++ test/nl/knaw/huygens/repository/variation/model/projecta/ProjectAGeneralTestDoc.java
@@ -0,0 +1,7 @@
+package nl.knaw.huygens.repository.variation.model.projecta;
+
+import nl.knaw.huygens.repository.variation.model.GeneralTestDoc;
+
+public class ProjectAGeneralTestDoc extends GeneralTestDoc {
+  public String projectAGeneralTestDocValue;
+}
