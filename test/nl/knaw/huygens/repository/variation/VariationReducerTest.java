736d2b020eb89fdd433f99511cf7b596a9219fb4
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-20 09:36:36 +0000

    re #1367: Every class has shows it's known variations, when requested at the web service.

--- test/nl/knaw/huygens/repository/variation/VariationReducerTest.java
+++ test/nl/knaw/huygens/repository/variation/VariationReducerTest.java
@@ -9,11 +9,13 @@ import org.junit.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 
 import nl.knaw.huygens.repository.model.util.DocumentTypeRegister;
 import nl.knaw.huygens.repository.storage.mongo.MongoDiff;
 import nl.knaw.huygens.repository.variation.model.GeneralTestDoc;
 import nl.knaw.huygens.repository.variation.model.TestConcreteDoc;
+import nl.knaw.huygens.repository.variation.model.projecta.ProjectAGeneralTestDoc;
 import nl.knaw.huygens.repository.variation.model.projectb.TestDoc;
 
 public class VariationReducerTest {
@@ -28,71 +30,54 @@ public class VariationReducerTest {
 
   @Test
   public void testReduce() throws IOException {
-    String x = "{\"testbasedoc\":{\"name\":[{\"v\":\"a\", \"a\":[\"projectb\"]}]}, \"projectb-testdoc\": {\"blah\": \"stuff\"}}";
+    String x = "{\"testconcretedoc\":{\"name\":[{\"v\":\"b\", \"a\":[\"blub\"]}, {\"v\":\"a\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"generaltestdoc\":{\"generalTestDocValue\":[{\"v\":\"a\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"projecta-projectageneraltestdoc\":{\"projectAGeneralTestDocValue\":\"test\"}}";
     JsonNode t = m.readTree(x);
-    TestDoc val = reducer.reduce(t, TestDoc.class);
-    TestDoc testVal = new TestDoc();
+    ProjectAGeneralTestDoc val = reducer.reduce(t, ProjectAGeneralTestDoc.class);
+    ProjectAGeneralTestDoc testVal = new ProjectAGeneralTestDoc();
     testVal.name = "a";
-    testVal.blah = "stuff";
-    assertEquals(null, MongoDiff.diffDocuments(val, testVal));
-  }
-
-  @Test
-  public void testReduceSpecificDataOnly() throws IOException {
-    String x = "{\"projectb-testdoc\": {\"blah\": \"stuff\"}}";
-    JsonNode t = m.readTree(x);
-    TestDoc val = reducer.reduce(t, TestDoc.class);
-    TestDoc testVal = new TestDoc();
-    testVal.blah = "stuff";
+    testVal.generalTestDocValue = "a";
+    testVal.projectAGeneralTestDocValue = "test";
+    testVal.setVariations(Lists.newArrayList("testconcretedoc", "generaltestdoc", "projectageneraltestdoc"));
     assertEquals(null, MongoDiff.diffDocuments(val, testVal));
   }
 
   @Test
   public void testReduceCommonDataOnly() throws IOException {
-    String x = "{\"testbasedoc\":{\"name\":[{\"v\":\"b\", \"a\":[\"blub\"]}, {\"v\":\"a\", \"a\":[\"projectb\"]}]}}";
+    String x = "{\"testconcretedoc\":{\"name\":[{\"v\":\"b\", \"a\":[\"blub\"]}, {\"v\":\"a\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"generaltestdoc\":{\"generalTestDocValue\":[{\"v\":\"a\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"projecta-projectageneraltestdoc\":{\"projectAGeneralTestDocValue\":\"test\"}}";
     JsonNode t = m.readTree(x);
-    TestDoc val = reducer.reduce(t, TestDoc.class);
-    TestDoc testVal = new TestDoc();
+    TestConcreteDoc val = reducer.reduce(t, TestConcreteDoc.class);
+    TestConcreteDoc testVal = new TestConcreteDoc();
     testVal.name = "a";
+    testVal.setVariations(Lists.newArrayList("testconcretedoc", "generaltestdoc", "projectageneraltestdoc"));
+    assertEquals(null, MongoDiff.diffDocuments(val, testVal));
+  }
+
+  @Test
+  public void testReduceRolDataAndCommonData() throws IOException {
+    String x = "{\"testconcretedoc\":{\"name\":[{\"v\":\"b\", \"a\":[\"blub\"]}, {\"v\":\"a\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"generaltestdoc\":{\"generalTestDocValue\":[{\"v\":\"a\", \"a\":[\"projecta\",\"^default\"]}]},"
+               + "\"projecta-projectageneraltestdoc\":{\"projectAGeneralTestDocValue\":\"test\"}}";
+    JsonNode t = m.readTree(x);
+    GeneralTestDoc val = reducer.reduce(t, GeneralTestDoc.class);
+    GeneralTestDoc testVal = new GeneralTestDoc();
+    testVal.name = "a";
+    testVal.generalTestDocValue = "a";
+    testVal.setVariations(Lists.newArrayList("testconcretedoc", "generaltestdoc", "projectageneraltestdoc"));
     assertEquals(null, MongoDiff.diffDocuments(val, testVal));
   }
 
   @Test
   public void testReduceMissingVariation() throws IOException {
-    String x = "{\"common\":{\"name\":[{\"v\":\"b\", \"a\":[\"blub\"]}]}}";
+    String x = "{\"testbasedoc\":{\"name\":[{\"v\":\"b\", \"a\":[\"blub\",\"^default\"]}]}}";
     JsonNode t = m.readTree(x);
     TestDoc val = reducer.reduce(t, TestDoc.class);
     TestDoc testVal = new TestDoc();
+    testVal.setVariations(Lists.newArrayList("testbasedoc"));
     assertEquals(null, MongoDiff.diffDocuments(val, testVal));
-  }
-
-  @Test
-  public void testReduceBaseObject() throws IOException {
-    String x = "{\"testconcretedoc\":{\"name\":[{\"v\":\"name\", \"a\":[\"projecta\",\"^default\"]}]},"
-               + "\"generaltestdoc\":{\"generalTestDocValue\":[{\"v\":\"gtdc\", \"a\":[\"projecta\",\"^default\"]}]},"
-               + "\"projecta-projectageneraltestdoc\": {\"projectAGeneralTestDocValue\": \"stuff\"}}";
-    
-    JsonNode t = m.readTree(x);
-    TestConcreteDoc actual = reducer.reduce(t, TestConcreteDoc.class);
-    TestConcreteDoc expected = new TestConcreteDoc();
-    expected.name = "name";
-    
-    assertEquals(null, MongoDiff.diffDocuments(expected, actual));
-  }
-  
-  @Test
-  public void testReduceSharedObject() throws IOException {
-    String x = "{\"testconcretedoc\":{\"name\":[{\"v\":\"name\", \"a\":[\"projecta\",\"^default\"]}]},"
-               + "\"generaltestdoc\":{\"generalTestDocValue\":[{\"v\":\"gtdcv\", \"a\":[\"projecta\",\"^default\"]}]},"
-               + "\"projecta-projectageneraltestdoc\": {\"projectAGeneralTestDocValue\": \"stuff\"}}";
-    
-    JsonNode t = m.readTree(x);
-    GeneralTestDoc actual = reducer.reduce(t, GeneralTestDoc.class);
-    GeneralTestDoc expected = new GeneralTestDoc();
-    expected.name = "name";
-    expected.generalTestDocValue="gtdcv";
-    
-    assertEquals(null, MongoDiff.diffDocuments(expected, actual));
   }
 
   @Test(expected = VariationException.class)
