320a0903886261ad4ce48ab698d73095faad1d75
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-21 13:54:00 +0000

    Refactored

--- src/nl/knaw/huygens/repository/resources/SitemapResource.java
+++ src/nl/knaw/huygens/repository/resources/SitemapResource.java
@@ -1,6 +1,5 @@
 package nl.knaw.huygens.repository.resources;
 
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,15 +11,16 @@ import javax.ws.rs.core.MediaType;
 import nl.knaw.huygens.repository.model.Sitemap;
 import nl.knaw.huygens.repository.util.APIDesc;
 
-
 @Path("api")
 public class SitemapResource {
+
   private static Sitemap lastSitemap;
 
   @GET
-  @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON})
+  @Produces({ MediaType.TEXT_HTML, MediaType.APPLICATION_JSON })
   @APIDesc("Generates a structured sitemap.")
   public Sitemap getSitemap(@QueryParam("refresh") boolean shouldRefresh, @Context Application app) {
+    // TODO synchronize
     if (shouldRefresh || lastSitemap == null) {
       lastSitemap = new Sitemap(app);
     }
