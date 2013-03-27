0c513c107d26e84b9f1cf05fcfba66240915c994
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-27 14:58:41 +0000

    Inject DocumentTypeRegistry for site map

--- src/nl/knaw/huygens/repository/resources/SitemapResource.java
+++ src/nl/knaw/huygens/repository/resources/SitemapResource.java
@@ -9,12 +9,22 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import nl.knaw.huygens.repository.model.Sitemap;
+import nl.knaw.huygens.repository.model.util.DocumentTypeRegister;
 import nl.knaw.huygens.repository.util.APIDesc;
+
+import com.google.inject.Inject;
 
 @Path("api")
 public class SitemapResource {
 
   private static Sitemap lastSitemap;
+
+  private final DocumentTypeRegister docTypeRegistry;
+
+  @Inject
+  public SitemapResource(final DocumentTypeRegister registry) {
+    docTypeRegistry = registry;
+  }
 
   @GET
   @Produces({ MediaType.TEXT_HTML, MediaType.APPLICATION_JSON })
@@ -22,8 +32,9 @@ public class SitemapResource {
   public Sitemap getSitemap(@QueryParam("refresh") boolean shouldRefresh, @Context Application app) {
     // TODO synchronize
     if (shouldRefresh || lastSitemap == null) {
-      lastSitemap = new Sitemap(app);
+      lastSitemap = new Sitemap(app, docTypeRegistry);
     }
     return lastSitemap;
   }
+
 }
