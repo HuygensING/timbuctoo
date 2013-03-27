131a7b8e210f43edf9e3750ff3d55ee114530d92
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-27 00:26:28 +0000

    Refactored

--- src/nl/knaw/huygens/repository/modules/RepositoryBasicModule.java
+++ src/nl/knaw/huygens/repository/modules/RepositoryBasicModule.java
@@ -3,14 +3,6 @@ package nl.knaw.huygens.repository.modules;
 import javax.validation.Validation;
 import javax.validation.Validator;
 
-import org.apache.commons.configuration.ConfigurationException;
-
-import com.google.inject.AbstractModule;
-import com.google.inject.Binder;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.google.inject.name.Names;
-
 import nl.knaw.huygens.repository.persistence.PersistenceManager;
 import nl.knaw.huygens.repository.persistence.handle.HandleManager;
 import nl.knaw.huygens.repository.persistence.handle.HandleManagerFactory;
@@ -18,44 +10,45 @@ import nl.knaw.huygens.repository.storage.Storage;
 import nl.knaw.huygens.repository.storage.generic.StorageConfiguration;
 import nl.knaw.huygens.repository.util.Configuration;
 
+import org.apache.commons.configuration.ConfigurationException;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Names;
+
 public class RepositoryBasicModule extends AbstractModule {
-  private Configuration conf;
-  
-  public RepositoryBasicModule(String configurationPath) {
+
+  private final Configuration config;
+
+  public RepositoryBasicModule(String configPath) {
     try {
-      conf = new Configuration(configurationPath);
+      config = new Configuration(configPath);
     } catch (ConfigurationException e) {
-      e.printStackTrace();
       throw new RuntimeException(e);
     }
   }
 
   @Override
   protected void configure() {
-    Names.bindProperties(this.binder(), conf.getAll());
-    bind(Configuration.class).toInstance(conf);
-    
+    Names.bindProperties(binder(), config.getAll());
+    bind(Configuration.class).toInstance(config);
     bind(PersistenceManager.class).to(HandleManager.class);
-    
-    configureStorage(conf);
-  }
-  
-  private void configureStorage(Configuration conf) {
-    Class<? extends Storage> cls = new StorageConfiguration(conf).getType().getCls();
+
+    Class<? extends Storage> cls = new StorageConfiguration(config).getType().getCls();
     bind(Storage.class).to(cls);
   }
-  
-  @Provides @Singleton
+
+  @Provides
+  @Singleton
   HandleManager provideHandleManager() {
-    return new HandleManagerFactory(conf).createPersistenceManager();
+    return new HandleManagerFactory(config).createPersistenceManager();
   }
-  
-  @Provides @Singleton
+
+  @Provides
+  @Singleton
   Validator provideValidator() {
     return Validation.buildDefaultValidatorFactory().getValidator();
   }
-  
-  public Binder getBinder() {
-    return binder();
-  }
+
 }
