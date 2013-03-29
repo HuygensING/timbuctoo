6198834d31b49d980f08ba3269d2ad4bee232349
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-29 23:58:22 +0000

    Refactored

--- src/nl/knaw/huygens/repository/persistence/PersistenceManager.java
+++ src/nl/knaw/huygens/repository/persistence/PersistenceManager.java
@@ -1,10 +1,27 @@
 package nl.knaw.huygens.repository.persistence;
 
-
 public interface PersistenceManager {
+
+  /**
+   * @param urlToPersist
+   * @return
+   * @throws PersistenceException
+   */
   String persistURL(String urlToPersist) throws PersistenceException;
-  
+
+  /**
+   * @param objectId
+   * @param collectionId
+   * @return
+   * @throws PersistenceException
+   */
   String persistObject(String objectId, String collectionId) throws PersistenceException;
 
-  String getPersistentURL(String persistenceID) throws PersistenceException;
+  /**
+   * @param persistentId
+   * @return
+   * @throws PersistenceException
+   */
+  String getPersistentURL(String persistentId) throws PersistenceException;
+
 }
