5c68f8863a06e3eba5099d29294eab0a3ee41fa8
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-03-29 11:06:35 +0000

    Generalized exception

--- src/nl/knaw/huygens/repository/persistence/PersistenceException.java
+++ src/nl/knaw/huygens/repository/persistence/PersistenceException.java
@@ -1,14 +1,22 @@
 package nl.knaw.huygens.repository.persistence;
 
+/**
+ * Signals that an persistance error has occurred.
+ */
 public class PersistenceException extends Exception {
 
-  /**
-   * 
-   */
   private static final long serialVersionUID = 1L;
-         
-  public PersistenceException(Exception ex){
-    super(ex);
+
+  public PersistenceException(String message) {
+    super(message);
   }
-  
+
+  public PersistenceException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public PersistenceException(Throwable cause) {
+    super(cause);
+  }
+
 }
