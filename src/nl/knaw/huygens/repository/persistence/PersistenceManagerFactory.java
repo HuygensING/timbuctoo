2d33037815e9f7c706abda8e68c5243dc3f7e79a
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-02 14:40:32 +0000

    Refactored

--- src/nl/knaw/huygens/repository/persistence/PersistenceManagerFactory.java
+++ src/nl/knaw/huygens/repository/persistence/PersistenceManagerFactory.java
@@ -3,15 +3,15 @@ package nl.knaw.huygens.repository.persistence;
 import nl.knaw.huygens.repository.persistence.handle.HandleManager;
 import nl.knaw.huygens.repository.util.Configuration;
 
-public class TheFactory {
+public class PersistenceManagerFactory {
 
   private final Configuration config;
 
-  public TheFactory(Configuration config) {
+  public PersistenceManagerFactory(Configuration config) {
     this.config = config;
   }
 
-  public PersistenceManager createPersistenceManager() {
+  public PersistenceManager newPersistenceManager() {
     if (config.getBooleanSetting("use-handle-system", true)) {
       return HandleManager.newHandleManager(config);
     } else {
