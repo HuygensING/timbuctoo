d9524c3a09b8bedf5290aafa9ff271a485014717
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-02 13:34:49 +0000

    Reformatted source code

--- src/nl/knaw/huygens/repository/server/RepositoryCtxListener.java
+++ src/nl/knaw/huygens/repository/server/RepositoryCtxListener.java
@@ -1,17 +1,14 @@
 package nl.knaw.huygens.repository.server;
 
-
-
+import nl.knaw.huygens.repository.modules.RepositoryBasicModule;
+import nl.knaw.huygens.repository.modules.RepositoryServletModule;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
-import nl.knaw.huygens.repository.modules.RepositoryBasicModule;
-import nl.knaw.huygens.repository.modules.RepositoryServletModule;
-
-
 public class RepositoryCtxListener extends GuiceServletContextListener {
+
   private static RepositoryServletModule servletModule;
   private static RepositoryBasicModule module;
   private static Injector injector;
@@ -22,7 +19,7 @@ public class RepositoryCtxListener extends GuiceServletContextListener {
 
   @Override
   protected Injector getInjector() {
-    synchronized(this) {
+    synchronized (this) {
       if (injector == null) {
         if (module == null) {
           module = new RepositoryBasicModule("../config.xml");
