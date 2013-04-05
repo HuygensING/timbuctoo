c31c7017c361a262e1f988be48ce8107bb5ec7d8
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-05 11:59:38 +0000

    Sitemap no longer is a Document. Added Provider for Sitemap.

--- src/nl/knaw/huygens/repository/model/Sitemap.java
+++ src/nl/knaw/huygens/repository/model/Sitemap.java
@@ -5,24 +5,18 @@ import java.util.List;
 import javax.ws.rs.core.Application;
 
 import nl.knaw.huygens.repository.model.util.DocumentTypeRegister;
-import nl.knaw.huygens.repository.model.util.IDPrefix;
 import nl.knaw.huygens.repository.resources.RESTAutoResource;
-import nl.knaw.huygens.repository.storage.Storage;
 import nl.knaw.huygens.repository.util.JAXUtils;
 import nl.knaw.huygens.repository.util.JAXUtils.API;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.Lists;
 
-@IDPrefix("STM")
-public class Sitemap extends Document {
+public class Sitemap {
 
-  public List<API> availableAPIList;
-  private String defaultVRE;
+  public final String description = "Repository Sitemap";
+  public final List<API> availableAPIList;
 
   public Sitemap(Application application, DocumentTypeRegister registry) {
-    setId(String.format("T%d", System.currentTimeMillis()));
-
     availableAPIList = Lists.newArrayList();
     for (Class<?> cls : application.getClasses()) {
       List<API> apis = JAXUtils.generateAPIs(cls);
@@ -36,28 +30,6 @@ public class Sitemap extends Document {
         availableAPIList.addAll(apis);
       }
     }
-  }
-
-  @Override
-  public String getDescription() {
-    return "Repository Sitemap";
-  }
-
-  @Override
-  public void fetchAll(Storage storage) {
-    // No-op;
-  }
-
-  @Override
-  @JsonProperty("!defaultVRE")
-  public String getDefaultVRE() {
-    return defaultVRE;
-  }
-
-  @Override
-  @JsonProperty("!defaultVRE")
-  public void setDefaultVRE(String defaultVRE) {
-    this.defaultVRE = defaultVRE;
   }
 
 }
