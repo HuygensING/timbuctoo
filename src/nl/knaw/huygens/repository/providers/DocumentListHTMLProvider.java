61852c3400e2ca0e6a3b2fcd6fb40e0f9fdb16f3
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-06 13:31:56 +0000

    Refactored HTML providers

--- src/nl/knaw/huygens/repository/providers/DocumentListHTMLProvider.java
+++ src/nl/knaw/huygens/repository/providers/DocumentListHTMLProvider.java
@@ -7,7 +7,6 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.WildcardType;
 import java.util.List;
-import java.util.Map;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
@@ -20,14 +19,8 @@ import nl.knaw.huygens.repository.model.Document;
 
 import org.apache.commons.lang.StringEscapeUtils;
 
-import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.jaxrs.json.annotation.EndpointConfig;
-import com.fasterxml.jackson.jaxrs.json.util.AnnotationBundleKey;
-import com.google.common.base.Strings;
-import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
@@ -37,24 +30,16 @@ import com.google.inject.name.Named;
 @Singleton
 public class DocumentListHTMLProvider implements MessageBodyWriter<List<? extends Document>> {
 
-  private final JsonFactory factory;
-  private final Map<AnnotationBundleKey, ObjectWriter> writers;
-  private final String preamble;
+  private final HTMLProviderHelper helper;
 
   @Inject
   public DocumentListHTMLProvider(@Named("html.defaultstylesheet") String stylesheetLink, @Named("public_url") String publicURL) {
-    factory = new JsonFactory();
-    writers = Maps.newHashMap();
-    preamble = getPreamble(stylesheetLink, publicURL);
+    helper = new HTMLProviderHelper(stylesheetLink, publicURL);
   }
 
   @Override
   public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-    return accept(mediaType) && accept(type) && accept(genericType);
-  }
-
-  private boolean accept(MediaType mediaType) {
-    return MediaType.TEXT_HTML_TYPE.equals(mediaType);
+    return helper.accept(mediaType) && accept(type) && accept(genericType);
   }
 
   private boolean accept(Class<?> type) {
@@ -81,38 +66,25 @@ public class DocumentListHTMLProvider implements MessageBodyWriter<List<? extend
   }
 
   @Override
-  public long getSize(List<? extends Document> doc, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+  public long getSize(List<? extends Document> docs, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
     return -1;
   }
 
   @Override
   public void writeTo(List<? extends Document> docs, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream out)
       throws IOException, WebApplicationException {
-    write(out, preamble);
-    write(out, String.format("<title>%1$s</title></head><body><h1>%1$s</h1>", getTitle(docs)));
+    helper.writeHeader(out, getTitle(docs));
 
-    JsonGenerator jgen = new HTMLGenerator(factory.createGenerator(out));
-    ObjectWriter writer = getObjectWriter(annotations);
+    JsonGenerator jgen = helper.getGenerator(out);
+    ObjectWriter writer = helper.getObjectWriter(annotations);
     for (Document doc : docs) {
-      write(out, "<h2>");
-      write(out, getDocTitle(doc));
-      write(out, "</h2>");
+      helper.write(out, "<h2>");
+      helper.write(out, getDocTitle(doc));
+      helper.write(out, "</h2>");
       writer.writeValue(jgen, doc);
     }
 
-    write(out, "</body></html>");
-  }
-
-  private void write(OutputStream out, String text) throws IOException {
-    out.write(text.getBytes("UTF-8"));
-  }
-
-  private String getPreamble(String stylesheetLink, String publicURL) {
-    String value = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\">";
-    if (!Strings.isNullOrEmpty(stylesheetLink)) {
-      value += "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + publicURL + stylesheetLink + "\"/>";
-    }
-    return value;
+    helper.writeFooter(out);
   }
 
   private String getTitle(List<? extends Document> docs) {
@@ -123,19 +95,6 @@ public class DocumentListHTMLProvider implements MessageBodyWriter<List<? extend
   private String getDocTitle(Document doc) {
     String description = doc.getDescription();
     return (description != null) ? StringEscapeUtils.escapeHtml(description) : "";
-  }
-
-  // FIXME make thread safe
-  private ObjectWriter getObjectWriter(Annotation[] annotations) {
-    AnnotationBundleKey key = new AnnotationBundleKey(annotations);
-    ObjectWriter writer = writers.get(key);
-    if (writer == null) {
-      ObjectMapper mapper = new ObjectMapper();
-      EndpointConfig endpointConfig = EndpointConfig.forWriting(mapper, annotations, null);
-      writer = endpointConfig.getWriter();
-      writers.put(key, writer);
-    }
-    return writer;
   }
 
 }
