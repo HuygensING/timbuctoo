2550a5545c655a1cf5b393dfac830921c6937850
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-09 14:40:20 +0000

    A timer added so the time of the import is measured.

--- src/nl/knaw/huygens/repository/importer/database/PersonImporter.java
+++ src/nl/knaw/huygens/repository/importer/database/PersonImporter.java
@@ -1,5 +1,7 @@
 package nl.knaw.huygens.repository.importer.database;
 
+import java.util.Date;
+
 import nl.knaw.huygens.repository.BasicInjectionModule;
 import nl.knaw.huygens.repository.managers.StorageManager;
 import nl.knaw.huygens.repository.model.dwcbia.DWCScientist;
@@ -17,10 +19,13 @@ public class PersonImporter {
 
     GenericImporter importer = new GenericImporter();
 
+    long beginTime = new Date().getTime();
     importer.importData("resources/DWCScientistMapping.properties", storageManager, DWCScientist.class);
     importer.importData("resources/RAACivilServantMapping.properties", storageManager, RAACivilServant.class);
+    long endTime = new Date().getTime();
+
+    System.out.println("Import duration: " + ((endTime - beginTime) / 1000) + " seconds");
 
     storageManager.ensureIndices();
   }
-
 }
