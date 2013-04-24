8929535dbe92afc36524ba379eb678944c8327a2
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-24 22:40:37 +0000

    Renamed class

--- src/nl/knaw/huygens/repository/index/IndexerFactory.java
+++ src/nl/knaw/huygens/repository/index/IndexerFactory.java
@@ -2,39 +2,39 @@ package nl.knaw.huygens.repository.index;
 
 import java.util.Map;
 
-import com.google.common.collect.Maps;
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
-
 import nl.knaw.huygens.repository.model.Document;
 import nl.knaw.huygens.repository.pubsub.Hub;
 import nl.knaw.huygens.repository.util.RepositoryException;
 
+import com.google.common.collect.Maps;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
 @Singleton
-public class IndexFactory {
+public class IndexerFactory {
+
   private final LocalSolrServer server;
   private final ModelIterator modelIterator;
-  private Map<Class<? extends Document>, DocumentIndexer<? extends Document>> indexerCache;
+  private Map<Class<? extends Document>, DocumentIndexer<? extends Document>> indexers;
   private final Hub hub;
 
   @Inject
-  public IndexFactory(ModelIterator modelIterator, LocalSolrServer server, Hub hub) {
+  public IndexerFactory(ModelIterator modelIterator, LocalSolrServer server, Hub hub) {
     this.server = server;
     this.modelIterator = modelIterator;
-    this.indexerCache = Maps.newHashMap();
+    this.indexers = Maps.newHashMap();
     this.hub = hub;
   }
 
   @SuppressWarnings("unchecked")
   public <T extends Document> DocumentIndexer<T> getIndexForType(Class<T> cls) {
-    if (indexerCache.containsKey(cls)) {
-      return (DocumentIndexer<T>) indexerCache.get(cls);
+    if (indexers.containsKey(cls)) {
+      return (DocumentIndexer<T>) indexers.get(cls);
     }
     DocumentIndexer<T> rv = new DocumentIndexer<T>(cls, modelIterator, server, hub);
-    indexerCache.put(cls, rv);
+    indexers.put(cls, rv);
     return rv;
   }
-
 
   public void flushIndices() throws RepositoryException {
     try {
@@ -53,4 +53,5 @@ public class IndexFactory {
       ex.printStackTrace();
     }
   }
+
 }
