37bd77d35a1486dfae404e091eecf397a6d437da
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-24 23:00:39 +0000

    Refactored

--- src/nl/knaw/huygens/repository/util/Paths.java
+++ src/nl/knaw/huygens/repository/util/Paths.java
@@ -2,8 +2,13 @@ package nl.knaw.huygens.repository.util;
 
 public class Paths {
 
-	public static String pathInUserHome(String path) {
-		path = Character.toString(path.charAt(0)).equals("/") ? path : "/" + path;
-		return System.getProperty("user.home") + path;
-	}
+  public static String pathInUserHome(String path) {
+    path = Character.toString(path.charAt(0)).equals("/") ? path : "/" + path;
+    return System.getProperty("user.home") + path;
+  }
+
+  private Paths() {
+    throw new AssertionError("Non-instantiable class");
+  }
+
 }
