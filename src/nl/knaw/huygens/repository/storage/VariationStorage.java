af187185be1d8a2109949d61030c960f08d8094a
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-25 13:02:01 +0000

    re #1417: Storage and VariationStorage split on interface level and additional changes to make the system compile and succeed all the unittests.

---
+++ src/nl/knaw/huygens/repository/storage/VariationStorage.java
@@ -0,0 +1,21 @@
+package nl.knaw.huygens.repository.storage;
+
+import java.io.IOException;
+import java.util.List;
+
+import nl.knaw.huygens.repository.model.Document;
+
+public interface VariationStorage extends Storage {
+
+  <T extends Document> List<T> getAllVariations(Class<T> type, String id) throws IOException;
+
+  /**
+   * Get the given variation of a document.
+   * @param type
+   * @param id
+   * @param variation
+   * @return
+   * @throws IOException 
+   */
+  <T extends Document> T getVariation(Class<T> type, String id, String variation) throws IOException;
+}
