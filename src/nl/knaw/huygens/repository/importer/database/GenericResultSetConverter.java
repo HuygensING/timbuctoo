a27e0659bcba2ecf95cb62dfa7ba8704e25f787a
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-25 22:49:27 +0000

    Prevent unnecessary spaces in names

--- src/nl/knaw/huygens/repository/importer/database/GenericResultSetConverter.java
+++ src/nl/knaw/huygens/repository/importer/database/GenericResultSetConverter.java
@@ -13,6 +13,8 @@ import java.util.Map;
 import nl.knaw.huygens.repository.model.Document;
 import nl.knaw.huygens.repository.model.util.Datable;
 
+import org.apache.commons.lang.StringUtils;
+
 public class GenericResultSetConverter<T extends Document> {
   private Map<String, List<String>> propertyMapping;
   private Class<T> type;
@@ -22,22 +24,22 @@ public class GenericResultSetConverter<T extends Document> {
     this.type = type;
   }
 
-  public List<T> convert(ResultSet resultSet) throws SQLException, InstantiationException, IllegalAccessException, NoSuchMethodException,
-      SecurityException, IllegalArgumentException, InvocationTargetException {
+  public List<T> convert(ResultSet resultSet) throws SQLException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException,
+      InvocationTargetException {
     List<T> returnValue = new ArrayList<T>();
 
     //Field[] fields = type.getDeclaredFields();
-    
+
     List<Field> fields = new ArrayList<Field>();
-    
+
     getAllFields(fields, type);
 
     T instance = null;
 
     while (resultSet.next()) {
-      
+
       instance = type.newInstance();
-      
+
       for (Field field : fields) {
         if (propertyMapping.containsKey(field.getName())) {
           field.setAccessible(true);
@@ -53,42 +55,36 @@ public class GenericResultSetConverter<T extends Document> {
 
     return returnValue;
   }
-  
-  private <U> void getAllFields(List<Field> fieldList, Class<U> type){
+
+  private <U> void getAllFields(List<Field> fieldList, Class<U> type) {
     fieldList.addAll(Arrays.asList(type.getDeclaredFields()));
-    if(type.getSuperclass() != null){
+    if (type.getSuperclass() != null) {
       getAllFields(fieldList, type.getSuperclass());
     }
   }
 
   private <U> Object getFieldValue(ResultSet resultSet, List<String> fieldNames, Class<U> type) throws SQLException {
-    Object object = null;
-
     if (type == String.class) {
-      object = getStringValue(resultSet, fieldNames);
+      return getStringValue(resultSet, fieldNames);
+    } else if (type == Datable.class) {
+      return new Datable(resultSet.getString(fieldNames.get(0)));
+    } else {
+      throw new RuntimeException(type.getName() + " not supported yet.");
     }
-    else if(type == Datable.class){
-      object = new Datable(resultSet.getString(fieldNames.get(0)));
-    }
-    else{
-      throw new RuntimeException( type.getName() + " not supported yet.");
-    }
-
-    return object;
   }
 
-  private String getStringValue(ResultSet resultSet, Collection<String> fieldNames) throws SQLException {
-    StringBuilder sb = new StringBuilder();
-    String value = null;
-    
-    for (String field : fieldNames) {
-      value = resultSet.getString(field);
-      if(value != null)
-      sb.append(value);
-      sb.append(" ");
+  private String getStringValue(ResultSet resultSet, Collection<String> fields) throws SQLException {
+    StringBuilder builder = new StringBuilder();
+    for (String field : fields) {
+      String value = StringUtils.trimToEmpty(resultSet.getString(field));
+      if (value.length() != 0) {
+        if (builder.length() != 0) {
+          builder.append(' ');
+        }
+        builder.append(value);
+      }
     }
-    
-    return sb.toString().trim();
+    return builder.toString();
   }
 
 }
