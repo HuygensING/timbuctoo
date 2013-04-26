796d1cf6a1f0c7b30cea1967f8cf75c2080fa9ca
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-26 10:00:47 +0000

    Refactored search resource

--- src/nl/knaw/huygens/repository/resources/SearchResource.java
+++ src/nl/knaw/huygens/repository/resources/SearchResource.java
@@ -1,47 +1,66 @@
 package nl.knaw.huygens.repository.resources;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import nl.knaw.huygens.repository.managers.SearchManager;
+import nl.knaw.huygens.repository.managers.StorageManager;
+import nl.knaw.huygens.repository.model.Document;
 import nl.knaw.huygens.repository.model.Search;
+import nl.knaw.huygens.repository.model.util.DocumentTypeRegister;
 import nl.knaw.huygens.repository.storage.generic.JsonViews;
 import nl.knaw.huygens.repository.util.APIDesc;
 
 import org.apache.solr.client.solrj.SolrServerException;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 @Path("search")
 public class SearchResource {
 
   @Inject
-  private SearchManager manager;
+  private SearchManager searchManager;
+  @Inject
+  private StorageManager storageManager;
+  @Inject
+  private DocumentTypeRegister docTypeRegistry;
 
   @GET
   @Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_HTML })
   @JsonView(JsonViews.WebView.class)
   @APIDesc("Searches the Solr index.")
-  public Search handlePostJson(@QueryParam("q") String term, @QueryParam("core") String core, @QueryParam("sort") String sort) {
+  public List<? extends Document> handlePostJson(@QueryParam("q") String term, @QueryParam("core") String core, @QueryParam("sort") String sort) {
     System.err.println(" term = " + term);
     System.err.println(" core = " + core);
     System.err.println(" sort = " + sort);
     try {
-      return manager.search(term, sort, core);
+      Search search = searchManager.search(term, sort, core);
+      Class<? extends Document> type = docTypeRegistry.getClassFromTypeString(core);
+      return convert(type, search.getIds());
     } catch (SolrServerException e) {
       e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
     }
-    return new Search(new ArrayList<String>(), core, term, sort, new Date().toString());
+    throw new WebApplicationException(Response.Status.NOT_FOUND);
+  }
+
+  private <T extends Document> List<T> convert(Class<T> type, List<String> ids) {
+    List<T> list = Lists.newArrayList();
+    for (String id : ids) {
+      list.add(storageManager.getDocument(type, id));
+    }
+    return list;
   }
 
 }
