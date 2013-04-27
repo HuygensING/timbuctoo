98346aa313360eb75ef9f23d663886617fd8a19a
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-27 22:17:16 +0000

    More realistic model

--- src/nl/knaw/huygens/repository/model/raa/RAACivilServant.java
+++ src/nl/knaw/huygens/repository/model/raa/RAACivilServant.java
@@ -1,7 +1,5 @@
 package nl.knaw.huygens.repository.model.raa;
 
-import nl.knaw.huygens.repository.model.CivilServant;
-
-public class RAACivilServant extends CivilServant{
+public class RAACivilServant extends RAAPerson {
 
 }
