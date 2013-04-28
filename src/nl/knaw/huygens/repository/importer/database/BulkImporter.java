fe8a2c23bb46db3446f0f6916c2cbf511458c9c2
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-28 11:09:47 +0000

    Renamed class

---
+++ src/nl/knaw/huygens/repository/importer/database/BulkImporter.java
@@ -0,0 +1,33 @@
+package nl.knaw.huygens.repository.importer.database;
+
+import java.util.Date;
+
+import nl.knaw.huygens.repository.BasicInjectionModule;
+import nl.knaw.huygens.repository.managers.StorageManager;
+import nl.knaw.huygens.repository.model.dwcbia.DWCScientist;
+import nl.knaw.huygens.repository.model.raa.RAACivilServant;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class BulkImporter {
+
+  public static void main(String[] args) throws Exception {
+    Injector injector = Guice.createInjector(new BasicInjectionModule("config.xml"));
+    StorageManager storageManager = injector.getInstance(StorageManager.class);
+    storageManager.getStorage().empty();
+
+    GenericImporter importer = new GenericImporter();
+
+    long beginTime = new Date().getTime();
+    // importer.importData("resources/DWCPlaceMapping.properties", storageManager, DWCPlace.class);
+    importer.importData("resources/DWCScientistMapping.properties", storageManager, DWCScientist.class);
+    importer.importData("resources/RAACivilServantMapping.properties", storageManager, RAACivilServant.class);
+    long endTime = new Date().getTime();
+
+    System.out.println("Import duration: " + ((endTime - beginTime) / 1000) + " seconds");
+
+    storageManager.ensureIndices();
+  }
+
+}
