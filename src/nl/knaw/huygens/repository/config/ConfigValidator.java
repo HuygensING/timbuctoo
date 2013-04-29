86945d5025c5f6c456c87cd48ab4743f43056a5a
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-04-29 15:27:53 +0000

    Added first version of configuration validator

---
+++ src/nl/knaw/huygens/repository/config/ConfigValidator.java
@@ -0,0 +1,43 @@
+package nl.knaw.huygens.repository.config;
+
+import nl.knaw.huygens.repository.Configuration;
+import nl.knaw.huygens.repository.model.util.DocumentTypeRegister;
+
+/**
+ * Validates the Configuration of the Repository Project.
+ */
+public class ConfigValidator {
+
+  private final Configuration config;
+  private final DocumentTypeRegister registry;
+  private boolean error;
+
+  public ConfigValidator(Configuration config, DocumentTypeRegister registry) {
+    this.config = config;
+    this.registry = registry;
+  }
+
+  public void validate() {
+    error = false;
+
+    validateDocTypes("doctypes");
+    validateDocTypes("indexeddoctypes");
+    validateDocTypes("variationdoctypes");
+    validateDocTypes("versioneddoctypes");
+    validateDocTypes("defaultdoctype");
+
+    if (error) {
+      throw new RuntimeException("Configuration errors");
+    }
+  }
+
+  private void validateDocTypes(String configKey) {
+    for (String type : config.getSettings(configKey)) {
+      if (registry.getClassFromTypeString(type) == null) {
+        System.err.printf("Configuration key '%s': '%s' is not a document type%n", configKey, type);
+        error = true;
+      }
+    }
+  }
+
+}
