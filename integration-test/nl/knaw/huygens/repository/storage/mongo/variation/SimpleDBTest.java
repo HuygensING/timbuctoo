ff053d401e73e63c4fcba7376e13ebe9534948a0
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-03 13:28:39 +0000

    re #1417: Update of the SimpleDBTest. Made the use case more realistic.

--- integration-test/nl/knaw/huygens/repository/storage/mongo/variation/SimpleDBTest.java
+++ integration-test/nl/knaw/huygens/repository/storage/mongo/variation/SimpleDBTest.java
@@ -10,8 +10,8 @@ import nl.knaw.huygens.repository.config.Configuration;
 import nl.knaw.huygens.repository.config.DocTypeRegistry;
 import nl.knaw.huygens.repository.storage.generic.StorageConfiguration;
 import nl.knaw.huygens.repository.storage.mongo.MongoDiff;
-import nl.knaw.huygens.repository.variation.model.projecta.OtherDoc;
-import nl.knaw.huygens.repository.variation.model.projectb.TestDoc;
+import nl.knaw.huygens.repository.variation.model.projecta.ProjectAGeneralTestDoc;
+import nl.knaw.huygens.repository.variation.model.projectb.ProjectBGeneralTestDoc;
 
 import org.junit.Test;
 
@@ -26,59 +26,59 @@ public class SimpleDBTest {
     Configuration config = mock(Configuration.class);
     DocTypeRegistry docTypeRegistry = new DocTypeRegistry(config.getSetting("model-packages"));
     MongoModifiableVariationStorage s = new MongoModifiableVariationStorage(conf, docTypeRegistry);
-    s.db.getCollection("testbasedoc").drop();
-    s.db.getCollection("testbasedoc-versions").drop();
+    s.db.getCollection("testconcretedoc").drop();
+    s.db.getCollection("testconcretedoc-versions").drop();
     String docId = "TST0001";
     try {
-      s.db.getCollection("testbasedoc").remove(new BasicDBObject("_id", docId));
+      s.db.getCollection("testconcretedoc").remove(new BasicDBObject("_id", docId));
     } catch (Exception ex) {
       System.err.println("Caught exception trying to remove item...");
       ex.printStackTrace();
     }
 
     try {
-      TestDoc doc = new TestDoc();
+      ProjectBGeneralTestDoc doc = new ProjectBGeneralTestDoc();
       doc.name = "blub";
-      doc.blah = "Floo";
+      doc.projectBGeneralTestDocValue = "Floo";
       doc.setRev(0);
       doc.setId(docId);
-      s.addItem(TestDoc.class, doc);
-      OtherDoc otherDoc = new OtherDoc();
+      s.addItem(ProjectBGeneralTestDoc.class, doc);
+      ProjectAGeneralTestDoc otherDoc = new ProjectAGeneralTestDoc();
       otherDoc.name = "blob";
-      otherDoc.otherThing = "Flups";
+      otherDoc.projectAGeneralTestDocValue = "Flups";
       otherDoc.setId(docId);
       otherDoc.setRev(0);
-      s.updateItem(OtherDoc.class, docId, otherDoc);
-      TestDoc returnedItem = s.getItem(TestDoc.class, docId);
+      s.updateItem(ProjectAGeneralTestDoc.class, docId, otherDoc);
+      ProjectBGeneralTestDoc returnedItem = s.getItem(ProjectBGeneralTestDoc.class, docId);
       BasicDBObject expectedChange = new BasicDBObject();
       expectedChange.append("^rev", 1);
       //@variations are added by the VariationReducer
-      expectedChange.append("@variations", Lists.newArrayList("projectb-testdoc", "testbasedoc", "projecta-otherdoc"));
+      expectedChange.append("@variations", Lists.newArrayList("projectb-projectbgeneraltestdoc", "generaltestdoc", "testconcretedoc", "projecta-projectageneraltestdoc"));
 
       assertEquals(expectedChange, MongoDiff.diffDocuments(doc, returnedItem));
 
-      TestDoc doc2 = new TestDoc();
+      ProjectBGeneralTestDoc doc2 = new ProjectBGeneralTestDoc();
       doc2.name = "blubber";
-      doc2.blah = "Floo";
+      doc2.projectBGeneralTestDocValue = "Floo";
       doc2.setId(docId);
       doc2.setRev(1);
-      s.updateItem(TestDoc.class, docId, doc2);
-      returnedItem = s.getItem(TestDoc.class, docId);
+      s.updateItem(ProjectBGeneralTestDoc.class, docId, doc2);
+      returnedItem = s.getItem(ProjectBGeneralTestDoc.class, docId);
       expectedChange.put("^rev", 2);
       assertEquals(expectedChange, MongoDiff.diffDocuments(doc2, returnedItem));
 
-      s.deleteItem(TestDoc.class, docId, null);
+      s.deleteItem(ProjectBGeneralTestDoc.class, docId, null);
       expectedChange.put("^deleted", true);
       expectedChange.put("^rev", 3);
       expectedChange.put("name", "blubber");
-      returnedItem = s.getItem(TestDoc.class, docId);
+      returnedItem = s.getItem(ProjectBGeneralTestDoc.class, docId);
       assertEquals(expectedChange, MongoDiff.diffDocuments(doc, returnedItem));
     } catch (Exception ex) {
       ex.printStackTrace();
       fail();
     } finally {
-      s.db.getCollection("testbasedoc").drop();
-      s.db.getCollection("testbasedoc-versions").drop();
+      s.db.getCollection("testconcretedoc").drop();
+      s.db.getCollection("testconcretedoc-versions").drop();
     }
 
   }
