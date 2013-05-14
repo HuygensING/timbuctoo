0b3fe162753a2ef04ab1c8df74db8a55f93d412a
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-14 07:57:32 +0000

    re #1438: The SecurityFilter was not sufficient anymore. Therefore the AnnotatedSecurityFilterFactory replaced it. This factory will create a filter, that is similar to the SecurityFilter, for each method with the annotation RolesAllowed. For this the RESTAutoResourceTest and the ServletInjectionModule had to be changed. To verify this issue, a unittest for the SiteMapResource was created.

--- src/nl/knaw/huygens/repository/config/ServletInjectionModule.java
+++ src/nl/knaw/huygens/repository/config/ServletInjectionModule.java
@@ -15,8 +15,9 @@ public class ServletInjectionModule extends JerseyServletModule {
   protected void configureServlets() {
     Map<String, String> params = Maps.newHashMap();
     params.put(PackagesResourceConfig.PROPERTY_PACKAGES, "nl.knaw.huygens.repository.resources;com.fasterxml.jackson.jaxrs.json;nl.knaw.huygens.repository.providers");
-    params.put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, "com.sun.jersey.api.container.filter.LoggingFilter;nl.knaw.huygens.repository.server.security.SecurityFilter");
-    params.put(ResourceConfig.PROPERTY_RESOURCE_FILTER_FACTORIES, "com.sun.jersey.api.container.filter.RolesAllowedResourceFilterFactory");
+    params.put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, "com.sun.jersey.api.container.filter.LoggingFilter");
+    params.put(ResourceConfig.PROPERTY_RESOURCE_FILTER_FACTORIES,
+        "nl.knaw.huygens.repository.server.security.AnnotatedSecurityFilterFactory;com.sun.jersey.api.container.filter.RolesAllowedResourceFilterFactory");
     params.put(ResourceConfig.PROPERTY_CONTAINER_RESPONSE_FILTERS, "com.sun.jersey.api.container.filter.LoggingFilter");
     params.put(ServletContainer.PROPERTY_WEB_PAGE_CONTENT_REGEX, "/static.*");
     filter("/*").through(GuiceContainer.class, params);
