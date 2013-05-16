8a7605b946750573b138bb807f223a3f2096e9df
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-16 13:59:47 +0000

    Refactored

--- src/nl/knaw/huygens/repository/util/RepositoryException.java
+++ src/nl/knaw/huygens/repository/util/RepositoryException.java
@@ -2,18 +2,22 @@ package nl.knaw.huygens.repository.util;
 
 public class RepositoryException extends RuntimeException {
 
-  private static final long serialVersionUID = -5175017807899393836L;
-
-  public RepositoryException(Throwable cause) {
-    super(cause);
-  }
+  private static final long serialVersionUID = 1L;
 
   public RepositoryException() {
     super();
   }
 
-  public RepositoryException(String msg) {
-    super(msg);
+  public RepositoryException(String message) {
+    super(message);
+  }
+
+  public RepositoryException(Throwable cause) {
+    super(cause);
+  }
+
+  public RepositoryException(String message, Throwable cause) {
+    super(message, cause);
   }
 
 }
