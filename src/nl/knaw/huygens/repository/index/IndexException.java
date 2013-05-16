bc9f890b9e949bf7fb3c1109a0283489c7e08f3a
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-16 13:59:47 +0000

    Refactored

--- src/nl/knaw/huygens/repository/index/IndexException.java
+++ src/nl/knaw/huygens/repository/index/IndexException.java
@@ -4,8 +4,20 @@ public class IndexException extends Exception {
 
   private static final long serialVersionUID = 1L;
 
+  public IndexException() {
+    super();
+  }
+
   public IndexException(String message) {
     super(message);
+  }
+
+  public IndexException(Throwable cause) {
+    super(cause);
+  }
+
+  public IndexException(String message, Throwable cause) {
+    super(message, cause);
   }
 
 }
