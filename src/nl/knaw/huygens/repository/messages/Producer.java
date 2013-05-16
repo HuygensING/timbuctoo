756f00098894fdcc61d9ecb859983398ed64ddaf
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-16 13:54:09 +0000

    First message broker code

---
+++ src/nl/knaw/huygens/repository/messages/Producer.java
@@ -0,0 +1,37 @@
+package nl.knaw.huygens.repository.messages;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+public class Producer {
+
+  private Connection connection;
+  private Session session;
+  private MessageProducer producer;
+
+  public Producer(ConnectionFactory factory, String queue) throws JMSException {
+    connection = factory.createConnection();
+    connection.start();
+    session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    Destination destination = session.createQueue(queue);
+    producer = session.createProducer(destination);
+    producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+  }
+
+  public void send(String text) throws JMSException {
+    TextMessage message = session.createTextMessage(text);
+    producer.send(message);
+  }
+
+  public void close() throws JMSException {
+    session.close();
+    connection.close();
+  }
+
+}
