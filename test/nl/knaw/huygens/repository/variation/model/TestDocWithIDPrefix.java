253dd43131abb6c02d03d50dbf120e6a58a82833
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-16 15:03:32 +0000

    re #1311:  Unit tests added for delete, add MongoModifiableVariationStorage. The test replaces the SimpleDBTest.

---
+++ test/nl/knaw/huygens/repository/variation/model/TestDocWithIDPrefix.java
@@ -0,0 +1,8 @@
+package nl.knaw.huygens.repository.variation.model;
+
+import nl.knaw.huygens.repository.model.annotations.IDPrefix;
+
+@IDPrefix("TDI")
+public class TestDocWithIDPrefix extends GeneralTestDoc {
+
+}
