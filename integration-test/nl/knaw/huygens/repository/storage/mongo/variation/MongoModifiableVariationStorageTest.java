6ec6f07d90571a455f2613c748e026f709cfac9c
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-17 10:37:35 +0000

    re #1311: unittests added, testcases commented out, that still have to be implemented.

--- integration-test/nl/knaw/huygens/repository/storage/mongo/variation/MongoModifiableVariationStorageTest.java
+++ integration-test/nl/knaw/huygens/repository/storage/mongo/variation/MongoModifiableVariationStorageTest.java
@@ -2,6 +2,7 @@ package nl.knaw.huygens.repository.storage.mongo.variation;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -14,6 +15,7 @@ import nl.knaw.huygens.repository.config.DocTypeRegistry;
 import nl.knaw.huygens.repository.storage.generic.StorageConfiguration;
 import nl.knaw.huygens.repository.storage.mongo.MongoDiff;
 import nl.knaw.huygens.repository.variation.VariationException;
+import nl.knaw.huygens.repository.variation.model.GeneralTestDoc;
 import nl.knaw.huygens.repository.variation.model.TestConcreteDoc;
 import nl.knaw.huygens.repository.variation.model.TestDocWithIDPrefix;
 
@@ -21,9 +23,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.collect.Lists;
 import com.mongodb.MongoException;
 
 public class MongoModifiableVariationStorageTest {
+  private static final String DEFAULT_ID = "TCD000000001";
   private static final String DB_NAME = "test";
   private MongoModifiableVariationStorage instance;
   private DocTypeRegistry docTypeRegistry;
@@ -37,9 +41,14 @@ public class MongoModifiableVariationStorageTest {
     when(storageConfiguration.getUser()).thenReturn("test");
     when(storageConfiguration.getPassword()).thenReturn("test");
 
+    //docTypeRegistry = new DocTypeRegistry("nl.knaw.huygens.repository.variation.model");
     docTypeRegistry = mock(DocTypeRegistry.class);
     when(docTypeRegistry.getCollectionId(TestConcreteDoc.class)).thenReturn("testconcretedoc");
-    when(docTypeRegistry.getCollectionId(TestDocWithIDPrefix.class)).thenReturn("testdocwithidprefix");
+    when(docTypeRegistry.getCollectionId(TestDocWithIDPrefix.class)).thenReturn("testconcretedoc");
+    when(docTypeRegistry.getCollectionId(GeneralTestDoc.class)).thenReturn("testconcretedoc");
+
+    doReturn(TestConcreteDoc.class).when(docTypeRegistry).getClassFromTypeString("testconcretedoc");
+    doReturn(GeneralTestDoc.class).when(docTypeRegistry).getClassFromTypeString("generaltestdoc");
 
     instance = new MongoModifiableVariationStorage(storageConfiguration, docTypeRegistry);
   }
@@ -70,15 +79,13 @@ public class MongoModifiableVariationStorageTest {
 
     instance.addItem(TestDocWithIDPrefix.class, input);
 
-    verifyCollectionSize(1, "testdocwithidprefix-versions");
-    verifyCollectionSize(1, "testdocwithidprefix-versions");
+    verifyCollectionSize(1, "testconcretedoc");
+    verifyCollectionSize(1, "testconcretedoc-versions");
   }
 
   @Test
   public void testAddItemWithId() throws IOException {
-    String id = "TCD000000001";
-
-    TestConcreteDoc input = createTestDoc(id, "test");
+    TestConcreteDoc input = createTestDoc(DEFAULT_ID, "test");
 
     Class<TestConcreteDoc> type = TestConcreteDoc.class;
     instance.addItem(type, input);
@@ -89,13 +96,24 @@ public class MongoModifiableVariationStorageTest {
 
   @Test(expected = MongoException.class)
   public void testAddItemTwice() throws IOException {
-    String id = "TCD000000001";
-
-    TestConcreteDoc input = createTestDoc(id, "test");
+    TestConcreteDoc input = createTestDoc(DEFAULT_ID, "test");
 
     Class<TestConcreteDoc> type = TestConcreteDoc.class;
     instance.addItem(type, input);
     instance.addItem(type, input);
+  }
+
+  @Test
+  public void addItemSubType() throws IOException {
+    GeneralTestDoc input = new GeneralTestDoc();
+    input.setId(DEFAULT_ID);
+    input.name = "subType";
+    input.generalTestDocValue = "test";
+
+    instance.addItem(GeneralTestDoc.class, input);
+
+    verifyCollectionSize(1, "testconcretedoc");
+    verifyCollectionSize(1, "testconcretedoc-versions");
   }
 
   @Test
@@ -136,24 +154,22 @@ public class MongoModifiableVariationStorageTest {
 
   @Test(expected = IOException.class)
   public void testUpdateItemNonExistent() throws IOException {
-    String id = "TCD000000001";
-    TestConcreteDoc expected = createTestDoc(id, "test");
+    TestConcreteDoc expected = createTestDoc(DEFAULT_ID, "test");
 
     Class<TestConcreteDoc> type = TestConcreteDoc.class;
 
     expected.name = "updated";
-    instance.updateItem(type, id, expected);
+    instance.updateItem(type, DEFAULT_ID, expected);
   }
 
   @Test
   public void testDeletetem() throws IOException {
-    String id = "TCD000000001";
-    TestConcreteDoc input = createTestDoc(id, "test");
+    TestConcreteDoc input = createTestDoc(DEFAULT_ID, "test");
 
     Class<TestConcreteDoc> type = TestConcreteDoc.class;
     instance.addItem(type, input);
 
-    instance.deleteItem(type, id, null);
+    instance.deleteItem(type, DEFAULT_ID, null);
 
     verifyCollectionSize(1, "testconcretedoc");
     verifyCollectionSize(1, "testconcretedoc-versions");
@@ -162,61 +178,57 @@ public class MongoModifiableVariationStorageTest {
 
   @Test(expected = IOException.class)
   public void testDeleteItemNonExistent() throws IOException {
-    String id = "TCD000000001";
-
     Class<TestConcreteDoc> type = TestConcreteDoc.class;
 
-    instance.deleteItem(type, id, null);
+    instance.deleteItem(type, DEFAULT_ID, null);
   }
 
   //MongoVariationStorageImpl tests
+
   @Test
   public void testGetItem() throws IOException {
-    String id = "TCD0000000001";
-    TestConcreteDoc expected = createTestDoc(id, "getItem");
+    TestConcreteDoc expected = createTestDoc(DEFAULT_ID, "getItem");
 
     Class<TestConcreteDoc> type = TestConcreteDoc.class;
     instance.addItem(type, expected);
 
-    TestConcreteDoc actual = instance.getItem(type, id);
+    TestConcreteDoc actual = instance.getItem(type, DEFAULT_ID);
 
     assertEquals(null, MongoDiff.diffDocuments(expected, actual));
   }
 
   @Test
   public void testGetItemUpdatedItem() throws IOException {
-    String id = "TCD0000000001";
-    TestConcreteDoc input = createTestDoc(id, "getItem");
+    TestConcreteDoc input = createTestDoc(DEFAULT_ID, "getItem");
 
     Class<TestConcreteDoc> type = TestConcreteDoc.class;
     instance.addItem(type, input);
 
     input.name = "getItem2";
-    instance.updateItem(type, id, input);
+    instance.updateItem(type, DEFAULT_ID, input);
 
-    TestConcreteDoc expected = createTestDoc(id, "getItem2");
+    TestConcreteDoc expected = createTestDoc(DEFAULT_ID, "getItem2");
     expected.setRev(1);
 
-    TestConcreteDoc actual = instance.getItem(type, id);
+    TestConcreteDoc actual = instance.getItem(type, DEFAULT_ID);
 
     assertEquals(null, MongoDiff.diffDocuments(expected, actual));
   }
 
   @Test
   public void testGetItemDeletedItem() throws IOException {
-    String id = "TCD0000000001";
-    TestConcreteDoc input = createTestDoc(id, "getItem");
+    TestConcreteDoc input = createTestDoc(DEFAULT_ID, "getItem");
 
     Class<TestConcreteDoc> type = TestConcreteDoc.class;
     instance.addItem(type, input);
 
-    instance.deleteItem(type, id, null);
+    instance.deleteItem(type, DEFAULT_ID, null);
 
-    TestConcreteDoc expected = createTestDoc(id, "getItem");
+    TestConcreteDoc expected = createTestDoc(DEFAULT_ID, "getItem");
     expected.setRev(1);
     expected.setDeleted(true);
 
-    TestConcreteDoc actual = instance.getItem(type, id);
+    TestConcreteDoc actual = instance.getItem(type, DEFAULT_ID);
 
     assertEquals(null, MongoDiff.diffDocuments(expected, actual));
   }
@@ -226,6 +238,126 @@ public class MongoModifiableVariationStorageTest {
     TestConcreteDoc item = instance.getItem(TestConcreteDoc.class, "TCD000000001");
     assertNull(item);
   }
+
+  @Test
+  public void testGetItemSubType() throws IOException {
+    GeneralTestDoc input = new GeneralTestDoc();
+    input.setId(DEFAULT_ID);
+    input.name = "subType";
+    input.generalTestDocValue = "test";
+
+    GeneralTestDoc expected = new GeneralTestDoc();
+    expected.setId(DEFAULT_ID);
+    expected.name = "subType";
+    expected.generalTestDocValue = "test";
+    expected.setCurrentVariation("model");
+    expected.setVariations(Lists.newArrayList("generaltestdoc", "testconcretedoc"));
+
+    Class<GeneralTestDoc> type = GeneralTestDoc.class;
+    instance.addItem(type, input);
+
+    GeneralTestDoc actual = instance.getItem(type, DEFAULT_ID);
+
+    assertEquals(null, MongoDiff.diffDocuments(expected, actual));
+  }
+
+  @Test
+  public void testGetAllVariations() throws IOException {
+    GeneralTestDoc input = new GeneralTestDoc();
+    input.setId(DEFAULT_ID);
+    input.name = "subType";
+    input.generalTestDocValue = "test";
+
+    instance.addItem(GeneralTestDoc.class, input);
+    List<TestConcreteDoc> variations = instance.getAllVariations(TestConcreteDoc.class, DEFAULT_ID);
+
+    assertEquals(2, variations.size());
+  }
+
+  //  @Test
+  //  public void testGetAllVariationsOfSubType() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetVariation() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetVariationNonExisting() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetVariationOfNonExistingItem() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetAllByType() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetAllByTypeSubType() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetAllByTypeNoneFound() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetAllRevisions() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetAllRevisionsOfSubType() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetByMultipleIds() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetByMultipleIdsNotAllFound() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetByMultipleIdsNonFound() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetLastChanged() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetLastChangedNonFound() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testFetchAll() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testGetIdsForQuery() {
+  //    fail("Yet to be implemented");
+  //  }
+  //
+  //  @Test
+  //  public void testEnsureIndex() {
+  //    fail("Yet to be implemented");
+  //  }
 
   //  @Test
   //  public void testGetVersion() {
