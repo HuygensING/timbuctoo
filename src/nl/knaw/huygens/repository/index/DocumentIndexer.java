b3174e18dc56fc005e399c040ff336e7acad4a4d
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-27 11:57:11 +0000

    Move exception handling to IndexManager

--- src/nl/knaw/huygens/repository/index/DocumentIndexer.java
+++ src/nl/knaw/huygens/repository/index/DocumentIndexer.java
@@ -3,18 +3,17 @@ package nl.knaw.huygens.repository.index;
 import java.util.List;
 
 import nl.knaw.huygens.repository.model.Document;
-import nl.knaw.huygens.repository.util.RepositoryException;
 
 public interface DocumentIndexer<T extends Document> {
 
-  <U extends T> void add(List<U> entities) throws RepositoryException;
+  <U extends T> void add(List<U> entities) throws IndexException;
 
-  <U extends T> void modify(List<U> entity) throws RepositoryException;
+  <U extends T> void modify(List<U> entity) throws IndexException;
 
-  void remove(String id) throws RepositoryException;
+  void remove(String id) throws IndexException;
 
-  void removeAll() throws RepositoryException;
+  void removeAll() throws IndexException;
 
-  void flush() throws RepositoryException;
+  void flush() throws IndexException;
 
 }
