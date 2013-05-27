09d7ee3b01ead0ee1735db8d4929041f6e66d51e
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-27 09:33:22 +0000

    re #1541: comments improved and license added.

--- src/nl/knaw/huygens/repository/server/security/apis/ApisAuthorizationServerResourceFilter.java
+++ src/nl/knaw/huygens/repository/server/security/apis/ApisAuthorizationServerResourceFilter.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package nl.knaw.huygens.repository.server.security.apis;
 
 import javax.servlet.http.HttpServletResponse;
@@ -27,7 +46,7 @@ import com.sun.jersey.spi.container.ResourceFilter;
 
 /**
  * This class is a ResourceFilter implementation of the AuthorizationServerFilter of the 'apis resource server library'.
- * All the functionality is copy pasted except for the CORS-code. This code should be added a javax.servlet.Filter and not in a ResourceFilter.
+ * All the functionality is copy-pasted except for the CORS-code. This code should be added a javax.servlet.Filter and not in a ResourceFilter.
  *  
  * @author martijnm
  *
