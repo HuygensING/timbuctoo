4f0aa77030a2ec820224b0e5a5f4cacd0fb5dc10
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-28 10:28:11 +0000

    Update messaging stuff

--- src/nl/knaw/huygens/repository/messages/Producer.java
+++ src/nl/knaw/huygens/repository/messages/Producer.java
@@ -5,6 +5,7 @@ import javax.jms.ConnectionFactory;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
@@ -26,6 +27,14 @@ public class Producer {
 
   public void send(String text) throws JMSException {
     TextMessage message = session.createTextMessage(text);
+    producer.send(message);
+  }
+
+  public void send(String type, String id, String action) throws JMSException {
+    Message message = session.createMessage();
+    message.setStringProperty("type", type);
+    message.setStringProperty("id", id);
+    message.setStringProperty("action", action);
     producer.send(message);
   }
 
