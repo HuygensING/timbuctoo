37eb2b844afa2d51952aa2738da70e14421c8e54
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-05-31 09:23:01 +0000

    re #1552: MongoObjectMapper for converting a class with it's declared fields to an objectmap. This map is used to generate a query to find objects that satisfy the criteria of the example class.

---
+++ src/nl/knaw/huygens/repository/storage/mongo/MongoObjectMapper.java
@@ -0,0 +1,85 @@
+package nl.knaw.huygens.repository.storage.mongo;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Maps;
+
+/**
+ * This class converts a Java object to a map with a String key and value. 
+ * The keys are the fieldnames used in the mongo database. 
+ * The values are the current value of the object.
+ * @author martijnm
+ *
+ */
+public class MongoObjectMapper {
+  private static final Class<JsonProperty> ANNOTATION_TO_RETRIEVE = JsonProperty.class;
+  private static final String GET_ACCESSOR = "get";
+
+  public <T> Map<String, String> mapObject(Class<T> type, T example) {
+    if (type == null) {
+      throw new IllegalArgumentException("'type' cannot be null.");
+    }
+
+    if (example == null) {
+      throw new IllegalArgumentException("'example' cannot be null.");
+    }
+
+    Map<String, String> objectMap = Maps.<String, String> newHashMap();
+    Field[] fields = type.getDeclaredFields();
+
+    try {
+      for (Field field : fields) {
+        Class<?> fieldType = field.getType();
+        if (fieldType.isPrimitive() || String.class.equals(fieldType)) {
+          field.setAccessible(true);
+          Object value = field.get(example);
+          if (value != null) {
+            objectMap.put(getFieldName(type, field), String.valueOf(value));
+          }
+        }
+      }
+    } catch (IllegalAccessException ex) {
+      ex.printStackTrace();
+    }
+
+    return objectMap;
+  }
+
+  private String getFieldName(Class<?> type, Field field) {
+    JsonProperty annotation = field.getAnnotation(ANNOTATION_TO_RETRIEVE);
+    if (annotation != null) {
+      return annotation.value();
+    }
+
+    Method method = getMethodOfField(type, field.getName());
+
+    if (method != null && method.getAnnotation(ANNOTATION_TO_RETRIEVE) != null) {
+      return method.getAnnotation(ANNOTATION_TO_RETRIEVE).value();
+    }
+    return field.getName();
+  }
+
+  private Method getMethodOfField(Class<?> type, String fieldName) {
+    Method method = null;
+    try {
+      method = type.getMethod(getMethodName(fieldName, MongoObjectMapper.GET_ACCESSOR));
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    } catch (SecurityException e) {
+      e.printStackTrace();
+    }
+
+    return method;
+  }
+
+  private String getMethodName(String fieldName, String accessor) {
+    char[] fieldNameChars = fieldName.toCharArray();
+
+    fieldNameChars[0] = Character.toUpperCase(fieldNameChars[0]);
+
+    return accessor.concat(String.valueOf(fieldNameChars));
+  }
+}
