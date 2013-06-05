5b016b20a71bc36f4441dde3c6bb6173f72d1ad4
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-06-05 23:45:40 +0000

    Confine knowledge of conversion rules to variations package

--- src/nl/knaw/huygens/repository/variation/TypeConverter.java
+++ src/nl/knaw/huygens/repository/variation/TypeConverter.java
@@ -1,24 +1,34 @@
 package nl.knaw.huygens.repository.variation;
 
+import java.util.Map;
+
 import nl.knaw.huygens.repository.config.DocTypeRegistry;
 import nl.knaw.huygens.repository.model.Document;
 
+import com.google.common.collect.Maps;
+
 /**
  * Converts between (domain) model type tokens and variation names.
- * 
- * (intermediate step: encapsulate behavior of DocTypeRegistry)
+ * The conversion rules are used by VariationReducer and VariationInducer only.
  */
 class TypeConverter {
 
-  private final DocTypeRegistry registry;
+  private final Map<String, Class<? extends Document>> map;
 
   public TypeConverter(DocTypeRegistry registry) {
-    this.registry = registry;
+    map = Maps.newHashMap();
+    for (Class<? extends Document> type : registry.getDocumentTypes()) {
+      map.put(type.getSimpleName().toLowerCase(), type);
+    }
   }
 
   @SuppressWarnings("unchecked")
   public <T extends Document> Class<? extends T> getClass(String id) {
-    return (Class<? extends T>) registry.getClassFromMongoTypeString(id);
+    return (Class<? extends T>) map.get(normalize(id));
+  }
+
+  private String normalize(String typeString) {
+    return typeString.replaceFirst("[a-z]*-", "");
   }
 
 }
