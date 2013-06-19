22bb279f846f7029a41952aa377c7e8ab0fb5931
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-06-19 06:41:29 +0000

    Close resource(s) at end of lifecycle servlet

--- src/nl/knaw/huygens/repository/RepoContextListener.java
+++ src/nl/knaw/huygens/repository/RepoContextListener.java
@@ -1,8 +1,11 @@
 package nl.knaw.huygens.repository;
 
+import javax.servlet.ServletContextEvent;
+
 import nl.knaw.huygens.repository.config.BasicInjectionModule;
 import nl.knaw.huygens.repository.config.Configuration;
 import nl.knaw.huygens.repository.config.ServletInjectionModule;
+import nl.knaw.huygens.repository.managers.StorageManager;
 
 import org.apache.commons.configuration.ConfigurationException;
 
@@ -12,23 +15,33 @@ import com.google.inject.Module;
 import com.google.inject.servlet.GuiceServletContextListener;
 
 /**
- * Creates the injector for the repository servlet.
+ * Creates and manages the injector for the repository servlet.
  */
 public class RepoContextListener extends GuiceServletContextListener {
 
+  private Injector injector;
+
   @Override
   protected Injector getInjector() {
-    Configuration config = getConfiguration();
-    Module baseModule = new BasicInjectionModule(config);
-    Module servletModule = new ServletInjectionModule();
-    return Guice.createInjector(baseModule, servletModule);
-  }
-
-  private Configuration getConfiguration() {
     try {
-      return new Configuration(Configuration.DEFAULT_CONFIG_FILE);
+      Configuration config = new Configuration();
+      Module baseModule = new BasicInjectionModule(config);
+      Module servletModule = new ServletInjectionModule();
+      injector = Guice.createInjector(baseModule, servletModule);
+      return injector;
     } catch (ConfigurationException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+    if (injector != null) {
+      StorageManager storageManager = injector.getInstance(StorageManager.class);
+      if (storageManager != null) {
+        storageManager.close();
+      }
+      injector = null;
     }
   }
 
