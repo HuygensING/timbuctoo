a1844f4e269b5dffef1049668e33aa4a7faf37dd
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-06-25 14:54:41 +0000

    fix the build.

---
+++ test/nl/knaw/huygens/repository/storage/mongo/model/TestSystemDocument.java
@@ -0,0 +1,82 @@
+package nl.knaw.huygens.repository.storage.mongo.model;
+
+import nl.knaw.huygens.repository.indexdata.IndexAnnotation;
+import nl.knaw.huygens.repository.model.SystemDocument;
+import nl.knaw.huygens.repository.model.annotations.IDPrefix;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@IDPrefix("TSD")
+public class TestSystemDocument extends SystemDocument {
+  private String name;
+  private String testValue1;
+  private String testValue2;
+  @JsonProperty("propAnnotated")
+  private String annotatedProperty;
+  private String propWithAnnotatedAccessors;
+
+  @Override
+  @JsonProperty("!currentVariation")
+  public String getCurrentVariation() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  @JsonProperty("!currentVariation")
+  public void setCurrentVariation(String currentVariation) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  @JsonIgnore
+  @IndexAnnotation(fieldName = "desc")
+  public String getDescription() {
+    return name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getTestValue1() {
+    return testValue1;
+  }
+
+  public void setTestValue1(String testValue1) {
+    this.testValue1 = testValue1;
+  }
+
+  public String getTestValue2() {
+    return testValue2;
+  }
+
+  public void setTestValue2(String testValue2) {
+    this.testValue2 = testValue2;
+  }
+
+  public String getAnnotatedProperty() {
+    return annotatedProperty;
+  }
+
+  public void setAnnotatedProperty(String annotatedProperty) {
+    this.annotatedProperty = annotatedProperty;
+  }
+
+  @JsonProperty("pwaa")
+  public String getPropWithAnnotatedAccessors() {
+    return propWithAnnotatedAccessors;
+  }
+
+  @JsonProperty("pwaa")
+  public void setPropWithAnnotatedAccessors(String propWithAnnotatedAccessors) {
+    this.propWithAnnotatedAccessors = propWithAnnotatedAccessors;
+  }
+
+}
