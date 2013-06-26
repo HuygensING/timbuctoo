a302f86b7616e00b7fe122c2ee48407a5ef4e0ab
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-06-26 18:44:01 +0000

    Moved annotations

--- src/nl/knaw/huygens/repository/storage/RevisionChanges.java
+++ src/nl/knaw/huygens/repository/storage/RevisionChanges.java
@@ -5,6 +5,9 @@ import java.util.List;
 import nl.knaw.huygens.repository.model.Document;
 
 public interface RevisionChanges<T extends Document> {
+
   public String getId();
+
   public List<T> getRevisions();
+
 }
