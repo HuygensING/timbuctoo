64155cf8985e54f977ab473ccab10e94ff6a5f79
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-06-26 18:50:34 +0000

    Moved annotations

--- src/nl/knaw/huygens/repository/model/atla/AtlaPerson.java
+++ src/nl/knaw/huygens/repository/model/atla/AtlaPerson.java
@@ -1,7 +1,7 @@
 package nl.knaw.huygens.repository.model.atla;
 
+import nl.knaw.huygens.repository.annotations.DocumentTypeName;
 import nl.knaw.huygens.repository.model.Person;
-import nl.knaw.huygens.repository.model.annotations.DocumentTypeName;
 
 @DocumentTypeName("atlaperson")
 public class AtlaPerson extends Person {
--- src/nl/knaw/huygens/repository/model/dwcbia/DWCScientist.java
+++ src/nl/knaw/huygens/repository/model/dwcbia/DWCScientist.java
@@ -1,6 +1,6 @@
 package nl.knaw.huygens.repository.model.dwcbia;
 
-import nl.knaw.huygens.repository.model.annotations.DocumentTypeName;
+import nl.knaw.huygens.repository.annotations.DocumentTypeName;
 
 @DocumentTypeName("dwcscientist")
 public class DWCScientist extends DWCPerson {
