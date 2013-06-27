5f511f803f24320706f3153ea4a2217097c51708
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-06-27 08:55:02 +0000

    Atlantische Gids: import places

--- src/nl/knaw/huygens/repository/importer/database/AtlantischeGidsImporter.java
+++ src/nl/knaw/huygens/repository/importer/database/AtlantischeGidsImporter.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 import nl.knaw.huygens.repository.managers.StorageManager;
-import nl.knaw.huygens.repository.model.Person;
 import nl.knaw.huygens.repository.model.atla.AtlaPerson;
+import nl.knaw.huygens.repository.model.atla.AtlaPlace;
 import nl.knaw.huygens.repository.util.Progress;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,15 +14,23 @@ import com.google.common.collect.Maps;
 
 public class AtlantischeGidsImporter {
 
-  // Maps from project id to storage id
+  // Maps for converting from project id to storage id
   private Map<String, String> personIdMap;
+  private Map<String, String> placeIdMap;
 
   public AtlantischeGidsImporter() {
     personIdMap = Maps.newHashMap();
+    placeIdMap = Maps.newHashMap();
   }
 
   public void importData(String path, StorageManager storageManager) throws Exception {
-    File file = new File(path + "keywords/keyw_pers.json");
+    importPersons(storageManager, path + "keywords/keyw_pers.json");
+    importPlaces(storageManager, path + "keywords/keyw_geo.json");
+  }
+
+  private void importPersons(StorageManager storageManager, String path) throws Exception {
+    System.out.printf("%n=== Import documents of type '%s'%n", AtlaPerson.class.getSimpleName());
+    File file = new File(path);
     if (file.canRead()) {
       Progress progress = new Progress();
       ObjectMapper mapper = new ObjectMapper();
@@ -32,7 +40,7 @@ public class AtlantischeGidsImporter {
           progress.step();
           AtlaPerson person = new AtlaPerson();
           person.name = entry.getValue();
-          storageManager.addDocument(Person.class, person);
+          storageManager.addDocument(AtlaPerson.class, person);
           personIdMap.put(entry.getKey(), person.getId());
         }
       }
@@ -42,8 +50,36 @@ public class AtlantischeGidsImporter {
     }
   }
 
-  public static class PersonKeywords {
+  private void importPlaces(StorageManager storageManager, String path) throws Exception {
+    System.out.printf("%n=== Import documents of type '%s'%n", AtlaPlace.class.getSimpleName());
+    File file = new File(path);
+    if (file.canRead()) {
+      Progress progress = new Progress();
+      ObjectMapper mapper = new ObjectMapper();
+      PlaceKeywords keywords = mapper.readValue(file, PlaceKeywords.class);
+      for (Map<String, String> map : keywords.keywords_geo) {
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+          progress.step();
+          AtlaPlace place = new AtlaPlace();
+          place.name = entry.getValue();
+          storageManager.addDocument(AtlaPlace.class, place);
+          placeIdMap.put(entry.getKey(), place.getId());
+        }
+      }
+      progress.done();
+    } else {
+      System.err.println("Cannot read " + file.getCanonicalPath());
+    }
+  }
+
+  // --- Helper classes ------------------------------------------------
+
+  private static class PersonKeywords {
     public List<Map<String, String>> keywords_pers;
+  }
+
+  private static class PlaceKeywords {
+    public List<Map<String, String>> keywords_geo;
   }
 
 }
