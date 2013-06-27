cf052a9cac574467cdcf9fe95291fcbb0429d96a
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-06-27 08:55:02 +0000

    Atlantische Gids: import places

---
+++ src/nl/knaw/huygens/repository/model/atla/AtlaPlace.java
@@ -0,0 +1,9 @@
+package nl.knaw.huygens.repository.model.atla;
+
+import nl.knaw.huygens.repository.annotations.DocumentTypeName;
+import nl.knaw.huygens.repository.model.Place;
+
+@DocumentTypeName("atlaplace")
+public class AtlaPlace extends Place {
+
+}
