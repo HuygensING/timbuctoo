241c9a583c11950c133328a424867b4cc7d8a697
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-06-27 13:44:18 +0000

    re #1375: Bug fix of MongoDiff. When the variations property of the new object was null, a null pointer exception was thrown. This is now solved. The other part of the commit, with the right models.

---
+++ src/nl/knaw/huygens/repository/model/Reference.java
@@ -0,0 +1,77 @@
+package nl.knaw.huygens.repository.model;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * A class that represents a link to other objects.
+ * @author martijnm
+ *
+ */
+
+public class Reference {
+  private String id;
+  private Class<? extends Document> type;
+  private String displayName;
+  private String variation;
+
+  // default constructor for deserializing.
+  public Reference() {
+
+  }
+
+  public Reference(Class<? extends Document> type, String id, String variation) {
+    this.type = type;
+    this.id = id;
+    this.variation = variation;
+    this.displayName = type.getSimpleName().toLowerCase() + (StringUtils.isBlank(variation) ? "" : " (" + variation + ")");
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public Class<? extends Document> getType() {
+    return type;
+  }
+
+  public void setType(Class<? extends Document> objectType) {
+    this.type = objectType;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getVariation() {
+    return variation;
+  }
+
+  public void setVariation(String variation) {
+    this.variation = variation;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    // a null reference will never be an instance of Reference.
+    if (obj instanceof Reference) {
+      Reference other = (Reference) obj;
+      boolean isEqual = ObjectUtils.equals(id, other.id);
+      isEqual &= ObjectUtils.equals(variation, other.variation);
+      isEqual &= ObjectUtils.equals(type, other.type);
+
+      return isEqual;
+    }
+
+    return false;
+
+  }
+}
