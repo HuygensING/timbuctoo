8d7cc22fe142e66d516c244475005ffc0d8ff004
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-01 17:10:35 +0000

    Refactored

--- src/nl/knaw/huygens/repository/model/Person.java
+++ src/nl/knaw/huygens/repository/model/Person.java
@@ -9,9 +9,9 @@ import nl.knaw.huygens.repository.model.util.Datable;
 @DocumentTypeName("person")
 public class Person extends DomainDocument {
 
-  public String name;
-  public Datable birthDate;
-  public Datable deathDate;
+  private String name;
+  private Datable birthDate;
+  private Datable deathDate;
 
   @Override
   public String getDescription() {
@@ -23,14 +23,26 @@ public class Person extends DomainDocument {
     return name;
   }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
   @IndexAnnotation(fieldName = "facet_s_birthDate", isFaceted = true, canBeEmpty = true)
   public Datable getBirthDate() {
     return birthDate;
   }
 
+  public void setBirthDate(Datable birthDate) {
+    this.birthDate = birthDate;
+  }
+
   @IndexAnnotation(fieldName = "facet_s_deathDate", isFaceted = true, canBeEmpty = true)
   public Datable getDeathDate() {
     return deathDate;
+  }
+
+  public void setDeathDate(Datable deathDate) {
+    this.deathDate = deathDate;
   }
 
 }
