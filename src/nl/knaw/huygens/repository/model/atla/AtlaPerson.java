2053e90dc284724fe8c8794c1f563eb2daa273fb
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-04 10:42:31 +0000

    Removed obsolete classes

--- src/nl/knaw/huygens/repository/model/atla/AtlaPerson.java
+++
@@ -1,9 +0,0 @@
-package nl.knaw.huygens.repository.model.atla;
-
-import nl.knaw.huygens.repository.annotations.DocumentTypeName;
-import nl.knaw.huygens.repository.model.Person;
-
-@DocumentTypeName("atlaperson")
-public class AtlaPerson extends Person {
-
-}
