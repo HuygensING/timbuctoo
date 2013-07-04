765d32baab4ae159223a17dbdb7a401c572866ac
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-04 10:42:31 +0000

    Removed obsolete classes

--- src/nl/knaw/huygens/repository/model/atla/AtlaPlace.java
+++
@@ -1,9 +0,0 @@
-package nl.knaw.huygens.repository.model.atla;
-
-import nl.knaw.huygens.repository.annotations.DocumentTypeName;
-import nl.knaw.huygens.repository.model.Place;
-
-@DocumentTypeName("atlaplace")
-public class AtlaPlace extends Place {
-
-}
