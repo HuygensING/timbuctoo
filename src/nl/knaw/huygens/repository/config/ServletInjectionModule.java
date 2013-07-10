3682653dfa0dd22e833c11737b911ef5b0421e9c
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-10 11:18:43 +0000

    re #1666: remove the dependency on the apis server vor authentication / authorization.

--- src/nl/knaw/huygens/repository/config/ServletInjectionModule.java
+++ src/nl/knaw/huygens/repository/config/ServletInjectionModule.java
@@ -3,8 +3,6 @@ package nl.knaw.huygens.repository.config;
 import java.util.Map;
 
 import nl.knaw.huygens.repository.CORSFilter;
-import nl.knaw.huygens.repository.server.security.apis.ApisAuthorizationResourceFilterFactory;
-import nl.knaw.huygens.repository.server.security.apis.SecurityContextCreatorResourceFilterFactory;
 
 import com.google.common.collect.Maps;
 import com.sun.jersey.api.container.filter.LoggingFilter;
@@ -22,8 +20,7 @@ public class ServletInjectionModule extends JerseyServletModule {
     Map<String, String> params = Maps.newHashMap();
     params.put(PackagesResourceConfig.PROPERTY_PACKAGES, "nl.knaw.huygens.repository.resources;com.fasterxml.jackson.jaxrs.json;nl.knaw.huygens.repository.providers");
     params.put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, getClassNamesString(LoggingFilter.class));
-    params.put(ResourceConfig.PROPERTY_RESOURCE_FILTER_FACTORIES,
-        getClassNamesString(ApisAuthorizationResourceFilterFactory.class, SecurityContextCreatorResourceFilterFactory.class, RolesAllowedResourceFilterFactory.class));
+    params.put(ResourceConfig.PROPERTY_RESOURCE_FILTER_FACTORIES, getClassNamesString(RolesAllowedResourceFilterFactory.class));
     params.put(ResourceConfig.PROPERTY_CONTAINER_RESPONSE_FILTERS, getClassNamesString(LoggingFilter.class, CORSFilter.class));
     params.put(ServletContainer.PROPERTY_WEB_PAGE_CONTENT_REGEX, "/static.*");
     filter("/*").through(GuiceContainer.class, params);
