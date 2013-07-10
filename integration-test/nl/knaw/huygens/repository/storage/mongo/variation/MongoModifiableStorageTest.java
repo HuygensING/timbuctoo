4026602ac880df6712e9c61dbe2b556c7ca75dfe
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-10 11:07:03 +0000

    Refactored

--- integration-test/nl/knaw/huygens/repository/storage/mongo/variation/MongoModifiableStorageTest.java
+++ integration-test/nl/knaw/huygens/repository/storage/mongo/variation/MongoModifiableStorageTest.java
@@ -33,7 +33,7 @@ import org.junit.Test;
 import com.google.common.collect.Lists;
 import com.mongodb.MongoException;
 
-public class MongoModifiableVariationStorageTest extends MongoStorageTestBase {
+public class MongoModifiableStorageTest extends MongoStorageTestBase {
   private static final String DEFAULT_ID = "TCD000000001";
   private MongoVariationStorage instance;
   private static DocTypeRegistry docTypeRegistry;
