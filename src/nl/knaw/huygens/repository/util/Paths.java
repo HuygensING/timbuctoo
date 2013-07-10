5d449e7a45d09b5145f2bac4f0c78858c309b1ca
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-10 09:31:20 +0000

    Restored utility class

--- src/nl/knaw/huygens/repository/util/Paths.java
+++ src/nl/knaw/huygens/repository/util/Paths.java
@@ -4,8 +4,7 @@ public class Paths {
 
   public static String pathInUserHome(String path) {
     path = Character.toString(path.charAt(0)).equals("/") ? path : "/" + path;
-    return "C:/Data/eclipse-home" + path;
-    // return System.getProperty("user.home") + path;
+    return System.getProperty("user.home") + path;
   }
 
   private Paths() {
