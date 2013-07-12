a902447043a551d556b9f6b4dd718a4edc84d728
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-12 14:02:39 +0000

    re #1665: Faceted search made possible in the SearchManager.

--- src/nl/knaw/huygens/repository/managers/SearchManager.java
+++ src/nl/knaw/huygens/repository/managers/SearchManager.java
@@ -5,8 +5,11 @@ import java.util.Date;
 import java.util.List;
 
 import nl.knaw.huygens.repository.index.LocalSolrServer;
+import nl.knaw.huygens.repository.model.Document;
+import nl.knaw.huygens.repository.model.Person;
 import nl.knaw.huygens.repository.model.SearchResult;
 import nl.knaw.huygens.solr.FacetCount;
+import nl.knaw.huygens.solr.FacetParameter;
 import nl.knaw.huygens.solr.FacetedSearchParameters;
 
 import org.apache.solr.client.solrj.SolrServerException;
@@ -31,8 +34,10 @@ public class SearchManager {
     this.server = server;
   }
 
-  public SearchResult search(String core, FacetedSearchParameters searchParameters) throws SolrServerException {
-    QueryResponse response = server.getQueryResponse(searchParameters.getTerm(), getFacetFieldNames(), searchParameters.getSort(), core);
+  public SearchResult search(Class<? extends Document> type, String core, FacetedSearchParameters searchParameters) throws SolrServerException {
+
+    String searchTerm = createSearchTerm(type, searchParameters);
+    QueryResponse response = server.getQueryResponse(searchTerm, getFacetFieldNames(type), searchParameters.getSort(), core);
     SolrDocumentList documents = response.getResults();
 
     List<FacetCount> facets = getFacetCounts(response.getFacetFields());
@@ -43,10 +48,39 @@ public class SearchManager {
       ids.add(document.getFieldValue("id").toString());
     }
 
-    SearchResult searchResult = new SearchResult(ids, core, searchParameters.getTerm(), searchParameters.getSort(), new Date().toString());
+    SearchResult searchResult = new SearchResult(ids, core, searchTerm, searchParameters.getSort(), new Date().toString());
     searchResult.setFacets(facets);
 
     return searchResult;
+  }
+
+  private String createSearchTerm(Class<? extends Document> type, FacetedSearchParameters searchParameters) {
+    List<FacetParameter> facetValues = searchParameters.getFacetValues();
+    if (facetValues != null && !facetValues.isEmpty()) {
+      StringBuffer buffer = new StringBuffer(String.format("+%s:(%s)", getFullTextSearchFields(type).get(0), searchParameters.getTerm()));
+      for (FacetParameter facetParameter : facetValues) {
+        buffer.append(" +");
+        buffer.append(facetParameter.getName());
+        buffer.append(":(");
+
+        boolean isFirstValue = true;
+        List<String> values = facetParameter.getValues();
+        for (String value : values) {
+          if (isFirstValue) {
+            isFirstValue = false;
+          } else {
+            buffer.append(" ");
+          }
+
+          buffer.append(value);
+        }
+        buffer.append(")");
+      }
+      return buffer.toString();
+
+    }
+
+    return String.format("facet_t_name:(%s)", searchParameters.getTerm());
   }
 
   private List<FacetCount> getFacetCounts(List<FacetField> facetFields) {
@@ -68,8 +102,20 @@ public class SearchManager {
     return facets;
   }
 
-  private Collection<String> getFacetFieldNames() {
-    return Sets.newHashSet("facet_s_birthDate");
+  private Collection<String> getFacetFieldNames(Class<? extends Document> type) {
+    if (Person.class.equals(type)) {
+      return Sets.newHashSet("facet_t_name", "facet_s_birthDate", "facet_s_deathDate");
+    }
+
+    return Sets.newHashSet();
+  }
+
+  private List<String> getFullTextSearchFields(Class<? extends Document> type) {
+    if (Person.class.equals(type)) {
+      return Lists.newArrayList("facet_t_name");
+    }
+    return Lists.newArrayList();
+
   }
 
 }
