a54cd94277a3d90a111c12f4cf7d2902824be209
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-15 11:39:37 +0000

    Analyze generated reference codes of archives

---
+++ src/nl/knaw/huygens/repository/util/TokenHandler.java
@@ -0,0 +1,16 @@
+package nl.knaw.huygens.repository.util;
+
+/**
+ * Performs an action for a specified token.
+ */
+public interface TokenHandler {
+
+  /**
+   * Handles the specified token.
+   * @param token the token to handle.
+   * @return <code>true</code> if processing is to be continued,
+   * <code>false</code> otherwise.
+   */
+  boolean handle(Token token);
+
+}
