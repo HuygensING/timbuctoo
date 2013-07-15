b116b6c8374da969596162d9a282b7d5de9e6e37
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-15 15:01:50 +0000

    re #1638: let the SearchManager use the FacetFinder.

--- src/nl/knaw/huygens/repository/search/SearchManager.java
+++ src/nl/knaw/huygens/repository/search/SearchManager.java
@@ -8,13 +8,9 @@ import nl.knaw.huygens.repository.index.LocalSolrServer;
 import nl.knaw.huygens.repository.model.Document;
 import nl.knaw.huygens.repository.model.Person;
 import nl.knaw.huygens.repository.model.SearchResult;
-import nl.knaw.huygens.repository.model.atlg.ATLGArchive;
-import nl.knaw.huygens.repository.model.atlg.ATLGArchiver;
-import nl.knaw.huygens.repository.model.atlg.ATLGLegislation;
 import nl.knaw.huygens.solr.FacetCount;
 import nl.knaw.huygens.solr.FacetInfo;
 import nl.knaw.huygens.solr.FacetParameter;
-import nl.knaw.huygens.solr.FacetType;
 import nl.knaw.huygens.solr.FacetedSearchParameters;
 
 import org.apache.commons.lang.StringUtils;
@@ -26,7 +22,6 @@ import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -34,16 +29,17 @@ import com.google.inject.Singleton;
 public class SearchManager {
 
   private final LocalSolrServer server;
+  private final FacetFinder facetFinder;
 
   @Inject
-  public SearchManager(LocalSolrServer server) {
+  public SearchManager(LocalSolrServer server, FacetFinder facetFinder) {
     this.server = server;
+    this.facetFinder = facetFinder;
   }
 
   public SearchResult search(Class<? extends Document> type, String core, FacetedSearchParameters searchParameters) throws SolrServerException {
-
     String searchTerm = createSearchTerm(type, searchParameters);
-    Map<String, FacetInfo> facetInfoMap = getFacetFieldNames(type);
+    Map<String, FacetInfo> facetInfoMap = facetFinder.findFacets(type);
     QueryResponse response = server.getQueryResponse(searchTerm, facetInfoMap.keySet(), searchParameters.getSort(), core);
     SolrDocumentList documents = response.getResults();
 
@@ -100,34 +96,6 @@ public class SearchManager {
     }
 
     return facets;
-  }
-
-  // TODO refactor to reflective code
-  private Map<String, FacetInfo> getFacetFieldNames(Class<? extends Document> type) {
-    Map<String, FacetInfo> facetMap = Maps.newHashMap();
-    if (Person.class.equals(type)) {
-      facetMap.put("facet_t_name", new FacetInfo().setName("facet_t_name").setType(FacetType.LIST).setTitle("Name"));
-      facetMap.put("facet_s_birthDate", new FacetInfo().setName("facet_s_birthDate").setType(FacetType.LIST).setTitle("Birth date"));
-      facetMap.put("facet_s_deathDate", new FacetInfo().setName("facet_s_deathDate").setType(FacetType.LIST).setTitle("Death date"));
-    } else if (ATLGArchive.class.equals(type)) {
-      facetMap.put("facet_s_refcode", new FacetInfo().setName("facet_s_refcode").setType(FacetType.LIST).setTitle("Reference code"));
-      facetMap.put("facet_s_place", new FacetInfo().setName("facet_s_place").setType(FacetType.LIST).setTitle("Place"));
-      facetMap.put("facet_s_subject", new FacetInfo().setName("facet_s_subject").setType(FacetType.LIST).setTitle("Subject"));
-      facetMap.put("facet_s_person", new FacetInfo().setName("facet_s_person").setType(FacetType.LIST).setTitle("Person"));
-      facetMap.put("facet_t_text", new FacetInfo().setName("facet_t_text").setType(FacetType.LIST).setTitle("Notes"));
-    } else if (ATLGLegislation.class.equals(type)) {
-      facetMap.put("facet_t_text", new FacetInfo().setName("facet_t_text").setType(FacetType.LIST).setTitle("Reference"));
-      facetMap.put("facet_s_date", new FacetInfo().setName("facet_s_date").setType(FacetType.LIST).setTitle("Date"));
-      facetMap.put("facet_s_place", new FacetInfo().setName("facet_s_place").setType(FacetType.LIST).setTitle("Place"));
-      facetMap.put("facet_s_subject", new FacetInfo().setName("facet_s_subject").setType(FacetType.LIST).setTitle("Subject"));
-      facetMap.put("facet_s_person", new FacetInfo().setName("facet_s_person").setType(FacetType.LIST).setTitle("Person"));
-    } else if (ATLGArchiver.class.equals(type)) {
-      facetMap.put("facet_t_text", new FacetInfo().setName("facet_t_text").setType(FacetType.LIST).setTitle("History"));
-      facetMap.put("facet_s_place", new FacetInfo().setName("facet_s_place").setType(FacetType.LIST).setTitle("Place"));
-      facetMap.put("facet_s_subject", new FacetInfo().setName("facet_s_subject").setType(FacetType.LIST).setTitle("Subject"));
-      facetMap.put("facet_s_person", new FacetInfo().setName("facet_s_person").setType(FacetType.LIST).setTitle("Person"));
-    }
-    return facetMap;
   }
 
   private List<String> getFullTextSearchFields(Class<? extends Document> type) {
