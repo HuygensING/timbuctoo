e95644147b9171e33afe168ae0a96741cc2f1a28
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-15 13:54:28 +0000

    re #1638: FacetFinderImplemented and tested. IndexAnnotation expanded with the properties title and facetType.

--- src/nl/knaw/huygens/repository/search/FacetFinder.java
+++ src/nl/knaw/huygens/repository/search/FacetFinder.java
@@ -1,13 +1,55 @@
 package nl.knaw.huygens.repository.search;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
-import org.apache.commons.lang.NotImplementedException;
-
+import nl.knaw.huygens.repository.annotations.IndexAnnotation;
+import nl.knaw.huygens.repository.annotations.IndexAnnotations;
+import nl.knaw.huygens.repository.model.Document;
 import nl.knaw.huygens.solr.FacetInfo;
 
+import com.google.common.collect.Maps;
+
 public class FacetFinder {
-  public Map<String, FacetInfo> findFacets() {
-    throw new NotImplementedException();
+  private static final Class<IndexAnnotations> INDEX_ANNOTATIONS_CLASS = IndexAnnotations.class;
+  private static final Class<IndexAnnotation> INDEX_ANNOTATION_CLASS = IndexAnnotation.class;
+
+  @SuppressWarnings("unchecked")
+  public Map<String, FacetInfo> findFacets(Class<? extends Document> type) {
+    Map<String, FacetInfo> facetMap = Maps.newHashMap();
+
+    Method[] methods = type.getMethods();
+
+    for (Method method : methods) {
+      if (method.isAnnotationPresent(INDEX_ANNOTATION_CLASS)) {
+        IndexAnnotation indexAnnotation = method.getAnnotation(INDEX_ANNOTATION_CLASS);
+        addFacet(facetMap, indexAnnotation);
+
+      } else if (method.isAnnotationPresent(INDEX_ANNOTATIONS_CLASS)) {
+        IndexAnnotations indexAnnotations = method.getAnnotation(INDEX_ANNOTATIONS_CLASS);
+        IndexAnnotation[] values = indexAnnotations.value();
+        for (IndexAnnotation indexAnnotation : values) {
+          addFacet(facetMap, indexAnnotation);
+        }
+      }
+    }
+
+    Class<?> superclass = type.getSuperclass();
+    if (!Document.class.equals(superclass)) {
+      facetMap.putAll(findFacets((Class<? extends Document>) type.getSuperclass()));
+    }
+
+    return facetMap;
+  }
+
+  private void addFacet(Map<String, FacetInfo> facets, IndexAnnotation indexAnnotation) {
+    if (indexAnnotation.isFaceted()) {
+      FacetInfo info = createFacetInfo(indexAnnotation);
+      facets.put(info.getName(), info);
+    }
+  }
+
+  private FacetInfo createFacetInfo(IndexAnnotation indexAnnotation) {
+    return new FacetInfo().setName(indexAnnotation.fieldName()).setTitle(indexAnnotation.title()).setType(indexAnnotation.facetType());
   }
 }
---
+++ test/nl/knaw/huygens/repository/search/model/ComplexAnnotatedClassNoneFaceted.java
@@ -0,0 +1,26 @@
+package nl.knaw.huygens.repository.search.model;
+
+import nl.knaw.huygens.repository.annotations.IndexAnnotation;
+import nl.knaw.huygens.repository.annotations.IndexAnnotations;
+import nl.knaw.huygens.repository.model.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class ComplexAnnotatedClassNoneFaceted extends Document {
+
+  @Override
+  @JsonIgnore
+  @IndexAnnotation(fieldName = "desc")
+  public String getDisplayName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @IndexAnnotations({ @IndexAnnotation(fieldName = "facet_t_complex1", accessors = "getDisplayName", isFaceted = false, title = "Complex1"),
+      @IndexAnnotation(fieldName = "facet_t_complex2", accessors = "getSimpleIndexField", isFaceted = false, title = "Complex2") })
+  public SimpleAnnotatedClass getSimpleAnnotatedClass() {
+
+    return null;
+  }
+
+}
