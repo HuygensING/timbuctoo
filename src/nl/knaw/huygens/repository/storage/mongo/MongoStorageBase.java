fb6c92540c3108f809a08c3e6602daef3943e24e
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-16 11:53:43 +0000

    Eliminated code duplication

---
+++ src/nl/knaw/huygens/repository/storage/mongo/MongoStorageBase.java
@@ -0,0 +1,65 @@
+package nl.knaw.huygens.repository.storage.mongo;
+
+import java.util.Set;
+
+import nl.knaw.huygens.repository.config.DocTypeRegistry;
+
+import org.mongojack.JacksonDBCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+
+/**
+ * Implementation base for Mongo storage classes.
+ */
+public class MongoStorageBase {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MongoStorageBase.class);
+
+  protected static final String COUNTER_COLLECTION_NAME = "counters";
+
+  protected final DocTypeRegistry docTypeRegistry;
+
+  protected Mongo mongo;
+  protected String dbName;
+  protected DB db;
+
+  protected JacksonDBCollection<Counter, String> counterCol;
+  protected Set<String> documentCollections;
+
+  public MongoStorageBase(DocTypeRegistry registry) {
+    docTypeRegistry = registry;
+  }
+
+  public void empty() {
+    db.cleanCursors(true);
+    mongo.dropDatabase(dbName);
+    db = mongo.getDB(dbName);
+  }
+
+  public void close() {
+    db.cleanCursors(true);
+    mongo.close();
+    LOG.info("Closed");
+  }
+
+  public DB getDB() {
+    return db;
+  }
+
+  public void resetDB(DB db) {
+    this.db = db;
+  }
+
+  // -------------------------------------------------------------------
+
+  public static class Counter {
+    @JsonProperty("_id")
+    public String id;
+    public int next;
+  }
+
+}
