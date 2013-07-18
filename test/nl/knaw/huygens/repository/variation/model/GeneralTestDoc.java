4c612b414e4f96c53cc08bf89de1ec484465a806
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-18 14:33:42 +0000

    Refactored

--- test/nl/knaw/huygens/repository/variation/model/GeneralTestDoc.java
+++ test/nl/knaw/huygens/repository/variation/model/GeneralTestDoc.java
@@ -4,4 +4,10 @@ public class GeneralTestDoc extends TestConcreteDoc {
 
   public String generalTestDocValue;
 
+  public GeneralTestDoc() {}
+
+  public GeneralTestDoc(String id) {
+    setId(id);
+  }
+
 }
