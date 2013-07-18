ed41965b0f2ae2ed22b827a2c968080d375de3ff
walter <walter@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-18 18:02:55 +0000

    Refactored

--- src/nl/knaw/huygens/repository/resources/RESTAutoResource.java
+++ src/nl/knaw/huygens/repository/resources/RESTAutoResource.java
@@ -18,6 +18,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
 import nl.knaw.huygens.repository.config.DocTypeRegistry;
@@ -33,43 +34,52 @@ import com.google.inject.Inject;
 public class RESTAutoResource {
 
   private static final String ID_PARAM = "id";
+  private static final String ID_PATH = "/{id: [a-zA-Z][a-zA-Z][a-zA-Z]\\d+}";
 
   public static final String ENTITY_PARAM = "entityType";
 
-  private final StorageManager storageManager;
   private final DocTypeRegistry docTypeRegistry;
+  private final StorageManager storageManager;
 
   @Inject
-  public RESTAutoResource(StorageManager manager, DocTypeRegistry registry) {
-    storageManager = manager;
+  public RESTAutoResource(DocTypeRegistry registry, StorageManager manager) {
     docTypeRegistry = registry;
+    storageManager = manager;
   }
+
+  // --- API -----------------------------------------------------------
 
   @GET
   @Path("/all")
   @Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_HTML })
   @JsonView(JsonViews.WebView.class)
-  public List<? extends Document> getAllDocs(@PathParam(ENTITY_PARAM) String entityType, @QueryParam("rows") @DefaultValue("200") int rows, @QueryParam("start") int start) {
+  public List<? extends Document> getAllDocs( //
+      @PathParam(ENTITY_PARAM) String entityType, //
+      @QueryParam("rows") @DefaultValue("200") int rows, //
+      @QueryParam("start") int start //
+  ) {
     Class<? extends Document> type = getDocType(entityType);
     return storageManager.getAllLimited(type, start, rows);
   }
 
   @POST
-  @Consumes(MediaType.APPLICATION_JSON)
   @Path("/all")
+  @Consumes(MediaType.APPLICATION_JSON)
   @JsonView(JsonViews.WebView.class)
   @RolesAllowed("USER")
-  public <T extends Document> Response post(@PathParam(ENTITY_PARAM) String entityType, Document input, @Context UriInfo uriInfo) throws IOException {
-
+  public <T extends Document> Response post( //
+      @PathParam(ENTITY_PARAM) String entityType, //
+      Document input, //
+      @Context UriInfo uriInfo //
+  ) throws IOException {
     @SuppressWarnings("unchecked")
     Class<T> type = (Class<T>) getDocType(entityType);
-
     Class<? extends Document> inputType = input.getClass();
 
     //TODO: find a better way to test if input type is the same as the entity type.
     //@see redmine issue #1419
     if (!type.isAssignableFrom(inputType) || !inputType.isAssignableFrom(type)) {
-      throw new WebApplicationException(Response.Status.BAD_REQUEST);
+      throw new WebApplicationException(Status.BAD_REQUEST);
     }
 
     @SuppressWarnings("unchecked")
@@ -77,28 +87,31 @@ public class RESTAutoResource {
     storageManager.addDocument(type, typedDoc);
 
     String location = uriInfo.getBaseUri().toString() + "resources/" + entityType + "/" + input.getId();
-    return Response.status(Response.Status.CREATED).header("Location", location).build();
+    return Response.status(Status.CREATED).header("Location", location).build();
   }
 
   @GET
+  @Path(ID_PATH)
   @Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_HTML })
-  @Path("/{id: [a-zA-Z][a-zA-Z][a-zA-Z]\\d+}")
   @JsonView(JsonViews.WebView.class)
-  public Document getDoc(@PathParam(ENTITY_PARAM) String entityType, @PathParam(ID_PARAM) String id) {
+  public Document getDoc( //
+      @PathParam(ENTITY_PARAM) String entityType, //
+      @PathParam(ID_PARAM) String id //
+  ) {
     Class<? extends Document> type = getDocType(entityType);
-    Document doc = storageManager.getCompleteDocument(type, id);
-    if (doc == null) {
-      throw new WebApplicationException(Response.Status.NOT_FOUND);
-    }
-    return doc;
+    return checkNotNull(storageManager.getCompleteDocument(type, id), Status.NOT_FOUND);
   }
 
   @PUT
+  @Path(ID_PATH)
   @Consumes(MediaType.APPLICATION_JSON)
-  @Path("/{id: [a-zA-Z][a-zA-Z][a-zA-Z]\\d+}")
   @JsonView(JsonViews.WebView.class)
   @RolesAllowed("USER")
-  public <T extends Document> void putDoc(@PathParam(ENTITY_PARAM) String entityType, @PathParam(ID_PARAM) String id, Document input) throws IOException {
+  public <T extends Document> void putDoc( //
+      @PathParam(ENTITY_PARAM) String entityType, //
+      @PathParam(ID_PARAM) String id, //
+      Document input //
+  ) throws IOException {
     @SuppressWarnings("unchecked")
     Class<T> type = (Class<T>) getDocType(entityType);
     Class<? extends Document> inputType = input.getClass();
@@ -106,7 +119,7 @@ public class RESTAutoResource {
     //TODO: find a better way to test if input type is the same as the entity type.
     //@see redmine issue #1419
     if (!type.isAssignableFrom(inputType) || !inputType.isAssignableFrom(type)) {
-      throw new WebApplicationException(Response.Status.BAD_REQUEST);
+      throw new WebApplicationException(Status.BAD_REQUEST);
     }
 
     try {
@@ -115,49 +128,54 @@ public class RESTAutoResource {
       storageManager.modifyDocument(type, typedDoc);
     } catch (IOException ex) {
       // only if the document version does not exist an IOException is thrown.
-      throw new WebApplicationException(Response.Status.NOT_FOUND);
+      throw new WebApplicationException(Status.NOT_FOUND);
     }
-
   }
 
   @DELETE
-  @Path("/{id: [a-zA-Z][a-zA-Z][a-zA-Z]\\d+}")
+  @Path(ID_PATH)
   @JsonView(JsonViews.WebView.class)
   @RolesAllowed("USER")
-  public <T extends Document> Response delete(@PathParam(ENTITY_PARAM) String entityType, @PathParam(ID_PARAM) String id) throws IOException {
+  public <T extends Document> Response delete( //
+      @PathParam(ENTITY_PARAM) String entityType, //
+      @PathParam(ID_PARAM) String id //
+  ) throws IOException {
     @SuppressWarnings("unchecked")
     Class<T> type = (Class<T>) getDocType(entityType);
-    T typedDoc = storageManager.getDocument(type, id);
-
-    if (typedDoc == null) {
-      throw new WebApplicationException(Response.Status.NOT_FOUND);
-    }
-
+    T typedDoc = checkNotNull(storageManager.getDocument(type, id), Status.NOT_FOUND);
     storageManager.removeDocument(type, typedDoc);
-
-    return Response.status(Response.Status.OK).build();
+    return Response.status(Status.OK).build();
   }
 
   @GET
+  @Path(ID_PATH + "/{variation: \\w+}")
   @JsonView(JsonViews.WebView.class)
-  @Path("/{id: [a-zA-Z][a-zA-Z][a-zA-Z]\\d+}/{variation: \\w+}")
   @Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_HTML })
-  public DomainDocument getDocOfVariation(@PathParam(ENTITY_PARAM) String entityType, @PathParam(ID_PARAM) String id, @PathParam("variation") String variation) {
+  public DomainDocument getDocOfVariation( //
+      @PathParam(ENTITY_PARAM) String entityType, //
+      @PathParam(ID_PARAM) String id, //
+      @PathParam("variation") String variation //
+  ) {
     @SuppressWarnings("unchecked")
     Class<? extends DomainDocument> type = (Class<? extends DomainDocument>) getDocType(entityType);
-    DomainDocument doc = storageManager.getCompleteVariation(type, id, variation);
-    if (doc == null) {
-      throw new WebApplicationException(Response.Status.NOT_FOUND);
-    }
-    return doc;
+    return checkNotNull(storageManager.getCompleteVariation(type, id, variation), Status.NOT_FOUND);
   }
 
+  // -------------------------------------------------------------------
+
   private Class<? extends Document> getDocType(String entityType) {
-    Class<? extends Document> type = docTypeRegistry.getTypeForIName(entityType);
-    if (type == null) {
-      throw new WebApplicationException(Response.Status.NOT_FOUND);
+    return checkNotNull(docTypeRegistry.getTypeForIName(entityType), Status.NOT_FOUND);
+  }
+
+  /**
+   * Checks the specified reference and throws a {@code WebApplicationException}
+   * with the specified status if the reference is {@code null}.
+   */
+  private <T> T checkNotNull(T reference, Status status) {
+    if (reference == null) {
+      throw new WebApplicationException(status);
     }
-    return type;
+    return reference;
   }
 
 }
