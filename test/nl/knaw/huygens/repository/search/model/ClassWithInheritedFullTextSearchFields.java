e82eab2b8b9270e8f03538f6b286870cccf0b43e
martijnm <martijnm@bb2f4cab-7318-0410-9de6-9ead3dd4af17>
2013-07-18 11:35:44 +0000

    Tests added for FullTextFieldFinder

---
+++ test/nl/knaw/huygens/repository/search/model/ClassWithInheritedFullTextSearchFields.java
@@ -0,0 +1,5 @@
+package nl.knaw.huygens.repository.search.model;
+
+public class ClassWithInheritedFullTextSearchFields extends SimpleClassWithOneFullTextSearchField {
+
+}
---
+++ test/nl/knaw/huygens/repository/search/model/SimpleClassWithOneFullTextSearchField.java
@@ -0,0 +1,29 @@
+package nl.knaw.huygens.repository.search.model;
+
+import nl.knaw.huygens.repository.annotations.IndexAnnotation;
+import nl.knaw.huygens.repository.model.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class SimpleClassWithOneFullTextSearchField extends Document {
+
+  private String simpleIndexField;
+
+  @Override
+  @JsonIgnore
+  @IndexAnnotation(fieldName = "desc")
+  public String getDisplayName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @IndexAnnotation(fieldName = "facet_t_simple", isFaceted = true, title = "Simple")
+  public String getSimpleIndexField() {
+    return simpleIndexField;
+  }
+
+  public void setSimpleIndexField(String simpleIndexField) {
+    this.simpleIndexField = simpleIndexField;
+  }
+
+}
