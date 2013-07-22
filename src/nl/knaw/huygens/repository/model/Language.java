0cd75f43b8bbab50b076fa65b09d4a9ce9a52539
walter <walter@walters-imac.fritz.box>
2013-07-22 15:50:51 +0200

    Use map for storing language codes

--- src/nl/knaw/huygens/repository/model/Language.java
+++ src/nl/knaw/huygens/repository/model/Language.java
@@ -1,75 +1,51 @@
 package nl.knaw.huygens.repository.model;
 
-import java.util.List;
+import java.util.Map;
 
 import nl.knaw.huygens.repository.annotations.DocumentTypeName;
 import nl.knaw.huygens.repository.annotations.IDPrefix;
 
-import com.google.common.collect.Lists;
-
-// TODO add validation
+import com.google.common.collect.Maps;
 
 /**
  * Denotes a language, catering for ISO 639-2 and 639-1 codes.
- * 
- * See: <code>http://www.loc.gov/standards/iso639-2/php/code_list.php</code>.
  */
 @IDPrefix("LAN")
 @DocumentTypeName("language")
 public class Language extends DomainDocument {
 
-  /** Codes, at least one, first is default. */
-  private List<String> codes;
+  /** Codes, e.g. "iso_639_2". */
+  private Map<String, String> codes;
   /** English name. */
-  private String nameEng;
-  /** French name. */
-  private String nameFra;
-  /** ISO code, or not. */
-  boolean iso;
+  private String name;
 
   public Language() {
-    codes = Lists.newArrayListWithCapacity(3);
+    codes = Maps.newHashMapWithExpectedSize(2);
   }
 
   @Override
   public String getDisplayName() {
-    return nameEng;
+    return getName();
   }
 
-  public List<String> getCodes() {
+  public Map<String, String> getCodes() {
     return codes;
   }
 
-  public void setCodes(List<String> codes) {
+  public void setCodes(Map<String, String> codes) {
     this.codes = codes;
   }
 
-  public void addCode(String code) {
-    codes.add(code);
+  public void addCode(String key, String value) {
+    codes.put(key, value);
   }
 
-  public String getNameEng() {
-    return nameEng;
+  public String getName() {
+    return name;
   }
 
-  public void setNameEng(String name) {
-    nameEng = name;
+  public void setName(String name) {
+    this.name = name;
   }
-
-  public String getNameFra() {
-    return nameFra;
-  }
-
-  public void setNameFra(String name) {
-    nameFra = name;
-  }
-
-  public boolean isIso() {
-    return iso;
-  }
-
-  public void setIso(boolean iso) {
-    this.iso = iso;
-  };
 
 }
