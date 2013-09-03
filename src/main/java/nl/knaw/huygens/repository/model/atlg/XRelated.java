be3152bb8207ecf64998e3a98784acc653d6c2bd
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-03 16:37:58 +0200

    tools package created and XRelated added to the model package as a quick fix.

---
+++ src/main/java/nl/knaw/huygens/repository/model/atlg/XRelated.java
@@ -0,0 +1,14 @@
+package nl.knaw.huygens.repository.model.atlg;
+
+import org.apache.commons.lang.StringUtils;
+
+// TODO Should eventually be refactored when the relations between objects are rethought.
+public class XRelated {
+  public String type;
+  public String[] ids;
+
+  @Override
+  public String toString() {
+    return String.format("  %s: %s", type, StringUtils.join(ids, " ##"));
+  }
+}
