5b0f2fcf372a18ac42a7d196b826cb9195d84b8b
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-05 10:33:44 +0200

    split repository core and rest

---
+++ repository-rest/src/test/java/nl/knaw/huygens/repository/model/projectb/ProjectBGeneralTestDoc.java
@@ -0,0 +1,9 @@
+package nl.knaw.huygens.repository.variation.model.projectb;
+
+import nl.knaw.huygens.repository.variation.model.GeneralTestDoc;
+
+public class ProjectBGeneralTestDoc extends GeneralTestDoc {
+
+  public String projectBGeneralTestDocValue;
+
+}
