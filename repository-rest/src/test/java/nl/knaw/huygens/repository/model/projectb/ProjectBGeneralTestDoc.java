2a1c1b936f5b5b663faf076aaf66b6e7479283d6
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-05 11:25:33 +0200

    add the needed models for the unittests

--- repository-rest/src/test/java/nl/knaw/huygens/repository/model/projectb/ProjectBGeneralTestDoc.java
+++
@@ -1,9 +0,0 @@
-package nl.knaw.huygens.repository.variation.model.projectb;
-
-import nl.knaw.huygens.repository.variation.model.GeneralTestDoc;
-
-public class ProjectBGeneralTestDoc extends GeneralTestDoc {
-
-  public String projectBGeneralTestDocValue;
-
-}
