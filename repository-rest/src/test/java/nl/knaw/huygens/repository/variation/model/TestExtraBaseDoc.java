73a7f933d341739d07faff66d28ac5d893ca469f
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-05 11:26:17 +0200

    add needed models for the unit tests

---
+++ repository-rest/src/test/java/nl/knaw/huygens/repository/variation/model/TestExtraBaseDoc.java
@@ -0,0 +1,19 @@
+package nl.knaw.huygens.repository.variation.model;
+
+import nl.knaw.huygens.repository.annotations.DocumentTypeName;
+import nl.knaw.huygens.repository.annotations.IndexAnnotation;
+import nl.knaw.huygens.repository.model.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@DocumentTypeName("testextrabasedoc")
+public class TestExtraBaseDoc extends Document {
+
+  @Override
+  @JsonIgnore
+  @IndexAnnotation(fieldName = "desc")
+  public String getDisplayName() {
+    return null;
+  }
+
+}
---
+++ repository-rest/src/test/java/nl/knaw/huygens/repository/variation/model/projectb/TestDoc.java
@@ -0,0 +1,9 @@
+package nl.knaw.huygens.repository.variation.model.projectb;
+
+import nl.knaw.huygens.repository.variation.model.TestInheritsFromTestBaseDoc;
+
+public class TestDoc extends TestInheritsFromTestBaseDoc {
+
+  public String blah;
+
+}
