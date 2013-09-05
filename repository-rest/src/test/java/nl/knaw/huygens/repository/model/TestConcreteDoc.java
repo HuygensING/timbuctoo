7cab76b9de91120a572ff3858b6a19486b7e9e83
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-05 10:33:44 +0200

    split repository core and rest

---
+++ repository-rest/src/test/java/nl/knaw/huygens/repository/model/TestConcreteDoc.java
@@ -0,0 +1,25 @@
+package nl.knaw.huygens.repository.variation.model;
+
+import nl.knaw.huygens.repository.annotations.IndexAnnotation;
+import nl.knaw.huygens.repository.model.DomainDocument;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class TestConcreteDoc extends DomainDocument {
+
+  public String name;
+
+  public TestConcreteDoc() {}
+
+  public TestConcreteDoc(String id) {
+    setId(id);
+  }
+
+  @Override
+  @JsonIgnore
+  @IndexAnnotation(fieldName = "desc")
+  public String getDisplayName() {
+    return null;
+  }
+
+}
