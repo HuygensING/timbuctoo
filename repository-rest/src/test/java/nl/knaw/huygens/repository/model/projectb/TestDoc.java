4ffb749e7fa16eb680914e6ab5461d6721c46806
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-05 11:25:33 +0200

    add the needed models for the unittests

--- repository-rest/src/test/java/nl/knaw/huygens/repository/model/projectb/TestDoc.java
+++
@@ -1,9 +0,0 @@
-package nl.knaw.huygens.repository.variation.model.projectb;
-
-import nl.knaw.huygens.repository.variation.model.TestInheritsFromTestBaseDoc;
-
-public class TestDoc extends TestInheritsFromTestBaseDoc {
-
-  public String blah;
-
-}
