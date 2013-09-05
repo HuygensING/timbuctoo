5fe20bb7a7675565d0ad632f471662bd527164ad
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-05 11:26:17 +0200

    add needed models for the unit tests

---
+++ repository-rest/src/test/java/nl/knaw/huygens/repository/variation/model/GeneralTestDoc.java
@@ -0,0 +1,13 @@
+package nl.knaw.huygens.repository.variation.model;
+
+public class GeneralTestDoc extends TestConcreteDoc {
+
+  public String generalTestDocValue;
+
+  public GeneralTestDoc() {}
+
+  public GeneralTestDoc(String id) {
+    setId(id);
+  }
+
+}
---
+++ repository-rest/src/test/java/nl/knaw/huygens/repository/variation/model/TestBaseDoc.java
@@ -0,0 +1,18 @@
+package nl.knaw.huygens.repository.variation.model;
+
+import nl.knaw.huygens.repository.model.Document;
+
+/**
+ * Basic POJO object used for testing
+ */
+public abstract class TestBaseDoc extends Document {
+
+  /** Simple string prop for testing */
+  public String name;
+
+  @Override
+  public String getDisplayName() {
+    return name;
+  }
+
+}
