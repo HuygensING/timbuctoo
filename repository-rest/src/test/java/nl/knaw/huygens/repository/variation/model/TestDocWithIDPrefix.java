34165474ddb848622f2623be3ef4c1a249def943
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-05 13:52:43 +0200

    clean-up of the unit tests of the REST-project.

--- repository-rest/src/test/java/nl/knaw/huygens/repository/variation/model/TestDocWithIDPrefix.java
+++
@@ -1,8 +0,0 @@
-package nl.knaw.huygens.repository.variation.model;
-
-import nl.knaw.huygens.repository.annotations.IDPrefix;
-
-@IDPrefix("TDI")
-public class TestDocWithIDPrefix extends GeneralTestDoc {
-
-}
