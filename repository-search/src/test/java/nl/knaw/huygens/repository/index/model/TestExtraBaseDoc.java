09f68141a0fcc017f551ce6289fad2cef9c55bb2
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-09 16:11:10 +0200

    re #1620: moved the index-package to the search-project

---
+++ repository-search/src/test/java/nl/knaw/huygens/repository/index/model/TestExtraBaseDoc.java
@@ -0,0 +1,19 @@
+package nl.knaw.huygens.repository.index.model;
+
+import nl.knaw.huygens.repository.annotations.DocumentTypeName;
+import nl.knaw.huygens.repository.facet.IndexAnnotation;
+import nl.knaw.huygens.repository.model.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@DocumentTypeName("testextrabasedoc")
+public class TestExtraBaseDoc extends Document {
+
+  @Override
+  @JsonIgnore
+  @IndexAnnotation(fieldName = "desc")
+  public String getDisplayName() {
+    return null;
+  }
+
+}
