da3ec60039ed049f788374c7b744e6e1759e7a41
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-09-09 17:44:10 +0200

    Made pathInUserHome member of Configuration

--- repository-core/src/main/java/nl/knaw/huygens/repository/config/Configuration.java
+++ repository-core/src/main/java/nl/knaw/huygens/repository/config/Configuration.java
@@ -95,8 +95,7 @@ public class Configuration {
     return rv;
   }
 
-  // TODO should not be static
-  public static String pathInUserHome(String path) {
+  public String pathInUserHome(String path) {
     path = Character.toString(path.charAt(0)).equals("/") ? path : "/" + path;
     return System.getProperty("user.home") + path;
   }
