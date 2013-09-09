a6dfa76446923421f16836bbccee26312c93f8eb
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-09 16:02:16 +0200

    re #1620: made DocumentTest independent of index test models.

---
+++ repository-core/src/test/java/nl/knaw/huygens/repository/model/ExplicitlyAnnotatedModelWithIndexAnnotations.java
@@ -0,0 +1,40 @@
+package nl.knaw.huygens.repository.model;
+
+import nl.knaw.huygens.repository.facet.IndexAnnotation;
+import nl.knaw.huygens.repository.facet.IndexAnnotations;
+import nl.knaw.huygens.repository.model.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ExplicitlyAnnotatedModelWithIndexAnnotations extends Document {
+
+  @Override
+  @IndexAnnotation(fieldName = "id")
+  public String getId() {
+    return "";
+  }
+
+  @Override
+  @JsonIgnore
+  @IndexAnnotation(fieldName = "desc")
+  public String getDisplayName() {
+    return null;
+  }
+
+  @IndexAnnotations(value = { @IndexAnnotation(fieldName = "test"), @IndexAnnotation(fieldName = "test2") })
+  public String getString() {
+    return "";
+  }
+
+  @Override
+  @JsonProperty("!currentVariation")
+  public String getCurrentVariation() {
+    return null;
+  }
+
+  @Override
+  @JsonProperty("!currentVariation")
+  public void setCurrentVariation(String defaultVRE) {}
+
+}
