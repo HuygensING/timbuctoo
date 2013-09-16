7a459be8643b46edee1f942e2b07ebe770bcd63d
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-09-16 16:15:34 +0200

    Fixed config.js

--- repository-dev-config-war/src/main/webapp/static/config.js
+++ repository-dev-config-war/src/main/webapp/static/config.js
@@ -1,4 +1,4 @@
 var config = {
-		serverUri: "localhost:8080/repository",
-		securityUri: "demo17.huygens.knaw.nl"
-}
+  serverUri: "localhost:8080/repository",
+  securityUri: "demo17.huygens.knaw.nl"
+};
--- repository-prod-config-war/src/main/webapp/static/config.js
+++ repository-prod-config-war/src/main/webapp/static/config.js
@@ -1,4 +1,4 @@
 var config = {
-		serverUri: "repository.huygens.knaw.nl",
-		securityUri: "demo17.huygens.knaw.nl"
-}
+  serverUri: "repository.huygens.knaw.nl",
+  securityUri: "demo17.huygens.knaw.nl"
+};
