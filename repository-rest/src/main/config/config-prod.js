eccf82b6befeb8c737c966b0c9ce53ab0f611462
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-16 10:07:24 +0200

    Changed the way, the project gets the right config files. Now the the REST-project war is merged with a configuration war.

---
+++ repository-rest/src/main/config/config-prod.js
@@ -0,0 +1,4 @@
+var config = {
+		serverUri: "repository.huygens.knaw.nl",
+		securityUri: "demo17.huygens.knaw.nl"
+}
---
+++ repository-rest/src/main/config/config-test.js
@@ -0,0 +1,4 @@
+var config = {
+		serverUri: "demo17.huygens.knaw.nl/repository",
+		securityUri: "demo17.huygens.knaw.nl"
+}
