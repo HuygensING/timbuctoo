023be922a19d8e2970c3f92f53c8b38c84731431
walter <walter.ravenek@huygens.knaw.nl>
2013-09-17 14:25:05 +0200

    Added utility class

---
+++ repository-core/src/test/java/nl/knaw/huygens/repository/util/TimeUtilsTest.java
@@ -0,0 +1,25 @@
+package nl.knaw.huygens.repository.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TimeUtilsTest {
+
+  @Test
+  public void testInvalidConversionToMillis() {
+    Assert.assertEquals(-1L, TimeUtils.hhmmssToMillis("0:00:00:00")); // length
+    Assert.assertEquals(-1L, TimeUtils.hhmmssToMillis("0:00:00")); // length
+    Assert.assertEquals(-1L, TimeUtils.hhmmssToMillis("00-00-00")); // separator
+    Assert.assertEquals(-1L, TimeUtils.hhmmssToMillis("00:00:ss")); // digits
+  }
+
+  @Test
+  public void testValidConversionToMillis() {
+    Assert.assertEquals(0L, TimeUtils.hhmmssToMillis("00:00:00"));
+    Assert.assertEquals(5 * 1000L, TimeUtils.hhmmssToMillis("00:00:05"));
+    Assert.assertEquals(5 * 60 * 1000L, TimeUtils.hhmmssToMillis("00:05:00"));
+    Assert.assertEquals(5 * 60 * 60 * 1000L, TimeUtils.hhmmssToMillis("05:00:00"));
+    Assert.assertEquals(3661000L, TimeUtils.hhmmssToMillis("01:01:01"));
+  }
+
+}
