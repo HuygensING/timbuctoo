ec4ab2523eb859e8514e6512d45dceca12b5756b
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-23 12:12:42 +0200

    re #1683: Made the IndexManager less dependend of ActiveMQ.

--- repository-core/src/main/java/nl/knaw/huygens/repository/messages/Broker.java
+++ repository-core/src/main/java/nl/knaw/huygens/repository/messages/Broker.java
@@ -2,119 +2,28 @@ package nl.knaw.huygens.repository.messages;
 
 import javax.jms.JMSException;
 
-import nl.knaw.huygens.repository.config.Configuration;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.broker.BrokerService;
-import org.apache.activemq.usage.MemoryUsage;
-import org.apache.activemq.usage.StoreUsage;
-import org.apache.activemq.usage.SystemUsage;
-import org.apache.activemq.usage.TempUsage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
-
-/**
- * Encapsulates an embedded ActiveMQ message broker.
- */
-@Singleton
-public class Broker {
-
-  private static final Logger LOG = LoggerFactory.getLogger(Broker.class);
+public interface Broker {
 
   public static final String BROKER_NAME = "repo-broker";
   public static final String INDEX_QUEUE = "index";
-
   // Message headers
   public static final String PROP_ACTION = "action";
   public static final String PROP_DOC_TYPE = "type";
   public static final String PROP_DOC_ID = "id";
-
   // Index actions
   public static final String INDEX_ADD = "add"; // add item
   public static final String INDEX_DEL = "del"; // delete item
   public static final String INDEX_MOD = "mod"; // update item
   public static final String INDEX_END = "end"; // stop processing
 
-  private final String url;
+  public abstract Producer newProducer(String queue, String name) throws JMSException;
 
-  private BrokerService brokerService;
+  public abstract Consumer newConsumer(String queue, String name) throws JMSException;
 
-  @Inject
-  public Broker(Configuration config) {
-    url = "vm://" + BROKER_NAME;
-    LOG.info("Message broker URL: '{}'", url);
-    createBrokerService(config);
-  }
+  public abstract Browser newBrowser(String queue) throws JMSException;
 
-  public Producer newProducer(String queue, String name) throws JMSException {
-    ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(url);
-    return new Producer(factory, queue, name);
-  }
+  public abstract void start() throws JMSException;
 
-  public Consumer newConsumer(String queue, String name) throws JMSException {
-    ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(url);
-    return new Consumer(factory, queue, name);
-  }
-
-  public Browser newBrowser(String queue) throws JMSException {
-    ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(url);
-    return new Browser(factory, queue);
-  }
-
-  private static final String KEY_PERSISTENT = "messages.persistent";
-  private static final String KEY_MEMORY = "messages.system_usage.memory_mb";
-  private static final String KEY_STORE = "messages.system_usage.store_mb";
-  private static final String KEY_TEMP = "messages.system_usage.temp_mb";
-  private static long MEGA_BYTE = 1024 * 1024;
-
-  /**
-   * Creates an embeded message broker
-   */
-  private void createBrokerService(Configuration config) {
-    brokerService = new BrokerService();
-    brokerService.setBrokerName(BROKER_NAME);
-    brokerService.setPersistent(config.getBooleanSetting(KEY_PERSISTENT, false));
-
-    SystemUsage systemManager = new SystemUsage();
-    MemoryUsage memoryUsage = new MemoryUsage();
-    memoryUsage.setLimit(config.getIntSetting(KEY_MEMORY, 10) * MEGA_BYTE);
-    systemManager.setMemoryUsage(memoryUsage);
-
-    StoreUsage storeUsage = new StoreUsage();
-    storeUsage.setLimit(config.getIntSetting(KEY_STORE, 100) * MEGA_BYTE);
-    systemManager.setStoreUsage(storeUsage);
-
-    TempUsage tempUsage = new TempUsage();
-    tempUsage.setLimit(config.getIntSetting(KEY_TEMP, 20) * MEGA_BYTE);
-    systemManager.setTempUsage(tempUsage);
-
-    brokerService.setSystemUsage(systemManager);
-  }
-
-  public void start() throws JMSException {
-    try {
-      brokerService.start();
-    } catch (Exception e) {
-      LOG.error("Failed to start broker service", e);
-      throw new JMSException(e.getMessage());
-    }
-  }
-
-  public void close() {
-    if (brokerService != null) {
-      LOG.info("Closing");
-      try {
-        brokerService.stop();
-      } catch (Exception e) {
-        LOG.error("Failed to close broker service", e);
-        // Allow other services to close, if possible
-      } finally {
-        brokerService = null;
-      }
-    }
-  }
+  public abstract void close();
 
 }
--- repository-core/src/main/java/nl/knaw/huygens/repository/messages/Producer.java
+++ repository-core/src/main/java/nl/knaw/huygens/repository/messages/Producer.java
@@ -1,58 +1,13 @@
 package nl.knaw.huygens.repository.messages;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.DeliveryMode;
-import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public interface Producer {
 
-public class Producer {
+  public abstract void send(String action, String type, String id) throws JMSException;
 
-  private static final Logger LOG = LoggerFactory.getLogger(Producer.class);
+  public abstract void close() throws JMSException;
 
-  private final String name;
-  private Connection connection;
-  private Session session;
-  private MessageProducer producer;
-
-  public Producer(ConnectionFactory factory, String queue, String name) throws JMSException {
-    this.name = name;
-    LOG.info("Creating '{}'", name);
-    connection = factory.createConnection();
-    connection.start();
-    session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-    Destination destination = session.createQueue(queue);
-    producer = session.createProducer(destination);
-    producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-    LOG.info("Created '{}'", name);
-  }
-
-  public void send(String action, String type, String id) throws JMSException {
-    Message message = session.createMessage();
-    message.setStringProperty(Broker.PROP_ACTION, action);
-    message.setStringProperty(Broker.PROP_DOC_TYPE, type);
-    message.setStringProperty(Broker.PROP_DOC_ID, id);
-    producer.send(message);
-  }
-
-  public void close() throws JMSException {
-    LOG.info("Closing '{}'", name);
-    session.close();
-    connection.close();
-  }
-
-  public void closeQuietly() {
-    try {
-      close();
-    } catch (JMSException e) {
-      LOG.error("Error while closing", e);
-    }
-  }
+  public abstract void closeQuietly();
 
 }
