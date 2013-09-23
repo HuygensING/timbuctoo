2efc5f80ff308175f16dae0e834e11fb4d371d24
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-09-23 15:08:43 +0200

    re #1683: A specific tools implementation made to do the indexing without ActiveMQ. This did not solve the problem with the application not stopping. That is solved by the added System.exit at the end of the main thread of AtlantischeGidsImporter

--- repository-tools/src/main/java/nl/knaw/huygens/repository/tools/config/ToolsInjectionModule.java
+++ repository-tools/src/main/java/nl/knaw/huygens/repository/tools/config/ToolsInjectionModule.java
@@ -2,10 +2,10 @@ package nl.knaw.huygens.repository.tools.config;
 
 import nl.knaw.huygens.repository.config.BasicInjectionModule;
 import nl.knaw.huygens.repository.config.Configuration;
-import nl.knaw.huygens.repository.messages.ActiveMQBroker;
 import nl.knaw.huygens.repository.messages.Broker;
+import nl.knaw.huygens.repository.tools.messages.ToolsBroker;
 
-public class ToolsInjectionModule extends BasicInjectionModule{
+public class ToolsInjectionModule extends BasicInjectionModule {
 
   public ToolsInjectionModule(Configuration config) {
     super(config);
@@ -13,7 +13,7 @@ public class ToolsInjectionModule extends BasicInjectionModule{
 
   @Override
   protected void configure() {
-    bind(Broker.class).to(ActiveMQBroker.class);
+    bind(Broker.class).to(ToolsBroker.class);
     super.configure();
   }
 }
