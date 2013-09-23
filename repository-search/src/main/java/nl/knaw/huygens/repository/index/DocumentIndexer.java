98eec5141082b22a9682c1790c4fca7b882eaccc
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-09-23 14:26:13 +0200

    Moved responsibility of handling variations to DomainDocumentIndexer

--- repository-search/src/main/java/nl/knaw/huygens/repository/index/DocumentIndexer.java
+++ repository-search/src/main/java/nl/knaw/huygens/repository/index/DocumentIndexer.java
@@ -1,15 +1,13 @@
 package nl.knaw.huygens.repository.index;
 
-import java.util.List;
-
 import nl.knaw.huygens.repository.model.Document;
 
 // T must be a base type
 public interface DocumentIndexer<T extends Document> {
 
-  <U extends T> void add(List<U> variations) throws IndexException;
+  void add(Class<T> type, String id) throws IndexException;
 
-  <U extends T> void modify(List<U> variations) throws IndexException;
+  void modify(Class<T> type, String id) throws IndexException;
 
   void remove(String id) throws IndexException;
 
