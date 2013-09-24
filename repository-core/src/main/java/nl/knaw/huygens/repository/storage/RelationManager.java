a5b731b7d630fb0794c7b245e9d7085ee34e2a2b
walter <walter.ravenek@huygens.knaw.nl>
2013-09-24 16:23:27 +0200

    Added RelationManager

---
+++ repository-core/src/main/java/nl/knaw/huygens/repository/storage/RelationManager.java
@@ -0,0 +1,92 @@
+package nl.knaw.huygens.repository.storage;
+
+import nl.knaw.huygens.repository.config.DocTypeRegistry;
+import nl.knaw.huygens.repository.model.Document;
+import nl.knaw.huygens.repository.model.Reference;
+import nl.knaw.huygens.repository.model.Relation;
+import nl.knaw.huygens.repository.model.RelationType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RelationManager {
+
+  private static final Logger LOG = LoggerFactory.getLogger(RelationManager.class);
+
+  private final DocTypeRegistry registry;
+  private final StorageManager storageManager;
+
+  public RelationManager(DocTypeRegistry registry, StorageManager storageManager) {
+    this.registry = registry;
+    this.storageManager = storageManager;
+  }
+
+  public RelationBuilder getBuilder() {
+    return new RelationBuilder();
+  }
+
+  public class RelationBuilder {
+    private Relation relation;
+
+    public RelationBuilder() {
+      relation = new Relation();
+    }
+
+    public RelationBuilder type(Reference ref) {
+      relation.setTypeRef(ref);
+      return this;
+    }
+
+    public RelationBuilder source(Reference ref) {
+      relation.setSourceRef(ref);
+      return this;
+    }
+
+    public RelationBuilder source(Class<? extends Document> typeToken, String id) {
+      return source(new Reference(typeToken, id));
+    }
+
+    public RelationBuilder target(Reference ref) {
+      relation.setTargetRef(ref);
+      return this;
+    }
+
+    public RelationBuilder target(Class<? extends Document> typeToken, String id) {
+      return target(new Reference(typeToken, id));
+    }
+
+    public Relation build() {
+      if (relation.getTypeRef() == null) {
+        LOG.error("Missing relation type ref");
+        return null;
+      }
+      if (relation.getSourceRef() == null) {
+        LOG.error("Missing source ref");
+        return null;
+      }
+      if (relation.getTargetRef() == null) {
+        LOG.error("Missing target ref");
+        return null;
+      }
+      RelationType relationType = storageManager.getDocument(RelationType.class, relation.getTypeRef().getId());
+      if (relationType == null) {
+        LOG.error("Unknown relation type {}", relation.getTypeRef().getId());
+        return null;
+      }
+      String iname = relation.getSourceRef().getType();
+      Class<? extends Document> actualType = registry.getTypeForIName(iname);
+      if (!relationType.getSourceDocType().isAssignableFrom(actualType)) {
+        LOG.error("Incompatible source type {}", iname);
+        return null;
+      }
+      iname = relation.getTargetRef().getType();
+      actualType = registry.getTypeForIName(iname);
+      if (!relationType.getTargetDocType().isAssignableFrom(actualType)) {
+        LOG.error("Incompatible target type {}", iname);
+        return null;
+      }
+      return relation;
+    }
+  }
+
+}
