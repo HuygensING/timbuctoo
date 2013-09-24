48042d223b2d13a60d00bfd93df5bd4ffa4e4253
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-09-25 01:38:04 +0200

    Use RelationManager for implied relations

--- repository-core/src/main/java/nl/knaw/huygens/repository/storage/RelationManager.java
+++ repository-core/src/main/java/nl/knaw/huygens/repository/storage/RelationManager.java
@@ -1,5 +1,7 @@
 package nl.knaw.huygens.repository.storage;
 
+import java.util.List;
+
 import nl.knaw.huygens.repository.config.DocTypeRegistry;
 import nl.knaw.huygens.repository.model.Document;
 import nl.knaw.huygens.repository.model.Reference;
@@ -9,6 +11,12 @@ import nl.knaw.huygens.repository.model.RelationType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
 public class RelationManager {
 
   private static final Logger LOG = LoggerFactory.getLogger(RelationManager.class);
@@ -16,9 +24,39 @@ public class RelationManager {
   private final DocTypeRegistry registry;
   private final StorageManager storageManager;
 
+  @Inject
   public RelationManager(DocTypeRegistry registry, StorageManager storageManager) {
     this.registry = registry;
     this.storageManager = storageManager;
+  }
+
+  /**
+   * Returns the relation type with the specified id,
+   * or null if it does not exist.
+   */
+  public RelationType getRelationType(String id) {
+    return storageManager.getDocument(RelationType.class, id);
+  }
+
+  /**
+   * Returns the relation type with the specified reference,
+   * or null if it does not exist.
+   */
+  public RelationType getRelationType(Reference reference) {
+    Preconditions.checkArgument(reference.getType().equals("relationtype"), "got type %s", reference.getType());
+    return getRelationType(reference.getId());
+  }
+
+  public List<RelationType> getSynonyms(RelationType type) {
+    return Lists.newArrayList(type);
+  }
+
+  public List<RelationType> getInverses(RelationType type) {
+    if (type.isSymmetric()) {
+      return Lists.newArrayList(type);
+    } else {
+      return Lists.newArrayList();
+    }
   }
 
   public RelationBuilder getBuilder() {
