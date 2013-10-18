c115839c528a20d17150bc4cfdbd703cc135b59c
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-10-18 11:06:47 +0200

    Added some utility methods

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/config/TypeRegistryTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/config/TypeRegistryTest.java
@@ -1,8 +1,12 @@
 package nl.knaw.huygens.timbuctoo.config;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.model.Entity;
+import nl.knaw.huygens.timbuctoo.model.SystemEntity;
 import nl.knaw.huygens.timbuctoo.variation.model.GeneralTestDoc;
 import nl.knaw.huygens.timbuctoo.variation.model.TestBaseDoc;
 import nl.knaw.huygens.timbuctoo.variation.model.TestConcreteDoc;
@@ -12,7 +16,7 @@ import nl.knaw.huygens.timbuctoo.variation.model.TestInheritsFromTestBaseDoc;
 import org.junit.Test;
 
 /**
- * Tests for the DocTypeRegistry. Watch-out the register is highly
+ * Tests for the TypeRegistry. Watch-out the register is highly
  * dependent on the getCollectionName method. When that
  * implementation changes, a lot of tests will fail.
  * 
@@ -94,6 +98,85 @@ public class TypeRegistryTest {
   public void testRegisterPackageAbstractClass() {
     TypeRegistry registry = new TypeRegistry(MODEL_PACKAGE);
     assertNull(registry.getTypeForIName("testbasedoc"));
+  }
+
+  // --- tests of static utilities -------------------------------------
+
+  @Test
+  public void testIsEntity() {
+    assertFalse(TypeRegistry.isEntity(NotAnEntity.class));
+    assertTrue(TypeRegistry.isEntity(AnEntity.class));
+    assertTrue(TypeRegistry.isEntity(ASystemEntity.class));
+    assertTrue(TypeRegistry.isEntity(ADomainEntity.class));
+  }
+
+  @Test
+  public void testIsSystemEntity() {
+    assertFalse(TypeRegistry.isSystemEntity(NotAnEntity.class));
+    assertFalse(TypeRegistry.isSystemEntity(AnEntity.class));
+    assertTrue(TypeRegistry.isSystemEntity(ASystemEntity.class));
+    assertFalse(TypeRegistry.isSystemEntity(ADomainEntity.class));
+  }
+
+  @Test
+  public void testIsDomainEntity() {
+    assertFalse(TypeRegistry.isDomainEntity(NotAnEntity.class));
+    assertFalse(TypeRegistry.isDomainEntity(AnEntity.class));
+    assertFalse(TypeRegistry.isDomainEntity(ASystemEntity.class));
+    assertTrue(TypeRegistry.isDomainEntity(ADomainEntity.class));
+  }
+
+  @Test(expected = ClassCastException.class)
+  public void testToEntityFails() {
+    TypeRegistry.toEntity(NotAnEntity.class);
+  }
+
+  @Test
+  public void testToEntitySucceeds() {
+    assertTrue(TypeRegistry.isEntity(TypeRegistry.toEntity(ASystemEntity.class)));
+  }
+
+  @Test(expected = ClassCastException.class)
+  public void testToSystemEntityFails() {
+    TypeRegistry.toSystemEntity(ADomainEntity.class);
+  }
+
+  @Test
+  public void testToSystemEntitySucceeds() {
+    assertTrue(TypeRegistry.isSystemEntity(TypeRegistry.toSystemEntity(ASystemEntity.class)));
+  }
+
+  @Test(expected = ClassCastException.class)
+  public void testToDomainEntityFails() {
+    TypeRegistry.toDomainEntity(ASystemEntity.class);
+  }
+
+  @Test
+  public void testToDomainEntitySucceeds() {
+    assertTrue(TypeRegistry.isDomainEntity(TypeRegistry.toDomainEntity(ADomainEntity.class)));
+  }
+
+  private static class NotAnEntity {}
+
+  private static class AnEntity extends Entity {
+    @Override
+    public String getDisplayName() {
+      return null;
+    }
+  }
+
+  private static class ASystemEntity extends SystemEntity {
+    @Override
+    public String getDisplayName() {
+      return null;
+    }
+  }
+
+  private static class ADomainEntity extends DomainEntity {
+    @Override
+    public String getDisplayName() {
+      return null;
+    }
   }
 
 }
