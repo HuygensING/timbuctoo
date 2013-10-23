d0ed48db978b0c26b0419e891c5bcd903930bc26
BramB <bram.buitendijk@huygens.knaw.nl>
2013-10-23 12:21:33 +0200

    small change in cruise_config

--- cruise_config.rb
+++ cruise_config.rb
@@ -18,6 +18,6 @@ Project.configure do |project|
   # Ping Subversion for new revisions every 5 minutes (default: 30 seconds)
   project.scheduler.polling_interval = 5.minutes
 
-  project.source_control =  SourceControl::Git.new :repository => '/data/git/timbuctoo.git', :branch => 'develop'
+  project.source_control =  SourceControl::Git.new(:repository => '/data/git/timbuctoo.git', :branch => 'develop')
   project.do_clean_checkout :always
 end
