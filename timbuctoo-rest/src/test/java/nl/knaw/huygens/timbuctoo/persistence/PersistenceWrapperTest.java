5798bd9e2d5b57fc78baee1d6aeb17531580978c
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-10-25 12:35:55 +0200

    [#1832] Made the PersistenceService do something. Made the PersistenceWrapper use Class instead of a type string.

--- timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/persistence/PersistenceWrapperTest.java
+++ timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/persistence/PersistenceWrapperTest.java
@@ -7,42 +7,50 @@ import static org.mockito.Mockito.when;
 import nl.knaw.huygens.persistence.PersistenceException;
 import nl.knaw.huygens.persistence.PersistenceManager;
 import nl.knaw.huygens.timbuctoo.config.Paths;
+import nl.knaw.huygens.timbuctoo.config.TypeRegistry;
+import nl.knaw.huygens.timbuctoo.rest.providers.model.TestConcreteDoc;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class PersistenceWrapperTest {
 
+  private static final Class<TestConcreteDoc> DEFAULT_TYPE = TestConcreteDoc.class;
   private PersistenceManager persistenceManager;
+  private TypeRegistry typeRegistry;
 
   @Before
   public void setUp() {
     persistenceManager = mock(PersistenceManager.class);
+    typeRegistry = mock(TypeRegistry.class);
+    when(typeRegistry.getXNameForType(DEFAULT_TYPE)).thenReturn("testconcretedocs");
   }
 
   private PersistenceWrapper createInstance(String url) {
-    return new PersistenceWrapper(url, persistenceManager);
+    return new PersistenceWrapper(url, persistenceManager, typeRegistry);
   }
 
   @Test
   public void testPersistObjectSucces() throws PersistenceException {
     PersistenceWrapper persistenceWrapper = createInstance("http://test.nl");
-    persistenceWrapper.persistObject("test", "1234");
-    verify(persistenceManager).persistURL("http://test.nl/" + Paths.DOMAIN_PREFIX + "/test/1234");
+    persistenceWrapper.persistObject(DEFAULT_TYPE, "1234");
+    verify(typeRegistry).getXNameForType(DEFAULT_TYPE);
+    verify(persistenceManager).persistURL("http://test.nl/" + Paths.DOMAIN_PREFIX + "/testconcretedocs/1234");
   }
 
   @Test
   public void testPersistObjectSuccesUrlEndOnSlash() throws PersistenceException {
     PersistenceWrapper persistenceWrapper = createInstance("http://test.nl/");
-    persistenceWrapper.persistObject("test", "1234");
-    verify(persistenceManager).persistURL("http://test.nl/" + Paths.DOMAIN_PREFIX + "/test/1234");
+    persistenceWrapper.persistObject(DEFAULT_TYPE, "1234");
+    verify(typeRegistry).getXNameForType(DEFAULT_TYPE);
+    verify(persistenceManager).persistURL("http://test.nl/" + Paths.DOMAIN_PREFIX + "/testconcretedocs/1234");
   }
 
   @Test(expected = PersistenceException.class)
   public void testPersistObjectException() throws PersistenceException {
     when(persistenceManager.persistURL(anyString())).thenThrow(new PersistenceException("error"));
     PersistenceWrapper persistenceWrapper = createInstance("http://test.nl/");
-    persistenceWrapper.persistObject("test", "1234");
+    persistenceWrapper.persistObject(DEFAULT_TYPE, "1234");
   }
 
 }
