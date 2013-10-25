d4ec3f8a1599b119a757a31f7bfdc2f0d8f7b942
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-10-25 10:40:08 +0200

    [#1865] Fix DWC/RAA/CKCC import

--- timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/importer/database/BulkImporter.java
+++ timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/importer/database/BulkImporter.java
@@ -2,14 +2,13 @@ package nl.knaw.huygens.timbuctoo.tools.importer.database;
 
 import nl.knaw.huygens.timbuctoo.config.BasicInjectionModule;
 import nl.knaw.huygens.timbuctoo.config.Configuration;
-import nl.knaw.huygens.timbuctoo.config.TypeRegistry;
+import nl.knaw.huygens.timbuctoo.index.IndexException;
 import nl.knaw.huygens.timbuctoo.index.IndexManager;
-import nl.knaw.huygens.timbuctoo.index.IndexService;
-import nl.knaw.huygens.timbuctoo.messages.Broker;
+import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.model.dwcbia.DWCPlace;
 import nl.knaw.huygens.timbuctoo.model.dwcbia.DWCScientist;
 import nl.knaw.huygens.timbuctoo.model.raa.RAACivilServant;
-import nl.knaw.huygens.timbuctoo.storage.RelationManager;
+import nl.knaw.huygens.timbuctoo.storage.StorageIterator;
 import nl.knaw.huygens.timbuctoo.storage.StorageManager;
 
 import com.google.inject.Guice;
@@ -17,32 +16,17 @@ import com.google.inject.Injector;
 
 /**
  * Performs bulk import of test data, which is stored and indexed.
- * <br/>
- * There are three infrastructure components involved:<ul>
- * <li>Mongo for storage of the documents;</li>
- * <li>Solr for indexing of the documents;</li>
- * <li>ActiveMQ for communicating that documents have been stored
- * which need to be indexed.</li>
- * </ul>
- * Mongo depends on ActiveMQ, Solr depend on Mongo and ActiveMQ.
- * These dependencies imply the order in which the components must be
- * opened and closed.
  */
 public class BulkImporter {
-
-  public static boolean ATLG = false;
 
   public static void main(String[] args) throws Exception {
     Configuration config = new Configuration("config.xml");
     Injector injector = Guice.createInjector(new BasicInjectionModule(config));
 
-    Broker broker = null;
     StorageManager storageManager = null;
     IndexManager indexManager = null;
 
     try {
-      broker = injector.getInstance(Broker.class);
-      broker.start();
 
       storageManager = injector.getInstance(StorageManager.class);
       storageManager.clear();
@@ -50,38 +34,41 @@ public class BulkImporter {
       indexManager = injector.getInstance(IndexManager.class);
       indexManager.deleteAllDocuments();
 
-      IndexService service = injector.getInstance(IndexService.class);
-      Thread thread = new Thread(service);
-      thread.start();
-
       GenericImporter importer = new GenericImporter(storageManager);
 
       long start = System.currentTimeMillis();
-      if (ATLG) {
-        LanguageImporter languageImporter = new LanguageImporter(storageManager);
-        languageImporter.handleFile("testdata/iso-639-2-language-codes.txt", 5, false);
-        TypeRegistry registry = injector.getInstance(TypeRegistry.class);
-        RelationManager relationManager = new RelationManager(registry, storageManager);
-        new AtlantischeGidsImporter(registry, relationManager, storageManager, indexManager, "../AtlantischeGids/work/").importAll();
-      } else {
-        String resourceDir = "src/main/resources/";
-        importer.importData(resourceDir + "DWCPlaceMapping.properties", DWCPlace.class);
-        importer.importData(resourceDir + "DWCScientistMapping.properties", DWCScientist.class);
-        importer.importData(resourceDir + "RAACivilServantMapping.properties", RAACivilServant.class);
-        CKCCPersonImporter csvImporter = new CKCCPersonImporter(storageManager);
-        csvImporter.handleFile(resourceDir + "testdata/ckcc-persons.txt", 9, false);
-      }
 
-      // Signal we're done
-      DefaultImporter.sendEndOfDataMessage(broker);
+      System.out.println();
+      System.out.println("---------------------------------");
+      System.out.println("--  Pass 1 - basic properties  --");
+      System.out.println("---------------------------------");
+
+      String resourceDir = "src/main/resources/";
+      importer.importData(resourceDir + "DWCPlaceMapping.properties", DWCPlace.class);
+      importer.importData(resourceDir + "DWCScientistMapping.properties", DWCScientist.class);
+      importer.importData(resourceDir + "RAACivilServantMapping.properties", RAACivilServant.class);
+      CKCCPersonImporter csvImporter = new CKCCPersonImporter(storageManager);
+      csvImporter.handleFile(resourceDir + "testdata/ckcc-persons.txt", 9, false);
+
+      System.out.println();
+      System.out.println("--------------------------");
+      System.out.println("--  Pass 2 - relations  --");
+      System.out.println("--------------------------");
+
+      // Nothing to do
+
+      System.out.println();
+      System.out.println("-------------------------");
+      System.out.println("--  Pass 3 - indexing  --");
+      System.out.println("-------------------------");
+      System.out.println();
+
+      indexEntities(storageManager, indexManager, DWCPlace.class);
+      indexEntities(storageManager, indexManager, DWCScientist.class);
+      indexEntities(storageManager, indexManager, RAACivilServant.class);
 
       long time = (System.currentTimeMillis() - start) / 1000;
       System.out.printf("%n=== Used %d seconds%n%n", time);
-
-      DefaultImporter.waitForCompletion(thread, 5 * 60 * 1000);
-
-      time = (System.currentTimeMillis() - start) / 1000;
-      System.out.printf("%n=== Used %d seconds%n", time);
 
     } finally {
       // Close resources
@@ -91,8 +78,21 @@ public class BulkImporter {
       if (storageManager != null) {
         storageManager.close();
       }
-      if (broker != null) {
-        broker.close();
+    }
+  }
+
+  private static <T extends DomainEntity> void indexEntities(StorageManager storageManager, IndexManager indexManager, Class<T> type) throws IndexException {
+    System.out.println(".. " + type.getSimpleName());
+    StorageIterator<T> iterator = null;
+    try {
+      iterator = storageManager.getAll(type);
+      while (iterator.hasNext()) {
+        T entity = iterator.next();
+        indexManager.addDocument(type, entity.getId());
+      }
+    } finally {
+      if (iterator != null) {
+        iterator.close();
       }
     }
   }
