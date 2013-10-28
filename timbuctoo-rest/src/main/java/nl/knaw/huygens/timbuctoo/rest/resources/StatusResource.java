7ad7bf322002a572dd521b06affe3b854f74db74
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-10-28 17:28:01 +0100

    Added index status to status resource

--- timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/rest/resources/StatusResource.java
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/rest/resources/StatusResource.java
@@ -7,6 +7,7 @@ import javax.ws.rs.core.MediaType;
 
 import nl.knaw.huygens.timbuctoo.annotations.APIDesc;
 import nl.knaw.huygens.timbuctoo.config.Paths;
+import nl.knaw.huygens.timbuctoo.index.IndexManager;
 import nl.knaw.huygens.timbuctoo.rest.util.Status;
 import nl.knaw.huygens.timbuctoo.storage.StorageManager;
 
@@ -16,10 +17,12 @@ import com.google.inject.Inject;
 public class StatusResource {
 
   private final StorageManager storageManager;
+  private final IndexManager indexManager;
 
   @Inject
-  public StatusResource(StorageManager storageManager) {
+  public StatusResource(StorageManager storageManager, IndexManager indexManager) {
     this.storageManager = storageManager;
+    this.indexManager = indexManager;
   }
 
   @GET
@@ -28,6 +31,7 @@ public class StatusResource {
   public Status getStatus() {
     Status status = new Status();
     status.setStorageStatus(storageManager.getStatus());
+    status.setIndexStatus(indexManager.getStatus());
     return status;
   }
 
--- timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/rest/util/Status.java
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/rest/util/Status.java
@@ -1,10 +1,12 @@
 package nl.knaw.huygens.timbuctoo.rest.util;
 
+import nl.knaw.huygens.timbuctoo.index.IndexStatus;
 import nl.knaw.huygens.timbuctoo.storage.StorageStatus;
 
 public class Status {
 
   private StorageStatus storageStatus;
+  private IndexStatus indexStatus;
 
   public StorageStatus getStorageStatus() {
     return storageStatus;
@@ -12,6 +14,14 @@ public class Status {
 
   public void setStorageStatus(StorageStatus status) {
     storageStatus = status;
+  }
+
+  public IndexStatus getIndexStatus() {
+    return indexStatus;
+  }
+
+  public void setIndexStatus(IndexStatus status) {
+    indexStatus = status;
   }
 
 }
