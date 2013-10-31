3e764b50fcafcc2b1ff5891aa44805eafacd9365
walter <walter.ravenek@huygens.knaw.nl>
2013-10-31 15:54:13 +0100

    [#1877] Show counts for all indexed scopes

--- timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/IndexStatus.java
+++ timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/IndexStatus.java
@@ -1,29 +1,34 @@
 package nl.knaw.huygens.timbuctoo.index;
 
 import java.util.List;
+import java.util.Map;
 
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.util.KV;
 import nl.knaw.huygens.timbuctoo.vre.Scope;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 public class IndexStatus {
 
-  private final List<KV<Long>> domainEntityCounts;
+  private final Map<String, List<KV<Long>>> counts;
 
   public IndexStatus() {
-    domainEntityCounts = Lists.newArrayList();
+    counts = Maps.newHashMap();
   }
 
-  public List<KV<Long>> getDomainEntityCounts() {
-    return domainEntityCounts;
+  public Map<String, List<KV<Long>>> getCounts() {
+    return counts;
   }
 
   public void addDomainEntityCount(Scope scope, Class<? extends DomainEntity> type, long count) {
-    if ("full".equals(scope.getId())) {
-      domainEntityCounts.add(new KV<Long>(type.getSimpleName(), count));
+    List<KV<Long>> list = counts.get(scope.getId());
+    if (list == null) {
+      list = Lists.newArrayList();
+      counts.put(scope.getId(), list);
     }
+    list.add(new KV<Long>(type.getSimpleName(), count));
   }
 
 }
