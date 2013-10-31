07faa9cbd2ddd01523a35fe086a9f58c486cd2ab
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-10-31 13:22:38 +0100

    [#1859] moved the SystemModels for the tests to the timbuctoo.model package

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/mongo/MongoStorageTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/mongo/MongoStorageTest.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.Map;
 
 import nl.knaw.huygens.timbuctoo.config.TypeRegistry;
-import nl.knaw.huygens.timbuctoo.storage.mongo.model.TestSystemEntity;
+import nl.knaw.huygens.timbuctoo.model.TestSystemEntity;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
