28e49d78956c9a3debd6e8d3dcaf97d15b694e70
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-11-02 21:48:38 +0100

    Refactored

--- timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/rest/resources/SiteMapResourceTest.java
+++ timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/rest/resources/SiteMapResourceTest.java
@@ -7,17 +7,13 @@ import org.junit.Test;
 
 import com.google.common.collect.Lists;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 public class SiteMapResourceTest extends WebServiceTestSetup {
 
   @Test
   public void testGetSitemap() {
     setUpUserRoles(USER_ID, Lists.newArrayList("USER"));
-    WebResource resource = super.resource();
-
-    ClientResponse response = resource.path(Paths.SYSTEM_PREFIX).path("api").header("Authorization", "bearer 12333322abef").get(ClientResponse.class);
-
+    ClientResponse response = resource().path(Paths.SYSTEM_PREFIX).path("api").header("Authorization", "bearer 12333322abef").get(ClientResponse.class);
     assertEquals(ClientResponse.Status.OK, response.getClientResponseStatus());
   }
 
