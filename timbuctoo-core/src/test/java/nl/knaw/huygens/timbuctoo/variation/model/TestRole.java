34041cc315d2fdc071de86eb94594ea7fd09b590
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-11-04 14:16:05 +0100

    [#1859] Moved the recursion code from the MongoObjectMapper to the VariationInducer. This way the MongoObjectMapper can be used for mapping Role objects aswell. Changed the VariationInducer, so it can induce roles.

---
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/variation/model/TestRole.java
@@ -0,0 +1,15 @@
+package nl.knaw.huygens.timbuctoo.variation.model;
+
+import nl.knaw.huygens.timbuctoo.model.Role;
+
+public class TestRole extends Role {
+  private String roleName;
+
+  public String getRoleName() {
+    return roleName;
+  }
+
+  public void setRoleName(String roleName) {
+    this.roleName = roleName;
+  }
+}
