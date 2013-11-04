4790ed27bb23df63924cc93b3da9366b203e1189
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-11-04 15:45:22 +0100

    [#1402] Use logback for logging

--- timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/ToolBase.java
+++ timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/ToolBase.java
@@ -1,13 +1,8 @@
 package nl.knaw.huygens.timbuctoo.tools;
 
+/**
+ * Class on which all tools should be based.
+ */
 public class ToolBase {
-
-  private static final String LOG4J_CONFIG = "log4j.configuration";
-
-  static {
-    if (System.getProperty(LOG4J_CONFIG) == null) {
-      System.setProperty(LOG4J_CONFIG, "timbuctoo-log4j.properties");
-    }
-  }
 
 }
--- timbuctoo-tools/src/test/java/nl/knaw/huygens/timbuctoo/tools/importer/database/SubDocumentExtension.java
+++
@@ -1,5 +0,0 @@
-package nl.knaw.huygens.timbuctoo.tools.importer.database;
-
-public class SubDocumentExtension extends DocumentExtensionWithStringField {
-
-}
