7f55d16b92b5c89ce7ca4eb2fa440ba709deb543
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-11-15 16:20:37 +0100

    Removed more redundant code

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/mongo/MongoUtilsTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/mongo/MongoUtilsTest.java
@@ -2,19 +2,14 @@ package nl.knaw.huygens.timbuctoo.storage.mongo;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
 import nl.knaw.huygens.timbuctoo.storage.mongo.MongoDiffTest.Foo;
 
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
 
 public class MongoUtilsTest {
 
@@ -37,15 +32,6 @@ public class MongoUtilsTest {
     } catch (IOException e) {
       fail();
     }
-  }
-
-  @Test
-  public void testGetCollection() {
-    DB db = Mockito.mock(DB.class);
-    DBCollection col = Mockito.mock(DBCollection.class);
-    when(db.getCollection("foo")).thenReturn(col);
-    MongoUtils.getCollection(db, Foo.class);
-    verify(db).getCollection("foo");
   }
 
 }
