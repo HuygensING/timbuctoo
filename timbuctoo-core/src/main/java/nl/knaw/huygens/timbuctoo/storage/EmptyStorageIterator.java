f64c2058f79596231bc191d5043f2380aff9405b
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-11-16 11:38:52 +0100

    Added EmptyStorageIterator

---
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/EmptyStorageIterator.java
@@ -0,0 +1,42 @@
+package nl.knaw.huygens.timbuctoo.storage;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * An iterator over an empty collection stored entities.
+ */
+public class EmptyStorageIterator<T> implements StorageIterator<T> {
+
+  @Override
+  public boolean hasNext() {
+    return false;
+  }
+
+  @Override
+  public T next() {
+    throw new IllegalStateException();
+  }
+
+  @Override
+  public void remove() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void close() {}
+
+  @Override
+  public int size() {
+    return 0;
+  }
+
+  @Override
+  public void skip(int count) {}
+
+  @Override
+  public List<T> getSome(int limit) {
+    return Collections.emptyList();
+  }
+
+}
