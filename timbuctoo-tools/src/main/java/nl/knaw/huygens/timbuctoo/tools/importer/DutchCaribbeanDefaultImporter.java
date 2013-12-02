caa4de75823f6d50dd443b29a6f828e737d9b308
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2013-12-02 14:10:35 +0100

    Removed unused code

--- timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/importer/DutchCaribbeanDefaultImporter.java
+++ timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/importer/DutchCaribbeanDefaultImporter.java
@@ -7,7 +7,6 @@ import nl.knaw.huygens.timbuctoo.index.IndexException;
 import nl.knaw.huygens.timbuctoo.index.IndexManager;
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.model.Entity;
-import nl.knaw.huygens.timbuctoo.model.EntityRef;
 import nl.knaw.huygens.timbuctoo.storage.RelationManager;
 import nl.knaw.huygens.timbuctoo.storage.StorageIterator;
 import nl.knaw.huygens.timbuctoo.storage.StorageManager;
@@ -113,18 +112,6 @@ public abstract class DutchCaribbeanDefaultImporter extends DefaultImporter {
     System.out.println();
     System.out.println(storageManager.getStatus());
     System.out.println(indexManager.getStatus());
-  }
-
-  protected <T extends Entity> EntityRef newEntityRef(Class<T> type, T entity) {
-    String itype = typeRegistry.getINameForType(type);
-    String xtype = typeRegistry.getXNameForType(type);
-    return new EntityRef(itype, xtype, entity.getId(), entity.getDisplayName());
-  }
-
-  protected <T extends Entity> EntityRef newEntityRef(Class<T> type, String id) {
-    String itype = typeRegistry.getINameForType(type);
-    String xtype = typeRegistry.getXNameForType(type);
-    return new EntityRef(itype, xtype, id, null);
   }
 
 }
