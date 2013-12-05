8594d07d3d1c2f8857b9cb36912d55aacfeb769a
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-12-05 13:42:28 +0100

    [#1846] Comments added

--- timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/rest/util/CustomHeaders.java
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/rest/util/CustomHeaders.java
@@ -1,5 +1,9 @@
 package nl.knaw.huygens.timbuctoo.rest.util;
 
+/**
+ * An helper interface which contains all the custom headers used in timbuctoo.
+ *
+ */
 public interface CustomHeaders {
   String VRE_ID_KEY = "VRE_ID";
 }
