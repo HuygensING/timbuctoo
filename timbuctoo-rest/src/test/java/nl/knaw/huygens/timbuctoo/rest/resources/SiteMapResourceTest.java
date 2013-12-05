d181351af154de205e9e43d18d5936b31e8df390
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-12-05 15:59:56 +0100

    [#1846] VREAuthorizationResourceFilter created to block users from editing data that is not their scope. It now only works with DomainEntities, SystemEntities are still to come.

--- timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/rest/resources/SiteMapResourceTest.java
+++ timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/rest/resources/SiteMapResourceTest.java
@@ -1,5 +1,6 @@
 package nl.knaw.huygens.timbuctoo.rest.resources;
 
+import static nl.knaw.huygens.timbuctoo.security.UserRoles.USER_ROLE;
 import static org.junit.Assert.assertEquals;
 import nl.knaw.huygens.timbuctoo.config.Paths;
 
@@ -12,7 +13,7 @@ public class SiteMapResourceTest extends WebServiceTestSetup {
 
   @Test
   public void testGetSitemap() {
-    setUpUserWithRoles(USER_ID, Lists.newArrayList("USER"));
+    setUpUserWithRoles(USER_ID, Lists.newArrayList(USER_ROLE));
     ClientResponse response = resource().path(Paths.SYSTEM_PREFIX).path("api").header("Authorization", "bearer 12333322abef").get(ClientResponse.class);
     assertEquals(ClientResponse.Status.OK, response.getClientResponseStatus());
   }
