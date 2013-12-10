1af1e99c45017736e5b0f1007a0600d13b12b482
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-12-10 10:12:13 +0100

    [#1979] The PrototypeCreator now fills in most of the 'simple' fields

--- timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/util/PrototypeCreator.java
+++ timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/util/PrototypeCreator.java
@@ -1,6 +1,7 @@
 package nl.knaw.huygens.timbuctoo.tools.util;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -8,13 +9,16 @@ import java.util.Collection;
 import java.util.Map;
 
 import nl.knaw.huygens.timbuctoo.config.TypeRegistry;
-import nl.knaw.huygens.timbuctoo.model.DomainEntity;
+import nl.knaw.huygens.timbuctoo.model.util.Datable;
+import nl.knaw.huygens.timbuctoo.model.util.PersonName;
+import nl.knaw.huygens.timbuctoo.model.util.PersonNameComponent.Type;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.common.reflect.ClassPath;
 import com.google.common.reflect.ClassPath.ClassInfo;
 
@@ -33,12 +37,17 @@ public class PrototypeCreator {
       Class<?> type = Class.forName(info.getName());
       if (TypeRegistry.isDomainEntity(type) && !Modifier.isAbstract(type.getModifiers())) {
         LOG.info("DomainEntity found {}", info.getName());
-        createEntity(TypeRegistry.toDomainEntity(type));
+
+        try {
+          LOG.info("instance: \n{}", new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT).writeValueAsString(createInstance(TypeRegistry.toDomainEntity(type))));
+        } catch (JsonProcessingException e) {
+          e.printStackTrace();
+        }
       }
     }
   }
 
-  private <T extends DomainEntity> void createEntity(Class<T> type) throws InstantiationException, IllegalAccessException {
+  private <T> T createInstance(Class<T> type) throws InstantiationException, IllegalAccessException {
     T instance = type.newInstance();
 
     for (Method method : type.getMethods()) {
@@ -46,50 +55,74 @@ public class PrototypeCreator {
         setValue(method, instance);
       } catch (IllegalArgumentException e) {
         LOG.error("Illegal argument for {}.", method.getName());
-        // TODO Auto-generated catch block
-        //e.printStackTrace();
       } catch (InvocationTargetException e) {
         LOG.error("invocation of {} went wrong", method.getName());
-        // e.printStackTrace();
       }
     }
 
-    try {
-      LOG.info("instance: {}", new ObjectMapper().writeValueAsString(instance));
-    } catch (JsonProcessingException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+    for (Field field : type.getFields()) {
+      try {
+        setValue(field, instance);
+      } catch (IllegalArgumentException e) {
+        LOG.error("Illegal argument for {}.", field.getName());
+      }
+    }
+
+    return instance;
+
+  }
+
+  private <T> void setValue(Field field, T instance) throws IllegalArgumentException, IllegalAccessException {
+    if (!Modifier.isFinal(field.getModifiers())) {
+      field.setAccessible(true);
+      field.set(instance, generateValue(field.getType(), field.getName()));
     }
   }
 
-  private <T extends DomainEntity> void setValue(Method method, T instance) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+  private <T> void setValue(Method method, T instance) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
     Class<?>[] paramTypes = method.getParameterTypes();
     if (paramTypes.length == 1 && !Collection.class.isAssignableFrom(paramTypes[0]) && !Map.class.isAssignableFrom(paramTypes[0]) && method.getName().startsWith("set")) {
       method.invoke(instance, generateValue(paramTypes[0], method.getName()));
     }
   }
 
-  private Object generateValue(Class<?> paramType, String methodName) {
-    //    LOG.info("method name: {}", methodName);
-    //    LOG.info("type: {}", paramType);
-
-    if (Boolean.class.isAssignableFrom(paramType) || boolean.class.isAssignableFrom(paramType)) {
-      //LOG.info("returning boolean");
+  private Object generateValue(Class<?> type, String name) {
+    if (Boolean.class.isAssignableFrom(type) || boolean.class.isAssignableFrom(type)) {
       return false;
-    } else if (Integer.class.isAssignableFrom(paramType) || int.class.isAssignableFrom(paramType)) {
-      //LOG.info("returning int");
+    } else if (Integer.class.isAssignableFrom(type) || int.class.isAssignableFrom(type)) {
       return 42;
-    } else if (Long.class.isAssignableFrom(paramType) || long.class.isAssignableFrom(paramType)) {
+    } else if (Long.class.isAssignableFrom(type) || long.class.isAssignableFrom(type)) {
       return 42l;
-    } else if (Double.class.isAssignableFrom(paramType) || double.class.isAssignableFrom(paramType)) {
-      // LOG.info("returning double");
+    } else if (Double.class.isAssignableFrom(type) || double.class.isAssignableFrom(type)) {
       return Math.random() * 100;
-    } else if (String.class.isAssignableFrom(paramType)) {
-      //      LOG.info("returning String");
-      return methodName;
+    } else if (String.class.isAssignableFrom(type)) {
+      return name;
+    } else if (Class.class.isAssignableFrom(type)) {
+      return type;
+    } else if (Datable.class.isAssignableFrom(type)) {
+      return new Datable("20130411");
+    } else if (PersonName.class.isAssignableFrom(type)) {
+      return createName();
     }
 
-    //    LOG.warn("returning null");
+    try {
+      return createInstance(type);
+    } catch (InstantiationException e) {
+      LOG.error("instantian exception for type {}", type.getSimpleName());
+    } catch (IllegalAccessException e) {
+      LOG.error("illegal access exception for type {}", type.getSimpleName());
+    }
+
+    LOG.warn("returning null for {} of type {}", name, type.getSimpleName());
     return null;
+
+  }
+
+  private PersonName createName() {
+    PersonName personName = new PersonName();
+    personName.addNameComponent(Type.FORENAME, "forename");
+    personName.addNameComponent(Type.SURNAME, "surname");
+
+    return personName;
   }
 }
