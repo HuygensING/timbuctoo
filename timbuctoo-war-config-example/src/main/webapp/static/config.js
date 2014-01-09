00432505acd64a6585df938daaf49b112d967181
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-01-09 11:41:34 +0100

    [#2058] example config added.

---
+++ timbuctoo-war-config-example/src/main/webapp/static/config.js
@@ -0,0 +1,11 @@
+var config = {
+	/*
+	 * serverUri should be pointing to the public url of the application, when a
+	 * proxy is used this should point to the proxy address.
+	 */
+	serverUri : "localhost:8080/timbuctoo",
+	/*
+	 * securtityUri should point to url where the user can login. 
+	 */
+	securityUri : "{url to security server used for user identification}"
+};
