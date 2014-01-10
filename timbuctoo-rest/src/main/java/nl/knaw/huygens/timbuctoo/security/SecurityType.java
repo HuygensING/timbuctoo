fe96a8369ba49a49fa5a75a64770f40c3319cd1a
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-01-10 17:11:40 +0100

    [#2058] Updated licenses and file headers.

--- timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/security/SecurityType.java
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/security/SecurityType.java
@@ -2,23 +2,24 @@ package nl.knaw.huygens.timbuctoo.security;
 
 /*
 * #%L
-* =======
-* Copyright (C) 2012 - 2014 Huygens ING
-* =======
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public
-* License along with this program. If not, see
-* <http://www.gnu.org/licenses/gpl-3.0.html>.
-* #L%
+ * Timbuctoo REST api
+ * =======
+ * Copyright (C) 2012 - 2014 Huygens ING
+ * =======
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
 */
 
 /**
