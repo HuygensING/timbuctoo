8f3b2c09c35eee7e3f8b51d19e2a95392047669c
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-03-11 11:25:49 +0100

    [#1786] ScopeManager implemented.

--- timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/ScopeManager.java
+++ timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/ScopeManager.java
@@ -1,20 +1,49 @@
 package nl.knaw.huygens.timbuctoo.index;
 
 import java.util.List;
+import java.util.Map;
 
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.vre.Scope;
 
 public class ScopeManager {
 
+  private final List<Scope> scopes;
+  private Map<String, Index> indexes;
+  private final IndexNameCreator indexNameCreator;
+
+  protected ScopeManager(List<Scope> scopes, IndexNameCreator indexNameCreator) {
+    this.scopes = scopes;
+    this.indexNameCreator = indexNameCreator;
+    indexes = createIndexes();
+  }
+
   public List<Scope> getAllScopes() {
-    // TODO Auto-generated method stub
-    return null;
+    return scopes;
   }
 
   public Index getIndexFor(Scope scope, Class<? extends DomainEntity> type) {
-    // TODO Auto-generated method stub
+    String indexName = indexNameCreator.getIndexNameFor(scope, type);
+
+    Index index = indexes.get(indexName);
+    if (index == null) {
+      index = new NoOpIndex();
+    }
+
+    return index;
+  }
+
+  protected Map<String, Index> createIndexes() {
     return null;
+  }
+
+  protected static class NoOpIndex implements Index {
+
+    @Override
+    public void add(Class<? extends DomainEntity> type, String id) {
+
+    }
+
   }
 
 }
