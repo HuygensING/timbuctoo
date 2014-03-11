f4b000c065e5f0fc41ecf23c15dc83ac603dcfc0
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-03-11 12:44:46 +0100

    [#1786] Implemented the IndexNameCreator

--- timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/IndexNameCreator.java
+++ timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/IndexNameCreator.java
@@ -1,14 +1,18 @@
 package nl.knaw.huygens.timbuctoo.index;
 
+import nl.knaw.huygens.timbuctoo.config.TypeRegistry;
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.vre.Scope;
 
 public class IndexNameCreator {
 
-  public String getIndexNameFor(Scope scope, Class<? extends DomainEntity> type) {
-    return null;
-    // TODO Auto-generated method stub
+  private final TypeRegistry registry;
 
+  public IndexNameCreator(TypeRegistry registry) {
+    this.registry = registry;
   }
 
+  public String getIndexNameFor(Scope scope, Class<? extends DomainEntity> type) {
+    return String.format("%s.%s", scope.getId(), registry.getINameForType(type));
+  }
 }
