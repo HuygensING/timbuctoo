c15585779d1b9cebb4c00c7763f75e4e7a5d7564
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-03-20 13:01:45 +0100

    Code clean up

--- timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/util/metadata/PoorMansEnumFieldMetaDataGenerator.java
+++ timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/util/metadata/PoorMansEnumFieldMetaDataGenerator.java
@@ -6,14 +6,24 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
-public class PoorMansEnumFieldMetaDataGenerator extends EnumValueFieldMetaDataGenerator {
+public class PoorMansEnumFieldMetaDataGenerator extends FieldMetaDataGenerator {
 
   public PoorMansEnumFieldMetaDataGenerator(TypeFacade containingType, TypeNameGenerator typeNameGenerator) {
     super(containingType);
   }
 
   @Override
+  protected Map<String, Object> constructValue(Field field) {
+    Map<String, Object> metadataMap = Maps.newHashMap();
+    metadataMap.put(TYPE_FIELD, containingType.getTypeNameOfField(field));
+
+    addValueToValueMap(field, metadataMap);
+
+    return metadataMap;
+  }
+
   protected void addValueToValueMap(Field field, Map<String, Object> metadataMap) {
     List<Object> values = Lists.newArrayList();
 
