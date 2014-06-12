05947f9f2c3a4decb78ef7085cc510bb34548ea7
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-06-12 10:46:09 +0200

    [#1786] Implemented SolrIndexFactory

--- timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/SolrIndexFactory.java
+++ timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/SolrIndexFactory.java
@@ -1,11 +1,26 @@
 package nl.knaw.huygens.timbuctoo.index;
 
+import nl.knaw.huygens.facetedsearch.FacetedSearchLibrary;
+import nl.knaw.huygens.solr.AbstractSolrServer;
+
+import com.google.inject.Inject;
 
 public class SolrIndexFactory implements IndexFactory {
 
+  private final SolrInputDocumentCreator solrDocumentCreator;
+  private final AbstractSolrServer solrServer;
+  private final FacetedSearchLibrary facetedSearchLibrary;
+
+  @Inject
+  public SolrIndexFactory(SolrInputDocumentCreator solrInputDocumentCreator, AbstractSolrServer solrServer, FacetedSearchLibrary facetedSearchLibrary) {
+    this.solrDocumentCreator = solrInputDocumentCreator;
+    this.solrServer = solrServer;
+    this.facetedSearchLibrary = facetedSearchLibrary;
+
+  }
+
   @Override
   public Index createIndexFor(String name) {
-    // TODO Auto-generated method stub
-    return null;
+    return new SolrIndex(name, solrDocumentCreator, solrServer, facetedSearchLibrary);
   }
 }
