fe5f1e911b655c8b20a11627ff43f222814ec75a
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-06-20 16:27:22 +0200

    [#2529] Implemented the FacetFieldConverter

---
+++ timbuctoo-search/src/test/java/nl/knaw/huygens/timbuctoo/search/converters/FacetFieldConveterTest.java
@@ -0,0 +1,31 @@
+package nl.knaw.huygens.timbuctoo.search.converters;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import nl.knaw.huygens.solr.SearchParameters;
+import nl.knaw.huygens.solr.SearchParametersV1;
+
+import org.junit.Test;
+
+public class FacetFieldConveterTest {
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testAddToV1() {
+    // setup
+    SearchParametersV1 searchParametersV1 = new SearchParametersV1();
+    String name1 = "name1";
+    String name2 = "name2";
+
+    SearchParameters searchParameters = new SearchParameters();
+    searchParameters.setFacetFields(new String[] { name1, name2 });
+
+    FacetFieldConverter instance = new FacetFieldConverter();
+
+    // action
+    instance.addToV1(searchParameters, searchParametersV1);
+
+    // verify
+    assertThat(searchParametersV1.getFacetFields(), containsInAnyOrder(name1, name2));
+
+  }
+}
