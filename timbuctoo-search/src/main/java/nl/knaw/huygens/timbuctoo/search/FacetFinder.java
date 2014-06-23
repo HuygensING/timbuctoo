9c9159de31f56162d9a6fa5654454db8679b6e7e
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-06-23 16:01:52 +0200

    Code clean up

--- timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/search/FacetFinder.java
+++ timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/search/FacetFinder.java
@@ -24,57 +24,20 @@ package nl.knaw.huygens.timbuctoo.search;
 
 import java.lang.reflect.Method;
 import java.util.List;
-import java.util.Map;
 
 import nl.knaw.huygens.facetedsearch.model.FacetDefinition;
 import nl.knaw.huygens.facetedsearch.model.FacetDefinitionBuilder;
 import nl.knaw.huygens.facetedsearch.model.FacetType;
-import nl.knaw.huygens.solr.FacetInfo;
 import nl.knaw.huygens.timbuctoo.facet.IndexAnnotation;
 import nl.knaw.huygens.timbuctoo.facet.IndexAnnotations;
 import nl.knaw.huygens.timbuctoo.model.Entity;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 public class FacetFinder {
 
   private static final Class<IndexAnnotations> INDEX_ANNOTATIONS_CLASS = IndexAnnotations.class;
   private static final Class<IndexAnnotation> INDEX_ANNOTATION_CLASS = IndexAnnotation.class;
-
-  @SuppressWarnings("unchecked")
-  public Map<String, FacetInfo> findFacets(Class<? extends Entity> type) {
-    Map<String, FacetInfo> facetMap = Maps.newHashMap();
-
-    Method[] methods = type.getMethods();
-
-    for (Method method : methods) {
-      if (method.isAnnotationPresent(INDEX_ANNOTATION_CLASS)) {
-        IndexAnnotation indexAnnotation = method.getAnnotation(INDEX_ANNOTATION_CLASS);
-        addFacet(facetMap, indexAnnotation);
-
-      } else if (method.isAnnotationPresent(INDEX_ANNOTATIONS_CLASS)) {
-        IndexAnnotations indexAnnotations = method.getAnnotation(INDEX_ANNOTATIONS_CLASS);
-        IndexAnnotation[] values = indexAnnotations.value();
-        for (IndexAnnotation indexAnnotation : values) {
-          addFacet(facetMap, indexAnnotation);
-        }
-      }
-    }
-
-    return facetMap;
-  }
-
-  private void addFacet(Map<String, FacetInfo> facets, IndexAnnotation indexAnnotation) {
-    if (indexAnnotation.isFaceted()) {
-      FacetInfo info = createFacetInfo(indexAnnotation);
-      facets.put(info.getName(), info);
-    }
-  }
-
-  private FacetInfo createFacetInfo(IndexAnnotation indexAnnotation) {
-    return new FacetInfo().setName(indexAnnotation.fieldName()).setTitle(indexAnnotation.title()).setType(indexAnnotation.facetType());
-  }
 
   public List<FacetDefinition> findFacetDefinitions(Class<? extends Entity> type) {
     List<FacetDefinition> facetDefinitions = Lists.newArrayList();
