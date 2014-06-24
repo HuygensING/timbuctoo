267a437af83b5c407daf5bb5dea7e1ee0de56d43
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-06-24 09:30:16 +0200

    Let timbuctoo use the IndexDescription.

--- timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/IndexDescriptionFactory.java
+++ timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/index/IndexDescriptionFactory.java
@@ -1,13 +1,41 @@
 package nl.knaw.huygens.timbuctoo.index;
 
 import nl.knaw.huygens.facetedsearch.model.parameters.IndexDescription;
+import nl.knaw.huygens.facetedsearch.model.parameters.IndexDescriptionBuilder;
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
+import nl.knaw.huygens.timbuctoo.search.FacetFinder;
+import nl.knaw.huygens.timbuctoo.search.FullTextSearchFieldFinder;
+import nl.knaw.huygens.timbuctoo.search.IndexedFieldFinder;
+import nl.knaw.huygens.timbuctoo.search.SortableFieldFinder;
 
 public class IndexDescriptionFactory {
 
-  public IndexDescription create(Class<? extends DomainEntity> type) {
-    // TODO Auto-generated method stub
-    return null;
+  private final IndexDescriptionBuilder indexDescriptionBuilder;
+  private final FacetFinder facetFinder;
+  private final FullTextSearchFieldFinder fullTextSearchFieldFinder;
+  private final IndexedFieldFinder indexedFieldFinder;
+  private final SortableFieldFinder sortableFieldFinder;
+
+  public IndexDescriptionFactory( // 
+      IndexDescriptionBuilder indexDescriptionBuilder, //
+      FacetFinder facetFinder, //
+      FullTextSearchFieldFinder fullTextSearchFieldFinder, //
+      IndexedFieldFinder indexedFieldFinder, //
+      SortableFieldFinder sortableFieldFinder) {
+
+    this.indexDescriptionBuilder = indexDescriptionBuilder;
+    this.facetFinder = facetFinder;
+    this.fullTextSearchFieldFinder = fullTextSearchFieldFinder;
+    this.indexedFieldFinder = indexedFieldFinder;
+    this.sortableFieldFinder = sortableFieldFinder;
   }
 
+  public IndexDescription create(Class<? extends DomainEntity> type) {
+    indexDescriptionBuilder.setFacetDefinitions(facetFinder.findFacetDefinitions(type)) //
+        .setFullTextSearchFields(fullTextSearchFieldFinder.findFields(type)) //
+        .setIndexedFields(indexedFieldFinder.findFields(type)) //
+        .setSortFields(sortableFieldFinder.findFields(type));
+
+    return indexDescriptionBuilder.build();
+  }
 }
---
+++ timbuctoo-search/src/main/java/nl/knaw/huygens/timbuctoo/search/IndexedFieldFinder.java
@@ -0,0 +1,15 @@
+package nl.knaw.huygens.timbuctoo.search;
+
+import java.util.Set;
+
+import nl.knaw.huygens.timbuctoo.facet.IndexAnnotation;
+
+public class IndexedFieldFinder extends AbstractFieldFinder {
+
+  @Override
+  protected void addField(Set<String> fields, IndexAnnotation indexAnnotation) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
