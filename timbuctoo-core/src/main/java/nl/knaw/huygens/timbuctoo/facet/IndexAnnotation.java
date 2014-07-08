674c85b196b3f7f141168b61ff6cdf77a6bd3ef0
walter <walter.ravenek@huygens.knaw.nl>
2014-07-08 11:47:49 +0200

    Fixed comment

--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/facet/IndexAnnotation.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/facet/IndexAnnotation.java
@@ -37,7 +37,7 @@ import java.lang.annotation.Target;
  *   <dynamicField name="dynamic_sort_*" type="alphaOnlySort" indexed="true" stored="false" multiValued="false"/>
  *   <dynamicField name="dynamic_k_*" type="string" indexed="true" stored="true" multiValued="false"/>
  *   <dynamicField name="dynamic_s_*" type="string" indexed="true" stored="true" multiValued="true"/>
- *   <dynamicField name="dynamic_t_*" type="textgen" indexed="true" stored="true" multiValued="true"/>
+ *   <dynamicField name="dynamic_t_*" type="text_repo" indexed="true" stored="true" multiValued="true"/>
  *   <dynamicField name="dynamic_i_*" type="int" indexed="true" stored="true" multiValued="true"/>
  *   <dynamicField name="dynamic_b_*" type="boolean" indexed="true" stored="true" multiValued="true"/>
  *   <dynamicField name="dynamic_d_*" type="date" indexed="true" stored="true" multiValued="true"/>
