3b1fc490f0152c95760373d0a66351f832c0a848
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-08-01 15:54:13 +0200

    [#2628] Make it possible to search with facets

---
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/rest/providers/ObjectMapperContext.java
@@ -0,0 +1,29 @@
+package nl.knaw.huygens.timbuctoo.rest.providers;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+import nl.knaw.huygens.facetedsearch.model.parameters.FacetParameter;
+import nl.knaw.huygens.facetedsearch.serialization.FacetParameterDeserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+@Provider
+public class ObjectMapperContext implements ContextResolver<ObjectMapper> {
+  private ObjectMapper mapper;
+
+  public ObjectMapperContext() {
+    mapper = new ObjectMapper();
+    SimpleModule module = new SimpleModule();
+    module.addDeserializer(FacetParameter.class, new FacetParameterDeserializer());
+    mapper.registerModule(module);
+  }
+
+  @Override
+  public ObjectMapper getContext(Class<?> type) {
+    System.out.println("get ObjectMapper");
+
+    return mapper;
+  }
+}
