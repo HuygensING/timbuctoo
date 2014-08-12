5d19d5511832f51637c91559147d751bdf9c1846
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-08-12 16:34:12 +0200

    [#2635] Added a method to SolrInputDocGenerator to index RangeFacets

--- timbuctoo-vre/src/test/java/nl/knaw/huygens/timbuctoo/index/solr/SolrInputDocGeneratorTest.java
+++ timbuctoo-vre/src/test/java/nl/knaw/huygens/timbuctoo/index/solr/SolrInputDocGeneratorTest.java
@@ -42,7 +42,6 @@ import nl.knaw.huygens.timbuctoo.search.model.ClassWithMultipleFacetTypes;
 
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.SolrInputField;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import test.timbuctoo.index.model.TestExtraBaseDoc;
@@ -140,7 +139,6 @@ public class SolrInputDocGeneratorTest {
     assertEquals(expected, actual);
   }
 
-  @Ignore("Ignore until the code is implemented")
   @Test
   // Datable is currently the only supported type for ranges.
   public void testGetResultOneDocumentWithDatableRangeFacetField() throws NoSuchMethodException {
