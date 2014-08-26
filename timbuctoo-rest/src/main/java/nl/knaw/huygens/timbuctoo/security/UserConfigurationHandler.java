21d2a363cb466c7304f9c561a8ed921d7582a4c4
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-08-26 13:08:34 +0200

    [#2650] Make sure there is only one instance of the user configuration handler.

--- timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/security/UserConfigurationHandler.java
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/security/UserConfigurationHandler.java
@@ -14,7 +14,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
+@Singleton
 public class UserConfigurationHandler {
 
   private static final Logger LOG = LoggerFactory.getLogger(UserConfigurationHandler.class);
