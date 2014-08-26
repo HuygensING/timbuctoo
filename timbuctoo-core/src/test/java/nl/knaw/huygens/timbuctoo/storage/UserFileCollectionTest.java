85f44bf78606fef70875befe25a648d38bbcd29f
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-08-26 10:05:44 +0200

    [#2650] Added missing tests for FileCollections

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/UserFileCollectionTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/UserFileCollectionTest.java
@@ -8,207 +8,245 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.sameInstance;
+
+import java.util.List;
+
 import nl.knaw.huygens.timbuctoo.model.User;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.collect.Lists;
+
 public class UserFileCollectionTest extends FileCollectionTest<User> {
-	private static final String PERSISTENT_ID = "persistentId";
-	private UserFileCollection instance;
+  private static final String PERSISTENT_ID = "persistentId";
+  private UserFileCollection instance;
 
-	@Before
-	public void setUp() {
-		instance = new UserFileCollection();
-	}
+  @Before
+  public void setUp() {
+    instance = new UserFileCollection();
+  }
 
-	@Test
-	public void addUserShouldGiveUserAndReturnTheId() {
-		User user = createUserWithPersistentId();
-		String expectedId = "USER000000000001";
+  @Test
+  public void addUserShouldGiveUserAndReturnTheId() {
+    User user = createUserWithPersistentId();
+    String expectedId = "USER000000000001";
 
-		verifyAddReturnsAnIdAndAddsItToTheEntity(user, expectedId);
-	}
+    verifyAddReturnsAnIdAndAddsItToTheEntity(user, expectedId);
+  }
 
-	@Test
-	public void addUserIncrementsTheId() {
-		User user1 = createUserWithPersistentId();
-		User user2 = createUserWithPersistentId();
-		User user3 = createUserWithPersistentId();
+  @Test
+  public void addUserIncrementsTheId() {
+    User user1 = createUserWithPersistentId();
+    User user2 = createUserWithPersistentId();
+    User user3 = createUserWithPersistentId();
 
-		String expectedId = "USER000000000003";
+    String expectedId = "USER000000000003";
 
-		verifyAddIncrementsTheId(user1, user2, user3, expectedId);
-	}
+    verifyAddIncrementsTheId(user1, user2, user3, expectedId);
+  }
 
-	private User createUserWithPersistentId() {
-		User user = new User();
-		user.setPersistentId(PERSISTENT_ID);
-		return user;
-	}
+  private User createUserWithPersistentId() {
+    User user = new User();
+    user.setPersistentId(PERSISTENT_ID);
+    return user;
+  }
 
-	@Test
-	public void addUserShouldAddTheUserToItsInnerCollection() {
-		User user = createUserWithPersistentId();
+  @Test
+  public void addUserShouldAddTheUserToItsInnerCollection() {
+    User user = createUserWithPersistentId();
 
-		verifyAddAddsTheEntityToItsCollection(user);
-	}
+    verifyAddAddsTheEntityToItsCollection(user);
+  }
 
-	@Test
-	public void theAddedUserCannotBeFoundIfItDidNotContainAPersistentIdButCantBeGet() {
-		User user = new User();
+  @Test
+  public void theAddedUserCannotBeFoundIfItDidNotContainAPersistentIdButCantBeGet() {
+    User user = new User();
 
-		String id = instance.add(user);
+    String id = instance.add(user);
 
-		assertThat(instance.findItem(user), is(nullValue(User.class)));
-		assertThat(instance.get(id), is(equalTo(user)));
-	}
+    assertThat(instance.findItem(user), is(nullValue(User.class)));
+    assertThat(instance.get(id), is(equalTo(user)));
+  }
 
-	@Test
-	public void findUserShouldSearchTheUserByPersistentId() {
-		User user = createUserWithPersistentId();
+  @Test
+  public void findUserShouldSearchTheUserByPersistentId() {
+    User user = createUserWithPersistentId();
 
-		instance.add(user);
-		User foundUser = instance.findItem(user);
+    instance.add(user);
+    User foundUser = instance.findItem(user);
 
-		assertThat(foundUser, is(equalTo(user)));
-	}
+    assertThat(foundUser, is(equalTo(user)));
+  }
 
-	@Test
-	public void findUserReturnsNullIfTheUserParametersHasNoPersistentId() {
-		User user = createUserWithPersistentId();
-		User userToFind = new User();
+  @Test
+  public void findUserReturnsNullIfTheUserParametersHasNoPersistentId() {
+    User user = createUserWithPersistentId();
+    User userToFind = new User();
 
-		instance.add(user);
-		User foundUser = instance.findItem(userToFind);
+    instance.add(user);
+    User foundUser = instance.findItem(userToFind);
 
-		assertThat(foundUser, is(nullValue(User.class)));
-	}
+    assertThat(foundUser, is(nullValue(User.class)));
+  }
 
-	@Test
-	public void findUserReturnsNullIfTheUserParametersIsNull() {
-		User user = createUserWithPersistentId();
-		User userToFind = null;
+  @Test
+  public void findUserReturnsNullIfTheUserParametersIsNull() {
+    User user = createUserWithPersistentId();
+    User userToFind = null;
 
-		instance.add(user);
-		User foundUser = instance.findItem(userToFind);
+    instance.add(user);
+    User foundUser = instance.findItem(userToFind);
 
-		assertThat(foundUser, is(nullValue(User.class)));
-	}
+    assertThat(foundUser, is(nullValue(User.class)));
+  }
 
-	@Test
-	public void findUserReturnsNullIfTheUserIsNotKnown() {
-		User user = createUserWithPersistentId();
+  @Test
+  public void findUserReturnsNullIfTheUserIsNotKnown() {
+    User user = createUserWithPersistentId();
 
-		User foundUser = instance.findItem(user);
+    User foundUser = instance.findItem(user);
 
-		assertThat(foundUser, is(nullValue(User.class)));
-	}
+    assertThat(foundUser, is(nullValue(User.class)));
+  }
 
-	@Test
-	public void getReturnsNullIfTheIdInsertedIsNull() {
-		User user = createUserWithPersistentId();
+  @Test
+  public void getReturnsNullIfTheIdInsertedIsNull() {
+    User user = createUserWithPersistentId();
 
-		instance.add(user);
-		User foundUser = instance.get(null);
+    instance.add(user);
+    User foundUser = instance.get(null);
 
-		assertThat(foundUser, is(nullValue(User.class)));
-	}
+    assertThat(foundUser, is(nullValue(User.class)));
+  }
 
-	@Test
-	public void getAllShouldReturnAStorageIteratorWithAllTheKnownUsers() {
-		// setup
-		User user1 = createUserWithPersistentId();
-		User user2 = createUserWithPersistentId();
-		User user3 = createUserWithPersistentId();
+  @Test
+  public void getAllShouldReturnAStorageIteratorWithAllTheKnownUsers() {
+    // setup
+    User user1 = createUserWithPersistentId();
+    User user2 = createUserWithPersistentId();
+    User user3 = createUserWithPersistentId();
 
-		instance.add(user1);
-		instance.add(user2);
-		instance.add(user3);
+    instance.add(user1);
+    instance.add(user2);
+    instance.add(user3);
 
-		verifyGetAllReturnsAllTheKnownEntities(containsInAnyOrder(user1, user2, user3));
-	}
+    verifyGetAllReturnsAllTheKnownEntities(containsInAnyOrder(user1, user2, user3));
+  }
 
-	@Test
-	public void getAlldReturnsAnEmptyStorageIteratorWhenNoUsersAreKnown() {
+  @Test
+  public void getAlldReturnsAnEmptyStorageIteratorWhenNoUsersAreKnown() {
 
-		// action
-		StorageIterator<User> users = instance.getAll();
+    // action
+    StorageIterator<User> users = instance.getAll();
 
-		// verify
-		assertThat(users, is(notNullValue()));
-		assertThat(users.getAll(), is(empty()));
+    // verify
+    assertThat(users, is(notNullValue()));
+    assertThat(users.getAll(), is(empty()));
 
-	}
+  }
 
-	@Test
-	public void updateUserSearchesTheUserByIdAndReplacesTheUserInTheCollection() {
-		// setup
-		User user1 = createUserWithPersistentId();
-		String id = instance.add(user1);
+  @Test
+  public void updateUserSearchesTheUserByIdAndReplacesTheUserInTheCollection() {
+    // setup
+    User user1 = createUserWithPersistentId();
+    String id = instance.add(user1);
 
-		User user2 = createUserWithPersistentId();
-		user2.setId(id);
-		user2.setCommonName("test");
+    User user2 = createUserWithPersistentId();
+    user2.setId(id);
+    user2.setCommonName("test");
 
-		// action
-		instance.updateItem(user2);
+    // action
+    instance.updateItem(user2);
 
-		// verify
-		assertThat(instance.get(id), is(sameInstance(user2)));
-	}
+    // verify
+    assertThat(instance.get(id), is(sameInstance(user2)));
+  }
 
-	@Test
-	public void updateUserDoesNothingIfTheUpdateUserHasNoId() {
-		// setup
-		User user1 = createUserWithPersistentId();
-		String id = instance.add(user1);
+  @Test
+  public void updateUserDoesNothingIfTheUpdateUserHasNoId() {
+    // setup
+    User user1 = createUserWithPersistentId();
+    String id = instance.add(user1);
 
-		User user2 = createUserWithPersistentId();
-		user2.setCommonName("test");
+    User user2 = createUserWithPersistentId();
+    user2.setCommonName("test");
 
-		// action
-		instance.updateItem(user2);
+    // action
+    instance.updateItem(user2);
 
-		// verify
-		assertThat(instance.get(id), is(sameInstance(user1)));
-	}
+    // verify
+    assertThat(instance.get(id), is(sameInstance(user1)));
+  }
 
-	@Test
-	public void deleteUserSearchesTheUserByIdAndRemovesItFromTheCollection() {
-		// setup
-		User user1 = createUserWithPersistentId();
-		String id = instance.add(user1);
+  @Test
+  public void deleteUserSearchesTheUserByIdAndRemovesItFromTheCollection() {
+    // setup
+    User user1 = createUserWithPersistentId();
+    String id = instance.add(user1);
 
-		User user2 = createUserWithPersistentId();
-		user2.setId(id);
-		user2.setCommonName("test");
+    User user2 = createUserWithPersistentId();
+    user2.setId(id);
+    user2.setCommonName("test");
 
-		// action
-		instance.deleteItem(user2);
+    // action
+    instance.deleteItem(user2);
 
-		// verify
-		assertThat(instance.get(id), is(nullValue(User.class)));
-	}
+    // verify
+    assertThat(instance.get(id), is(nullValue(User.class)));
+  }
 
-	@Test
-	public void deleteUserDoesNothingIfTheUpdateUserHasNoId() {
-		// setup
-		User user1 = createUserWithPersistentId();
-		String id = instance.add(user1);
+  @Test
+  public void deleteUserDoesNothingIfTheUpdateUserHasNoId() {
+    // setup
+    User user1 = createUserWithPersistentId();
+    String id = instance.add(user1);
 
-		User user2 = createUserWithPersistentId();
-		user2.setCommonName("test");
+    User user2 = createUserWithPersistentId();
+    user2.setCommonName("test");
 
-		// action
-		instance.deleteItem(user2);
+    // action
+    instance.deleteItem(user2);
 
-		// verify
-		assertThat(instance.get(id), is(sameInstance(user1)));
-	}
+    // verify
+    assertThat(instance.get(id), is(sameInstance(user1)));
+  }
 
-	@Override
-	protected FileCollection<User> getInstance() {
-		return instance;
-	}
+  @Test
+  public void intializeWithAListOfUsersMakesItPossibleToRetrieveThem() {
+    // setup
+    User user1 = createUserWithPersistentId();
+    String userId = "test";
+    user1.setId(userId);
+    List<User> users = Lists.newArrayList(user1);
+
+    // action
+    UserFileCollection collections = new UserFileCollection(users);
+
+    // verify
+    assertThat(collections.get(userId), sameInstance(user1));
+    assertThat(collections.findItem(user1), sameInstance(user1));
+  }
+
+  @Test
+  public void asArrayContainsAllTheItemsOfTheFileCollection() {
+    // setup
+    User user1 = createUserWithPersistentId();
+    User user2 = createUserWithPersistentId();
+    User user3 = createUserWithPersistentId();
+
+    instance.add(user1);
+    instance.add(user2);
+    instance.add(user3);
+
+    // action
+    User[] users = instance.asArray();
+
+    assertThat(users.length, is(equalTo(3)));
+  }
+
+  @Override
+  protected FileCollection<User> getInstance() {
+    return instance;
+  }
 }
--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/VREAuthorizationFileCollectionTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/VREAuthorizationFileCollectionTest.java
@@ -8,7 +8,11 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.fail;
+
+import java.util.List;
+
 import nl.knaw.huygens.timbuctoo.model.VREAuthorization;
 
 import org.junit.Before;
@@ -18,205 +22,236 @@ import org.junit.Test;
 import com.google.common.collect.Lists;
 
 public class VREAuthorizationFileCollectionTest extends FileCollectionTest<VREAuthorization> {
-	private static final String USER_ID = "USER000000000001";
-	private static final String VRE_ID = "vreId";
-	private static final String USER_ID2 = "USER000000000002";
-	private static final String USER_ID1 = "USER000000000003";
-	private VREAuthorizationFileCollection instance;
+  private static final String USER_ID = "USER000000000001";
+  private static final String VRE_ID = "vreId";
+  private static final String USER_ID2 = "USER000000000002";
+  private static final String USER_ID1 = "USER000000000003";
+  private VREAuthorizationFileCollection instance;
 
-	@Before
-	public void setUp() {
-		instance = new VREAuthorizationFileCollection();
-	}
+  @Before
+  public void setUp() {
+    instance = new VREAuthorizationFileCollection();
+  }
 
-	@Test
-	public void addShouldGiveAuthorizationAndReturnTheId() {
-		VREAuthorization authorization = createVREAuthorizationWithDefaultVREId(USER_ID);
+  @Test
+  public void addShouldGiveAuthorizationAndReturnTheId() {
+    VREAuthorization authorization = createVREAuthorizationWithDefaultVREId(USER_ID);
 
-		String expectedId = "VREA000000000001";
+    String expectedId = "VREA000000000001";
 
-		verifyAddReturnsAnIdAndAddsItToTheEntity(authorization, expectedId);
-	}
+    verifyAddReturnsAnIdAndAddsItToTheEntity(authorization, expectedId);
+  }
 
-	@Test
-	public void addShouldCreateAnIdHigherThanTheHighest() {
-		VREAuthorization authorization1 = createVREAuthorizationWithDefaultVREId(USER_ID);
-		VREAuthorization authorization2 = createVREAuthorizationWithDefaultVREId(USER_ID1);
-		VREAuthorization authorization3 = createVREAuthorizationWithDefaultVREId(USER_ID2);
+  @Test
+  public void addShouldCreateAnIdHigherThanTheHighest() {
+    VREAuthorization authorization1 = createVREAuthorizationWithDefaultVREId(USER_ID);
+    VREAuthorization authorization2 = createVREAuthorizationWithDefaultVREId(USER_ID1);
+    VREAuthorization authorization3 = createVREAuthorizationWithDefaultVREId(USER_ID2);
 
-		String expectedId = "VREA000000000003";
+    String expectedId = "VREA000000000003";
 
-		verifyAddIncrementsTheId(authorization1, authorization2, authorization3, expectedId);
-	}
+    verifyAddIncrementsTheId(authorization1, authorization2, authorization3, expectedId);
+  }
 
-	@Test
-	public void addAddsTheAuthorizationToItsCollection() {
-		VREAuthorization authorization = createVREAuthorizationWithDefaultVREId(USER_ID);
+  @Test
+  public void addAddsTheAuthorizationToItsCollection() {
+    VREAuthorization authorization = createVREAuthorizationWithDefaultVREId(USER_ID);
 
-		verifyAddAddsTheEntityToItsCollection(authorization);
-	}
+    verifyAddAddsTheEntityToItsCollection(authorization);
+  }
 
-	// TODO: Add tests for adding without user id or VRE id.
+  // TODO: Add tests for adding without user id or VRE id.
 
-	@Test
-	public void addReturnsTheExistingIdIfTheUserAllreadyHasAnAuthroizationForTheVRE() {
-		VREAuthorization authorization1 = createVREAuthorizationWithDefaultVREId(USER_ID);
-		VREAuthorization authorization2 = createVREAuthorizationWithDefaultVREId(USER_ID);
+  @Test
+  public void addReturnsTheExistingIdIfTheUserAllreadyHasAnAuthroizationForTheVRE() {
+    VREAuthorization authorization1 = createVREAuthorizationWithDefaultVREId(USER_ID);
+    VREAuthorization authorization2 = createVREAuthorizationWithDefaultVREId(USER_ID);
 
-		String expectedId = "VREA000000000001";
+    String expectedId = "VREA000000000001";
 
-		instance.add(authorization1);
+    instance.add(authorization1);
 
-		String actualId = instance.add(authorization2);
+    String actualId = instance.add(authorization2);
 
-		assertThat(actualId, is(equalTo(expectedId)));
-	}
+    assertThat(actualId, is(equalTo(expectedId)));
+  }
 
-	@Test
-	public void findSearchesTheAuthorizationWithUserAndVREId() {
-		VREAuthorization authorization = setupInstanceWithAuthorization(USER_ID);
+  @Test
+  public void findSearchesTheAuthorizationWithUserAndVREId() {
+    VREAuthorization authorization = setupInstanceWithAuthorization(USER_ID);
 
-		VREAuthorization exampleVREAuthorization = createVREAuthorizationWithDefaultVREId(USER_ID);
-		VREAuthorization exampleVREAuthorization1 = createVREAuthorizationWithDefaultVREId(USER_ID1);
+    VREAuthorization exampleVREAuthorization = createVREAuthorizationWithDefaultVREId(USER_ID);
+    VREAuthorization exampleVREAuthorization1 = createVREAuthorizationWithDefaultVREId(USER_ID1);
 
-		assertThat(instance.findItem(exampleVREAuthorization), is(equalTo(authorization)));
-		assertThat(instance.findItem(exampleVREAuthorization1), is(nullValue(VREAuthorization.class)));
-	}
+    assertThat(instance.findItem(exampleVREAuthorization), is(equalTo(authorization)));
+    assertThat(instance.findItem(exampleVREAuthorization1), is(nullValue(VREAuthorization.class)));
+  }
 
-	@Test
-	public void findSearchesDoesNotFindAnythingWithoutUserId() {
-		setupInstanceWithAuthorization(USER_ID);
+  @Test
+  public void findSearchesDoesNotFindAnythingWithoutUserId() {
+    setupInstanceWithAuthorization(USER_ID);
 
-		VREAuthorization exampleAuthorization = createVREAuthorizationWithDefaultVREId(null);
+    VREAuthorization exampleAuthorization = createVREAuthorizationWithDefaultVREId(null);
 
-		assertThat(instance.findItem(exampleAuthorization), is(nullValue(VREAuthorization.class)));
-	}
+    assertThat(instance.findItem(exampleAuthorization), is(nullValue(VREAuthorization.class)));
+  }
 
-	private VREAuthorization setupInstanceWithAuthorization(String userId) {
-		VREAuthorization authorization = createVREAuthorizationWithDefaultVREId(userId);
-		instance.add(authorization);
+  private VREAuthorization setupInstanceWithAuthorization(String userId) {
+    VREAuthorization authorization = createVREAuthorizationWithDefaultVREId(userId);
+    instance.add(authorization);
 
-		return authorization;
-	}
+    return authorization;
+  }
 
-	@Test
-	public void findSearchesDoesNotFindAnythingWithoutAuthorizationId() {
-		setupInstanceWithAuthorization(USER_ID);
+  @Test
+  public void findSearchesDoesNotFindAnythingWithoutAuthorizationId() {
+    setupInstanceWithAuthorization(USER_ID);
 
-		VREAuthorization exampleAuthorization = new VREAuthorization(null, USER_ID);
+    VREAuthorization exampleAuthorization = new VREAuthorization(null, USER_ID);
 
-		assertThat(instance.findItem(exampleAuthorization), is(nullValue(VREAuthorization.class)));
-	}
+    assertThat(instance.findItem(exampleAuthorization), is(nullValue(VREAuthorization.class)));
+  }
 
-	@Test
-	public void getReturnsNullIfTheIdIsNull() {
-		setupInstanceWithAuthorization(USER_ID);
-		String nullId = null;
+  @Test
+  public void getReturnsNullIfTheIdIsNull() {
+    setupInstanceWithAuthorization(USER_ID);
+    String nullId = null;
 
-		VREAuthorization vreAuthorization = instance.get(nullId);
+    VREAuthorization vreAuthorization = instance.get(nullId);
 
-		assertThat(vreAuthorization, is(nullValue(VREAuthorization.class)));
-	}
+    assertThat(vreAuthorization, is(nullValue(VREAuthorization.class)));
+  }
 
-	@Test
-	public void getAllReturnsAllTheKnowEntities() {
-		VREAuthorization authorization1 = createVREAuthorizationWithDefaultVREId(USER_ID);
-		VREAuthorization authorization2 = createVREAuthorizationWithDefaultVREId(USER_ID1);
-		VREAuthorization authorization3 = createVREAuthorizationWithDefaultVREId(USER_ID2);
+  @Test
+  public void getAllReturnsAllTheKnowEntities() {
+    VREAuthorization authorization1 = createVREAuthorizationWithDefaultVREId(USER_ID);
+    VREAuthorization authorization2 = createVREAuthorizationWithDefaultVREId(USER_ID1);
+    VREAuthorization authorization3 = createVREAuthorizationWithDefaultVREId(USER_ID2);
 
-		instance.add(authorization1);
-		instance.add(authorization2);
-		instance.add(authorization3);
+    instance.add(authorization1);
+    instance.add(authorization2);
+    instance.add(authorization3);
 
-		verifyGetAllReturnsAllTheKnownEntities(containsInAnyOrder(authorization1, authorization2, authorization3));
-	}
+    verifyGetAllReturnsAllTheKnownEntities(containsInAnyOrder(authorization1, authorization2, authorization3));
+  }
 
-	@Test
-	public void getAlldReturnsAnEmptyStorageIteratorWhenNoAuthorizationsAreKnown() {
+  @Test
+  public void getAlldReturnsAnEmptyStorageIteratorWhenNoAuthorizationsAreKnown() {
 
-		// action
-		StorageIterator<VREAuthorization> authorizations = instance.getAll();
+    // action
+    StorageIterator<VREAuthorization> authorizations = instance.getAll();
 
-		// verify
-		assertThat(authorizations, is(notNullValue()));
-		assertThat(authorizations.getAll(), is(empty()));
-	}
+    // verify
+    assertThat(authorizations, is(notNullValue()));
+    assertThat(authorizations.getAll(), is(empty()));
+  }
 
-	@Test
-	public void updateSearchesWithTheCombinationOfVREIdAndUserId() {
-		// setup
-		VREAuthorization original = createVREAuthorizationWithDefaultVREId(USER_ID);
-		original.setRoles(Lists.newArrayList("role1"));
+  @Test
+  public void updateSearchesWithTheCombinationOfVREIdAndUserId() {
+    // setup
+    VREAuthorization original = createVREAuthorizationWithDefaultVREId(USER_ID);
+    original.setRoles(Lists.newArrayList("role1"));
 
-		String id = instance.add(original);
+    String id = instance.add(original);
 
-		VREAuthorization itemToUpdate = createVREAuthorizationWithDefaultVREId(USER_ID);
-		String role2 = "role2";
-		itemToUpdate.setRoles(Lists.newArrayList(role2));
+    VREAuthorization itemToUpdate = createVREAuthorizationWithDefaultVREId(USER_ID);
+    String role2 = "role2";
+    itemToUpdate.setRoles(Lists.newArrayList(role2));
 
-		// action
-		instance.updateItem(itemToUpdate);
+    // action
+    instance.updateItem(itemToUpdate);
 
-		// verify
-		VREAuthorization updated = instance.get(id);
+    // verify
+    VREAuthorization updated = instance.get(id);
 
-		assertThat(updated, is(notNullValue(VREAuthorization.class)));
-		assertThat(updated.getRoles(), contains(role2));
-	}
+    assertThat(updated, is(notNullValue(VREAuthorization.class)));
+    assertThat(updated.getRoles(), contains(role2));
+  }
 
-	// TODO find a better way to test this.
-	@Test
-	public void updateDoesNothingIfTheAuthorizationCannotBeFound() {
-		// setup
-		VREAuthorization original = createVREAuthorizationWithDefaultVREId(USER_ID);
-		String role1 = "role1";
-		original.setRoles(Lists.newArrayList(role1));
+  // TODO find a better way to test this.
+  @Test
+  public void updateDoesNothingIfTheAuthorizationCannotBeFound() {
+    // setup
+    VREAuthorization original = createVREAuthorizationWithDefaultVREId(USER_ID);
+    String role1 = "role1";
+    original.setRoles(Lists.newArrayList(role1));
 
-		String id = instance.add(original);
+    String id = instance.add(original);
 
-		VREAuthorization itemToUpdate = createVREAuthorizationWithDefaultVREId(USER_ID1);
-		itemToUpdate.setRoles(Lists.newArrayList("role2"));
+    VREAuthorization itemToUpdate = createVREAuthorizationWithDefaultVREId(USER_ID1);
+    itemToUpdate.setRoles(Lists.newArrayList("role2"));
 
-		// action
-		instance.updateItem(itemToUpdate);
+    // action
+    instance.updateItem(itemToUpdate);
 
-		// verify
-		VREAuthorization updated = instance.get(id);
+    // verify
+    VREAuthorization updated = instance.get(id);
 
-		assertThat(updated, is(notNullValue(VREAuthorization.class)));
-		assertThat(updated.getRoles(), contains(role1));
-	}
+    assertThat(updated, is(notNullValue(VREAuthorization.class)));
+    assertThat(updated.getRoles(), contains(role1));
+  }
 
-	@Test
-	public void deleteDeterminesTheAuthorizationByVREIdAndUserId() {
-		// setup
-		VREAuthorization original = createVREAuthorizationWithDefaultVREId(USER_ID);
-		String role1 = "role1";
-		original.setRoles(Lists.newArrayList(role1));
+  @Test
+  public void deleteDeterminesTheAuthorizationByVREIdAndUserId() {
+    // setup
+    VREAuthorization original = createVREAuthorizationWithDefaultVREId(USER_ID);
+    String role1 = "role1";
+    original.setRoles(Lists.newArrayList(role1));
 
-		String id = instance.add(original);
+    String id = instance.add(original);
 
-		VREAuthorization itemToDelete = createVREAuthorizationWithDefaultVREId(USER_ID);
+    VREAuthorization itemToDelete = createVREAuthorizationWithDefaultVREId(USER_ID);
 
-		// action
-		instance.deleteItem(itemToDelete);
+    // action
+    instance.deleteItem(itemToDelete);
 
-		// verify
-		assertThat(instance.get(id), is(nullValue(VREAuthorization.class)));
-	}
+    // verify
+    assertThat(instance.get(id), is(nullValue(VREAuthorization.class)));
+  }
 
-	@Ignore
-	@Test
-	public void deleteDoesNothingIfAuthorizationCannotBeFound() {
-		fail("Yet to be implemented");
-	}
+  @Ignore
+  @Test
+  public void deleteDoesNothingIfAuthorizationCannotBeFound() {
+    fail("Yet to be implemented");
+  }
 
-	private VREAuthorization createVREAuthorizationWithDefaultVREId(String userId) {
-		return new VREAuthorization(VRE_ID, userId);
-	}
+  @Test
+  public void initializeWithAListOfAuthorizationsMakesThemRetrievable() {
+    VREAuthorization authorization = createVREAuthorizationWithDefaultVREId(USER_ID);
+    String id = "id";
+    authorization.setId(id);
+    List<VREAuthorization> authorizations = Lists.newArrayList(authorization);
 
-	@Override
-	protected FileCollection<VREAuthorization> getInstance() {
-		return instance;
-	}
+    VREAuthorizationFileCollection collection = new VREAuthorizationFileCollection(authorizations);
+
+    assertThat(collection.get(id), is(sameInstance(authorization)));
+    assertThat(collection.findItem(authorization), is(sameInstance(authorization)));
+  }
+
+  @Test
+  public void asArrayContainsAllTheItemsOfTheCollections() {
+    // setup
+    VREAuthorization authorization1 = createVREAuthorizationWithDefaultVREId(USER_ID);
+    VREAuthorization authorization2 = createVREAuthorizationWithDefaultVREId(USER_ID1);
+    VREAuthorization authorization3 = createVREAuthorizationWithDefaultVREId(USER_ID2);
+
+    instance.add(authorization1);
+    instance.add(authorization2);
+    instance.add(authorization3);
+
+    // action
+    VREAuthorization[] authorizations = instance.asArray();
+
+    // verify
+    assertThat(authorizations.length, is(equalTo(3)));
+  }
+
+  private VREAuthorization createVREAuthorizationWithDefaultVREId(String userId) {
+    return new VREAuthorization(VRE_ID, userId);
+  }
+
+  @Override
+  protected FileCollection<VREAuthorization> getInstance() {
+    return instance;
+  }
 }
