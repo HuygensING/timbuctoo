63b9593172607491692071bf90ae2c566610d3f6
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-09-23 17:08:53 +0200

    Moved the Repository instance to timbuctoo-instance package

---
+++ timbuctoo-instance/src/main/java/nl/knaw/huygens/repository/rest/RepoContextListener.java
@@ -0,0 +1,7 @@
+package nl.knaw.huygens.repository.rest;
+
+import nl.knaw.huygens.timbuctoo.rest.TimbuctooContextListener;
+
+public class RepoContextListener extends TimbuctooContextListener {
+
+}
