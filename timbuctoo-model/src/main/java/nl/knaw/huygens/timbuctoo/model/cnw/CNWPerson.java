1bdeb704bf91ab26ec76ab317095a44077258652
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2014-09-29 13:19:26 +0200

    Building the CNW model and importer

---
+++ timbuctoo-model/src/main/java/nl/knaw/huygens/timbuctoo/model/cnw/CNWPerson.java
@@ -0,0 +1,480 @@
+package nl.knaw.huygens.timbuctoo.model.cnw;
+
+import nl.knaw.huygens.timbuctoo.model.Person;
+
+public class CNWPerson extends Person {
+
+	private String verwijzingen = "";
+	private String opmerkingen = "";
+	private String notities = "";
+	private String aantekeningen = "";
+
+	String activities = "";
+	String altname = "";
+	String biodesurl = "";
+	String birth = "";
+	String bntlUrl = "";
+	String cenUrlAfz = "";
+	String cenUrlOntv = "";
+	String characteristic = "";
+	String country = "";
+	String date = "";
+	String day = "";
+	String dbngUrl = "";
+	String dbnlUrl = "";
+	String death = "";
+	String domain = "";
+	String domains = "";
+	String education = "";
+	String familyname = "";
+	String firstname = "";
+	String identifier = "";
+	String intraposition = "";
+	String koppelnaam = "";
+	String koppelname = "";
+	String levensbeschouwing = "";
+	String link = "";
+	String literatuur = "";
+	String month = "";
+	String name = "";
+	String names = "";
+	String nametype = "";
+	String network = "";
+	String occupation = "";
+	String opmPolitics = "";
+	String persname = "";
+	String persoon = "";
+	String place = "";
+	String politics = "";
+	String postposition = "";
+	String preposition = "";
+	String relatie = "";
+	String relatives = "";
+	String reltype = "";
+	String sex = "";
+	String soort = "";
+	String url = "";
+	String val = "";
+	String values = "";
+	String woonplaats = "";
+	String year = "";
+
+	public String getActivities() {
+		return activities;
+	}
+
+	public void setActivities(String activities) {
+		this.activities = activities;
+	}
+
+	public String getAltname() {
+		return altname;
+	}
+
+	public void setAltname(String altname) {
+		this.altname = altname;
+	}
+
+	public String getBiodesurl() {
+		return biodesurl;
+	}
+
+	public void setBiodesurl(String biodesurl) {
+		this.biodesurl = biodesurl;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getBntlUrl() {
+		return bntlUrl;
+	}
+
+	public void setBntlUrl(String bntlUrl) {
+		this.bntlUrl = bntlUrl;
+	}
+
+	public String getCenUrlAfz() {
+		return cenUrlAfz;
+	}
+
+	public void setCenUrlAfz(String cenUrlAfz) {
+		this.cenUrlAfz = cenUrlAfz;
+	}
+
+	public String getCenUrlOntv() {
+		return cenUrlOntv;
+	}
+
+	public void setCenUrlOntv(String cenUrlOntv) {
+		this.cenUrlOntv = cenUrlOntv;
+	}
+
+	public String getCharacteristic() {
+		return characteristic;
+	}
+
+	public void setCharacteristic(String characteristic) {
+		this.characteristic = characteristic;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getDbngUrl() {
+		return dbngUrl;
+	}
+
+	public void setDbngUrl(String dbngUrl) {
+		this.dbngUrl = dbngUrl;
+	}
+
+	public String getDbnlUrl() {
+		return dbnlUrl;
+	}
+
+	public void setDbnlUrl(String dbnlUrl) {
+		this.dbnlUrl = dbnlUrl;
+	}
+
+	public String getDeath() {
+		return death;
+	}
+
+	public void setDeath(String death) {
+		this.death = death;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	public String getDomains() {
+		return domains;
+	}
+
+	public void setDomains(String domains) {
+		this.domains = domains;
+	}
+
+	public String getEducation() {
+		return education;
+	}
+
+	public void setEducation(String education) {
+		this.education = education;
+	}
+
+	public String getFamilyname() {
+		return familyname;
+	}
+
+	public void setFamilyname(String familyname) {
+		this.familyname = familyname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getIntraposition() {
+		return intraposition;
+	}
+
+	public void setIntraposition(String intraposition) {
+		this.intraposition = intraposition;
+	}
+
+	public String getKoppelnaam() {
+		return koppelnaam;
+	}
+
+	public void setKoppelnaam(String koppelnaam) {
+		this.koppelnaam = koppelnaam;
+	}
+
+	public String getKoppelname() {
+		return koppelname;
+	}
+
+	public void setKoppelname(String koppelname) {
+		this.koppelname = koppelname;
+	}
+
+	public String getLevensbeschouwing() {
+		return levensbeschouwing;
+	}
+
+	public void setLevensbeschouwing(String levensbeschouwing) {
+		this.levensbeschouwing = levensbeschouwing;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getLiteratuur() {
+		return literatuur;
+	}
+
+	public void setLiteratuur(String literatuur) {
+		this.literatuur = literatuur;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getNametype() {
+		return nametype;
+	}
+
+	public void setNametype(String nametype) {
+		this.nametype = nametype;
+	}
+
+	public String getNetwork() {
+		return network;
+	}
+
+	public void setNetwork(String network) {
+		this.network = network;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public String getOpmPolitics() {
+		return opmPolitics;
+	}
+
+	public void setOpmPolitics(String opmPolitics) {
+		this.opmPolitics = opmPolitics;
+	}
+
+	public String getPersname() {
+		return persname;
+	}
+
+	public void setPersname(String persname) {
+		this.persname = persname;
+	}
+
+	public String getPersoon() {
+		return persoon;
+	}
+
+	public void setPersoon(String persoon) {
+		this.persoon = persoon;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getPolitics() {
+		return politics;
+	}
+
+	public void setPolitics(String politics) {
+		this.politics = politics;
+	}
+
+	public String getPostposition() {
+		return postposition;
+	}
+
+	public void setPostposition(String postposition) {
+		this.postposition = postposition;
+	}
+
+	public String getPreposition() {
+		return preposition;
+	}
+
+	public void setPreposition(String preposition) {
+		this.preposition = preposition;
+	}
+
+	public String getRelatie() {
+		return relatie;
+	}
+
+	public void setRelatie(String relatie) {
+		this.relatie = relatie;
+	}
+
+	public String getRelatives() {
+		return relatives;
+	}
+
+	public void setRelatives(String relatives) {
+		this.relatives = relatives;
+	}
+
+	public String getReltype() {
+		return reltype;
+	}
+
+	public void setReltype(String reltype) {
+		this.reltype = reltype;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getSoort() {
+		return soort;
+	}
+
+	public void setSoort(String soort) {
+		this.soort = soort;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getVal() {
+		return val;
+	}
+
+	public void setVal(String val) {
+		this.val = val;
+	}
+
+	public String getValues() {
+		return values;
+	}
+
+	public void setValues(String values) {
+		this.values = values;
+	}
+
+	public String getWoonplaats() {
+		return woonplaats;
+	}
+
+	public void setWoonplaats(String woonplaats) {
+		this.woonplaats = woonplaats;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+
+	public String getAantekeningen() {
+		return aantekeningen;
+	}
+
+	public void setAantekeningen(String aantekeningen) {
+		this.aantekeningen = aantekeningen;
+	}
+
+	public String getNotities() {
+		return notities;
+	}
+
+	public void setNotities(String notities) {
+		this.notities = notities;
+	}
+
+	public String getOpmerkingen() {
+		return opmerkingen;
+	}
+
+	public void setOpmerkingen(String opmerkingen) {
+		this.opmerkingen = opmerkingen;
+	}
+
+	public String getVerwijzingen() {
+		return verwijzingen;
+	}
+
+	public void setVerwijzingen(String verwijzingen) {
+		this.verwijzingen = verwijzingen;
+	}
+
+}
---
+++ timbuctoo-model/src/main/java/nl/knaw/huygens/timbuctoo/model/cnw/CNWRelation.java
@@ -0,0 +1,7 @@
+package nl.knaw.huygens.timbuctoo.model.cnw;
+
+import nl.knaw.huygens.timbuctoo.model.Relation;
+
+public class CNWRelation extends Relation {
+
+}
