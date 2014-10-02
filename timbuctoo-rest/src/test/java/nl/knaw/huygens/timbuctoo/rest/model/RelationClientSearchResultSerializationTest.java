706e01a53e678ae1a595b6debf6cd149cbd8f1c8
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2014-10-02 14:50:23 +0200

    Simplified test code

--- timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/rest/model/RelationClientSearchResultSerializationTest.java
+++ timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/rest/model/RelationClientSearchResultSerializationTest.java
@@ -26,7 +26,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 
 import java.util.List;
-import java.util.Map;
 
 import nl.knaw.huygens.timbuctoo.model.ClientRelationRepresentation;
 import nl.knaw.huygens.timbuctoo.model.ClientSearchResult;
@@ -34,64 +33,32 @@ import nl.knaw.huygens.timbuctoo.model.RelationClientSearchResult;
 
 import org.junit.Test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.collect.Lists;
 
 public class RelationClientSearchResultSerializationTest extends ClientSearchResultTest {
-  String[] keysWhenEmpty = new String[] { "sortableFields", "numFound", "results", "ids", "start", "rows", "refs" };
-  String[] keysWhenFilled = new String[] { "sortableFields", "numFound", "results", "ids", "start", "rows", "refs", "_next", "_prev" };
 
   @Test
-  public void testWhenObjectHasAllEmptyProperties() throws JsonProcessingException {
-    // setup
-    ClientSearchResult searchResult = createEmptySearchResult();
-
-    // action
-    Map<String, Object> jsonMap = createJsonMap(searchResult);
-
-    // verify
-    assertThat(jsonMap.keySet(), contains(getKeysWhenEmpty()));
+  public void testWhenObjectHasAllEmptyProperties()  {
+    ClientSearchResult result = new RelationClientSearchResult();
+    assertThat(getKeySet(result), contains("sortableFields", "numFound", "results", "ids", "start", "rows", "refs"));
   }
 
   @Test
   public void testPropertiesWhenAllPropertiesContainAValue() {
-    // setup
-    ClientSearchResult searchResult = createFilledSearchResult();
-
-    // action
-    Map<String, Object> jsonMap = createJsonMap(searchResult);
-
-    // verify
-    assertThat(jsonMap.keySet(), contains(getKeysWhenFilled()));
+    ClientSearchResult result = createFilledSearchResult();
+    assertThat(getKeySet(result), contains("sortableFields", "numFound", "results", "ids", "start", "rows", "refs", "_next", "_prev"));
   }
 
-  @Override
-  protected ClientSearchResult createFilledSearchResult() {
+  private ClientSearchResult createFilledSearchResult() {
     RelationClientSearchResult searchResult = new RelationClientSearchResult();
     setClientRelationSearchResultProperties(searchResult);
     searchResult.setRefs(createRefs());
-
     return searchResult;
   }
 
   private List<ClientRelationRepresentation> createRefs() {
-    ClientRelationRepresentation ref = new ClientRelationRepresentation(STRING_PLACEHOLDER, STRING_PLACEHOLDER, STRING_PLACEHOLDER, STRING_PLACEHOLDER, STRING_PLACEHOLDER, STRING_PLACEHOLDER);
-
+    ClientRelationRepresentation ref = new ClientRelationRepresentation(ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING);
     return Lists.newArrayList(ref);
   }
 
-  @Override
-  protected String[] getKeysWhenFilled() {
-    return keysWhenFilled;
-  }
-
-  @Override
-  protected String[] getKeysWhenEmpty() {
-    return keysWhenEmpty;
-  }
-
-  @Override
-  protected ClientSearchResult createEmptySearchResult() {
-    return new RelationClientSearchResult();
-  }
 }
