7662ed82b0d3205c14a88da713632509b2625a9b
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-10-13 14:01:09 +0200

    [#1833] Added test to set the pid and retrieve the version

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/mongo/MongoStorageIntegrationTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/mongo/MongoStorageIntegrationTest.java
@@ -3,6 +3,7 @@ package nl.knaw.huygens.timbuctoo.storage.mongo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
@@ -14,6 +15,7 @@ import nl.knaw.huygens.timbuctoo.storage.EntityInducer;
 import nl.knaw.huygens.timbuctoo.storage.EntityReducer;
 import nl.knaw.huygens.timbuctoo.storage.StorageException;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,6 +33,8 @@ import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
 
 public class MongoStorageIntegrationTest {
+  private static final Class<ProjectADomainEntity> DOMAIN_TYPE = ProjectADomainEntity.class;
+  private static final Change DEFAULT_CHANGE = new Change();
   private static final MongodStarter starter = MongodStarter.getDefaultInstance();
   private MongodExecutable mongodExe;
   private MongodProcess mongod;
@@ -60,11 +64,42 @@ public class MongoStorageIntegrationTest {
 
   @Test
   public void writeDomainEntityShouldAddAnItemToTheDatabase() throws StorageException {
+    ProjectADomainEntity entity = createEntityWithGeneralTestDocValue("test");
+
+    // action
+    String id = instance.addDomainEntity(DOMAIN_TYPE, entity, DEFAULT_CHANGE);
+
+    // verify
+    ProjectADomainEntity foundItem = instance.getItem(DOMAIN_TYPE, id);
+    assertThat(foundItem, isNotNullProjectADomainEntity());
+    assertThat(foundItem.generalTestDocValue, is(equalTo(entity.generalTestDocValue)));
+  }
+
+  @Test
+  public void setPIDFillsThePIDpropertyAndAddsTheVersionToVersionCollection() throws StorageException {
+    // setup
+    ProjectADomainEntity entity = createEntityWithGeneralTestDocValue("test");
+    String id = instance.addDomainEntity(DOMAIN_TYPE, entity, DEFAULT_CHANGE);
+
+    // action
+    String pid = "randomPID";
+    instance.setPID(DOMAIN_TYPE, id, pid);
+
+    // verify
+    ProjectADomainEntity foundItem = instance.getItem(DOMAIN_TYPE, id);
+    assertThat(foundItem, isNotNullProjectADomainEntity());
+    assertThat(foundItem.getPid(), is(notNullValue(String.class)));
+
+    assertThat(instance.getRevision(ProjectADomainEntity.class, id, 1), isNotNullProjectADomainEntity());
+  }
+
+  private Matcher<ProjectADomainEntity> isNotNullProjectADomainEntity() {
+    return is(notNullValue(ProjectADomainEntity.class));
+  }
+
+  private ProjectADomainEntity createEntityWithGeneralTestDocValue(String generalTestDocValue) {
     ProjectADomainEntity entity = new ProjectADomainEntity();
-    entity.generalTestDocValue = "test";
-
-    String id = instance.addDomainEntity(ProjectADomainEntity.class, entity, new Change());
-
-    assertThat(entity.generalTestDocValue, is(equalTo(instance.getItem(ProjectADomainEntity.class, id).generalTestDocValue)));
+    entity.generalTestDocValue = generalTestDocValue;
+    return entity;
   }
 }
