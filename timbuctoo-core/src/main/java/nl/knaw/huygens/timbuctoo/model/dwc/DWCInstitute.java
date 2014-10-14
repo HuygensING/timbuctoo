3f5a417903221e17c7cf632aa3bc4d001d7fe11e
Meindert Kroese <Meindert.Kroese@huygens.knaw.nl>
2014-10-14 14:29:11 +0200

    DWC model

---
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/model/dwc/DWCInstitute.java
@@ -0,0 +1,8 @@
+package nl.knaw.huygens.timbuctoo.model.dwc;
+
+import nl.knaw.huygens.timbuctoo.model.Collective;
+
+
+public class DWCInstitute extends Collective {
+
+}
---
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/model/dwc/DWCPlace.java
@@ -0,0 +1,34 @@
+package nl.knaw.huygens.timbuctoo.model.dwc;
+
+import nl.knaw.huygens.timbuctoo.model.Location;
+import nl.knaw.huygens.timbuctoo.model.util.PlaceName;
+
+public class DWCPlace extends Location {
+
+  private String country;
+  private String remarks;
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public void setName(String name) {
+    PlaceName pn = new PlaceName();
+    pn.setCountry(this.country);
+    pn.setSettlement(name);
+    addName("dutch", pn);
+  }
+
+  public String getRemarks() {
+    return remarks;
+  }
+
+  public void setRemarks(String remarks) {
+    this.remarks = remarks;
+  }
+
+}
