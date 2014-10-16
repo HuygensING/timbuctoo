1286180a30fc3964366b3d549bd588ea41e4c7b1
Meindert Kroese <Meindert.Kroese@huygens.knaw.nl>
2014-10-16 10:41:22 +0200

    Refactor: rename DCWInstitue -> DWCCollective

--- timbuctoo-model/src/main/java/nl/knaw/huygens/timbuctoo/model/dwc/DWCCollective.java
+++ timbuctoo-model/src/main/java/nl/knaw/huygens/timbuctoo/model/dwc/DWCCollective.java
@@ -3,6 +3,6 @@ package nl.knaw.huygens.timbuctoo.model.dwc;
 import nl.knaw.huygens.timbuctoo.model.Collective;
 
 
-public class DWCInstitute extends Collective {
+public class DWCCollective extends Collective {
 
 }
