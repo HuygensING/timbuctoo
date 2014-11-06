b0c93fe23c393132c8622013ec875cec6e37cd61
Meindert Kroese <Meindert.Kroese@huygens.knaw.nl>
2014-11-06 13:21:07 +0100

    Added namevariants to dwccollective

--- timbuctoo-model/src/main/java/nl/knaw/huygens/timbuctoo/model/dwc/DWCCollective.java
+++ timbuctoo-model/src/main/java/nl/knaw/huygens/timbuctoo/model/dwc/DWCCollective.java
@@ -1,8 +1,20 @@
 package nl.knaw.huygens.timbuctoo.model.dwc;
 
+import java.util.List;
+
 import nl.knaw.huygens.timbuctoo.model.Collective;
 
 
 public class DWCCollective extends Collective {
+
+  private List<String> nameVariants;
+
+  public List<String> getNameVariants() {
+    return nameVariants;
+  }
+
+  public void setNameVariants(List<String> nameVariants) {
+    this.nameVariants = nameVariants;
+  }
 
 }
