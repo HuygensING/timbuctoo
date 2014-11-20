54264f1c81ebb244d13e4af2a458582f61c1f91f
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-11-20 11:45:31 +0100

    Use different VRE url depending on the OAI URL

--- timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/oaipmh/neww/WWOAIPMHTool.java
+++ timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/oaipmh/neww/WWOAIPMHTool.java
@@ -9,34 +9,40 @@ import nl.knaw.huygens.timbuctoo.model.neww.WWPerson;
 import nl.knaw.huygens.timbuctoo.storage.StorageIterator;
 import nl.knaw.huygens.timbuctoo.tools.oaipmh.OAIRecordCreator;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.inject.Injector;
 
 public class WWOAIPMHTool {
+  private static final String VRE_ID = "WomenWriters";
   private final static Logger LOG = LoggerFactory.getLogger(WWOAIPMHTool.class);
   private static String oaiUrl;
 
   public static void main(String args[]) throws Exception {
-    String frontEndURL = "http://www.example.com";
-    String vreId = "WomenWriters";
     Injector injector = createInjectorWithoutSolr();
 
     Configuration config = injector.getInstance(Configuration.class);
     oaiUrl = config.getSetting("oai-url");
+    String frontEndURL = isProduction(oaiUrl) ? "http://resources.huygens.knaw.nl/womenwriters" : "http://www.example.com";
 
     OAIRecordCreator oaiRecordCreator = injector.getInstance(OAIRecordCreator.class);
     Repository repo = injector.getInstance(Repository.class);
 
     try {
-      createOAIRecordForThreeEntities(WWDocument.class, frontEndURL, vreId, oaiRecordCreator, repo);
+      createOAIRecordForThreeEntities(WWDocument.class, frontEndURL, VRE_ID, oaiRecordCreator, repo);
       //createOAIRecordForAllEntities(WWDocument.class, frontEndURL, vreId, oaiRecordCreator, repo);
-      createOAIRecordForThreeEntities(WWPerson.class, frontEndURL, vreId, oaiRecordCreator, repo);
+      createOAIRecordForThreeEntities(WWPerson.class, frontEndURL, VRE_ID, oaiRecordCreator, repo);
       //createOAIRecordForAllEntities(WWPerson.class, frontEndURL, vreId, oaiRecordCreator, repo);
     } finally {
       repo.close();
     }
+  }
+
+  private static boolean isProduction(String oaiUrl) {
+
+    return StringUtils.contains(oaiUrl, "localhost:9998");
   }
 
   private static <T extends DomainEntity> void createOAIRecordForThreeEntities(Class<T> type, String frontEndURL, String vreId, OAIRecordCreator oaiRecordCreator, Repository repo) {
