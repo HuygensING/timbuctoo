d7800adf6a1ea1902884512a31d3ef3ec981ff66
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-02-18 16:59:22 +0100

    [TIM-1] Cases for the IdGenerator test thought of no solution yet

---
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/neo4j/IdGeneratorTest.java
@@ -0,0 +1,25 @@
+package nl.knaw.huygens.timbuctoo.storage.neo4j;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore("Think of the implementation")
+public class IdGeneratorTest {
+  @Test
+  public void nextIdForCreatesAnIdWithAPrefixOfTheEntity() {
+    fail("Yet to be implemented");
+  }
+
+  @Test
+  public void nextIdForCreatesAnIdWith12NumbersAfterThePrefix() {
+    fail("Yet to be implemented");
+  }
+
+  @Test
+  public void nextIdForCreatesANewIdEachTimeItIsCalled() {
+    fail("Yet to be implemented");
+  }
+
+}
