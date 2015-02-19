2b656459e6e219d8109338b63d9fc3a2fba67abc
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-02-19 13:57:04 +0100

    Simple implementation of IdGenerator, does not support legacy ids yet.

--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/neo4j/IdGenerator.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/neo4j/IdGenerator.java
@@ -1,12 +1,21 @@
 package nl.knaw.huygens.timbuctoo.storage.neo4j;
 
+import java.util.UUID;
+
+import nl.knaw.huygens.timbuctoo.annotations.IDPrefix;
 import nl.knaw.huygens.timbuctoo.model.Entity;
 
 public class IdGenerator {
 
+  // TODO find a solution for support legacy id's 
   public String nextIdFor(Class<? extends Entity> type) {
-    // TODO Auto-generated method stub
-    return null;
+    return String.format("%s%s", getIdPrefix(type), UUID.randomUUID());
+  }
+
+  private String getIdPrefix(Class<? extends Entity> type) {
+    IDPrefix idPrefix = type.getAnnotation(IDPrefix.class);
+
+    return idPrefix != null ? idPrefix.value() : "UNKN";
   }
 
 }
