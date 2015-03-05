f09d8257dec3a84f7f441d47f7e8c1d9c8ad9c24
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-03-05 09:40:36 +0100

    [TIM-55] Added test if one of the FieldConverters throws an exception.

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/neo4j/RelationConverterTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/neo4j/RelationConverterTest.java
@@ -1,14 +1,16 @@
 package nl.knaw.huygens.timbuctoo.storage.neo4j;
 
 import static nl.knaw.huygens.timbuctoo.storage.neo4j.FieldConverterMockBuilder.newFieldConverter;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import nl.knaw.huygens.timbuctoo.model.Relation;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.neo4j.graphdb.Relationship;
 
@@ -16,36 +18,60 @@ import com.google.common.collect.Lists;
 
 public class RelationConverterTest {
 
-  @Test
-  public void addValuesToPropertyContainerCallsAllTheFieldConvertersThatAreNotOnTheIgnoreList() throws ConversionException {
-    // setup
-    String fieldToIgnore1 = "fieldToIgnore1";
-    String fieldToIgnore2 = "fieldToIgnore2";
-    List<String> fieldsToIgnore = Lists.newArrayList(fieldToIgnore1, fieldToIgnore2);
-    FieldConverter fieldConverterMockToIgnore1 = newFieldConverter().withName(fieldToIgnore1).build();
-    FieldConverter fieldConverterMockToIgnore2 = newFieldConverter().withName(fieldToIgnore2).build();
-    String name = "someName";
-    FieldConverter someFieldConverterMock = newFieldConverter().withName(name).withType(FieldType.ADMINISTRATIVE).build();
-    String otherName = "someOtherName";
-    FieldConverter someOtherFieldConverterMock = newFieldConverter().withName(otherName).withType(FieldType.REGULAR).build();
+  private static final String OTHER_FIELD_CONVERTER = "otherFieldConverter";
+  private static final String FIELD_CONVERTER = "fieldConverter";
+  private static final String FIELD_TO_IGNORE2 = "fieldToIgnore2";
+  private static final String FIELD_TO_IGNORE1 = "fieldToIgnore1";
+  private static final ArrayList<String> FIELD_TO_IGNORE = Lists.newArrayList(FIELD_TO_IGNORE1, FIELD_TO_IGNORE2);
+  private FieldConverter fieldConverterMockToIgnore1;
+  private FieldConverter fieldConverterMockToIgnore2;
+  private FieldConverter fieldConverterMock;
+  private FieldConverter someOtherFieldConverterMock;
+  private Relationship relationshipMock;
+  private Relation relation;
+  private RelationConverter<Relation, Relationship> instance;
 
-    RelationConverter<Relation, Relationship> instance = new RelationConverter<Relation, Relationship>(fieldsToIgnore);
+  @Before
+  public void setUp() {
+    fieldConverterMockToIgnore1 = newFieldConverter().withName(FIELD_TO_IGNORE1).build();
+    fieldConverterMockToIgnore2 = newFieldConverter().withName(FIELD_TO_IGNORE2).build();
+    fieldConverterMock = newFieldConverter().withName(FIELD_CONVERTER).withType(FieldType.ADMINISTRATIVE).build();
+    someOtherFieldConverterMock = newFieldConverter().withName(OTHER_FIELD_CONVERTER).withType(FieldType.REGULAR).build();
+
+    instance = new RelationConverter<Relation, Relationship>(FIELD_TO_IGNORE);
     instance.addFieldConverter(someOtherFieldConverterMock);
-    instance.addFieldConverter(someFieldConverterMock);
+    instance.addFieldConverter(fieldConverterMock);
     instance.addFieldConverter(fieldConverterMockToIgnore2);
     instance.addFieldConverter(fieldConverterMockToIgnore1);
 
-    Relationship relationshipMock = mock(Relationship.class);
-    Relation relation = new Relation();
+    relationshipMock = mock(Relationship.class);
+    relation = new Relation();
+  }
 
+  @Test
+  public void addValuesToPropertyContainerCallsAllTheFieldConvertersThatAreNotOnTheIgnoreList() throws Exception {
     // action
     instance.addValuesToPropertyContainer(relationshipMock, relation);
 
     // verify
-    verify(someFieldConverterMock).setPropertyContainerProperty(relationshipMock, relation);
+    verify(fieldConverterMock).setPropertyContainerProperty(relationshipMock, relation);
     verify(someOtherFieldConverterMock).setPropertyContainerProperty(relationshipMock, relation);
     verify(fieldConverterMockToIgnore1, never()).setPropertyContainerProperty(relationshipMock, relation);
     verify(fieldConverterMockToIgnore2, never()).setPropertyContainerProperty(relationshipMock, relation);
+  }
+
+  @Test(expected = ConversionException.class)
+  public void addValuesToPropertyContainerThrowsAConversionExceptionIfOneOfTheFieldMappersDoes() throws Exception {
+    // setup
+    doThrow(ConversionException.class).when(fieldConverterMock).setPropertyContainerProperty(relationshipMock, relation);
+
+    try {
+      // action
+      instance.addValuesToPropertyContainer(relationshipMock, relation);
+    } finally {
+      // verify
+      verify(fieldConverterMock).setPropertyContainerProperty(relationshipMock, relation);
+    }
   }
 
 }
