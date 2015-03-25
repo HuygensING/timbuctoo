2ea886fdcc3916d1d9acd53ca9ee853ff2e6d0d2
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-03-25 11:14:09 +0100

    Code clean-up

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/neo4j/Neo4JLegacyStorageWrapperTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/neo4j/Neo4JLegacyStorageWrapperTest.java
@@ -1,16 +1,27 @@
 package nl.knaw.huygens.timbuctoo.storage.neo4j;
 
+import static nl.knaw.huygens.timbuctoo.storage.neo4j.SubADomainEntityBuilder.aDomainEntity;
+import static nl.knaw.huygens.timbuctoo.storage.neo4j.SubARelationBuilder.aRelation;
+import static nl.knaw.huygens.timbuctoo.storage.neo4j.TestSystemEntityWrapperBuilder.aSystemEntity;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import nl.knaw.huygens.timbuctoo.config.TypeNames;
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.model.Entity;
 import nl.knaw.huygens.timbuctoo.model.util.Change;
+import nl.knaw.huygens.timbuctoo.storage.StorageException;
 import nl.knaw.huygens.timbuctoo.storage.neo4j.conversion.PropertyContainerConverterFactory;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InOrder;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
@@ -23,8 +34,11 @@ import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
 
 import test.model.BaseDomainEntity;
+import test.model.TestSystemEntityWrapper;
+import test.model.projecta.SubADomainEntity;
+import test.model.projecta.SubARelation;
 
-public abstract class Neo4JLegacyStorageWrapperTest {
+public class Neo4JLegacyStorageWrapperTest {
 
   protected static final Class<BaseDomainEntity> PRIMITIVE_DOMAIN_ENTITY_TYPE = BaseDomainEntity.class;
   protected static final String PRIMITIVE_DOMAIN_ENTITY_NAME = TypeNames.getInternalName(PRIMITIVE_DOMAIN_ENTITY_TYPE);
@@ -36,6 +50,9 @@ public abstract class Neo4JLegacyStorageWrapperTest {
   protected static final String ID = "id";
   protected static final Change CHANGE = new Change();
   protected static final String PID = "pid";
+  private static final Class<SubADomainEntity> DOMAIN_ENTITY_TYPE = SubADomainEntity.class;
+  private static final Class<SubARelation> RELATION_TYPE = SubARelation.class;
+  private static final Class<TestSystemEntityWrapper> SYSTEM_ENTITY_TYPE = TestSystemEntityWrapper.class;
 
   protected GraphDatabaseService dbMock;
   protected PropertyContainerConverterFactory propertyContainerConverterFactoryMock;
@@ -89,6 +106,297 @@ public abstract class Neo4JLegacyStorageWrapperTest {
 
   protected Answer<Object> setPIDOfEntity() {
     return new SetPIDAnswer();
+  }
+
+  @Test
+  public void addDomainEntityDelegatesToNeo4JStorageAddDomainEntity() throws Exception {
+    // setup
+    SubADomainEntity entity = aDomainEntity().build();
+    when(neo4JStorageMock.addDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE)).thenReturn(ID);
+
+    // action
+    String id = instance.addDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE);
+
+    // verify
+    assertThat(id, is(equalTo(ID)));
+    verify(neo4JStorageMock).addDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE);
+  }
+
+  @Test(expected = StorageException.class)
+  public void addDomainEntityThrowsAnExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    SubADomainEntity entity = aDomainEntity().build();
+    when(neo4JStorageMock.addDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE)).thenThrow(new StorageException());
+
+    // action
+    instance.addDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE);
+  }
+
+  @Test
+  public void getEntityForDomainEntityDelegatesToNeo4JStorageGetEntity() throws Exception {
+    // setup
+    SubADomainEntity entity = aDomainEntity().build();
+    when(neo4JStorageMock.getEntity(DOMAIN_ENTITY_TYPE, ID)).thenReturn(entity);
+
+    // action
+    SubADomainEntity actualEntity = instance.getEntity(DOMAIN_ENTITY_TYPE, ID);
+
+    // verify
+    assertThat(actualEntity, is(sameInstance(entity)));
+  }
+
+  @Test(expected = StorageException.class)
+  public void getEntityForDomainEntityThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    when(neo4JStorageMock.getEntity(DOMAIN_ENTITY_TYPE, ID)).thenThrow(new StorageException());
+
+    // action
+    instance.getEntity(DOMAIN_ENTITY_TYPE, ID);
+  }
+
+  @Test
+  public void updateDomainEntityDelegatesToNeo4JStorage() throws Exception {
+    // setup
+    SubADomainEntity entity = aDomainEntity().build();
+
+    // action
+    instance.updateDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE);
+
+    // verify
+    verify(neo4JStorageMock).updateDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE);
+  }
+
+  @Test(expected = StorageException.class)
+  public void updateDomainEntityThrowAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    SubADomainEntity entity = aDomainEntity().build();
+
+    doThrow(StorageException.class).when(neo4JStorageMock).updateDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE);
+
+    // action
+    instance.updateDomainEntity(DOMAIN_ENTITY_TYPE, entity, CHANGE);
+
+  }
+
+  @Test
+  public void deleteDomainEntityIsDelegatedToNeo4JStorage() throws Exception {
+    // action
+    instance.deleteDomainEntity(DOMAIN_ENTITY_TYPE, ID, CHANGE);
+
+    // verify
+    verify(neo4JStorageMock).deleteDomainEntity(DOMAIN_ENTITY_TYPE, ID, CHANGE);
+  }
+
+  @Test(expected = StorageException.class)
+  public void deleteDomainEntityThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    doThrow(StorageException.class).when(neo4JStorageMock).deleteDomainEntity(DOMAIN_ENTITY_TYPE, ID, CHANGE);
+
+    // action
+    instance.deleteDomainEntity(DOMAIN_ENTITY_TYPE, ID, CHANGE);
+  }
+
+  @Test
+  public void setPIDDelegatesToNeo4JStorageSetDomainEntityPID() throws Exception {
+    // action
+    instance.setPID(DOMAIN_ENTITY_TYPE, ID, PID);
+
+    // verify
+    verify(neo4JStorageMock).setDomainEntityPID(DOMAIN_ENTITY_TYPE, ID, PID);
+  }
+
+  @Test(expected = StorageException.class)
+  public void setPIDThrowsAStorageExceptionIfTheDelegateDoes() throws Exception {
+    // setup
+    doThrow(StorageException.class).when(neo4JStorageMock).setDomainEntityPID(DOMAIN_ENTITY_TYPE, ID, PID);
+
+    // action
+    instance.setPID(DOMAIN_ENTITY_TYPE, ID, PID);
+  }
+
+  @Test
+  public void addDomainEntityForRelationDelegatesToNeo4JStorageAddRelation() throws Exception {
+    // setup
+    SubARelation relation = aRelation().build();
+    when(neo4JStorageMock.addRelation(RELATION_TYPE, relation, CHANGE)).thenReturn(ID);
+
+    // action
+    String id = instance.addDomainEntity(RELATION_TYPE, relation, CHANGE);
+
+    // verify
+    assertThat(id, is(equalTo(ID)));
+    verify(neo4JStorageMock).addRelation(RELATION_TYPE, relation, CHANGE);
+  }
+
+  @Test(expected = StorageException.class)
+  public void addDomainEntityForRelationThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    SubARelation relation = aRelation().build();
+    when(neo4JStorageMock.addRelation(RELATION_TYPE, relation, CHANGE)).thenThrow(new StorageException());
+
+    // action
+    instance.addDomainEntity(RELATION_TYPE, relation, CHANGE);
+  }
+
+  @Test
+  public void getEntityForRelationDelegatesToNeo4JStorageGetRelation() throws Exception {
+    // setup
+    SubARelation relation = aRelation().build();
+    when(neo4JStorageMock.getRelation(RELATION_TYPE, ID)).thenReturn(relation);
+
+    // action
+    SubARelation actualRelation = instance.getEntity(RELATION_TYPE, ID);
+
+    // verify
+    assertThat(actualRelation, is(sameInstance(relation)));
+  }
+
+  @Test
+  public void updateDomainEntityForRelationDelegatesToNeo4JStorageAddRelation() throws Exception {
+    // setup
+    SubARelation entity = aRelation().build();
+
+    // action
+    instance.updateDomainEntity(RELATION_TYPE, entity, CHANGE);
+
+    // verify
+    verify(neo4JStorageMock).updateRelation(RELATION_TYPE, entity, CHANGE);
+  }
+
+  @Test(expected = StorageException.class)
+  public void updateDomainEntityForRelationThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    SubARelation entity = aRelation().build();
+
+    doThrow(StorageException.class).when(neo4JStorageMock).updateRelation(RELATION_TYPE, entity, CHANGE);
+
+    // action
+    instance.updateDomainEntity(RELATION_TYPE, entity, CHANGE);
+  }
+
+  @Test
+  public void setPIDForRelationDelegatesToNeo4JStorageSetRelationPID() throws Exception {
+    // action
+    instance.setPID(RELATION_TYPE, ID, PID);
+
+    // verify
+    verify(neo4JStorageMock).setRelationPID(RELATION_TYPE, ID, PID);
+  }
+
+  @Test(expected = StorageException.class)
+  public void setPIDForRelationThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    doThrow(StorageException.class).when(neo4JStorageMock).setRelationPID(RELATION_TYPE, ID, PID);
+
+    // action
+    instance.setPID(RELATION_TYPE, ID, PID);
+  }
+
+  @Test
+  public void getRevisionForRelationDelegatesTheCallToNeo4JStorageGetRelationRevision() throws Exception {
+    // setup
+    when(neo4JStorageMock.getRelationRevision(RELATION_TYPE, ID, FIRST_REVISION)).thenReturn(aRelation().build());
+
+    // action
+    SubARelation relation = instance.getRevision(RELATION_TYPE, ID, FIRST_REVISION);
+
+    // verify
+    assertThat(relation, is(notNullValue()));
+
+    verify(neo4JStorageMock).getRelationRevision(RELATION_TYPE, ID, FIRST_REVISION);
+  }
+
+  @Test(expected = StorageException.class)
+  public void getRevisionThrowsAStorageExceptionWhenNeo4JStorageGetRelationRevisionDoes() throws Exception {
+    // setup
+    when(neo4JStorageMock.getRelationRevision(RELATION_TYPE, ID, FIRST_REVISION)).thenThrow(new StorageException());
+
+    // action
+    instance.getRevision(RELATION_TYPE, ID, FIRST_REVISION);
+  }
+
+  @Test
+  public void addSystemEntityDelegatesToNeo4JStorage() throws Exception {
+    // setup
+    TestSystemEntityWrapper entity = aSystemEntity().build();
+    when(neo4JStorageMock.addSystemEntity(SYSTEM_ENTITY_TYPE, entity)).thenReturn(ID);
+
+    // action
+    String actualId = instance.addSystemEntity(SYSTEM_ENTITY_TYPE, entity);
+
+    // verify
+    assertThat(actualId, is(equalTo(ID)));
+    verify(neo4JStorageMock).addSystemEntity(SYSTEM_ENTITY_TYPE, entity);
+  }
+
+  @Test(expected = StorageException.class)
+  public void addSystemEntityThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    TestSystemEntityWrapper entity = aSystemEntity().build();
+    when(neo4JStorageMock.addSystemEntity(SYSTEM_ENTITY_TYPE, entity)).thenThrow(new StorageException());
+
+    // action
+    instance.addSystemEntity(SYSTEM_ENTITY_TYPE, entity);
+  }
+
+  @Test
+  public void getEntityForSystemEntityDelegatesToNeo4JStorageGetEntity() throws Exception {
+    // setup
+    TestSystemEntityWrapper entity = aSystemEntity().build();
+    when(neo4JStorageMock.getEntity(SYSTEM_ENTITY_TYPE, ID)).thenReturn(entity);
+
+    // action
+    TestSystemEntityWrapper actualEntity = instance.getEntity(SYSTEM_ENTITY_TYPE, ID);
+
+    // verify
+    assertThat(actualEntity, is(sameInstance(entity)));
+  }
+
+  @Test(expected = StorageException.class)
+  public void getEntityForSystemEntityThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    when(neo4JStorageMock.getEntity(SYSTEM_ENTITY_TYPE, ID)).thenThrow(new StorageException());
+
+    // action
+    instance.getEntity(SYSTEM_ENTITY_TYPE, ID);
+  }
+
+  @Test
+  public void updateSystemEntityDelegatesToNeo4JStorage() throws Exception {
+    // setup
+    TestSystemEntityWrapper entity = aSystemEntity().build();
+
+    // action
+    instance.updateSystemEntity(SYSTEM_ENTITY_TYPE, entity);
+
+    // verify
+    verify(neo4JStorageMock).updateSystemEntity(SYSTEM_ENTITY_TYPE, entity);
+  }
+
+  @Test(expected = StorageException.class)
+  public void updateSystemEntityThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    TestSystemEntityWrapper entity = aSystemEntity().build();
+    doThrow(StorageException.class).when(neo4JStorageMock).updateSystemEntity(SYSTEM_ENTITY_TYPE, entity);
+
+    // action
+    instance.updateSystemEntity(SYSTEM_ENTITY_TYPE, entity);
+  }
+
+  @Test
+  public void deleteSystemEntityDelegatesToNeo4JStorage() throws Exception {
+    // action
+    instance.deleteSystemEntity(SYSTEM_ENTITY_TYPE, ID);
+
+    // verify
+    verify(neo4JStorageMock).deleteSystemEntity(SYSTEM_ENTITY_TYPE, ID);
+  }
+
+  @Test(expected = StorageException.class)
+  public void deleteSystemEntityThrowsAStorageExceptionWhenTheDelegateDoes() throws Exception {
+    // setup
+    when(neo4JStorageMock.deleteSystemEntity(SYSTEM_ENTITY_TYPE, ID)).thenThrow(new StorageException());
+    // action
+    instance.deleteSystemEntity(SYSTEM_ENTITY_TYPE, ID);
   }
 
   private final class SetPIDAnswer implements Answer<Object> {
