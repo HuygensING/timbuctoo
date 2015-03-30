e6984c7041c261f968cc25db5c4b1a5e290bfdda
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-03-30 10:18:40 +0200

    [TIM-113] Moved common logic to an AbstractExtendablePropertyContainerConverter.

--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/neo4j/conversion/ExtendableNodeConverter.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/neo4j/conversion/ExtendableNodeConverter.java
@@ -1,109 +1,31 @@
 package nl.knaw.huygens.timbuctoo.storage.neo4j.conversion;
 
-import static nl.knaw.huygens.timbuctoo.model.Entity.MODIFIED_PROPERTY_NAME;
-import static nl.knaw.huygens.timbuctoo.model.Entity.REVISION_PROPERTY_NAME;
-import static nl.knaw.huygens.timbuctoo.storage.neo4j.conversion.FieldType.ADMINISTRATIVE;
-
-import java.util.Collection;
-import java.util.Map;
-
 import nl.knaw.huygens.timbuctoo.config.TypeNames;
 import nl.knaw.huygens.timbuctoo.model.Entity;
-import nl.knaw.huygens.timbuctoo.storage.neo4j.ConversionException;
 import nl.knaw.huygens.timbuctoo.storage.neo4j.EntityInstantiator;
 import nl.knaw.huygens.timbuctoo.storage.neo4j.NodeConverter;
 
 import org.neo4j.graphdb.DynamicLabel;
 import org.neo4j.graphdb.Node;
 
-import com.google.common.collect.Maps;
-
-class ExtendableNodeConverter<T extends Entity> implements NodeConverter<T>, ExtendablePropertyContainerConverter<Node, T> {
-
-  private final Class<T> type;
-  private final Map<String, PropertyConverter> fieldNamePropertyConverterMap;
-  private final EntityInstantiator entityInstantiator;
+class ExtendableNodeConverter<T extends Entity> extends AbstractExtendablePropertyContainerConverter<Node, T> implements NodeConverter<T>, ExtendablePropertyContainerConverter<Node, T> {
 
   public ExtendableNodeConverter(Class<T> type) {
     this(type, new EntityInstantiator());
   }
 
   public ExtendableNodeConverter(Class<T> type, EntityInstantiator entityInstantiator) {
-    this.type = type;
-    this.entityInstantiator = entityInstantiator;
-    fieldNamePropertyConverterMap = Maps.newHashMap();
-  }
-
-  @Override
-  public void addValuesToPropertyContainer(Node node, T entity) throws ConversionException {
-    addName(node);
-    for (PropertyConverter propertyConverter : getPropertyConverters()) {
-      propertyConverter.setPropertyContainerProperty(node, entity);
-    }
-  }
-
-  private Collection<PropertyConverter> getPropertyConverters() {
-    return fieldNamePropertyConverterMap.values();
-  }
-
-  private void addName(Node node) {
-    node.addLabel(DynamicLabel.label(TypeNames.getInternalName(type)));
-  }
-
-  @Override
-  public void addValuesToEntity(T entity, Node node) throws ConversionException {
-    for (PropertyConverter propertyConverter : getPropertyConverters()) {
-      propertyConverter.addValueToEntity(entity, node);
-    }
-  }
-
-  @Override
-  public void addPropertyConverter(PropertyConverter fieldWrapper) {
-    mapToFieldName(fieldWrapper);
-  }
-
-  private void mapToFieldName(PropertyConverter propertyConverter) {
-    fieldNamePropertyConverterMap.put(propertyConverter.getName(), propertyConverter);
-  }
-
-  @Override
-  public void updatePropertyContainer(Node node, Entity entity) throws ConversionException {
-    for (PropertyConverter propertyConverter : getPropertyConverters()) {
-      if (propertyConverter.getFieldType() != ADMINISTRATIVE) {
-        propertyConverter.setPropertyContainerProperty(node, entity);
-      }
-    }
+    super(type, entityInstantiator);
 
   }
 
   @Override
-  public void updateModifiedAndRev(Node node, Entity entity) throws ConversionException {
-    getPropertyConverterByFieldName(MODIFIED_PROPERTY_NAME).setPropertyContainerProperty(node, entity);
-    getPropertyConverterByFieldName(REVISION_PROPERTY_NAME).setPropertyContainerProperty(node, entity);
-  }
-
-  private PropertyConverter getPropertyConverterByFieldName(String fieldName) {
-    return fieldNamePropertyConverterMap.get(fieldName);
+  protected void executeCustomSerializationActions(Node propertyContainer, T entity) {
+    propertyContainer.addLabel(DynamicLabel.label(TypeNames.getInternalName(type)));
   }
 
   @Override
-  public T convertToEntity(Node propertyContainer) throws ConversionException, InstantiationException {
-    T entity = entityInstantiator.createInstanceOf(type);
-
-    addValuesToEntity(entity, propertyContainer);
-
-    return entity;
-  }
-
-  @Override
-  public String getPropertyName(String fieldName) {
-    if (!hasPropertyContainerForField(fieldName)) {
-      throw new FieldNonExistingException(type, fieldName);
-    }
-    return getPropertyConverterByFieldName(fieldName).getPropertyName();
-  }
-
-  private boolean hasPropertyContainerForField(String fieldName) {
-    return fieldNamePropertyConverterMap.containsKey(fieldName);
+  protected void executeCustomDeserializationActions(T entity, Node propertyContainer) {
+    // nothing to do
   }
 }
--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/neo4j/conversion/ExtendableRelationshipConverter.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/neo4j/conversion/ExtendableRelationshipConverter.java
@@ -1,16 +1,9 @@
 package nl.knaw.huygens.timbuctoo.storage.neo4j.conversion;
 
 import static nl.knaw.huygens.timbuctoo.model.Entity.ID_PROPERTY_NAME;
-import static nl.knaw.huygens.timbuctoo.model.Entity.MODIFIED_PROPERTY_NAME;
-import static nl.knaw.huygens.timbuctoo.model.Entity.REVISION_PROPERTY_NAME;
-
-import java.util.Collection;
-import java.util.Map;
-
 import nl.knaw.huygens.timbuctoo.config.TypeRegistry;
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.model.Relation;
-import nl.knaw.huygens.timbuctoo.storage.neo4j.ConversionException;
 import nl.knaw.huygens.timbuctoo.storage.neo4j.EntityInstantiator;
 import nl.knaw.huygens.timbuctoo.storage.neo4j.RelationshipConverter;
 
@@ -18,20 +11,14 @@ import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 
-import com.google.common.collect.Maps;
+class ExtendableRelationshipConverter<T extends Relation> extends AbstractExtendablePropertyContainerConverter<Relationship, T> implements RelationshipConverter<T>,
+    ExtendablePropertyContainerConverter<Relationship, T> {
 
-class ExtendableRelationshipConverter<T extends Relation> implements RelationshipConverter<T>, ExtendablePropertyContainerConverter<Relationship, T> {
-
-  private final Map<String, PropertyConverter> fieldNamePropertyConverterMap;
   private final TypeRegistry typeRegistry;
-  private final EntityInstantiator entityInstantiatorMock;
-  private final Class<T> type;
 
   public ExtendableRelationshipConverter(Class<T> type, TypeRegistry typeRegistry, EntityInstantiator entityInstantiatorMock) {
-    this.type = type;
+    super(type, entityInstantiatorMock);
     this.typeRegistry = typeRegistry;
-    this.entityInstantiatorMock = entityInstantiatorMock;
-    fieldNamePropertyConverterMap = Maps.newHashMap();
   }
 
   public ExtendableRelationshipConverter(Class<T> type, TypeRegistry typeRegistry) {
@@ -39,26 +26,18 @@ class ExtendableRelationshipConverter<T extends Relation> implements Relationshi
   }
 
   @Override
-  public void addValuesToPropertyContainer(Relationship relationship, T entity) throws ConversionException {
-    for (PropertyConverter propertyConverter : getPropertyConverters()) {
-      propertyConverter.setPropertyContainerProperty(relationship, entity);
-    }
-  }
+  protected void executeCustomSerializationActions(Relationship propertyContainer, T entity) {}
 
   @Override
-  public void addValuesToEntity(T entity, Relationship relationship) throws ConversionException {
-    for (PropertyConverter propertyConverter : getPropertyConverters()) {
-      propertyConverter.addValueToEntity(entity, relationship);
-    }
-
-    Node startNode = relationship.getStartNode();
+  protected void executeCustomDeserializationActions(T entity, Relationship propertyContainer) {
+    Node startNode = propertyContainer.getStartNode();
     if (startNode.hasProperty(ID_PROPERTY_NAME)) {
       entity.setSourceId((String) startNode.getProperty(ID_PROPERTY_NAME));
     }
 
     entity.setSourceType(getPrimitiveType(startNode));
 
-    Node endNode = relationship.getEndNode();
+    Node endNode = propertyContainer.getEndNode();
     if (endNode.hasProperty(ID_PROPERTY_NAME)) {
       entity.setTargetId((String) endNode.getProperty(ID_PROPERTY_NAME));
     }
@@ -76,59 +55,6 @@ class ExtendableRelationshipConverter<T extends Relation> implements Relationshi
     }
 
     throw new CorruptNodeException(node.getProperty(ID_PROPERTY_NAME));
-  }
-
-  @Override
-  public void addPropertyConverter(PropertyConverter fieldConverter) {
-    this.fieldNamePropertyConverterMap.put(fieldConverter.getName(), fieldConverter);
-  }
-
-  private Collection<PropertyConverter> getPropertyConverters() {
-    return fieldNamePropertyConverterMap.values();
-  }
-
-  @Override
-  public void updatePropertyContainer(Relationship relationship, T entity) throws ConversionException {
-    for (PropertyConverter propertyConverter : getPropertyConverters()) {
-      if (isRegularConverter(propertyConverter)) {
-        propertyConverter.setPropertyContainerProperty(relationship, entity);
-      }
-    }
-  }
-
-  private boolean isRegularConverter(PropertyConverter propertyConverter) {
-    return FieldType.REGULAR == propertyConverter.getFieldType();
-  }
-
-  @Override
-  public void updateModifiedAndRev(Relationship relationship, T entity) throws ConversionException {
-    getPropertyConverterByFieldName(REVISION_PROPERTY_NAME).setPropertyContainerProperty(relationship, entity);
-    getPropertyConverterByFieldName(MODIFIED_PROPERTY_NAME).setPropertyContainerProperty(relationship, entity);
-  }
-
-  private PropertyConverter getPropertyConverterByFieldName(String fieldName) {
-    return fieldNamePropertyConverterMap.get(fieldName);
-  }
-
-  @Override
-  public T convertToEntity(Relationship propertyContainer) throws ConversionException, InstantiationException {
-    T entity = entityInstantiatorMock.createInstanceOf(type);
-
-    addValuesToEntity(entity, propertyContainer);
-
-    return entity;
-  }
-
-  @Override
-  public String getPropertyName(String fieldName) {
-    if (!hasPropertyContainerForField(fieldName)) {
-      throw new FieldNonExistingException(type, fieldName);
-    }
-    return getPropertyConverterByFieldName(fieldName).getPropertyName();
-  }
-
-  private boolean hasPropertyContainerForField(String fieldName) {
-    return fieldNamePropertyConverterMap.containsKey(fieldName);
   }
 
 }
