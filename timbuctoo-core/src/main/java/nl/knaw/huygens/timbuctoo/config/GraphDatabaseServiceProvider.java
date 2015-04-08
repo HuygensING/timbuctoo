a889086f2fc7ca194ed7392661be8abe90792721
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-04-08 17:04:16 +0200

    [TIM-99] Fail fast for database connection added.

--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/config/GraphDatabaseServiceProvider.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/config/GraphDatabaseServiceProvider.java
@@ -11,14 +11,22 @@ import com.google.inject.Singleton;
 @Singleton
 public class GraphDatabaseServiceProvider implements Provider<GraphDatabaseService> {
 
+  private static final String PASSWORD = "test123";
+  private static final String USER = "neo4j";
+  private static final String URI = "http://localhost:7474";
+
   @Override
   public GraphDatabaseService get() {
-    RestAPI restApi = new RestAPIImpl("http://localhost:7474", "neo4j", "test123");
+    RestAPI restApi = new RestAPIImpl(URI, USER, PASSWORD);
     /* It is a wonder this works mapping a CypherRestGraphDatabase, that implements 
      * CypherRestGraphDatabase of Neo4J 2.1.7, to a CypherRestGraphDatabase of Neo4J 2.2.0.
      */
     CypherRestGraphDatabase graphDb = new CypherRestGraphDatabase(restApi);
+
+    if (graphDb.isAvailable(5000)) {
+      throw new RuntimeException("Database is not available on: " + URI);
+    }
+
     return graphDb;
   }
-
 }
