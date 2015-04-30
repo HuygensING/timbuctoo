8392c89ec162435c6512f20451d3ec88e647b72a
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-04-30 16:45:01 +0200

    [TIM-166] Implemented TinkerpopIterator.

--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/TinkerpopIterator.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/TinkerpopIterator.java
@@ -5,48 +5,76 @@ import java.util.List;
 
 import nl.knaw.huygens.timbuctoo.model.Entity;
 import nl.knaw.huygens.timbuctoo.storage.StorageIterator;
+import nl.knaw.huygens.timbuctoo.storage.graph.ConversionException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
 import com.tinkerpop.blueprints.Vertex;
 
 public class TinkerpopIterator<T extends Entity> implements StorageIterator<T> {
+  private static final Logger LOG = LoggerFactory.getLogger(TinkerpopIterator.class);
+  private Iterator<Vertex> delegate;
+  private VertexConverter<T> converter;
 
-  public TinkerpopIterator(VertexConverter<T> converter, Iterator<Vertex> iterator) {
-    // TODO Auto-generated constructor stub
+  public TinkerpopIterator(VertexConverter<T> converter, Iterator<Vertex> delegate) {
+    this.converter = converter;
+    this.delegate = delegate;
   }
 
   @Override
   public boolean hasNext() {
-    throw new UnsupportedOperationException("Yet to be implemented");
+    return delegate.hasNext();
   }
 
   @Override
   public T next() {
-    throw new UnsupportedOperationException("Yet to be implemented");
+    T item = null;
+    Vertex element = delegate.next();
+    try {
+      item = converter.convertToEntity(element);
+    } catch (ConversionException e) {
+      LOG.error("Element with \"{}\" cannot be converted.", ElementHelper.getIdProperty(element));
+      e.printStackTrace();
+    }
+    return item;
   }
 
   @Override
   public void remove() {
-    throw new UnsupportedOperationException("Yet to be implemented");
+    throw new UnsupportedOperationException("Method not supported");
   }
 
   @Override
   public StorageIterator<T> skip(int count) {
-    throw new UnsupportedOperationException("Yet to be implemented");
+    for (; count > 0 && delegate.hasNext(); count--) {
+      delegate.next();
+    }
+    return this;
   }
 
   @Override
   public List<T> getSome(int limit) {
-    throw new UnsupportedOperationException("Yet to be implemented");
+    List<T> some = Lists.newArrayList();
+    for (; limit > 0 && hasNext(); limit--) {
+      some.add(next());
+    }
+
+    return some;
   }
 
   @Override
   public List<T> getAll() {
-    throw new UnsupportedOperationException("Yet to be implemented");
+    List<T> all = Lists.newArrayList();
+    for (; hasNext();) {
+      all.add(next());
+    }
+
+    return all;
   }
 
   @Override
-  public void close() {
-    throw new UnsupportedOperationException("Yet to be implemented");
-  }
+  public void close() {}
 
 }
