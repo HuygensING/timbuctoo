da76383df55ac2d82c4c36b36bb9b6cf1352af31
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-05-01 10:13:17 +0200

    [TIM-166] Fixed CompositeMatcher, so it can be used for comparing collections.

--- timbuctoo-test-services/src/main/java/nl/knaw/huygens/hamcrest/CompositeMatcher.java
+++ timbuctoo-test-services/src/main/java/nl/knaw/huygens/hamcrest/CompositeMatcher.java
@@ -25,19 +25,18 @@ package nl.knaw.huygens.hamcrest;
 import java.util.List;
 
 import org.hamcrest.Description;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.internal.ReflectiveTypeFinder;
 
 import com.google.common.collect.Lists;
 
-public class CompositeMatcher<T> extends TypeSafeMatcher<T> {
+public class CompositeMatcher<T> extends TypeSafeDiagnosingMatcher<T> {
 
   private List<TypeSafeMatcher<T>> matchers;
-  private List<TypeSafeMatcher<T>> failed;
 
   public CompositeMatcher() {
     matchers = Lists.newArrayList();
-    failed = Lists.newArrayList();
   }
 
   public CompositeMatcher(ReflectiveTypeFinder typeFinder) {
@@ -49,42 +48,23 @@ public class CompositeMatcher<T> extends TypeSafeMatcher<T> {
     description.appendList("(", " " + "and" + " ", ")", getMatchers());
   }
 
-  @Override
-  protected void describeMismatchSafely(T item, Description mismatchDescription) {
-    for (final TypeSafeMatcher<T> matcher : getFailed()) {
-      matcher.describeMismatch(item, mismatchDescription);
-      mismatchDescription.appendText(" ");
-    }
-  }
-
-  @Override
-  protected boolean matchesSafely(T item) {
-    for (TypeSafeMatcher<T> matcher : getMatchers()) {
-      if (!matcher.matches(item)) {
-        addFailedMatcher(matcher);
-      }
-    }
-    return hasFailed();
-  }
-
   protected List<TypeSafeMatcher<T>> getMatchers() {
     return matchers;
-  }
-
-  protected List<TypeSafeMatcher<T>> getFailed() {
-    return failed;
-  }
-
-  protected boolean hasFailed() {
-    return getFailed().isEmpty();
-  }
-
-  protected boolean addFailedMatcher(TypeSafeMatcher<T> matcher) {
-    return getFailed().add(matcher);
   }
 
   protected void addMatcher(TypeSafeMatcher<T> propertyMatcher) {
     getMatchers().add(propertyMatcher);
   }
 
+  @Override
+  protected boolean matchesSafely(T item, Description mismatchDescription) {
+    boolean matched = true;
+    for (TypeSafeMatcher<T> matcher : getMatchers()) {
+      if (!matcher.matches(item)) {
+        matcher.describeMismatch(item, mismatchDescription);
+        matched = false;
+      }
+    }
+    return matched;
+  }
 }
