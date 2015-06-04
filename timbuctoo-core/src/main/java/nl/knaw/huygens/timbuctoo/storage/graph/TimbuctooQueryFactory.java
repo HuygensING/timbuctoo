207b0c16a4fa3f378074adc594232964284395d7
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-06-04 09:25:32 +0200

    Code clean-up

--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/graph/TimbuctooQueryFactory.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/graph/TimbuctooQueryFactory.java
@@ -2,16 +2,7 @@ package nl.knaw.huygens.timbuctoo.storage.graph;
 
 import nl.knaw.huygens.timbuctoo.model.Entity;
 
-import com.google.inject.Inject;
-
 public class TimbuctooQueryFactory {
-
-  private PropertyBusinessRules businessRules;
-
-  @Inject
-  public TimbuctooQueryFactory() {
-    this.businessRules = new PropertyBusinessRules();
-  }
 
   public TimbuctooQuery newQuery(Class<? extends Entity> type) {
     return new TimbuctooQuery();
