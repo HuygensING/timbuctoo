b28f82910e6598e48715a2e9330e074af612d0c0
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-06-04 16:10:08 +0200

    [TIM-213] Enabled the getAllRevisions for entities test and re-enabled the getEntities properties test.

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/TinkerPopLegacyStorageWrapperIntegrationTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/TinkerPopLegacyStorageWrapperIntegrationTest.java
@@ -296,7 +296,6 @@ public class TinkerPopLegacyStorageWrapperIntegrationTest extends StorageIntegra
     super.deleteByModifiedRemovesTheSystemEntityThatAreLastModifiedBeforeACertainDate();
   }
 
-  @Ignore
   @Test
   @Override
   public void getEntitiesByPropertyReturnsAStorageIteratorWithEntitesWithTheGivenPropertyAndValue() throws Exception {
@@ -310,7 +309,6 @@ public class TinkerPopLegacyStorageWrapperIntegrationTest extends StorageIntegra
     super.getAllRevisionsForRelationReturnsAllTheRevisionsOfARelation();
   }
 
-  @Ignore
   @Test
   @Override
   public void getAllRevisionsReturnsAllTheRevisionsOfADomainEntity() throws StorageException {
