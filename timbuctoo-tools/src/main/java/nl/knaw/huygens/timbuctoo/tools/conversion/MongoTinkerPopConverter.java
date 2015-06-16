37a2b7678368312c3199d10691c7ed1ddfd1bf4e
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-06-16 17:01:35 +0200

    [TIM-235] Simplistic import of DomainEntities added.

--- timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/conversion/MongoTinkerPopConverter.java
+++ timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/conversion/MongoTinkerPopConverter.java
@@ -1,7 +1,12 @@
 package nl.knaw.huygens.timbuctoo.tools.conversion;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import nl.knaw.huygens.timbuctoo.config.TypeRegistry;
+import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.model.Entity;
+import nl.knaw.huygens.timbuctoo.model.Relation;
 import nl.knaw.huygens.timbuctoo.model.SystemEntity;
 import nl.knaw.huygens.timbuctoo.storage.StorageException;
 import nl.knaw.huygens.timbuctoo.storage.StorageIterator;
@@ -17,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Maps;
 import com.google.inject.Injector;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
@@ -29,6 +35,7 @@ public class MongoTinkerPopConverter {
   private ElementConverterFactory converterFactory;
   private MongoStorage mongoStorage;
   private IdGenerator idGenerator;
+  private HashMap<String, String> oldIdNewIdMap;
 
   public MongoTinkerPopConverter(GraphStorage graphStorage, Graph graph, IdGenerator idGenerator, ElementConverterFactory converterFactory, TypeRegistry registry, MongoStorage mongoStorage) {
     this.graphStorage = graphStorage;
@@ -37,11 +44,11 @@ public class MongoTinkerPopConverter {
     this.converterFactory = converterFactory;
     this.registry = registry;
     this.mongoStorage = mongoStorage;
+    oldIdNewIdMap = Maps.newHashMap();
   }
 
   public static void main(String[] args) throws Exception {
-    LOG.info("Start importing system entities");
-
+    Stopwatch stopwatch = Stopwatch.createStarted();
     Injector injector = ToolsInjectionModule.createInjector();
     Graph graph = injector.getInstance(Graph.class);
     ElementConverterFactory converterFactory = injector.getInstance(ElementConverterFactory.class);
@@ -53,22 +60,45 @@ public class MongoTinkerPopConverter {
 
     MongoTinkerPopConverter converter = new MongoTinkerPopConverter(graphStorage, graph, idGenerator, converterFactory, registry, mongoStorage);
 
-    converter.convertSystemEntities();
+    try {
+      converter.convertSystemEntities();
+      converter.convertDomainEntities();
+    } finally {
+      graph.shutdown();
+      mongoStorage.close();
+    }
 
-    graph.shutdown();
-    mongoStorage.close();
+    LOG.info("Done in {}", stopwatch.stop());
+  }
 
+  private void convertDomainEntities() throws StorageException, IllegalAccessException {
+    for (Class<? extends DomainEntity> type : registry.getDomainEntityTypes()) {
+      LOG.info("converting {}", type.getSimpleName());
+      if (!Relation.class.isAssignableFrom(type)) {
+        convertDomainEntitiesOf(type);
+      }
+    }
+  }
+
+  private <T extends DomainEntity> void convertDomainEntitiesOf(Class<T> type) throws StorageException, IllegalAccessException {
+    //    int i = 0;
+    for (StorageIterator<T> iterator = mongoStorage.getDomainEntities(type); iterator.hasNext();) {
+      T entity = iterator.next();
+
+      convertEntity(type, entity);
+      //      i++;
+      //      if (i >= 5) {
+      //        break;
+      //      }
+    }
   }
 
   private void convertSystemEntities() throws Exception {
-    Stopwatch stopwatch = Stopwatch.createStarted();
 
     for (Class<? extends SystemEntity> type : registry.getSystemEntityTypes()) {
       LOG.info("converting {}", type.getSimpleName());
       convertSystemEntitiesOf(type);
     }
-
-    LOG.info("Done in {}", stopwatch.stop());
 
   }
 
@@ -83,25 +113,42 @@ public class MongoTinkerPopConverter {
   private <T extends Entity> void convertEntity(Class<T> type, T entity) throws ConversionException, StorageException, IllegalAccessException {
     EntityConversionChecker<T> conversionChecker = new EntityConversionChecker<T>(type, mongoStorage, graphStorage);
     String oldId = entity.getId();
-    String newId = addNewId(type, entity);
+    String newId = addNewIdToEntity(type, entity);
 
     LOG.info("Converting \"{}\" with old id \"{}\" and new id \"{}\"", type.getSimpleName(), oldId, newId);
 
-    addToGraph(type, entity);
+    addPropertiesToVertex(type, entity, getVertex(entity.getId(), entity.getRev()));
 
     conversionChecker.verifyConversion(oldId, newId);
   }
 
-  public <T extends Entity> void addToGraph(Class<T> type, T relationType) throws ConversionException {
+  private <T extends Entity> void addPropertiesToVertex(Class<T> type, T entity, Vertex vertex) throws ConversionException {
     VertexConverter<T> converter = converterFactory.forType(type);
 
-    Vertex vertex = graph.addVertex(null);
-    converter.addValuesToElement(vertex, relationType);
+    converter.addValuesToElement(vertex, entity);
   }
 
-  public <T extends Entity> String addNewId(Class<T> type, T entity) {
-    // add a new id, the old Id range was not dependable enough
-    String newId = idGenerator.nextIdFor(type);
+  private Vertex getVertex(String id, int rev) {
+    Iterable<Vertex> vertices = graph.query().has(Entity.ID_DB_PROPERTY_NAME, id)//
+        .has(Entity.REVISION_PROPERTY_NAME, rev).vertices();
+
+    Iterator<Vertex> iterator = vertices.iterator();
+    return iterator.hasNext() ? iterator.next() : graph.addVertex(null);
+  }
+
+  private <T extends Entity> String addNewIdToEntity(Class<T> type, T entity) {
+    String oldId = entity.getId();
+
+    String newId = null;
+    // if another variation is already converted reuse the id
+    if (oldIdNewIdMap.containsKey(oldId)) {
+      newId = oldIdNewIdMap.get(oldId);
+    } else {
+      // add a new id, the old Id range was not dependable enough
+      newId = idGenerator.nextIdFor(type);
+      oldIdNewIdMap.put(oldId, newId);
+    }
+
     entity.setId(newId);
     return newId;
   }
