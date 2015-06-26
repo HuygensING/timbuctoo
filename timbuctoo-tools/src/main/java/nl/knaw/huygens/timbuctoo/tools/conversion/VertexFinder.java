21acb64712f47ee8971b70762ca2267b52f15ab9
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-06-26 10:45:01 +0200

    [TIM-235] Implemented VertexFinder.

--- timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/conversion/VertexFinder.java
+++ timbuctoo-tools/src/main/java/nl/knaw/huygens/timbuctoo/tools/conversion/VertexFinder.java
@@ -1,11 +1,31 @@
 package nl.knaw.huygens.timbuctoo.tools.conversion;
 
+import nl.knaw.huygens.timbuctoo.model.Entity;
+import nl.knaw.huygens.timbuctoo.storage.graph.tinkerpop.IsLatestVersionOfVertex;
+
+import com.google.common.base.Predicate;
+import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 
 public class VertexFinder {
 
+  private static final Predicate<Vertex> IS_LATEST = new IsLatestVersionOfVertex();
+  private Graph graph;
+
+  public VertexFinder(Graph graph) {
+    this.graph = graph;
+  }
+
   public Vertex getLatestVertexById(String id) {
-    throw new UnsupportedOperationException("Yet to be implemented");
+    Iterable<Vertex> vertices = graph.query().has(Entity.ID_DB_PROPERTY_NAME, id).vertices();
+
+    for (Vertex vertex : vertices) {
+      if (IS_LATEST.apply(vertex)) {
+        return vertex;
+      }
+    }
+
+    return null;
   }
 
 }
