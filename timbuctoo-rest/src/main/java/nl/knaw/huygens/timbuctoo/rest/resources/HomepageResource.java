712fa291c1ba3e5629bd76e672b5ff6cda736078
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-07-07 15:24:06 +0200

    [TIM-248] Use jetty to run Timbuctoo locally.

---
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/rest/resources/HomepageResource.java
@@ -0,0 +1,23 @@
+package nl.knaw.huygens.timbuctoo.rest.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.InputStream;
+
+
+/**
+ * A resource to serve the index page.
+ */
+@Path("/")
+public class HomepageResource {
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public Response getHomepage(){
+    InputStream indexPage = Thread.currentThread()//
+        .getContextClassLoader().getResourceAsStream("index.html");
+    return Response.ok(indexPage).build();
+  }
+}
