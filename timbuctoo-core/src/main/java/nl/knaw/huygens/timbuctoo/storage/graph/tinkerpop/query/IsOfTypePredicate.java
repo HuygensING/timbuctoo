df61d1eaa8b4b26c2df027e7c8c639d567ab3d0b
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-07-10 13:58:36 +0200

    [TIM-298] Made IsOfTypePredicate moet restrictive.

--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/query/IsOfTypePredicate.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/query/IsOfTypePredicate.java
@@ -2,9 +2,10 @@ package nl.knaw.huygens.timbuctoo.storage.graph.tinkerpop.query;
 
 public final class IsOfTypePredicate implements com.tinkerpop.blueprints.Predicate {
   @Override
-  public boolean evaluate(Object first, Object second) {
-    if (first != null && (first instanceof String)) {
-      return ((String) first).contains((String) second);
+  public boolean evaluate(Object object, Object shouldApplyTo) {
+    if (object != null && (object instanceof String)) {
+
+      return ((String) object).matches(String.format("(.+\\W)?%s(\\W.+)?", shouldApplyTo));
     }
     return false;
   }
