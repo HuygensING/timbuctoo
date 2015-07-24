eef6a8e7af49b81c58b354b41021182b3cc43411
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-07-24 11:21:07 +0200

    Clean-up auto complete resource test.

--- timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/rest/resources/AutoCompleteResourceTest.java
+++ timbuctoo-rest/src/test/java/nl/knaw/huygens/timbuctoo/rest/resources/AutoCompleteResourceTest.java
@@ -1,5 +1,30 @@
 package nl.knaw.huygens.timbuctoo.rest.resources;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+import nl.knaw.huygens.timbuctoo.config.Configuration;
+import nl.knaw.huygens.timbuctoo.config.Paths;
+import nl.knaw.huygens.timbuctoo.config.TypeNames;
+import nl.knaw.huygens.timbuctoo.index.RawSearchUnavailableException;
+import nl.knaw.huygens.timbuctoo.rest.util.AutocompleteResultConverter;
+import nl.knaw.huygens.timbuctoo.rest.util.CustomHeaders;
+import nl.knaw.huygens.timbuctoo.vre.NotInScopeException;
+import nl.knaw.huygens.timbuctoo.vre.SearchException;
+import nl.knaw.huygens.timbuctoo.vre.VRE;
+import org.junit.Before;
+import org.junit.Test;
+import test.rest.model.projecta.ProjectADomainEntity;
+
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static nl.knaw.huygens.timbuctoo.config.Paths.DOMAIN_PREFIX;
 import static nl.knaw.huygens.timbuctoo.rest.util.QueryParameters.QUERY;
 import static nl.knaw.huygens.timbuctoo.rest.util.QueryParameters.ROWS;
@@ -12,35 +37,6 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriBuilder;
-
-import nl.knaw.huygens.timbuctoo.config.Configuration;
-import nl.knaw.huygens.timbuctoo.config.Paths;
-import nl.knaw.huygens.timbuctoo.config.TypeNames;
-import nl.knaw.huygens.timbuctoo.index.RawSearchUnavailableException;
-import nl.knaw.huygens.timbuctoo.rest.util.AutocompleteResultConverter;
-import nl.knaw.huygens.timbuctoo.rest.util.CustomHeaders;
-import nl.knaw.huygens.timbuctoo.vre.NotInScopeException;
-import nl.knaw.huygens.timbuctoo.vre.SearchException;
-import nl.knaw.huygens.timbuctoo.vre.VRE;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import test.rest.model.projecta.ProjectADomainEntity;
-
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.GenericType;
 
 public class AutoCompleteResourceTest extends WebServiceTestSetup {
   private static final String DEFAULT_QUERY = "*";
@@ -70,8 +66,6 @@ public class AutoCompleteResourceTest extends WebServiceTestSetup {
   @Test
   public void getLetsTheAutoCompleteResultProcessorProcessARawSearchResult() throws Exception {
     // setup
-    int customStart = 20;
-    int customRows = 50;
     VRE vre = mock(VRE.class);
     makeVREAvailable(vre, VRE_ID);
 
@@ -81,39 +75,22 @@ public class AutoCompleteResourceTest extends WebServiceTestSetup {
     convertedResultIsFoundFor(rawSearchResult);
 
     // action
-    resource().path(Paths.V2_PATH).path(DOMAIN_PREFIX).path(DEFAULT_COLLECTION).path(Paths.AUTOCOMPLETE_PATH) //
-        .queryParam(QUERY, SEARCH_PARAM).queryParam(START, "" + customStart).queryParam(ROWS, "" + customRows) //
-        .header(CustomHeaders.VRE_ID_KEY, VRE_ID).get(ClientResponse.class);
+    ClientResponse response = resource().path(Paths.V2_PATH).path(DOMAIN_PREFIX).path(DEFAULT_COLLECTION).path(Paths.AUTOCOMPLETE_PATH)//
+      .queryParam(QUERY, SEARCH_PARAM).header(CustomHeaders.VRE_ID_KEY, VRE_ID).get(ClientResponse.class);
 
     // verify
-    verify(vre).doRawSearch(DEFAULT_TYPE, SEARCH_PARAM, customStart, customRows);
+    responseStatusIs(response, Status.OK);
+
+    List<Map<String, Object>> entity = response.getEntity(new GenericType<List<Map<String, Object>>>() {});
+
+    assertThat(entity, hasSize(2));
+    verifyEntry(entity.get(0), KEY_VALUE1, VALUE_VALUE1);
+    verifyEntry(entity.get(1), KEY_VALUE2, VALUE_VALUE2);
+
   }
 
   @Test
   public void getHasDoesQueryAAsteriskByDefault() throws Exception {
-    // setup
-    int customStart = 20;
-    int customRows = 50;
-    VRE vre = mock(VRE.class);
-    makeVREAvailable(vre, VRE_ID);
-
-    List<Map<String, Object>> rawSearchResult = Lists.<Map<String, Object>> newArrayList();
-
-    when(vre.doRawSearch(DEFAULT_TYPE, SEARCH_PARAM, DEFAULT_START, DEFAULT_ROWS)).thenReturn(rawSearchResult);
-    convertedResultIsFoundFor(rawSearchResult);
-
-    // action
-    resource().path(Paths.V2_PATH).path(DOMAIN_PREFIX).path(DEFAULT_COLLECTION).path(Paths.AUTOCOMPLETE_PATH) //
-        .queryParam(START, "" + customStart).queryParam(ROWS, "" + customRows) //
-        .header(CustomHeaders.VRE_ID_KEY, VRE_ID).get(ClientResponse.class);
-
-    // verify
-    verify(vre).doRawSearch(DEFAULT_TYPE, DEFAULT_QUERY, customStart, customRows);
-  }
-
-  @Test
-  public void getInfluencesTheNumberOfResultsAndStartWhenTheQueryParametersAreSet() throws Exception {
-
     // setup
     VRE vre = mock(VRE.class);
     makeVREAvailable(vre, VRE_ID);
@@ -125,16 +102,32 @@ public class AutoCompleteResourceTest extends WebServiceTestSetup {
 
     // action
     ClientResponse response = resource().path(Paths.V2_PATH).path(DOMAIN_PREFIX).path(DEFAULT_COLLECTION).path(Paths.AUTOCOMPLETE_PATH)//
-        .queryParam(QUERY, SEARCH_PARAM).header(CustomHeaders.VRE_ID_KEY, VRE_ID).get(ClientResponse.class);
+      .header(CustomHeaders.VRE_ID_KEY, VRE_ID).get(ClientResponse.class);
 
     // verify
-    responseStatusIs(response, Status.OK);
+    verify(vre).doRawSearch(DEFAULT_TYPE, DEFAULT_QUERY, DEFAULT_START, DEFAULT_ROWS);
+  }
 
-    List<Map<String, Object>> entity = response.getEntity(new GenericType<List<Map<String, Object>>>() {});
+  @Test
+  public void getInfluencesTheNumberOfResultsAndStartWhenTheQueryParametersAreSet() throws Exception {
+    // setup
+    int customStart = 20;
+    int customRows = 50;
+    VRE vre = mock(VRE.class);
+    makeVREAvailable(vre, VRE_ID);
 
-    assertThat(entity, hasSize(2));
-    verifyEntry(entity.get(0), KEY_VALUE1, VALUE_VALUE1);
-    verifyEntry(entity.get(1), KEY_VALUE2, VALUE_VALUE2);
+    List<Map<String, Object>> rawSearchResult = Lists.<Map<String, Object>> newArrayList();
+
+    when(vre.doRawSearch(DEFAULT_TYPE, SEARCH_PARAM, DEFAULT_START, DEFAULT_ROWS)).thenReturn(rawSearchResult);
+    convertedResultIsFoundFor(rawSearchResult);
+
+    // action
+    resource().path(Paths.V2_PATH).path(DOMAIN_PREFIX).path(DEFAULT_COLLECTION).path(Paths.AUTOCOMPLETE_PATH) //
+      .queryParam(QUERY, SEARCH_PARAM).queryParam(START, "" + customStart).queryParam(ROWS, "" + customRows) //
+      .header(CustomHeaders.VRE_ID_KEY, VRE_ID).get(ClientResponse.class);
+
+    // verify
+    verify(vre).doRawSearch(DEFAULT_TYPE, SEARCH_PARAM, customStart, customRows);
   }
 
   private void convertedResultIsFoundFor(List<Map<String, Object>> rawSearchResult) {
