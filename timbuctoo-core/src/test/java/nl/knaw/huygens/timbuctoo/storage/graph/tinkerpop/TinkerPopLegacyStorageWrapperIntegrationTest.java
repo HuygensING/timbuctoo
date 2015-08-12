a5389b83a31c6dedca032d2612203b1fbb14d84d
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-08-12 13:13:56 +0200

    [TIM-388] Removed the test override, because it succeeds.

--- timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/TinkerPopLegacyStorageWrapperIntegrationTest.java
+++ timbuctoo-core/src/test/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/TinkerPopLegacyStorageWrapperIntegrationTest.java
@@ -2,8 +2,6 @@ package nl.knaw.huygens.timbuctoo.storage.graph.tinkerpop;
 
 import nl.knaw.huygens.timbuctoo.storage.DBIntegrationTestHelper;
 import nl.knaw.huygens.timbuctoo.storage.StorageIntegrationTest;
-import org.junit.Ignore;
-import org.junit.Test;
 
 public class TinkerPopLegacyStorageWrapperIntegrationTest extends StorageIntegrationTest {
 
@@ -11,11 +9,5 @@ public class TinkerPopLegacyStorageWrapperIntegrationTest extends StorageIntegra
   protected DBIntegrationTestHelper createDBIntegrationTestHelper() {
     return new TinkerPopDBIntegrationTestHelper();
   }
-
-  @Test
-  @Ignore
-  @Override
-  public void getEntityOrDefaultVariationForRelationReturnsTheEntityAndItsValuesIfItExistsElseItReturnsTheEntityWithTheValuesOfTheDefaultVariation() throws Exception {
-    super.getEntityOrDefaultVariationForRelationReturnsTheEntityAndItsValuesIfItExistsElseItReturnsTheEntityWithTheValuesOfTheDefaultVariation();
-  }
+  
 }
