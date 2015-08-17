95800b9a597612b86aa713da67ac936dc389f17c
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-08-17 12:23:17 +0200

    [TIM-391] Removed code duplication from Indexers.

---
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/index/AbstractIndexer.java
@@ -0,0 +1,32 @@
+package nl.knaw.huygens.timbuctoo.index;
+
+import nl.knaw.huygens.timbuctoo.Repository;
+import nl.knaw.huygens.timbuctoo.model.DomainEntity;
+import nl.knaw.huygens.timbuctoo.storage.StorageIterator;
+
+public abstract class AbstractIndexer implements Indexer {
+  protected final Repository repository;
+  protected final IndexManager indexManager;
+
+  public AbstractIndexer(Repository repository, IndexManager indexManager) {
+    this.indexManager = indexManager;
+    this.repository = repository;
+  }
+
+  @Override
+  public final void executeFor(IndexRequest request) throws IndexException {
+    Class<? extends DomainEntity> type = request.getType();
+    request.inProgress();
+    for (StorageIterator<? extends DomainEntity> domainEntities = repository.getDomainEntities(type); domainEntities.hasNext(); ) {
+      String id = domainEntities.next().getId();
+      executeIndexAction(type, id);
+    }
+    request.done();
+  }
+
+  protected abstract void executeIndexAction(Class<? extends DomainEntity> type, String id) throws IndexException;
+
+  protected IndexManager getIndexManager() {
+    return indexManager;
+  }
+}
--- timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/index/DeleteIndexer.java
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/index/DeleteIndexer.java
@@ -2,25 +2,15 @@ package nl.knaw.huygens.timbuctoo.index;
 
 import nl.knaw.huygens.timbuctoo.Repository;
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
-import nl.knaw.huygens.timbuctoo.storage.StorageIterator;
 
-public class DeleteIndexer implements Indexer {
-  private final Repository repository;
-  private final IndexManager indexManager;
+public class DeleteIndexer extends AbstractIndexer {
 
   public DeleteIndexer(Repository repository, IndexManager indexManager) {
-    this.repository = repository;
-    this.indexManager = indexManager;
+    super(repository, indexManager);
   }
 
   @Override
-  public void executeFor(IndexRequest request) throws IndexException {
-    Class<? extends DomainEntity> type = request.getType();
-    request.inProgress();
-    for (StorageIterator<? extends DomainEntity> domainEntities = repository.getDomainEntities(type); domainEntities.hasNext(); ) {
-      String id = domainEntities.next().getId();
-      indexManager.deleteEntity(type, id);
-    }
-    request.done();
+  protected void executeIndexAction(Class<? extends DomainEntity> type, String id) throws IndexException {
+    indexManager.deleteEntity(type, id);
   }
 }
