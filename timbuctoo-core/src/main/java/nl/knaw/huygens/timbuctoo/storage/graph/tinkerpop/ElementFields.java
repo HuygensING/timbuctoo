545d469ba5814f042824a093b162b943fc49cd94
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-10-28 10:10:58 +0100

    [TIM-447] Removed duplicate information of types and variations.

--- timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/ElementFields.java
+++ timbuctoo-core/src/main/java/nl/knaw/huygens/timbuctoo/storage/graph/tinkerpop/ElementFields.java
@@ -1,10 +1,12 @@
 package nl.knaw.huygens.timbuctoo.storage.graph.tinkerpop;
 
+import nl.knaw.huygens.timbuctoo.model.DomainEntity;
+
 public final class ElementFields {
   private ElementFields() {
     throw new RuntimeException("Class is not meant to be instantiated.");
   }
 
-  public static final String ELEMENT_TYPES = "types";
+  public static final String ELEMENT_TYPES = DomainEntity.DB_VARIATIONS_PROP_NAME;
   public static final String IS_LATEST = "isLatest";
 }
