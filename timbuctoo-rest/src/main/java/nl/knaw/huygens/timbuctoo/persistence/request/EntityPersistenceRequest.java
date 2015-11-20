83f9c9bed09c9ba0bf4da933bea2f6f0e6809123
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-11-20 13:32:57 +0100

    [TIM-403] Implements EntityPersistenceRequest's execute.

--- timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/persistence/request/EntityPersistenceRequest.java
+++ timbuctoo-rest/src/main/java/nl/knaw/huygens/timbuctoo/persistence/request/EntityPersistenceRequest.java
@@ -5,14 +5,19 @@ import nl.knaw.huygens.timbuctoo.messages.Action;
 import nl.knaw.huygens.timbuctoo.messages.ActionType;
 import nl.knaw.huygens.timbuctoo.model.DomainEntity;
 import nl.knaw.huygens.timbuctoo.persistence.PersistenceRequest;
+import nl.knaw.huygens.timbuctoo.persistence.Persister;
 import nl.knaw.huygens.timbuctoo.persistence.persister.PersisterFactory;
 
 class EntityPersistenceRequest implements PersistenceRequest {
+  private final Repository repository;
+  private final PersisterFactory persisterFactory;
   private final ActionType actionType;
   private final Class<? extends DomainEntity> type;
   private final String id;
 
   public EntityPersistenceRequest(Repository repository, PersisterFactory persisterFactory, ActionType actionType, Class<? extends DomainEntity> type, String id) {
+    this.repository = repository;
+    this.persisterFactory = persisterFactory;
     this.actionType = actionType;
     this.type = type;
     this.id = id;
@@ -25,6 +30,8 @@ class EntityPersistenceRequest implements PersistenceRequest {
 
   @Override
   public void execute() {
-    throw new UnsupportedOperationException("Not implemented yet");
+    Persister persister = persisterFactory.forActionType(actionType);
+    DomainEntity domainEntity = repository.getEntityOrDefaultVariation(type, id);
+    persister.execute(domainEntity);
   }
 }
