1a8c8ee482d7928dc139b563640058ab5b00e75f
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-12-14 11:57:11 +0100

    Splits the tests per resource.

---
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/AbstractV2_1EndpointFixture.java
@@ -0,0 +1,35 @@
+package nl.knaw.huygens.timbuctoo.server.rest;
+
+import nl.knaw.huygens.concordion.extensions.HttpRequest;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import java.util.AbstractMap;
+
+public class AbstractV2_1EndpointFixture {
+  public Response doHttpCommand(HttpRequest httpRequest) {
+    WebTarget target = ClientBuilder.newClient()
+      .target(httpRequest.server != null ? httpRequest.server : "http://acc.repository.huygens.knaw.nl")
+      .path(httpRequest.url);
+
+    for (AbstractMap.SimpleEntry<String, String> queryParameter : httpRequest.queryParameters) {
+      target = target.queryParam(queryParameter.getKey(), queryParameter.getValue());
+    }
+
+    Invocation.Builder request = target
+      .request();
+
+    for (AbstractMap.SimpleEntry<String, String> header : httpRequest.headers) {
+      request = request.header(header.getKey(), header.getValue());
+    }
+
+    if (httpRequest.body != null) {
+      return request.method(httpRequest.method, Entity.json(httpRequest.body));
+    } else {
+      return request.method(httpRequest.method);
+    }
+  }
+}
--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/WWPersonV2_1EndpointFixture.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/WWPersonV2_1EndpointFixture.java
@@ -4,26 +4,19 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import nl.knaw.huygens.concordion.extensions.HttpCommandExtension;
-import nl.knaw.huygens.concordion.extensions.HttpRequest;
 import nl.knaw.huygens.concordion.extensions.HttpResult;
 import nl.knaw.huygens.concordion.extensions.ReplaceEmbeddedStylesheetExtension;
 import org.concordion.api.extension.Extension;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @RunWith(ConcordionRunner.class)
-public class WWPersonV2_1EndpointFixture {
+public class WWPersonV2_1EndpointFixture extends AbstractV2_1EndpointFixture {
   private final ObjectMapper objectMapper;
   @Extension
   public HttpCommandExtension commandExtension = new HttpCommandExtension(this::doHttpCommand, false);
@@ -36,29 +29,6 @@ public class WWPersonV2_1EndpointFixture {
 
 //  @Rule
 //  public final ResourceTestRule resources = ResourceTestRule.builder().addResource(new WWPersonCollectionV2_1EndPoint()).build();
-
-  private Response doHttpCommand(HttpRequest httpRequest) {
-    WebTarget target = ClientBuilder.newClient()
-      .target(httpRequest.server != null ? httpRequest.server : "http://acc.repository.huygens.knaw.nl")
-      .path(httpRequest.url);
-
-    for (AbstractMap.SimpleEntry<String, String> queryParameter : httpRequest.queryParameters) {
-      target = target.queryParam(queryParameter.getKey(), queryParameter.getValue());
-    }
-
-    Invocation.Builder request = target
-      .request();
-
-    for (AbstractMap.SimpleEntry<String, String> header : httpRequest.headers) {
-      request = request.header(header.getKey(), header.getValue());
-    }
-
-    if (httpRequest.body != null) {
-      return request.method(httpRequest.method, Entity.json(httpRequest.body));
-    } else {
-      return request.method(httpRequest.method);
-    }
-  }
 
   public int getNumberOfItems(HttpResult result) {
     JsonNode jsonNode = getBody(result);
