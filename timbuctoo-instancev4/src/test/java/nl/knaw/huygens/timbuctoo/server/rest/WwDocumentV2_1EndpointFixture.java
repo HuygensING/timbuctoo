e6d2ac0a8257d477101525916142ce4366e47a7f
renevanderark <r.van.der.ark@gmail.com>
2015-12-16 15:38:07 +0100

    adds tests for invalid POST requests to wwdocuments

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/WwDocumentV2_1EndpointFixture.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/WwDocumentV2_1EndpointFixture.java
@@ -4,11 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import nl.knaw.huygens.concordion.extensions.HttpExpectation;
+import nl.knaw.huygens.concordion.extensions.HttpRequest;
 import nl.knaw.huygens.concordion.extensions.HttpResult;
 import org.concordion.integration.junit4.ConcordionRunner;
+import org.json.JSONException;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.JSONCompareResult;
 
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -53,6 +60,35 @@ public class WwDocumentV2_1EndpointFixture extends AbstractV2_1EndpointFixture {
 
   @Override
   public String validate(HttpExpectation expectation, HttpResult reality) {
-    return "";
+    if (expectation.body == null) {
+      return "";
+    }
+
+    try {
+      JSONCompareResult jsonCompareResult =
+              JSONCompare.compareJSON(expectation.body, reality.getBody(), JSONCompareMode.LENIENT);
+      return jsonCompareResult.getMessage();
+    } catch (JSONException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public String validatePostWithEmptyBodyResponse(HttpExpectation expectation, HttpResult reality) {
+    final String expected = "missing property '@type'";
+    return reality.getBody().contains(expected) ?
+            "" :
+            "Expected response to contain: '" + expected + "', but got: '" + reality.getBody() + "'";
+  }
+
+  public String getAuthenticationToken(){
+    List<AbstractMap.SimpleEntry<String, String>> headers = Lists.newArrayList();
+    headers.add(new AbstractMap.SimpleEntry<String, String>("Authorization",  "Basic dXNlcjpwYXNzd29yZA=="));
+
+    HttpRequest loginRequest =
+            new HttpRequest("POST", "/v2.1/authenticate", headers, null, null, Lists.newArrayList());
+
+    Response response = doHttpCommand(loginRequest);
+
+    return response.getHeaderString("x_auth_token");
   }
 }
