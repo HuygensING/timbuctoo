9c89c666b4d8ef0e69392cf3dc86f99bb0852dab
Martijn Maas <martijn.maas@huygens.knaw.nl>
2015-12-16 14:43:17 +0100

    Reimplements the validation of the authentication response.

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/AuthenticationV2_1EndpointFixture.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/AuthenticationV2_1EndpointFixture.java
@@ -3,13 +3,27 @@ package nl.knaw.huygens.timbuctoo.server.rest;
 import nl.knaw.huygens.concordion.extensions.HttpExpectation;
 import nl.knaw.huygens.concordion.extensions.HttpResult;
 import org.concordion.integration.junit4.ConcordionRunner;
+import org.json.JSONException;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.JSONCompareResult;
 
 @RunWith(ConcordionRunner.class)
 public class AuthenticationV2_1EndpointFixture extends AbstractV2_1EndpointFixture {
 
   @Override
   public String validate(HttpExpectation expectation, HttpResult reality) {
-    return "";
+    if (expectation.body == null) {
+      return "";
+    }
+
+    try {
+      JSONCompareResult jsonCompareResult =
+        JSONCompare.compareJSON(expectation.body, reality.getBody(), JSONCompareMode.LENIENT);
+      return jsonCompareResult.getMessage();
+    } catch (JSONException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
