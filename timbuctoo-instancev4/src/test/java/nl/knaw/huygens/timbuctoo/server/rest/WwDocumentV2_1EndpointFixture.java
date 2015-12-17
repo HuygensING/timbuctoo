71f3cfe8845f2c94f72d45c681082bc76a6af92f
renevanderark <r.van.der.ark@gmail.com>
2015-12-17 12:00:21 +0100

    adds regex comparator to WwDocumentV2_1EndpointFixture

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/WwDocumentV2_1EndpointFixture.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/WwDocumentV2_1EndpointFixture.java
@@ -9,9 +9,8 @@ import nl.knaw.huygens.concordion.extensions.HttpResult;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.json.JSONException;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONCompare;
-import org.skyscreamer.jsonassert.JSONCompareMode;
-import org.skyscreamer.jsonassert.JSONCompareResult;
+import org.skyscreamer.jsonassert.*;
+import org.skyscreamer.jsonassert.comparator.DefaultComparator;
 
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -77,10 +76,35 @@ public class WwDocumentV2_1EndpointFixture extends AbstractV2_1EndpointFixture {
     return validate(expectation.body, reality.getBody());
   }
 
+  private static class RegexJSONComparator extends DefaultComparator {
+
+    public RegexJSONComparator(JSONCompareMode mode) {
+      super(mode);
+    }
+
+    @Override
+    public void compareValues(String prefix, Object expectedValue, Object actualValue, JSONCompareResult result)
+            throws JSONException {
+
+      if (expectedValue instanceof String && ((String) expectedValue).startsWith("/") &&
+              ((String) expectedValue).endsWith("/")) {
+        RegularExpressionValueMatcher<Object> matcher = new RegularExpressionValueMatcher<>();
+        try {
+          matcher.equal(actualValue, ((String) expectedValue).substring(1, ((String) expectedValue).length() - 1));
+        } catch (ValueMatcherException e) {
+          result.fail(prefix, e);
+        }
+      } else {
+        super.compareValues(prefix, expectedValue, actualValue, result);
+      }
+    }
+  }
+
   private String validate(String expectationBody, String realityBody) {
     try {
       JSONCompareResult jsonCompareResult =
-              JSONCompare.compareJSON(expectationBody, realityBody, JSONCompareMode.LENIENT);
+              JSONCompare.compareJSON(expectationBody, realityBody, new RegexJSONComparator(JSONCompareMode.LENIENT));
+      System.err.println(realityBody);
       return jsonCompareResult.getMessage();
     } catch (JSONException e) {
       throw new RuntimeException(e);
