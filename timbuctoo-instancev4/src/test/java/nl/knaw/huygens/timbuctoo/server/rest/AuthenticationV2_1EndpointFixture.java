bc2faea4b54e32fead0ec4818f91618265758d41
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-01-04 13:19:35 +0100

    Allows an empty body response.

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/AuthenticationV2_1EndpointFixture.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/server/rest/AuthenticationV2_1EndpointFixture.java
@@ -1,9 +1,12 @@
 package nl.knaw.huygens.timbuctoo.server.rest;
 
+import io.dropwizard.testing.junit.ResourceTestRule;
 import nl.knaw.huygens.concordion.extensions.HttpExpectation;
 import nl.knaw.huygens.concordion.extensions.HttpResult;
+import org.apache.commons.lang3.StringUtils;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.json.JSONException;
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONCompare;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -15,6 +18,10 @@ import javax.ws.rs.client.ClientBuilder;
 @RunWith(ConcordionRunner.class)
 public class AuthenticationV2_1EndpointFixture extends AbstractV2_1EndpointFixture {
 
+  @ClassRule
+  public static final ResourceTestRule resources =
+    ResourceTestRule.builder().addResource(new AuthenticationEndPoint()).build();
+
   @Override
   public String validate(HttpExpectation expectation, HttpResult reality) {
     if (expectation.body == null) {
@@ -24,10 +31,13 @@ public class AuthenticationV2_1EndpointFixture extends AbstractV2_1EndpointFixtu
     return validateBody(expectation.body, reality.getBody());
   }
 
-  private String validateBody(String expectationBody, String realityBody) {
+  private String validateBody(String expectationBody, String actualBody) {
+    if(StringUtils.isBlank(actualBody)){
+      return "Actual body is empty";
+    }
     try {
       JSONCompareResult jsonCompareResult =
-        JSONCompare.compareJSON(expectationBody, realityBody, new RegexJsonComparator(JSONCompareMode.LENIENT));
+        JSONCompare.compareJSON(expectationBody, actualBody, new RegexJsonComparator(JSONCompareMode.LENIENT));
 
       return jsonCompareResult.getMessage();
     } catch (JSONException e) {
