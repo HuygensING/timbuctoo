a2e4e4c4e4950d7793e8babb62e669a62886ffb2
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-01-11 10:15:59 +0100

    Let FacetedSearchV2_1EndpointFixture use the local search.

---
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/rest/FacetedSearchV2_1Endpoint.java
@@ -0,0 +1,4 @@
+package nl.knaw.huygens.timbuctoo.server.rest;
+
+public class FacetedSearchV2_1Endpoint {
+}
--- timbuctoo-instancev4/src/spec/java/nl/knaw/huygens/timbuctoo/server/rest/FacetedSearchV2_1EndpointFixture.java
+++ timbuctoo-instancev4/src/spec/java/nl/knaw/huygens/timbuctoo/server/rest/FacetedSearchV2_1EndpointFixture.java
@@ -1,30 +1,36 @@
 package nl.knaw.huygens.timbuctoo.server.rest;
 
+import io.dropwizard.testing.junit.ResourceTestRule;
 import nl.knaw.huygens.concordion.extensions.HttpExpectation;
 import nl.knaw.huygens.concordion.extensions.HttpResult;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.concordion.api.FullOGNL;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.json.JSONException;
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.ArrayValueMatcher;
-import org.skyscreamer.jsonassert.Customization;
 import org.skyscreamer.jsonassert.JSONCompare;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.skyscreamer.jsonassert.JSONCompareResult;
-import org.skyscreamer.jsonassert.comparator.DefaultComparator;
 
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 
 @FullOGNL
 @RunWith(ConcordionRunner.class)
 public class FacetedSearchV2_1EndpointFixture extends AbstractV2_1EndpointFixture {
+
+  @ClassRule
+  public static ResourceTestRule resourcesTestRule = ResourceTestRule.builder()
+    .addResource(FacetedSearchV2_1Endpoint.class)
+    .build();
+
   public String isFullyQualified(String url) {
-    if (url.startsWith("http://") || url.startsWith("https://")) {
-      return "a fully qualified HTTP url";
-    } else {
+
+    if (StringUtils.isBlank(url) || !url.startsWith("http://") || !url.startsWith("https://")) {
       return "not a fully qualified HTTP url";
+    } else {
+      return "a fully qualified HTTP url";
     }
   }
 
@@ -53,6 +59,6 @@ public class FacetedSearchV2_1EndpointFixture extends AbstractV2_1EndpointFixtur
 
   @Override
   protected Client getClient() {
-    return ClientBuilder.newClient();
+    return resourcesTestRule.client();
   }
 }
