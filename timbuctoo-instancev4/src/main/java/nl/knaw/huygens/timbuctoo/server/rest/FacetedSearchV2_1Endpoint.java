e91266d547400cd1531b34b3fd102a3549ff3ecf
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-01-11 11:34:41 +0100

    Makes the url of the POST and the GET correspond.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/rest/FacetedSearchV2_1Endpoint.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/rest/FacetedSearchV2_1Endpoint.java
@@ -1,17 +1,27 @@
 package nl.knaw.huygens.timbuctoo.server.rest;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
+import java.util.UUID;
 
-@Path("/v2.1/search/wwpersons")
+@Path("/v2.1/search")
 public class FacetedSearchV2_1Endpoint {
 
   @POST
+  @Path("wwpersons")
   public Response post() {
-    URI uri = UriBuilder.fromUri("http://localhost:8080").build();
+    UUID uuid = UUID.randomUUID();
+    URI uri = UriBuilder.fromUri("http://localhost:8080/v2.1/search/{id}").build(new Object[]{uuid});
     return Response.created(uri).build();
+  }
+
+  @GET
+  @Path("{id: [a-z0-9\\-]+}")
+  public Response get() {
+    return Response.ok("{}").build();
   }
 }
