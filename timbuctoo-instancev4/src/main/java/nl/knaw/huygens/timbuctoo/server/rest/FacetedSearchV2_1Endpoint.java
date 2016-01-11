4488e9e8dfaac85d7576acc5ab4c956ac0201637
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-01-11 15:03:47 +0100

    Satisfies the faceted search GET test.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/rest/FacetedSearchV2_1Endpoint.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/rest/FacetedSearchV2_1Endpoint.java
@@ -1,19 +1,26 @@
 package nl.knaw.huygens.timbuctoo.server.rest;
 
+import com.google.common.collect.Lists;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 @Path("/v2.1/search")
+@Produces(APPLICATION_JSON)
 public class FacetedSearchV2_1Endpoint {
 
   @POST
   @Path("wwpersons")
-  public Response post() {
+  public Response post(SearchRequestV2_1 searchRequest) {
     UUID uuid = UUID.randomUUID();
     URI uri = UriBuilder.fromResource(FacetedSearchV2_1Endpoint.class).path("{id}").build(new Object[]{uuid});
     return Response.created(uri).build();
@@ -22,6 +29,60 @@ public class FacetedSearchV2_1Endpoint {
   @GET
   @Path("{id: [a-f0-9\\-]+}")
   public Response get() {
-    return Response.ok("{}").build();
+    return Response.ok(new SearchResponseV2_1()).build();
+  }
+
+  static class SearchRequestV2_1 {
+  }
+
+  static class SearchResponseV2_1 {
+    private List<Facet> facets;
+    private List<String> fullTextSearchFields;
+    private List<Ref> refs;
+    private List<String> sortableFields;
+    private int start;
+    private int rows;
+    private int numFound;
+
+    public SearchResponseV2_1(){
+      facets = Lists.newArrayList();
+      fullTextSearchFields = Lists.newArrayList();
+      refs = Lists.newArrayList();
+      sortableFields = Lists.newArrayList();
+    }
+
+    public List<Facet> getFacets() {
+      return facets;
+    }
+
+    public List<String> getFullTextSearchFields() {
+      return fullTextSearchFields;
+    }
+
+    public List<Ref> getRefs() {
+      return refs;
+    }
+
+    public List<String> getSortableFields() {
+      return sortableFields;
+    }
+
+    public int getStart() {
+      return start;
+    }
+
+    public int getRows() {
+      return rows;
+    }
+
+    public int getNumFound() {
+      return numFound;
+    }
+
+    private static class Facet {
+    }
+
+    private static class Ref {
+    }
   }
 }
