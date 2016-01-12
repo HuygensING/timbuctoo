f9e478c884edea5c217d2a3c79e6739fd86f4934
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-01-12 11:17:49 +0100

    Makes the file locations configurable.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/TimbuctooConfiguration.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/TimbuctooConfiguration.java
@@ -2,6 +2,19 @@ package nl.knaw.huygens.timbuctoo.server;
 
 
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class TimbuctooConfiguration extends Configuration {
+  @NotEmpty
+  private String loginsFilePath;
+  @NotEmpty
+  private String usersFilePath;
+
+  public String getLoginsFilePath() {
+    return loginsFilePath;
+  }
+
+  public String getUsersFilePath() {
+    return usersFilePath;
+  }
 }
--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/TimbuctooV4.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/TimbuctooV4.java
@@ -2,13 +2,16 @@ package nl.knaw.huygens.timbuctoo.server;
 
 import com.codahale.metrics.health.HealthCheck;
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import nl.knaw.huygens.timbuctoo.server.rest.AuthenticationV2_1EndPoint;
 import nl.knaw.huygens.timbuctoo.security.JsonBasedAuthenticator;
 import nl.knaw.huygens.timbuctoo.security.JsonBasedUserStore;
 import nl.knaw.huygens.timbuctoo.security.LoggedInUserStore;
-import nl.knaw.huygens.timbuctoo.util.Timeout;
+import nl.knaw.huygens.timbuctoo.server.rest.AuthenticationV2_1EndPoint;
 import nl.knaw.huygens.timbuctoo.server.rest.UserV2_1Endpoint;
+import nl.knaw.huygens.timbuctoo.util.Timeout;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,20 +27,35 @@ public class TimbuctooV4 extends Application<TimbuctooConfiguration> {
   }
 
   @Override
+  public void initialize(Bootstrap<TimbuctooConfiguration> bootstrap) {
+    /*
+     * Make it possible to use environment variables in the config.
+     * see: http://www.dropwizard.io/0.9.1/docs/manual/core.html#environment-variables
+     */
+    bootstrap.setConfigurationSourceProvider(
+      new SubstitutingSourceProvider(
+        bootstrap.getConfigurationSourceProvider(),
+        new EnvironmentVariableSubstitutor(false)));
+  }
+
+  @Override
   public void run(TimbuctooConfiguration configuration, Environment environment) throws Exception {
-    Path loginsPath = dataPath().resolve("logins.json");
+    Path loginsPath = Paths.get(configuration.getLoginsFilePath()); // dataPath().resolve("logins.json");
     JsonBasedAuthenticator authenticator = new JsonBasedAuthenticator(loginsPath, ENCRYPTION_ALGORITHM);
 
-    Path usersPath = dataPath().resolve("users.json");
+    Path usersPath = Paths.get(configuration.getUsersFilePath()); // dataPath().resolve("users.json");
     JsonBasedUserStore userStore = new JsonBasedUserStore(usersPath);
+
     LoggedInUserStore loggedInUserStore = new LoggedInUserStore(authenticator, userStore, new Timeout(8, HOURS));
+
+    // register REST endpoints
     environment.jersey().register(new AuthenticationV2_1EndPoint(loggedInUserStore));
     environment.jersey().register(new UserV2_1Endpoint(loggedInUserStore));
 
     // register health checks
     registerHealthCheck(environment, "Encryption algorithm", new EncryptionAlgorithmHealthCheck(ENCRYPTION_ALGORITHM));
-    registerHealthCheck(environment, "Local logins", new FileHealthCheck(loginsPath));
-    registerHealthCheck(environment, "Users", new FileHealthCheck(usersPath));
+    registerHealthCheck(environment, "Local logins file", new FileHealthCheck(loginsPath));
+    registerHealthCheck(environment, "Users file", new FileHealthCheck(usersPath));
   }
 
   private Path dataPath() {
