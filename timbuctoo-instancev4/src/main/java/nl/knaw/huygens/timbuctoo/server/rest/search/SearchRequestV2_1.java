5798d9af163c5ffabdccd2e95eeaa5dfcd74b2ed
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-02-19 10:33:29 +0100

    Supports the default request of the faceted search front-end library.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/rest/search/SearchRequestV2_1.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/rest/search/SearchRequestV2_1.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class SearchRequestV2_1 {
 
   private List<FacetValue> facetValues;
+  private List<Object> sortParameters;
+  private String term;
 
   public SearchRequestV2_1() {
     // set default value, so the users do not have to handle a null value.
@@ -21,5 +23,21 @@ public class SearchRequestV2_1 {
 
   public void setFacetValues(List<FacetValue> facetValues) {
     this.facetValues = facetValues;
+  }
+
+  public List<Object> getSortParameters() {
+    return sortParameters;
+  }
+
+  public void setSortParameters(List<Object> sortParameters) {
+    this.sortParameters = sortParameters;
+  }
+
+  public String getTerm() {
+    return term;
+  }
+
+  public void setTerm(String term) {
+    this.term = term;
   }
 }
