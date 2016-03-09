eaeff3e977f675f70d25c01721fb8b49e6057cff
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-03-09 15:13:07 +0100

    [TIM-677] Sets the user home to the directory of timbuctoo.

--- timbuctoo-instancev4/src/main/scripts/rpm_scripts/create_user_and_group.sh
+++ timbuctoo-instancev4/src/main/scripts/rpm_scripts/create_user_and_group.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GROUP_STRING=`getent group datarepository`
-if [ ! "$USER_STRING" ]; then
+if [ ! "$GROUP_STRING" ]; then
   echo "create group \"datarepository\""
   groupadd datarepository
 
@@ -10,6 +10,6 @@ fi
 USER_STRING=`getent passwd datarepository`
 if [ ! "$USER_STRING" ]; then
   echo "create user \"datarepository\""
-  useradd --system -g datarepository datarepository
+  useradd --system -g datarepository datarepository -d /opt/timbuctoo
 
 fi
