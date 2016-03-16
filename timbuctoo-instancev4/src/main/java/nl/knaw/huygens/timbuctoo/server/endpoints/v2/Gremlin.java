647fc7f4e935b365d9401793a9fbf6773d8c9f19
renevanderark <r.van.der.ark@gmail.com>
2016-03-16 15:15:39 +0100

    adds dump for more complex result types to Gremlin.java

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/Gremlin.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/Gremlin.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Lists;
 import nl.knaw.huygens.timbuctoo.server.GraphWrapper;
 import org.apache.tinkerpop.gremlin.groovy.jsr223.GremlinGroovyScriptEngine;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.BulkSet;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.structure.VertexProperty;
@@ -22,6 +23,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 import static org.apache.tinkerpop.gremlin.structure.Direction.IN;
 import static org.apache.tinkerpop.gremlin.structure.Direction.OUT;
@@ -74,33 +76,58 @@ public class Gremlin {
     }
   }
 
+  private void dumpVertex(Vertex vertex, StringBuilder result) {
+    result.append(String.format("Vertex [%s]:\n", vertex.id()));
+    ArrayList<VertexProperty<Object>> properties = Lists.newArrayList(vertex.properties());
+    properties.sort((o1, o2) -> java.text.Collator.getInstance().compare(o1.label(), o2.label()));
+    for (VertexProperty<Object> property : properties) {
+      result.append(String.format("  %s: %s\n", property.label(), property.value()));
+    }
+    for (Edge edge : ImmutableList.copyOf(vertex.edges(IN))) {
+      result.append(String.format("  <--[%s]-- v[%s]\n", edge.label(), edge.outVertex().id()));
+    }
+    for (Edge edge : ImmutableList.copyOf(vertex.edges(OUT))) {
+      result.append(String.format("  --[%s]--> v[%s]\n", edge.label(), edge.inVertex().id()));
+    }
+  }
+
+  private void dumpEdge(Edge edge, StringBuilder result) {
+    result.append(String.format("v[%s] --[%s]--> v[%s]\n",
+            edge.inVertex().id(), edge.label(), edge.outVertex().id()));
+  }
+
+  private void dumpItem(Object item, StringBuilder result) {
+    if (item instanceof Vertex) {
+      dumpVertex((Vertex) item, result);
+    } else if (item instanceof Edge) {
+      dumpEdge((Edge) item, result);
+    } else if (item instanceof BulkSet) {
+      BulkSet bulkSet = (BulkSet<Object>) item;
+      bulkSet.forEach((obj, amount) -> {
+        StringBuilder stringBuilder = new StringBuilder();
+        dumpItem(obj, stringBuilder);
+        result.append(stringBuilder.toString() + "\n");
+      });
+
+    } else if (item instanceof LinkedHashMap) {
+      LinkedHashMap lhm = (LinkedHashMap<String, Object>) item;
+      lhm.forEach((key, obj) -> {
+        StringBuilder stringBuilder = new StringBuilder();
+        dumpItem(obj, stringBuilder);
+        result.append(key + ": " +  stringBuilder.toString() + "\n");
+      });
+    } else {
+      result.append(item);
+      result.append("\n");
+    }
+  }
+
   private String evaluateQuery(String query) throws ScriptException {
     GraphTraversal traversalResult = (GraphTraversal) engine.eval(query, bindings);
     StringBuilder result = new StringBuilder();
     while (traversalResult.hasNext()) {
-      Object item = traversalResult.next();
-      if (item instanceof Vertex) {
-        Vertex vertex = (Vertex) item;
-        result.append(String.format("Vertex [%s]:\n", vertex.id()));
-        ArrayList<VertexProperty<Object>> properties = Lists.newArrayList(vertex.properties());
-        properties.sort((o1, o2) -> java.text.Collator.getInstance().compare(o1.label(), o2.label()));
-        for (VertexProperty<Object> property : properties) {
-          result.append(String.format("  %s: %s\n", property.label(), property.value()));
-        }
-        for (Edge edge : ImmutableList.copyOf(vertex.edges(IN))) {
-          result.append(String.format("  <--[%s]-- v[%s]\n", edge.label(), edge.outVertex().id()));
-        }
-        for (Edge edge : ImmutableList.copyOf(vertex.edges(OUT))) {
-          result.append(String.format("  --[%s]--> v[%s]\n", edge.label(), edge.inVertex().id()));
-        }
-      } else if (item instanceof Edge) {
-        Edge edge = (Edge) item;
-        result.append(String.format("v[%s] --[%s]--> v[%s]\n",
-          edge.inVertex().id(), edge.label(), edge.outVertex().id()));
-      } else {
-        result.append(item);
-        result.append("\n");
-      }
+
+      dumpItem(traversalResult.next(), result);
     }
     return result.toString();
   }
