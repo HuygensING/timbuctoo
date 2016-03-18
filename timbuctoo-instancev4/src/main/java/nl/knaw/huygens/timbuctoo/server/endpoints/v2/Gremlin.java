096cee26c415c0d1f2f4f96aa1cf60abbffee569
renevanderark <r.van.der.ark@gmail.com>
2016-03-18 11:58:04 +0100

    adds json results for vertices to Gremlin endpoint

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/Gremlin.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/Gremlin.java
@@ -1,8 +1,18 @@
 package nl.knaw.huygens.timbuctoo.server.endpoints.v2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import nl.knaw.huygens.timbuctoo.model.Datable;
+import nl.knaw.huygens.timbuctoo.model.LocationNames;
+import nl.knaw.huygens.timbuctoo.model.PersonNames;
+import nl.knaw.huygens.timbuctoo.search.EntityRef;
+import nl.knaw.huygens.timbuctoo.search.description.PropertyDescriptor;
+import nl.knaw.huygens.timbuctoo.search.description.property.PropertyDescriptorFactory;
+import nl.knaw.huygens.timbuctoo.search.description.propertyparser.PropertyParserFactory;
 import nl.knaw.huygens.timbuctoo.server.GraphWrapper;
 import org.apache.tinkerpop.gremlin.groovy.jsr223.GremlinGroovyScriptEngine;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
@@ -22,8 +32,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.apache.tinkerpop.gremlin.structure.Direction.IN;
 import static org.apache.tinkerpop.gremlin.structure.Direction.OUT;
@@ -36,11 +50,15 @@ public class Gremlin {
   private final GremlinGroovyScriptEngine engine;
   private final GraphWrapper wrapper;
   private final Bindings bindings;
+  private PropertyDescriptorFactory propertyDescriptorFactory;
+  private PropertyParserFactory propertyParserFactory;
 
   public Gremlin(GraphWrapper wrapper) {
     this.wrapper = wrapper;
     this.engine = new GremlinGroovyScriptEngine();
     this.bindings = engine.createBindings();
+    propertyParserFactory = new PropertyParserFactory();
+    propertyDescriptorFactory = new PropertyDescriptorFactory(propertyParserFactory);
   }
 
   @POST
@@ -55,6 +73,156 @@ public class Gremlin {
       return Response.status(500).entity(e.getMessage()).build();
     }
   }
+
+  @POST
+  @Consumes("text/plain")
+  @Produces("application/json")
+  public Response postJson(String query) {
+    bindings.put("g", wrapper.getGraph().traversal());
+    bindings.put("maria", wrapper.getGraph().traversal().V().has("tim_id", "077bf0b5-6b7d-45aa-89ff-6ecf2cfc549c"));
+    try {
+      return Response.ok(evaluateQueryJson(query)).build();
+    } catch (ScriptException e) {
+      LOG.error(e.getMessage(), e);
+      return Response.status(500).entity(e.getMessage()).build();
+    }
+  }
+
+  private static class JsonResult {
+    public Map<String, Long> counts;
+    public Map<String, List<EntityRef>> results;
+
+    public JsonResult(Map<String, Long> counts, Map<String, List<EntityRef>> results) {
+      this.counts = counts;
+      this.results = results;
+    }
+  }
+
+  private JsonResult evaluateQueryJson(String query) throws ScriptException {
+    GraphTraversal traversalResult = (GraphTraversal) engine.eval(query, bindings);
+    Map<String, List<EntityRef>> results = new HashMap<>();
+    Map<String, List<String>> resultIds = new HashMap<>();
+    Map<String, Long> resultCounts = new HashMap<>();
+
+    if (!traversalResult.hasNext()) {
+      return new JsonResult(resultCounts, results);
+    }
+
+    while (traversalResult.hasNext()) {
+      Object item = traversalResult.next();
+      if (item instanceof LinkedHashMap) {
+        LinkedHashMap lhm = (LinkedHashMap) item;
+        lhm.forEach((key, obj) -> {
+          if (obj instanceof Vertex) {
+            loadVertex(results, key, (Vertex) obj, resultIds);
+          }
+        });
+      } else if (item instanceof Vertex) {
+        loadVertex(results, "result", (Vertex) item, resultIds);
+      }
+    }
+
+    resultIds.forEach((key, obj) -> resultCounts.put(key, Long.valueOf(obj.size())));
+    return new JsonResult(resultCounts, results);
+  }
+
+  private void loadVertex(Map<String, List<EntityRef>> results, Object key, Vertex obj,
+                          Map<String, List<String>> resultCounts) {
+    if (!results.containsKey(key)) {
+      results.put((String) key, new ArrayList<>());
+      resultCounts.put((String) key, new ArrayList<>());
+    }
+
+    if (resultCounts.get(key).stream()
+            .filter((id) -> id.equals((String) obj.property("tim_id").value()))
+            .toArray().length == 0) {
+      resultCounts.get(key).add((String) obj.property("tim_id").value());
+    }
+
+    if (results.get(key).size() < 10) {
+      if (results.get(key).stream()
+              .filter((ref) -> ref.getId().equals((String) obj.property("tim_id").value()))
+              .toArray().length == 0) {
+        try {
+          results.get(key).add(mapVertex(obj));
+        } catch (IOException e) {
+          LOG.error(e.getMessage(), e);
+        }
+      }
+    }
+  }
+
+
+  private PropertyDescriptor createAuthorDescriptor() {
+    PropertyDescriptor authorNameDescriptor = propertyDescriptorFactory.getDerivedWithSeparator(
+            "isCreatedBy",
+            "wwperson_names",
+            propertyParserFactory.getParser(PersonNames.class),
+            "; ");
+    PropertyDescriptor authorTempNameDescriptor = propertyDescriptorFactory.getDerivedWithSeparator(
+            "isCreatedBy",
+            "wwperson_tempName",
+            propertyParserFactory.getParser(String.class),
+            "; ");
+
+    return propertyDescriptorFactory
+            .getComposite(authorNameDescriptor, authorTempNameDescriptor);
+  }
+
+  private EntityRef mapVertex(Vertex vertex) throws IOException {
+    String id = (String) vertex.property("tim_id").value();
+    String type = getVertexType(vertex);
+    String displayName = "";
+    Map<String, Object> data = Maps.newHashMap();
+
+    if (type.equals("person")) {
+      displayName = propertyDescriptorFactory.getComposite(
+              propertyDescriptorFactory.getLocal("wwperson_names", PersonNames.class),
+              propertyDescriptorFactory.getLocal("wwperson_tempName", String.class)).get(vertex);
+    } else if (type.equals("location")) {
+      displayName = propertyDescriptorFactory.getLocal("names", LocationNames.class).get(vertex);
+    } else if (type.equals("document")) {
+      PropertyDescriptor titleDescriptor = propertyDescriptorFactory.getLocal("wwdocument_title", String.class);
+      PropertyDescriptor dateDescriptor = propertyDescriptorFactory
+              .getLocal("wwdocument_date", Datable.class, "(", ")");
+
+      PropertyDescriptor documentDescriptor = propertyDescriptorFactory
+              .getAppender(titleDescriptor, dateDescriptor, " ");
+
+      displayName = propertyDescriptorFactory.getAppender(createAuthorDescriptor(), documentDescriptor, " - ")
+              .get(vertex);
+    } else if (type.equals("keyword")) {
+      PropertyDescriptor typeDescriptor = propertyDescriptorFactory.getLocal("keyword_type", String.class);
+      PropertyDescriptor valueDescriptor = propertyDescriptorFactory.getLocal("keyword_value", String.class);
+      displayName = propertyDescriptorFactory.getAppender(valueDescriptor, typeDescriptor, " - ")
+              .get(vertex);
+    } else if (type.equals("collective")) {
+      PropertyDescriptor typeDescriptor = propertyDescriptorFactory.getLocal("collective_type", String.class);
+      PropertyDescriptor valueDescriptor = propertyDescriptorFactory.getLocal("collective_name", String.class);
+      displayName = propertyDescriptorFactory.getAppender(valueDescriptor, typeDescriptor, " - ")
+              .get(vertex);
+    } else if (type.equals("language")) {
+      displayName = propertyDescriptorFactory.getLocal("language_name", String.class).get(vertex);
+    }
+
+
+    EntityRef ref = new EntityRef(type, id);
+    ref.setDisplayName(displayName);
+    ref.setData(data);
+
+    return ref;
+  }
+
+  private String getVertexType(Vertex vertex) throws IOException {
+    String types = (String) vertex.property("types").value();
+    ObjectMapper mapper = new ObjectMapper();
+    List<String> typeList = mapper.readValue(types,
+            mapper.getTypeFactory().constructCollectionType(List.class, String.class));
+    typeList.sort((o1, o2) -> o1.length() - o2.length());
+    return typeList.get(0);
+  }
+
+
 
 
   @GET
@@ -75,6 +243,7 @@ public class Gremlin {
       return Response.status(500).entity(e.getMessage()).build();
     }
   }
+
 
   private void dumpVertex(Vertex vertex, StringBuilder result) {
     result.append(String.format("Vertex [%s]:\n", vertex.id()));
@@ -110,15 +279,22 @@ public class Gremlin {
       });
 
     } else if (item instanceof LinkedHashMap) {
+      ObjectMapper mapper = new ObjectMapper();
+      Map<String, String> map = new HashMap<>();
+
       LinkedHashMap lhm = (LinkedHashMap<String, Object>) item;
       lhm.forEach((key, obj) -> {
         StringBuilder stringBuilder = new StringBuilder();
         dumpItem(obj, stringBuilder);
-        result.append(key + ": " +  stringBuilder.toString() + "\n");
+        map.put((String)key, stringBuilder.toString());
       });
+      try {
+        result.append(mapper.writeValueAsString(map));
+      } catch (JsonProcessingException e) {
+        LOG.error(e.getMessage(), e);
+      }
     } else {
       result.append(item);
-      result.append("\n");
     }
   }
 
