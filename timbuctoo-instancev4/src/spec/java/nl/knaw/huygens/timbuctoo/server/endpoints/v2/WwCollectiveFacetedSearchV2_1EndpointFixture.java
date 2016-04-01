9d9625c8f07023a17d7793084e89988e1c932173
Meindert Kroese <Meindert.Kroese@huygens.knaw.nl>
2016-04-01 10:04:55 +0200

    Better saved than sorry

---
+++ timbuctoo-instancev4/src/spec/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/WwCollectiveFacetedSearchV2_1EndpointFixture.java
@@ -0,0 +1,49 @@
+package nl.knaw.huygens.timbuctoo.server.endpoints.v2;
+
+import org.apache.commons.lang3.StringUtils;
+import org.concordion.api.FullOGNL;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+@FullOGNL
+@RunWith(ConcordionRunner.class)
+public class WwCollectiveFacetedSearchV2_1EndpointFixture extends AbstractV2_1EndpointFixture {
+
+  public String isFullyQualified(String url) {
+    if (StringUtils.isBlank(url) || (!url.startsWith("http://") && !url.startsWith("https://"))) {
+      return "not a fully qualified HTTP url";
+    } else {
+      return "a fully qualified HTTP url";
+    }
+  }
+
+  public String doRequestWithMalformedUuid() {
+    Response response = returnUrlToMockedOrRealServer(null)
+        .path("v2.1").path("search").path("malformedUUID")
+        .request()
+        .get();
+
+    return String.format("%s %s", response.getStatus(), response.getStatusInfo());
+  }
+
+  public String doGetOfUnknown() {
+    Response response = returnUrlToMockedOrRealServer(null)
+        .path("v2.1").path("search").path("dd8181cf-30b0-47e3-9787-61d09579306e")
+        .request()
+        .get();
+
+    return String.format("%s %s", response.getStatus(), response.getStatusInfo());
+  }
+
+  @Override
+  protected WebTarget returnUrlToMockedOrRealServer(String serverAddress) {
+    String defaultAddress = "http://acc.repository.huygens.knaw.nl";
+    String address = serverAddress != null ? serverAddress : defaultAddress;
+
+    return ClientBuilder.newClient().target(address);
+  }
+}
