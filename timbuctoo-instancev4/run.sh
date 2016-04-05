64b58c428ac51f447d94db65de5faffd1073d7e8
Jauco Noordzij <code@jauco.nl>
2016-04-05 11:32:30 +0200

    Better run script

--- timbuctoo-instancev4/run.sh
+++ timbuctoo-instancev4/run.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+cd $(dirname $0)
+if [ "$1" = '--test' ]; then
+	export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+fi
+
 echo running './target/appassembler/bin/timbuctoo server ./example_config.yaml'
 ./target/appassembler/bin/timbuctoo server ./example_config.yaml
+result="$?"
+if [ "$result" = 130 ]; then
+	exit 0
+else 
+	exit $result
+fi
