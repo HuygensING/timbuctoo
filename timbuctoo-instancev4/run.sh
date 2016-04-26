2c90d5478982e1430ba0d9e1243be3e9be90c2eb
Jauco Noordzij <code@jauco.nl>
2016-04-26 20:53:09 +0200

    add rebuild flag to run script

--- timbuctoo-instancev4/run.sh
+++ timbuctoo-instancev4/run.sh
@@ -9,7 +9,7 @@ DEBUG_HALT="n"
 FLIGHT_CONTROL=""
 
 OPTIND=1
-while getopts ":d:pf:c:y:h" opt; do
+while getopts ":d:pf:c:y:hrt" opt; do
     case "$opt" in
   d)
     DEBUG_PORT=$OPTARG
@@ -30,6 +30,12 @@ while getopts ":d:pf:c:y:h" opt; do
     echo -e "$0: run the timbuctoo command with some easier to remember switches\n\n-d    allow debugger to attach\n-p    pause until debugger attaches\n-f    make flight recording\n-c    command to run (defaults to server)\n-y    YAML config to load (defaults to ./example_config.yaml)" >&2
     exit 0
     ;;
+  r)
+    REBUILD=1
+    ;;
+  t)
+    RUN_TESTS=1
+    ;;
   \?)
     echo "Invalid option: -$OPTARG use -h for an overview of valid options" >&2
     exit 1
@@ -45,6 +51,21 @@ while getopts ":d:pf:c:y:h" opt; do
     ;;
   esac
 done
+
+if [ "$REBUILD" = 1 ]; then
+  cd ..
+  echo "Rebuilding"
+  if [ -z "$RUN_TESTS" ]; then
+    skipTests="-DskipTests"
+  fi
+  mvn clean package $skipTests -q
+  if [ $? != 0 ]; then
+    xmllint --xpath //file[error] timbuctoo-instancev4/target/checkstyle-result.xml
+    exit 1
+  fi
+  cd -
+fi
+
 if [ -n "$DEBUG_PORT" ]; then
 	JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=${DEBUG_HALT},address=${DEBUG_PORT}"
 fi
