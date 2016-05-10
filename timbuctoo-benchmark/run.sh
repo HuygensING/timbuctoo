dba7e4e190c367e44097cd2a6a8e754acba51099
renevanderark <r.van.der.ark@gmail.com>
2016-05-10 11:59:55 +0200

    adds jmc params to benchmark run.sh

--- timbuctoo-benchmark/run.sh
+++ timbuctoo-benchmark/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar target/benchmarks.jar -wf 1 -wi 5 -f 1 -i 5 -prof stack
+java -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=defaultrecording=true,dumponexit=true -jar target/benchmarks.jar -wf 1 -wi 5 -f 1 -i 5 -prof stack
