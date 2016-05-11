a79b1f0e7bcee1d2a705b6f9f47f0d25393c1812
Jauco Noordzij <code@jauco.nl>
2016-05-11 17:38:25 +0200

    niet gechecked!

--- timbuctoo-instancev4/src/main/scripts/timbuctoo-server/crontab.sh
+++ timbuctoo-instancev4/src/main/scripts/timbuctoo-server/crontab.sh
@@ -4,6 +4,3 @@
 export TIMBUCTOO_JENKINS_PROJECT="http://ci.huygens.knaw.nl/job/timbuctoo_develop"
 
 /data/timbuctoo/timbuctoo_update/auto_install.sh >> /data/timbuctoo/update_timbuctoo.log 2>&1
-
-
-asas
