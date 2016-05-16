98d745a64f22c5b00e9544bb142e655524512782
Jauco Noordzij <code@jauco.nl>
2016-05-16 12:14:59 +0200

    refactor: clean op optional handling

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/experimental/bulkupload/parsedworkbook/ParsedColumns.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/experimental/bulkupload/parsedworkbook/ParsedColumns.java
@@ -13,28 +13,29 @@ public class ParsedColumns {
     Row header = sheet.getRow(headerRow);
     final Cell headerCell = header.getCell(column);
 
-    Optional<String> caption = null;
     try {
-      caption = Helpers.getValueAsString(headerCell);
-      if (!caption.isPresent()) {
+      Optional<String> captionOpt = Helpers.getValueAsString(headerCell);
+      if (!captionOpt.isPresent()) {
         Helpers.addFailure(
           new CellReference(sheet.getSheetName(), headerRow, column, true, true),
           sheet.getWorkbook(),
           "The caption should not be blank."
         );
         return Optional.empty();
-      }
-      final String[] captionParts = caption.get().split(" ");
-      if (captionParts.length == 1) {
-        return Optional.of(new PropertyColumns(minRow, maxRow, headerRow, sheet, caption.get(), column));
-      } else if (captionParts.length == 2) {
-        return Optional.of(
-          new RelationColumns(minRow, maxRow, headerRow, sheet, captionParts[0], captionParts[1], column)
-        );
       } else {
-        Helpers.addFailure(headerCell, "The caption should either contain no spaces (for properties) or a " +
-          "relationName and a collectionName seperated by 1 space.");
-        return Optional.empty();
+        final String caption = captionOpt.get();
+        final String[] captionParts = caption.split(" ");
+        if (captionParts.length == 1) {
+          return Optional.of(new PropertyColumns(minRow, maxRow, headerRow, sheet, caption, column));
+        } else if (captionParts.length == 2) {
+          return Optional.of(
+            new RelationColumns(minRow, maxRow, headerRow, sheet, captionParts[0], captionParts[1], column)
+          );
+        } else {
+          Helpers.addFailure(headerCell, "The caption should either contain no spaces (for properties) or a " +
+            "relationName and a collectionName seperated by 1 space.");
+          return Optional.empty();
+        }
       }
     } catch (IOException e) {
       Helpers.addFailure(headerCell, "The caption should not contain an error.");
