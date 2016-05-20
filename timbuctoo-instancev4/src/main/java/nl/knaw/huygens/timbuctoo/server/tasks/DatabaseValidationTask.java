78a20747d0066aab5c468562adaac4cc1004f4ec
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-05-20 15:32:11 +0200

    Return the database validation not the optional.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/tasks/DatabaseValidationTask.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/tasks/DatabaseValidationTask.java
@@ -6,12 +6,16 @@ import com.google.common.collect.ImmutableMultimap;
 import io.dropwizard.servlets.tasks.Task;
 import nl.knaw.huygens.timbuctoo.server.BackgroundRunner;
 import nl.knaw.huygens.timbuctoo.server.healthchecks.ValidationResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import java.io.PrintWriter;
 import java.util.Optional;
 
 public class DatabaseValidationTask extends Task {
 
+  public static final Logger LOG = LoggerFactory.getLogger(DatabaseValidationTask.class);
   private BackgroundRunner<ValidationResult> validator;
 
   public DatabaseValidationTask(BackgroundRunner<ValidationResult> validator) {
@@ -26,7 +30,7 @@ public class DatabaseValidationTask extends Task {
     if (mostRecentResult.isPresent()) {
       ObjectMapper objectMapper = new ObjectMapper();
       objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-      String content = objectMapper.writeValueAsString(mostRecentResult);
+      String content = objectMapper.writeValueAsString(mostRecentResult.get());
 
       output.write(content);
     } else {
