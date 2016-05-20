e08fa371799c912f0cc8c44e16daaaab09e5f6a7
renevanderark <r.van.der.ark@gmail.com>
2016-05-20 13:18:53 +0200

    [TIM-770] adds test for Neo4JLuceneEntityFetcher.

---
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/crud/Neo4jLuceneEntityFetcherTest.java
@@ -0,0 +1,26 @@
+package nl.knaw.huygens.timbuctoo.crud;
+
+import org.junit.Test;
+
+public class Neo4jLuceneEntityFetcherTest {
+
+  @Test
+  public void getEntityLooksInLuceneIndexIfAvailable() {
+
+  }
+
+  @Test
+  public void getEntityInvokesSuperIfLuceneIndexIsNotAvailable() {
+
+  }
+
+  @Test
+  public void getEntityInvokesSuperIfNodeIsNotFoundInIndex() {
+
+  }
+
+  @Test
+  public void getEntityInvokesSuperIfLatestVersionOfVertexIsNotFound() {
+
+  }
+}
