bbeb888c8299156e40417fa773d87a33f046770a
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-06-02 16:42:32 +0200

    [TIM-835] Added a stopwatch to log the duration of the log creation.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/tasks/DbLogCreatorTask.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/tasks/DbLogCreatorTask.java
@@ -1,10 +1,12 @@
 package nl.knaw.huygens.timbuctoo.server.tasks;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableMultimap;
 import io.dropwizard.servlets.tasks.Task;
 import nl.knaw.huygens.timbuctoo.experimental.databaselog.DatabaseLogGenerator;
 import nl.knaw.huygens.timbuctoo.server.TinkerpopGraphManager;
+import org.slf4j.LoggerFactory;
 
 import java.io.PrintWriter;
 
@@ -22,6 +24,8 @@ public class DbLogCreatorTask extends Task {
 
   @Override
   public void execute(ImmutableMultimap<String, String> parameters, PrintWriter output) throws Exception {
+    Stopwatch stopwatch = Stopwatch.createStarted();
     logGenerator.generate();
+    LoggerFactory.getLogger(DbLogCreatorTask.class).info("Log creation took {}", stopwatch.stop());
   }
 }
