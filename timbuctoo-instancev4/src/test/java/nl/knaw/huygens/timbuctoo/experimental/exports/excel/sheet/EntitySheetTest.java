5af2f70e88e59629cac39a4ebe20c10ffebf5c39
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-06-08 13:24:23 +0200

    Make the EntitySheetTest data row order agnostic.

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/experimental/exports/excel/sheet/EntitySheetTest.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/experimental/exports/excel/sheet/EntitySheetTest.java
@@ -1,9 +1,14 @@
 package nl.knaw.huygens.timbuctoo.experimental.exports.excel.sheet;
 
+import com.google.common.collect.Lists;
+import nl.knaw.huygens.hamcrest.CompositeMatcher;
+import nl.knaw.huygens.hamcrest.PropertyEqualityMatcher;
 import nl.knaw.huygens.timbuctoo.model.vre.Collection;
 import nl.knaw.huygens.timbuctoo.model.vre.Vres;
 import nl.knaw.huygens.timbuctoo.server.GraphWrapper;
 import nl.knaw.huygens.timbuctoo.server.HuygensIng;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.tinkerpop.gremlin.structure.Graph;
@@ -16,17 +21,14 @@ import static nl.knaw.huygens.timbuctoo.util.JsonBuilder.jsnA;
 import static nl.knaw.huygens.timbuctoo.util.JsonBuilder.jsnO;
 import static nl.knaw.huygens.timbuctoo.util.TestGraphBuilder.newGraph;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 public class EntitySheetTest {
 
-
-  private static final int PROPERTY_NAME_ROW = 0;
-  private static final int PROPERTY_TYPE_ROW = 1;
-  private static final int PROPERTY_VALUE_DESCRIPTION_ROW = 2;
   private static final int TIM_ID_COL = 0;
   private static final int RELATION_COL = 1;
   private static final int NAMES_1_KEY_COL = 2;
@@ -34,14 +36,17 @@ public class EntitySheetTest {
   private static final int NAMES_2_KEY_COL = 4;
   private static final int NAMES_2_VALUE_COL = 5;
   private static final int TEMP_NAME_COL = 6;
-  private static final int NAMES_COL = 2;
   private final Vres mappings = HuygensIng.mappings;
   private final Collection collection = mappings.getCollection("wwpersons").get();
   private final String[] relationTypes = null;
   private final String acceptedProp = "wwrelation_accepted";
 
+  private static EntityRowMatcher likeRow() {
+    return new EntityRowMatcher();
+  }
+
   @Test
-  public void renderToWorkbookRendersAnEmptyExcelSheetToWorkBookWhenPresentedWithAnEmptySetOfVertices() {
+  public void renderToWorkbookRendersAnExcelSheetWithOnlyHeaderRows() {
     final GraphWrapper graphWrapper = mock(GraphWrapper.class);
     final SXSSFWorkbook workbook = new SXSSFWorkbook();
     final Graph graph = newGraph().build();
@@ -53,14 +58,14 @@ public class EntitySheetTest {
     instance.renderToWorkbook(graph.traversal().V().toSet());
 
     SXSSFSheet sheet = workbook.getSheet("wwpersons");
-    assertThat(sheet, instanceOf(SXSSFSheet.class));
-    assertCellEqual(sheet, PROPERTY_NAME_ROW, 0, "tim_id");
-    assertCellEqual(sheet, PROPERTY_TYPE_ROW, 0, "uuid");
-    assertThat(sheet.getRow(2).getCell(0), equalTo(null));
-
-    assertThat(sheet.getRow(PROPERTY_NAME_ROW).getCell(1), equalTo(null));
+    assertThat(Lists.newArrayList(sheet.rowIterator()), contains(
+      // header row 1
+      likeRow().withId("tim_id"),
+      // header row 2
+      likeRow().withId("uuid"),
+      // empty header row 3
+      likeRow()));
   }
-
 
   @Test
   public void renderToWorkbookRendersTheVerticesAsExcelCellsToTheSheet() throws IOException {
@@ -73,12 +78,10 @@ public class EntitySheetTest {
         v.withProperty("wwperson_tempName", "temp name");
         v.withProperty("wwperson_names", jsnO("list", jsnA(
           jsnO("components", jsnA(
-            jsnO("type", jsn("FORENAME"), "value", jsn("foreName 1")),
-            jsnO("type", jsn("SURNAME"), "value", jsn("surName 1"))
+            jsnO("type", jsn("FORENAME"), "value", jsn("foreName 1"))
           )),
           jsnO("components", jsnA(
-            jsnO("type", jsn("FORENAME"), "value", jsn("foreName 2")),
-            jsnO("type", jsn("SURNAME"), "value", jsn("surName 2"))
+            jsnO("type", jsn("FORENAME"), "value", jsn("foreName 2"))
           ))
         )).toString());
       })
@@ -99,52 +102,62 @@ public class EntitySheetTest {
     instance.renderToWorkbook(graph.traversal().V().toSet());
     SXSSFSheet sheet = workbook.getSheet("wwpersons");
 
-    // Property name headers
-    assertCellEqual(sheet, PROPERTY_NAME_ROW, TIM_ID_COL, "tim_id");
-    assertCellEqual(sheet, PROPERTY_NAME_ROW, RELATION_COL, "isRelatedTo");
-    assertCellEqual(sheet, PROPERTY_NAME_ROW, TEMP_NAME_COL, "tempName");
-    assertCellEqual(sheet, PROPERTY_NAME_ROW, NAMES_COL, "names");
-
-    // Property type headers
-    assertCellEqual(sheet, PROPERTY_TYPE_ROW, TIM_ID_COL, "uuid");
-    assertCellEqual(sheet, PROPERTY_TYPE_ROW, RELATION_COL, "relation");
-    assertCellEqual(sheet, PROPERTY_TYPE_ROW, TEMP_NAME_COL, "text");
-    assertCellEqual(sheet, PROPERTY_TYPE_ROW, NAMES_COL, "names");
-
-    // Property value description headers
-    assertCellEqual(sheet, PROPERTY_VALUE_DESCRIPTION_ROW, RELATION_COL, "wwpersons");
-    assertCellEqual(sheet, PROPERTY_VALUE_DESCRIPTION_ROW, NAMES_1_KEY_COL, "1");
-    assertCellEqual(sheet, PROPERTY_VALUE_DESCRIPTION_ROW, NAMES_2_KEY_COL, "2");
-
-    // tim_id values
-    assertCellEqual(sheet, 3, TIM_ID_COL, "123");
-    assertCellEqual(sheet, 5, TIM_ID_COL, "234");
-
-
-    // relation values
-    assertCellEqual(sheet, 5, RELATION_COL, "123");
-
-    // names columns
-    assertCellEqual(sheet, 3, NAMES_1_KEY_COL, "forename");
-    assertCellEqual(sheet, 4, NAMES_1_KEY_COL, "surname");
-
-    assertCellEqual(sheet, 3, NAMES_1_VALUE_COL, "foreName 1");
-    assertCellEqual(sheet, 4, NAMES_1_VALUE_COL, "surName 1");
-
-    assertCellEqual(sheet, 3, NAMES_2_KEY_COL, "forename");
-    assertCellEqual(sheet, 4, NAMES_2_KEY_COL, "surname");
-
-    assertCellEqual(sheet, 3, NAMES_2_VALUE_COL, "foreName 2");
-    assertCellEqual(sheet, 4, NAMES_2_VALUE_COL, "surName 2");
-
-    // tempName column
-    assertCellEqual(sheet, 3, TEMP_NAME_COL, "temp name");
-    assertCellEqual(sheet, 5, TEMP_NAME_COL, "tmp 2");
-
+    assertThat(Lists.newArrayList(sheet.rowIterator()), containsInAnyOrder(
+      // header row 1
+      likeRow().withId("tim_id").withRelation("isRelatedTo").withTempName("tempName").withNames("names", "", "", ""),
+      // header row 2
+      likeRow().withId("uuid").withRelation("relation").withTempName("text").withNames("names", "", "", ""),
+      // header row 3
+      likeRow().withRelation("wwpersons").withNames("1", "", "2", ""),
+      likeRow().withId("123").withTempName("temp name").withNames("forename", "foreName 1", "forename", "foreName 2"),
+      likeRow().withId("234").withTempName("tmp 2").withRelation("123")));
   }
-
 
   private void assertCellEqual(SXSSFSheet sheet, int row, int col, String value) {
     assertThat(sheet.getRow(row).getCell(col).getStringCellValue(), equalTo(value));
+  }
+
+  public static class EntityRowMatcher extends CompositeMatcher<Row> {
+
+    private EntityRowMatcher() {
+
+    }
+
+    public EntityRowMatcher withId(String id) {
+      this.addStringMatcher(id, TIM_ID_COL, "tim_id");
+      return this;
+    }
+
+    public EntityRowMatcher withTempName(String tempName) {
+      addStringMatcher(tempName, TEMP_NAME_COL, "tempName");
+      return this;
+    }
+
+    public EntityRowMatcher withRelation(String relationId) {
+      this.addStringMatcher(relationId, RELATION_COL, "relation");
+      return this;
+    }
+
+    public EntityRowMatcher withNames(String name1Key, String name1Value, String name2Key, String name2Value) {
+      this.addStringMatcher(name1Key, NAMES_1_KEY_COL, "name 1 key");
+      this.addStringMatcher(name1Value, NAMES_1_VALUE_COL, "name 1 value");
+
+      this.addStringMatcher(name2Key, NAMES_2_KEY_COL, "name 2 key");
+      this.addStringMatcher(name2Value, NAMES_2_VALUE_COL, "name 2 value");
+
+      return this;
+    }
+
+    private void addStringMatcher(final String value, final int col, final String desc) {
+      this.addMatcher(new PropertyEqualityMatcher<Row, String>(desc, value) {
+        @Override
+        protected String getItemValue(Row item) {
+          Cell cell = item.getCell(col);
+          return cell == null ? "" : cell.getStringCellValue();
+        }
+      });
+    }
+
+
   }
 }
