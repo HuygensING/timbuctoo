849c4aecd28234b7b6cf0ff51cb2d83e9ec029b3
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-06-28 12:32:29 +0200

    [TIM-911] Speed-up improve validation.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/experimental/databaselog/GraphLogValidator.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/experimental/databaselog/GraphLogValidator.java
@@ -1,19 +1,20 @@
 package nl.knaw.huygens.timbuctoo.experimental.databaselog;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import nl.knaw.huygens.timbuctoo.server.GraphWrapper;
-import nl.knaw.huygens.timbuctoo.util.Tuple;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.tinkerpop.gremlin.neo4j.process.traversal.LabelP;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
-import org.apache.tinkerpop.gremlin.structure.Element;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.T;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Set;
 
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.has;
@@ -27,54 +28,130 @@ public class GraphLogValidator {
   }
 
   @SuppressWarnings("unchecked")
-  public Set<Element> validate() {
-    LoggerFactory.getLogger(GraphLogValidator.class).info("Start Vertex validation");
-    Set<Element> elements = Sets.newHashSet();
-
+  public void writeReport(Writer writer) {
+    LOG.info("Start Vertex validation");
     Graph graph = graphWrapper.getGraph();
 
-    Map<Object, Object> next = graph.traversal().V()
-                                    .has("tim_id")
-                                    .has("rev")
-                                    .not(has(T.label, LabelP.of("searchresult"))) // ignore search results
-                                    // ignore the edge entries, because they give false positives
-                                    .not(has(T.label, LabelP.of("createEdgeEntry")))
-                                    .not(has(T.label, LabelP.of("updateEdgeEntry")))
-                                    .group().<Map<Object, List<Vertex>>>by(__.valueMap("tim_id", "rev")).next();
-    next.entrySet().stream().filter(entry -> ((List<Vertex>) entry.getValue()).size() < 2)
-        .forEach(entry -> {
-          Vertex vertex = ((List<Vertex>) entry.getValue()).get(0);
-          elements.add(vertex);
-        });
+    // Map<Object, Object> next = graph.traversal().V()
+    //                                 .has("tim_id")
+    //                                 .has("rev")
+    //                                 .not(has(T.label, LabelP.of("searchresult"))) // ignore search results
+    //                                 // ignore the edge entries, because they give false positives
+    //                                 .not(has(T.label, LabelP.of("createEdgeEntry")))
+    //                                 .not(has(T.label, LabelP.of("updateEdgeEntry")))
+    //                                 .group().<Map<Object, List<Vertex>>>by(__.valueMap("tim_id", "rev")).next();
+    // next.entrySet().stream().filter(entry -> ((List<Vertex>) entry.getValue()).size() < 2)
+    //     .forEach(entry -> {
+    //       Vertex vertex = ((List<Vertex>) entry.getValue()).get(0);
+    //       writeLogString(writer, "Vertex", vertex.id(), vertex.value("tim_id"), vertex.value("rev"));
+    //     });
+
+    Set<ValidationElement> vertexIdsAndVersions = graph.traversal().V()
+                                                       // ignore non domain vertices
+                                                       .not(has(T.label, LabelP.of("searchresult")))
+                                                       .not(has(T.label, LabelP.of("createEdgeEntry")))
+                                                       .not(has(T.label, LabelP.of("updateEdgeEntry")))
+                                                       .not(has(T.label, LabelP.of("createVertexEntry")))
+                                                       .not(has(T.label, LabelP.of("updateVertexEntry")))
+                                                       // ignore the corrupt vertices
+                                                       .has("tim_id")
+                                                       .has("rev")
+                                                       .map(v -> new ValidationElement(
+                                                         "Vertex",
+                                                         v.get().id(),
+                                                         v.get().value("tim_id"),
+                                                         v.get().value("rev")))
+                                                       .toSet();
+
+    Set<ValidationElement> vertexLogEntryIdsAndVersions = graph.traversal().V()
+                                                               .or(has(T.label, LabelP.of("createVertexEntry")),
+                                                                 has(T.label, LabelP.of("updateVertexEntry"))
+                                                               )
+                                                               .map(v -> new ValidationElement(
+                                                                 "Vertex",
+                                                                 v.get().id(),
+                                                                 v.get().value("tim_id"),
+                                                                 v.get().value("rev")))
+                                                               .toSet();
+
+    Set<ValidationElement> missingVertexLogEntries =
+      Sets.difference(vertexIdsAndVersions, vertexLogEntryIdsAndVersions);
+    missingVertexLogEntries.forEach(el -> el.writeLogString(writer));
 
 
     LOG.info("Start Edge validation");
-    Set<Tuple<String, Integer>> edgeIdsAndVersions = graph.traversal().E()
-                                                          // ignore system edges
-                                                          .not(__.hasLabel("VERSION_OF", "NEXT_ITEM"))
-                                                          // ignore the corrupt edges
-                                                          .has("tim_id")
-                                                          .has("rev")
-                                                          .map(e -> new Tuple<>(
-                                                            e.get().<String>value("tim_id"),
-                                                            e.get().<Integer>value("rev")))
-                                                          .toSet();
+    Set<ValidationElement> edgeIdsAndVersions = graph.traversal().E()
+                                                     // ignore system edges
+                                                     .not(__.hasLabel("VERSION_OF", "NEXT_ITEM"))
+                                                     // ignore the corrupt edges
+                                                     .has("tim_id")
+                                                     .has("rev")
+                                                     .map(e -> new ValidationElement(
+                                                       "Edge",
+                                                       e.get().id(),
+                                                       e.get().value("tim_id"),
+                                                       e.get().value("rev")))
+                                                     .toSet();
 
-    Set<Tuple<String, Integer>> edgeLogEntryIdsAndVersions = graph.traversal().V()
-                                                                  .or(__.has(T.label, LabelP.of("createEdgeEntry")),
-                                                                    __.has(T.label, LabelP.of("updateEdgeEntry"))
-                                                                  )
-                                                                  .map(e -> new Tuple<>(
-                                                                    e.get().<String>value("tim_id"),
-                                                                    e.get().<Integer>value("rev")))
-                                                                  .toSet();
+    Set<ValidationElement> edgeLogEntryIdsAndVersions = graph.traversal().V()
+                                                             .or(has(T.label, LabelP.of("createEdgeEntry")),
+                                                               has(T.label, LabelP.of("updateEdgeEntry"))
+                                                             )
+                                                             .map(e -> new ValidationElement(
+                                                               "Edge",
+                                                               e.get().id(),
+                                                               e.get().value("tim_id"),
+                                                               e.get().value("rev")))
+                                                             .toSet();
 
-    Set<Tuple<String, Integer>> missingLogEntries = Sets.difference(edgeIdsAndVersions, edgeLogEntryIdsAndVersions);
-
-    missingLogEntries.forEach(idRev -> elements.add(graph.traversal().E()
-                                                         .has("tim_id", idRev.getLeft())
-                                                         .has("rev", idRev.getRight()).next()));
-    return elements;
+    Set<ValidationElement> missingLogEntries = Sets.difference(edgeIdsAndVersions, edgeLogEntryIdsAndVersions);
+    missingLogEntries.forEach(el -> el.writeLogString(writer));
   }
 
+  private void writeLogString(Writer writer, String type, Object id, Object timId, Object rev) {
+    String logString = String
+      .format("%s with id '%s' and tim_id '%s' and rev '%s' has no log entry.%n", type, id, timId, rev);
+    try {
+      LOG.info("write \"{}\"", logString);
+      writer.write(logString);
+    } catch (IOException e) {
+      LOG.error("Cannot write log string \"{}\"", logString);
+    }
+  }
+
+  private static class ValidationElement {
+    public static final ArrayList<String> EXCLUDE_FIELDS = Lists.newArrayList("id", "type");
+    private final String type;
+    private final Object id;
+    private final Object timId;
+    private final Object rev;
+
+    private ValidationElement(String type, Object id, Object timId, Object rev) {
+      this.type = type;
+      this.id = id;
+      this.timId = timId;
+      this.rev = rev;
+    }
+
+    public void writeLogString(Writer writer) {
+      String logString = String
+        .format("%s with id '%s' and tim_id '%s' and rev '%s' has no log entry.%n", type, id, timId, rev);
+      try {
+        LOG.info("write \"{}\"", logString);
+        writer.write(logString);
+      } catch (IOException e) {
+        LOG.error("Cannot write log string \"{}\"", logString);
+      }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      return EqualsBuilder.reflectionEquals(this, obj, EXCLUDE_FIELDS);
+    }
+
+    @Override
+    public int hashCode() {
+      return HashCodeBuilder.reflectionHashCode(this, EXCLUDE_FIELDS);
+    }
+  }
 }
