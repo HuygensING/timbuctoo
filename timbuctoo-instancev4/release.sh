5dbdd1e58142f164a9a57d688dbc250fc45311fb
Jauco Noordzij <code@jauco.nl>
2016-06-29 17:44:00 +0200

    add release script

---
+++ timbuctoo-instancev4/release.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+currentversion=$(grep "<\!--marker for release.sh-->" pom.xml | grep -o '[^>]\+-SNAPSHOT')
+currentversionNoSnapshot=${currentversion%-SNAPSHOT}
+
+read -p "The current version is '$currentversionNoSnapshot' What should the next version be? e.g. 4.1.0 " newversion
+
+if [ -z "$newversion" ]; then
+  exit 1
+fi
+
+cat pom.xml | sed "s/$currentversion<\/version><\!--marker for release.sh-->/$currentversionNoSnapshot<\/version><\!--marker for release.sh-->/" > pom.changed
+mv pom.changed pom.xml
+
+git add pom.xml
+git commit -m "Release $currentversionNoSnapshot"
+
+git checkout stable
+git merge --ff-only master
+
+tagname=v$currentversionNoSnapshot
+git tag $tagname
+
+git checkout master
+
+cat pom.xml | sed "s/$currentversionNoSnapshot<\/version><\!--marker for release.sh-->/$newversion<\/version><\!--marker for release.sh-->/" > pom.changed
+mv pom.changed pom.xml
+
+git add pom.xml
+git commit -m "Start developing $newversion"
+
+git diff HEAD^^ HEAD^
+git diff HEAD^ HEAD
+
+read -p "The above changes were made by this script. Can I push it? [yn]" PUSH_IT
+
+if [ "$PUSH_IT" = y ]; then
+  git push origin master
+  git push origin stable
+  git push origin $tagname
+fi
