2d241676399e80c784ae5a2a62f0559be4cc93be
renevanderark <r.van.der.ark@gmail.com>
2016-06-30 13:51:02 +0200

    [TIM-897 TIM-906] adds tests for Vre.save().

---
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/model/vre/VreTest.java
@@ -0,0 +1,99 @@
+package nl.knaw.huygens.timbuctoo.model.vre;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import nl.knaw.huygens.timbuctoo.server.GraphWrapper;
+import org.apache.tinkerpop.gremlin.structure.Direction;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static nl.knaw.huygens.timbuctoo.model.vre.Vre.DATABASE_LABEL;
+import static nl.knaw.huygens.timbuctoo.model.vre.Vre.HAS_COLLECTION_RELATION_NAME;
+import static nl.knaw.huygens.timbuctoo.model.vre.Vre.KEYWORD_TYPES_PROPERTY_NAME;
+import static nl.knaw.huygens.timbuctoo.model.vre.Vre.VRE_NAME_PROPERTY_NAME;
+import static nl.knaw.huygens.timbuctoo.model.vre.VreBuilder.vre;
+import static nl.knaw.huygens.timbuctoo.util.TestGraphBuilder.newGraph;
+import static nl.knaw.huygens.timbuctoo.util.VertexMatcher.likeVertex;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class VreTest {
+  private GraphWrapper graphWrapper;
+
+  @Before
+  public void setUp() {
+    Graph graph = newGraph().build();
+    this.graphWrapper = mock(GraphWrapper.class);
+    given(graphWrapper.getGraph()).willReturn(graph);
+  }
+
+
+  @Test
+  public void saveCreatesAVertexForTheVre() {
+    final Vre vre = new Vre("VreName");
+
+    final Vertex result = vre.save(graphWrapper, Optional.empty());
+
+    assertThat(result, likeVertex()
+      .withLabel(DATABASE_LABEL)
+      .withProperty(VRE_NAME_PROPERTY_NAME, "VreName")
+    );
+  }
+
+  @Test
+  public void saveReplacesAnExistingVertexForTheVre() {
+    final Vertex existingVertex = graphWrapper.getGraph().addVertex(DATABASE_LABEL);
+    existingVertex.property(VRE_NAME_PROPERTY_NAME, "VreName");
+    final Vre vre = new Vre("VreName");
+
+    final Vertex result = vre.save(graphWrapper, Optional.empty());
+
+    assertThat(result, equalTo(existingVertex));
+  }
+
+  @Test
+  public void saveAddsKeywordTypesWhenAvailable() throws JsonProcessingException {
+    final Vre vre = new Vre("VreName");
+    Map<String, String> keyWordTypes = Maps.newHashMap();
+    keyWordTypes.put("typeA", "valueA");
+    keyWordTypes.put("typeB", "valueB");
+
+    final Vertex result = vre.save(graphWrapper, Optional.of(keyWordTypes));
+
+    assertThat(result.property(KEYWORD_TYPES_PROPERTY_NAME).value(),
+      equalTo(new ObjectMapper().writeValueAsString(keyWordTypes))
+    );
+  }
+
+  @Test
+  public void saveAddsVreCollectionsToTheVre() {
+    Vre vre = vre("VreName", "prefix")
+      .withCollection("prefixpersons")
+      .withCollection("prefixdocuments")
+      .build();
+
+    final Vertex savedVertex = vre.save(graphWrapper, Optional.empty());
+    final List<Vertex> result = Lists.newArrayList(savedVertex.vertices(Direction.OUT, HAS_COLLECTION_RELATION_NAME));
+
+    assertThat(result, containsInAnyOrder(
+      likeVertex()
+        .withLabel(Collection.DATABASE_LABEL)
+        .withProperty(Collection.COLLECTION_NAME_PROPERTY_NAME, "prefixpersons"),
+      likeVertex()
+        .withLabel(Collection.DATABASE_LABEL)
+        .withProperty(Collection.COLLECTION_NAME_PROPERTY_NAME, "prefixdocuments")
+    ));
+
+  }
+}
