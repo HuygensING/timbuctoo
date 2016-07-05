26def8e52cf53f1d56cbc487244a81ff0ead4eca
renevanderark <r.van.der.ark@gmail.com>
2016-07-05 09:21:29 +0200

    [TIM-897 TIM-928] Adds test for ReadableProperty.load().

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/model/properties/ReadablePropertyTest.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/model/properties/ReadablePropertyTest.java
@@ -1,16 +1,25 @@
 package nl.knaw.huygens.timbuctoo.model.properties;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.knaw.huygens.timbuctoo.model.properties.converters.HasOptions;
+import nl.knaw.huygens.timbuctoo.model.properties.converters.StringToUnencodedStringOfLimitedValuesConverter;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import static nl.knaw.huygens.timbuctoo.model.properties.ReadableProperty.CLIENT_PROPERTY_NAME;
 import static nl.knaw.huygens.timbuctoo.model.properties.ReadableProperty.DATABASE_LABEL;
 import static nl.knaw.huygens.timbuctoo.model.properties.ReadableProperty.PROPERTY_TYPE_NAME;
 import static nl.knaw.huygens.timbuctoo.util.TestGraphBuilder.newGraph;
 import static nl.knaw.huygens.timbuctoo.util.VertexMatcher.likeVertex;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 
 public class ReadablePropertyTest {
   private Graph graph;
@@ -41,5 +50,26 @@ public class ReadablePropertyTest {
       .withProperty(PROPERTY_TYPE_NAME, "type-id")
       .withLabel(DATABASE_LABEL)
     );
+  }
+
+  @Test
+  public void loadLoadsAPropertyConfiguration()
+    throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException,
+    IOException {
+
+    final String[] options = {"a", "b"};
+    final Vertex vertex = graph.addVertex(DATABASE_LABEL);
+    final String typeIdentifier = new StringToUnencodedStringOfLimitedValuesConverter(new String[]{})
+      .getUniqueTypeIdentifier();
+    vertex.property(CLIENT_PROPERTY_NAME, "clientName");
+    vertex.property(PROPERTY_TYPE_NAME, typeIdentifier);
+    vertex.property(LocalProperty.OPTIONS_PROPERTY_NAME, new ObjectMapper().writeValueAsString(options));
+    vertex.property(LocalProperty.DATABASE_PROPERTY_NAME, "dbName");
+
+    ReadableProperty result = ReadableProperty.load(vertex);
+
+    assertThat(result, instanceOf(LocalProperty.class));
+    assertThat(result.getUniqueTypeId(), equalTo(typeIdentifier));
+    assertThat(((LocalProperty) result).getOptions().get(), contains("a", "b"));
   }
 }
