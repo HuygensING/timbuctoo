9263c3bc32ec04ab186bc820dd0fd759e62656be
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-07-08 09:02:58 +0200

    Ignore empty lines in uses file.

--- timbuctoo-instancev4/src/main/resources/service/localUserImporter.sh
+++ timbuctoo-instancev4/src/main/resources/service/localUserImporter.sh
@@ -8,6 +8,12 @@ do
   then
     VALUES=($(echo $LINE | tr ";" "\n"))
 
+    if [[ -z $VALUES[0] ]]  && [[ -z $VALUES[1] ]] && [[ -z $VALUES[2] ]];
+    then
+      echo "Ignore line $LINE"
+      continue
+    fi
+
     REQUEST="userPid=${VALUES[0]}&userName=${VALUES[1]}&password=${VALUES[2]}&givenName=${VALUES[3]}&surname=${VALUES[4]}&emailAddress=${VALUES[5]}&organization=${VALUES[6]}&vreId=${VALUES[7]}&vreRole=${VALUES[8]}"
 
     echo -e ${REQUEST}
