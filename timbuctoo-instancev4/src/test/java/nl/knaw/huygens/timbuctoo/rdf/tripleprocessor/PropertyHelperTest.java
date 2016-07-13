afe7dbfe6af74b9fa9dba3ed35eb94fdc3b1f435
renevanderark <r.van.der.ark@gmail.com>
2016-07-13 09:26:08 +0200

    [TIM-954] Improves tests for PropertyHelper.

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/rdf/tripleprocessor/PropertyHelperTest.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/rdf/tripleprocessor/PropertyHelperTest.java
@@ -17,18 +17,45 @@ public class PropertyHelperTest {
     final String vreName = "vreName";
     final String entityTypeName = "newCollection";
     final CollectionDescription newCollectionDescription = new CollectionDescription(entityTypeName, vreName);
-    final CollectionDescription existingCollectionDescription =
-      new CollectionDescription("existingCollection", vreName);
-    final List<CollectionDescription> existingCollections = Lists.newArrayList(existingCollectionDescription);
+    final CollectionDescription existingCollectionDescription1 =
+      new CollectionDescription("existingCollection1", vreName);
+    final CollectionDescription existingCollectionDescription2 =
+      new CollectionDescription("existingCollection2", vreName);
+    final List<CollectionDescription> existingCollections = Lists.newArrayList(
+      existingCollectionDescription1,
+      existingCollectionDescription2,
+      newCollectionDescription
+    );
     final Vertex vertex = newGraph().build().addVertex();
-    vertex.property(existingCollectionDescription.createPropertyName("existing"), "value1");
-    vertex.property(CollectionDescription.getDefault(vreName).createPropertyName("unknownExisting"), "value2");
+    vertex.property(existingCollectionDescription1.createPropertyName("existing_1"), "value1");
+    vertex.property(existingCollectionDescription1.createPropertyName("existing_2"), "value2");
+    vertex.property(existingCollectionDescription2.createPropertyName("existing_1"), "value1");
+    vertex.property(existingCollectionDescription2.createPropertyName("existing_2"), "value2");
 
     new PropertyHelper().setCollectionProperties(vertex, newCollectionDescription, existingCollections);
 
     assertThat(vertex, likeVertex()
-      .withProperty(newCollectionDescription.createPropertyName("existing"), "value1")
-      .withProperty(newCollectionDescription.createPropertyName("unknownExisting"), "value2"));
+      .withProperty(newCollectionDescription.createPropertyName("existing_1"), "value1")
+      .withProperty(newCollectionDescription.createPropertyName("existing_2"), "value2")
+    );
+  }
+
+  @Test
+  public void setCollectionPropertiesDuplicatesTheExistingPropertiesFromTheDefaultCollection() {
+    final String vreName = "vreName";
+    final String entityTypeName = "newCollection";
+    final CollectionDescription newCollectionDescription = new CollectionDescription(entityTypeName, vreName);
+    final List<CollectionDescription> existingCollections = Lists.newArrayList(newCollectionDescription);
+    final Vertex vertex = newGraph().build().addVertex();
+    vertex.property(CollectionDescription.getDefault(vreName).createPropertyName("prop1"), "value1");
+    vertex.property(CollectionDescription.getDefault(vreName).createPropertyName("prop2"), "value2");
+
+    new PropertyHelper().setCollectionProperties(vertex, newCollectionDescription, existingCollections);
+
+    assertThat(vertex, likeVertex()
+      .withProperty(newCollectionDescription.createPropertyName("prop1"), "value1")
+      .withProperty(newCollectionDescription.createPropertyName("prop2"), "value2")
+    );
   }
 
   @Test
@@ -36,10 +63,9 @@ public class PropertyHelperTest {
     final String vreName = "vreName";
     final String entityTypeName = "newCollection";
     final CollectionDescription newCollectionDescription = new CollectionDescription(entityTypeName, vreName);
-    final List<CollectionDescription> existingCollections = Lists.newArrayList();
+    final List<CollectionDescription> existingCollections = Lists.newArrayList(newCollectionDescription);
     final Vertex vertex = newGraph().build().addVertex();
     final String unknownExisting = CollectionDescription.getDefault(vreName).createPropertyName("unknownExisting");
-
     vertex.property(unknownExisting, "value2");
 
     new PropertyHelper().setCollectionProperties(vertex, newCollectionDescription, existingCollections);
