d8e4d2f70f3016ea679881a631256e4f4bf662f3
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-07-15 16:09:23 +0200

    [TIM-954] Update the labels in the TypesHelper.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/TypesHelper.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/TypesHelper.java
@@ -1,18 +1,31 @@
 package nl.knaw.huygens.timbuctoo.rdf;
 
+import nl.knaw.huygens.timbuctoo.crud.changelistener.AddLabelChangeListener;
 import nl.knaw.huygens.timbuctoo.util.JsonBuilder;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
 import static nl.knaw.huygens.timbuctoo.util.JsonBuilder.jsnA;
 
 public class TypesHelper {
+  private final AddLabelChangeListener labelChangeListener;
+
+  public TypesHelper() {
+    this(new AddLabelChangeListener());
+  }
+
+  TypesHelper(AddLabelChangeListener labelChangeListener) {
+    this.labelChangeListener = labelChangeListener;
+  }
+
   public void updateTypeInformation(Vertex vertex, Set<Collection> collections) {
     Stream<String> typesStream =
       collections.stream().map(collection -> collection.getDescription().getEntityTypeName());
 
     vertex.property("types", jsnA(typesStream.map(JsonBuilder::jsn)).toString());
+    labelChangeListener.onUpdate(Optional.empty(), vertex);
   }
 }
--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/rdf/TypesHelperTest.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/rdf/TypesHelperTest.java
@@ -3,11 +3,14 @@ package nl.knaw.huygens.timbuctoo.rdf;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Sets;
+import nl.knaw.huygens.timbuctoo.crud.changelistener.AddLabelChangeListener;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -18,14 +21,24 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class TypesHelperTest {
+
+  private TypesHelper instance;
+  private AddLabelChangeListener labelChangeListener;
+
+  @Before
+  public void setup() {
+
+    labelChangeListener = mock(AddLabelChangeListener.class);
+    instance = new TypesHelper(labelChangeListener);
+  }
+
   @Test
   public void updateTypeInformationGeneratesANewTypesPropertyFromTheCollections() throws Exception {
+    Vertex vertex = mock(Vertex.class);
     Collection collection1 = mock(Collection.class);
     when(collection1.getDescription()).thenReturn(new CollectionDescription("entityTypeName", "vreName"));
     Collection collection2 = mock(Collection.class);
     when(collection2.getDescription()).thenReturn(new CollectionDescription("otherEntityType", "vreName"));
-    TypesHelper instance = new TypesHelper();
-    Vertex vertex = mock(Vertex.class);
 
     instance.updateTypeInformation(vertex, Sets.newHashSet(collection1, collection2));
 
@@ -35,6 +48,20 @@ public class TypesHelperTest {
     });
     // TODO Check if archetype is added
     assertThat(typesList, containsInAnyOrder("entityTypeName", "otherEntityType"));
+  }
+
+  @Test
+  public void updateTypeInformationUpdatesTheLabels() {
+    Collection collection1 = mock(Collection.class);
+    when(collection1.getDescription()).thenReturn(new CollectionDescription("entityTypeName", "vreName"));
+    Collection collection2 = mock(Collection.class);
+    when(collection2.getDescription()).thenReturn(new CollectionDescription("otherEntityType", "vreName"));
+
+    Vertex vertex = mock(Vertex.class);
+
+    instance.updateTypeInformation(vertex, Sets.newHashSet(collection1, collection2));
+
+    verify(labelChangeListener).onUpdate(Optional.empty(), vertex);
   }
 
 
