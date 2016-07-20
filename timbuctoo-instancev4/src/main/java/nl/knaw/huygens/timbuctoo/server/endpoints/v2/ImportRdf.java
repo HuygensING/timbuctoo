fffe6db5743e5b1e166cc5271e5ddc4536da7c13
renevanderark <r.van.der.ark@gmail.com>
2016-07-20 12:31:20 +0200

    [TIM-1006] Adds streamig file upload resource to ImportRdf.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/ImportRdf.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/ImportRdf.java
@@ -4,11 +4,14 @@ import nl.knaw.huygens.timbuctoo.model.vre.Vres;
 import nl.knaw.huygens.timbuctoo.rdf.RdfImporter;
 import nl.knaw.huygens.timbuctoo.server.GraphWrapper;
 import org.apache.jena.riot.Lang;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -35,6 +38,15 @@ public class ImportRdf {
     final ImportRunner importRunner = new ImportRunner(rdfImporter, triples);
 
     rfdExecutorService.submit(importRunner);
+  }
+
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  @POST
+  public void upload(@FormDataParam("file") final InputStream triples,
+                     @HeaderParam("VRE_ID") String vreName) {
+    final RdfImporter rdfImporter = new RdfImporter(graphWrapper, vreName, vres);
+
+    rdfImporter.importRdf(triples, Lang.NQUADS);
   }
 
   private static final class ImportRunner implements Runnable {
