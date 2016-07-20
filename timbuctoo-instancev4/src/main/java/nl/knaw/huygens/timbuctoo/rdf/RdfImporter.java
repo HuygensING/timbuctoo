e55fbc6073339b714a824449fa1baa1764133562
renevanderark <r.van.der.ark@gmail.com>
2016-07-20 12:15:05 +0200

    [TIM-1006] RdfImporter now streams triples and does batched commits; ImportRdf starts background task.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/RdfImporter.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/RdfImporter.java
@@ -1,12 +1,20 @@
 package nl.knaw.huygens.timbuctoo.rdf;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Lists;
 import nl.knaw.huygens.timbuctoo.model.vre.Vres;
 import nl.knaw.huygens.timbuctoo.server.GraphWrapper;
+import org.apache.jena.atlas.web.TypedInputStream;
 import org.apache.jena.graph.Triple;
-import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.system.StreamRDF;
+import org.apache.jena.sparql.core.Quad;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.List;
 
 public class RdfImporter {
   public static final Logger LOG = LoggerFactory.getLogger(RdfImporter.class);
@@ -29,16 +37,68 @@ public class RdfImporter {
     this.vres = vres;
   }
 
-  public void importRdf(Model model) {
+  public void importRdf(InputStream triples, Lang lang) {
     final Stopwatch stopwatch = Stopwatch.createStarted();
+    final StreamRDF tripleStreamReader = new TripleStreamReader();
+
+    RDFDataMgr.parse(tripleStreamReader, new TypedInputStream(triples), lang);
+
+    LOG.info("Import took {}", stopwatch.stop());
+
+  }
+
+  private void complete() {
+    graphWrapper.getGraph().tx().commit();
+    vres.reload();
+  }
+
+  private void prepare() {
     importPreparer.setupVre(vreName);
     importPreparer.setUpAdminVre();
     LOG.info("Starting import...");
-
-    model.getGraph().find(Triple.ANY).forEachRemaining(tripleImporter::importTriple);
-    graphWrapper.getGraph().tx().commit();
-    vres.reload();
-    LOG.info("Import took {}", stopwatch.stop());
   }
 
+
+  private final class TripleStreamReader implements StreamRDF {
+    private final List<Triple> batch = Lists.newArrayList();
+
+    @Override
+    public void start() {
+      prepare();
+    }
+
+    @Override
+    public void triple(Triple triple) {
+      batch.add(triple);
+      flushBatch(1000);
+    }
+
+    @Override
+    public void quad(Quad quad) {
+      batch.add(quad.asTriple());
+      flushBatch(1000);
+    }
+
+    private void flushBatch(int size) {
+      if (size < 0 || batch.size() >= size) {
+        batch.forEach(tripleImporter::importTriple);
+        graphWrapper.getGraph().tx().commit();
+        batch.clear();
+      }
+    }
+
+    @Override
+    public void base(String base) {
+    }
+
+    @Override
+    public void prefix(String prefix, String iri) {
+    }
+
+    @Override
+    public void finish() {
+      flushBatch(-1);
+      complete();
+    }
+  }
 }
