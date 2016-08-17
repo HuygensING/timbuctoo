1e6a396b0c2d528f6d8489c7065f8e30b83201a3
Jauco Noordzij <code@jauco.nl>
2016-08-17 11:58:49 +0200

    remove unused project

--- timbuctoo-benchmark/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=defaultrecording=true,dumponexit=true -jar target/benchmarks.jar -wf 1 -wi 5 -f 1 -i 5 -prof stack
