99227e7d75d8d7c300a1e8923cd80d0d70f48a01
Jauco Noordzij <code@jauco.nl>
2016-08-19 09:12:10 +0200

    wrong server in edit gui

--- timbuctoo-instancev4/src/main/resources/static/edit-gui/js/index.js
+++ timbuctoo-instancev4/src/main/resources/static/edit-gui/js/index.js
@@ -12767,8 +12767,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = {
 	apiUrl: {
-		"v2.1": "http://localhost:8080/v2.1",
-		"v4": "http://localhost:8080/v2.1"
+		"v2.1": "/v2.1",
+		"v4": "/v2.1"
 	},
 	apiVersion: "v4"
 };
