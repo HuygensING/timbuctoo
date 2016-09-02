91100f3f70e10a3172e1c8a96d2c269e23ed58cd
renevanderark <r.van.der.ark@gmail.com>
2016-09-02 16:51:36 +0200

    [TIM-1094] Adds SolrIO.

---
+++ timbuctoo-instancev4/src/main/scripts/index_scripts/complete-sample-runner.rb
@@ -0,0 +1,118 @@
+require './lib/timbuctoo_solr/timbuctoo_io'
+require './lib/timbuctoo_solr/solr_io'
+require './lib/timbuctoo_solr/default_mapper'
+require './lib/mixins/converters/to_year_converter'
+require './lib/mixins/converters/to_names_converter'
+
+
+
+@sample_default_config = {
+    :properties => [
+        { :name => '_id',  :converted_name => 'id'},
+        { :name => '@displayName', :converted_name => 'displayName_s'},
+        { :name => 'types', :converted_name => 'types_ss'},
+        { :name => 'gender', :converted_name => 'gender_s'},
+        { :name => 'birthDate', :converted_name => 'birthDate_i', :type => 'int'},
+        { :name => 'deathDate', :converted_name => 'deathDate_i', :type => 'int'},
+        { :name => 'notes', :converted_name => 'notes_t'},
+        { :name => 'children', :converted_name => 'children_s'},
+        { :name => ['^modified', 'timeStamp'], :converted_name => 'modified_l' }
+    ], :relations => [
+        { :relation_name => ['hasResidenceLocation', 'hasBirthPlace', 'hasDeathPlace'], :property_name => 'displayName', :converted_name => 'relatedLocations_ss' },
+        { :relation_name => 'hasBirthPlace', :property_name => 'displayName', :converted_name => 'birthPlace_ss' },
+        { :relation_name => 'hasDeathPlace', :property_name => 'displayName', :converted_name => 'deathPlace_ss' },
+    ]
+}
+
+@sample_custom_config = {
+    :properties => [
+        { :name => '_id',  :converted_name => 'id'},
+        { :name => 'names', :converted_name => 'name_t', :type => 'names'},
+        { :name => 'names', :converted_name => 'nameSort_s', :type => 'name_sort'},
+        { :name => 'names', :converted_name => 'displayName_s', :type => 'names_display_name'},
+        { :name => 'types', :converted_name => 'types_ss'},
+        { :name => 'gender', :converted_name => 'gender_s'},
+        { :name => 'birthDate', :converted_name => 'birthDate_i', :type => 'year'},
+        { :name => 'deathDate', :converted_name => 'deathDate_i', :type => 'year'},
+        { :name => 'notes', :converted_name => 'notes_t'},
+        { :name => 'children', :converted_name => 'children_s'},
+        { :name => ['^modified', 'timeStamp'], :converted_name => 'modified_l' },
+        { :name => '@displayName', :converted_name => '@displayName'}
+    ], :relations => [
+      { :relation_name => ['hasResidenceLocation', 'hasBirthPlace', 'hasDeathPlace'], :property_name => 'displayName', :converted_name => 'relatedLocations_ss' },
+      { :relation_name => 'hasBirthPlace', :property_name => 'displayName', :converted_name => 'birthPlace_ss' },
+      { :relation_name => 'hasDeathPlace', :property_name => 'displayName', :converted_name => 'deathPlace_ss' },
+  ]
+}
+
+
+
+class WwPersonConverter < DefaultMapper
+  include ToYearConverter
+  include ToNamesConverter
+end
+
+
+class SampleRunner
+
+  def initialize(sample_default_config, sample_custom_config)
+    @default_batch = []
+    @custom_batch = []
+    @timbuctoo_io = TimbuctooIO.new('http://localhost:8089', {:dump_files => true, :dump_dir => '/home/rar011/tmp'})
+    @default_mapper = DefaultMapper.new(sample_default_config)
+    @custom_mapper = WwPersonConverter.new(sample_custom_config)
+    @solr_io = SolrIO.new("http://localhost:8983/solr")
+  end
+
+
+  def process_record (record)
+    @default_batch << @default_mapper.convert(record)
+    @custom_batch << post_process_custom_mapping(record)
+
+    if @custom_batch.length >= 100
+      @solr_io.update("test_custom_index", @custom_batch)
+      @custom_batch = []
+    end
+
+    if @default_batch.length >= 100
+      @solr_io.update("test_default_index", @default_batch)
+      @default_batch = []
+    end
+
+  end
+
+  def post_process_custom_mapping(record)
+    converted_by_custom_mapper = @custom_mapper.convert(record)
+    if converted_by_custom_mapper['displayName_s'].empty?
+      converted_by_custom_mapper['name_t'] = converted_by_custom_mapper['@displayName'].sub('[TEMP] ', '')
+      converted_by_custom_mapper['nameSort_s'] = converted_by_custom_mapper['@displayName'].sub('[TEMP] ', '')
+      converted_by_custom_mapper['displayName_s'] = converted_by_custom_mapper['@displayName'].sub('[TEMP] ', '')
+    end
+    converted_by_custom_mapper.delete('@displayName')
+    converted_by_custom_mapper
+  end
+
+  def run
+    @solr_io.delete_index("test_custom_index")
+    @solr_io.delete_index("test_default_index")
+
+    @solr_io.create("test_custom_index")
+    @solr_io.create("test_default_index")
+
+    @solr_io.delete_data("test_custom_index")
+    @solr_io.delete_data("test_default_index")
+
+    @timbuctoo_io.scrape_collection('wwpersons', {:process_record => method(:process_record), :with_relations => true, :from_file => true}) # fancy ruby shorthand
+    #@timbuctoo_io.scrape_collection('wwpersons', {:process_record => -> (record) {process_record(record)}}) # short lambda syntax
+    #@timbuctoo_io.scrape_collection('wwpersons', {:process_record => lambda {|record| process_record(record)}}) # lambda syntax
+
+    @solr_io.update("test_custom_index", @custom_batch)
+    @solr_io.update("test_default_index", @default_batch)
+    @solr_io.commit("test_custom_index")
+    @solr_io.commit("test_default_index")
+  end
+end
+
+SampleRunner.new(@sample_default_config, @sample_custom_config).run
+
+
--- timbuctoo-instancev4/src/main/scripts/index_scripts/lib/timbuctoo_solr/solr_io.rb
+++ timbuctoo-instancev4/src/main/scripts/index_scripts/lib/timbuctoo_solr/solr_io.rb
@@ -1,2 +1,77 @@
+require 'net/http'
+require 'json'
+
 class SolrIO
+
+  # @param [String] base_url the solr base_url (usually including /solr)
+  # @param [String] authorization the authorization header
+  def initialize(base_url, authorization: nil)
+    @base_url = base_url
+    @authorization = authorization
+  end
+
+  # Creates a new solr index
+  # @param [String] index_name name of the index
+  # @param [String] config_set the config set used
+  def create(index_name, config_set: 'data_driven_schema_configs')
+    uri = URI.parse("#{@base_url}/admin/cores?action=CREATE&name=#{index_name}&instanceDir=#{index_name}&configSet=#{config_set}")
+    req = Net::HTTP::Post.new(uri)
+    req['Authorization'] = @authorization unless @authorization.nil?
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    response = http.request(req)
+
+    raise "Create of #{index_name} failed for url #{@base_url}" unless response.code.eql?('200')
+  end
+
+  # Sends an update request with a payload to solr index (without commit)
+  # @param [String] index_name name of the index
+  # @param [Hash|Array] payload the Json serializable payload
+  def update(index_name, payload)
+    uri = URI.parse("#{@base_url}/#{index_name}/update/")
+    req = Net::HTTP::Post.new(uri)
+    req.content_type = "application/json"
+    req['Authorization'] = @authorization unless @authorization.nil?
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    req.body = payload.to_json
+    response = http.request(req)
+
+    raise "Update on #{index_name} failed for url #{@base_url}" unless response.code.eql?('200')
+  end
+
+  # Sends a commit request to solr
+  # @param [String] index_name name of the index
+  def commit(index_name)
+    uri = URI.parse("#{@base_url}/#{index_name}/update?commit=true")
+    req = Net::HTTP::Post.new(uri)
+    req['Authorization'] = @authorization unless @authorization.nil?
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    response = http.request(req)
+
+    raise "Commit on #{index_name} failed for url #{@base_url}" unless response.code.eql?('200')
+  end
+
+  # Deletes all contents of a solr index
+  # @param [String] index_name name of the index
+  def delete_data(index_name)
+    uri = URI.parse("#{@base_url}/#{index_name}/update/")
+    req = Net::HTTP::Post.new(uri)
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    req.content_type = 'text/xml'
+    req['Authorization'] = @authorization unless @authorization.nil?
+    req.body = '<delete><query>*:*</query></delete>'
+    response = http.request(req)
+
+    raise "Delete on #{index_name} failed for url #{@base_url}" unless response.code.eql?('200')
+  end
+
+  def delete_index(index_name)
+    uri = URI.parse("#{@base_url}/admin/collections?action=DELETE&name=#{index_name}")
+    req = Net::HTTP::Post.new(uri)
+    req['Authorization'] = @authorization unless @authorization.nil?
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    response = http.request(req)
+
+    raise "Delete of #{index_name} failed for url #{@base_url}" unless response.code.eql?('200')
+  end
+
 end
