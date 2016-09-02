99c3da9c1e8962fa878123bfb379ee993ad4e05f
renevanderark <r.van.der.ark@gmail.com>
2016-09-02 13:28:07 +0200

    [TIM-1093] TimbuctooIO with docblocks and keyword arguments.

--- timbuctoo-instancev4/src/main/scripts/index_scripts/lib/solr/timbuctoo_io.rb
+++ timbuctoo-instancev4/src/main/scripts/index_scripts/lib/solr/timbuctoo_io.rb
@@ -3,37 +3,38 @@ require 'json'
 
 class TimbuctooIO
 
-  attr_accessor :base_url, :dump_dir, :dump_files
-
-  def initialize (base_url, options = {:dump_files => false, :dump_dir => nil})
+  # @param [String] base_url the timbuctoo server base url
+  # @params [Boolean] dump_files flag for dumping files
+  # @params [String] dump_dir the directory to dump the files in
+  def initialize (base_url, dump_files: false, dump_dir: "./")
     @base_url = base_url
-    @dump_files = options[:dump_files]
-    @dump_dir = options[:dump_dir]
+    @dump_files = dump_files
+    @dump_dir = dump_dir || './'
   end
 
 
+  # Scrapes an entire Timbuctoo collection
+  # @param [String] collection_name the name of the timbuctoo collection
+  # @param [lambda] process_record the function to invoke for each single record in the batch
+  # @param [Boolean] with_relations also scrape the direct relations of the records
+  # @param [Integer] batch_size number of records per batch
+  # @param [Boolean] from_file read from local file in stead of from http if the file is present
   def scrape_collection (collection_name,
-                         process_record = lambda {|record| puts record.inspect },
-                         with_relations = false,
-                         batch_size = 100)
+                         process_record: lambda {|record| puts record.inspect },
+                         with_relations: false,
+                         batch_size: 100,
+                         from_file: false)
 
     start_value = 0
     data = nil
     while data.nil? or data.length > 0
-      location = "#{@base_url}/v2.1/domain/#{collection_name}" +
-          "?rows=#{batch_size}" +
-          "&start=#{start_value}" +
-          (with_relations ? '&withRelations=true' : '')
-      uri = URI.parse(location)
-      req = Net::HTTP::Get.new(uri)
-      http = Net::HTTP.new(uri.hostname, uri.port)
+      json_data = nil
+      json_data = get_file_batch(batch_size, collection_name, start_value, with_relations) if from_file
+      json_data = get_http_batch(batch_size, collection_name, start_value, with_relations) if json_data.nil?
 
-      response = http.request(req)
+      dump_to_file(collection_name, batch_size, start_value, with_relations, json_data) if @dump_files
 
-      raise "http request to failed with status #{response.code}: #{location}" unless response.code.eql?("200")
-
-      dump_to_file(collection_name, batch_size, start_value, with_relations, response.body) if dump_files and !dump_dir.nil?
-      data = JSON.parse(response.body)
+      data = JSON.parse(json_data)
 
       data.each {|record| process_record.call(record) }
 
@@ -41,9 +42,36 @@ class TimbuctooIO
     end
   end
 
+  private
+  def get_file_batch(batch_size, collection_name, start_value, with_relations)
+    filename = get_dump_filename(batch_size, collection_name, start_value, with_relations)
+    return File.read filename if File.exists? filename
+    nil
+  end
+
+  def get_http_batch(batch_size, collection_name, start_value, with_relations)
+    location = "#{@base_url}/v2.1/domain/#{collection_name}" +
+        "?rows=#{batch_size}" +
+        "&start=#{start_value}" +
+        (with_relations ? '&withRelations=true' : '')
+    uri = URI.parse(location)
+    req = Net::HTTP::Get.new(uri)
+    http = Net::HTTP.new(uri.hostname, uri.port)
+
+    response = http.request(req)
+    raise "http request to failed with status #{response.code}: #{location}" unless response.code.eql?("200")
+    json_data = response.body
+  end
+
   def dump_to_file (collection_name, batch_size, start_value, with_relations, response_body)
-    filename = "#{dump_dir.sub(/\/$/, '')}/#{collection_name}_rows_#{batch_size}_start_#{start_value}_#{with_relations ? "with_relations" : ""}.json"
+    filename = get_dump_filename(batch_size, collection_name, start_value, with_relations)
     File.open(filename, 'w') { |file| file.write(response_body) }
+  end
+
+  def get_dump_filename(batch_size, collection_name, start_value, with_relations)
+    "#{@dump_dir.sub(/\/$/, '')}/" +
+        "#{collection_name}_rows_#{batch_size}_start_#{start_value}" +
+        "#{with_relations ? '_with_relations' : ''}.json"
   end
 
 end
--- timbuctoo-instancev4/src/main/scripts/index_scripts/run.rb
+++ timbuctoo-instancev4/src/main/scripts/index_scripts/run.rb
@@ -9,6 +9,6 @@ def process_record record
 end
 
 
-timbuctoo_io.scrape_collection("wwpersons", method(:process_record)) # fancy ruby shorthand
-#timbuctoo_io.scrape_collection("wwpersons", -> (record) {process_record(record)}) # short lambda syntax
-#timbuctoo_io.scrape_collection("wwpersons", lambda {|record| process_record(record)}) # lambda syntax
+timbuctoo_io.scrape_collection("wwpersons", {:process_record => method(:process_record) }) # fancy ruby shorthand
+#timbuctoo_io.scrape_collection("wwpersons", {:process_record => -> (record) {process_record(record)}}) # short lambda syntax
+#timbuctoo_io.scrape_collection("wwpersons", {:process_record => lambda {|record| process_record(record)}}) # lambda syntax
