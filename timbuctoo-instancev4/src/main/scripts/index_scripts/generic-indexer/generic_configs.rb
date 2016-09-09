1ec608303ef1aad06a053e4794c4e5a3be16d1a5
renevanderark <r.van.der.ark@gmail.com>
2016-09-09 11:49:34 +0200

    adds generic index script.

---
+++ timbuctoo-instancev4/src/main/scripts/index_scripts/generic-indexer/generic_configs.rb
@@ -0,0 +1,47 @@
+require 'open-uri'
+require 'json'
+
+class GenericConfigs
+
+  def initialize(timbuctoo_url:, vre_id:)
+    @timbuctoo_url = timbuctoo_url
+    @vre_id = vre_id
+  end
+
+  def fetch
+    open("#{@timbuctoo_url}/v2.1/metadata/#{@vre_id}?withCollectionInfo=true") do |f|
+      @metadata = JSON.parse(f.read)
+                      .map {|k, md| md}
+                      .reject {|md| md['unknown'] or md['relationCollection']}
+    end
+
+    open("#{@timbuctoo_url}/v2.1/metadata/Admin?withCollectionInfo=true") do |f|
+      @archetype_metadata = JSON.parse(f.read)
+                               .map {|k, md| md}
+                               .reject {|md| md['unknown'] or md['relationCollection']}
+    end
+
+    @metadata.map do |collection_metadata|
+      archetype_mapping = @archetype_metadata
+                           .select {|amd| amd['archetypeName'].eql?(collection_metadata['archetypeName']) }
+                           .first
+      {
+          :collection => collection_metadata['collectionName'],
+          :properties => archetype_mapping['properties']
+              .reject {|prop| prop['type'].eql?("relation") }
+              .map {|prop| prop['type'].eql?("datable") ?
+                    { :name => prop['name'], :converted_name => "#{prop['name']}_i", :type => "year" }
+                  : { :name => prop['name'], :converted_name => "#{prop['name']}_s" }
+              } <<  { :name => '@displayName', :converted_name => 'displayName_s'}  <<
+                    { :name => '_id', :converted_name => 'id' } ,
+          :relations => archetype_mapping['properties']
+              .select {|prop| prop['type'].eql?("relation") }
+              .map {|rel| {
+                      :relation_name => rel['name'],
+                      :property_name => 'displayName',
+                      :converted_name => "#{rel['name']}_ss"
+              }}
+      }
+    end
+  end
+end
