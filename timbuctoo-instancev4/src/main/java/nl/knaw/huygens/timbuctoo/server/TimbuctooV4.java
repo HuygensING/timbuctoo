67bf0ba4373cfb653aad490a6ccc123615cfe473
Jauco Noordzij <code@jauco.nl>
2016-09-12 10:08:12 +0200

    Turn pids into handle hyperlinks
    some pids were saved as only the UUID without the handle adress

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/TimbuctooV4.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/TimbuctooV4.java
@@ -44,6 +44,7 @@ import nl.knaw.huygens.timbuctoo.security.JsonBasedUserStore;
 import nl.knaw.huygens.timbuctoo.security.LoggedInUserStore;
 import nl.knaw.huygens.timbuctoo.server.databasemigration.DatabaseMigration;
 import nl.knaw.huygens.timbuctoo.server.databasemigration.FixDcarKeywordDisplayNameMigration;
+import nl.knaw.huygens.timbuctoo.server.databasemigration.MakePidsAbsoluteUrls;
 import nl.knaw.huygens.timbuctoo.server.databasemigration.ScaffoldMigrator;
 import nl.knaw.huygens.timbuctoo.server.endpoints.RootEndpoint;
 import nl.knaw.huygens.timbuctoo.server.endpoints.legacy.LegacyApiRedirects;
@@ -168,6 +169,7 @@ public class TimbuctooV4 extends Application<TimbuctooConfiguration> {
     LinkedHashMap<String, DatabaseMigration> migrations = new LinkedHashMap<>();
 
     migrations.put("fix-dcarkeywords-displayname-migration", new FixDcarKeywordDisplayNameMigration());
+    migrations.put("fix-pids-migration", new MakePidsAbsoluteUrls());
     final UriHelper uriHelper = new UriHelper(configuration.getBaseUri());
 
     final TinkerpopGraphManager graphManager = new TinkerpopGraphManager(configuration, migrations);
