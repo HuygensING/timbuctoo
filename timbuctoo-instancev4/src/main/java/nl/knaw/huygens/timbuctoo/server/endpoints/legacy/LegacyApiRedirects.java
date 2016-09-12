fab53c1579599182e89f3a76e4d657b76fed638c
Jauco Noordzij <code@jauco.nl>
2016-09-12 07:38:00 +0200

    fix the past three commits
    I had a really nice and relaxing weekend

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/legacy/LegacyApiRedirects.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/legacy/LegacyApiRedirects.java
@@ -5,21 +5,26 @@ import nl.knaw.huygens.timbuctoo.server.UriHelper;
 import nl.knaw.huygens.timbuctoo.server.endpoints.v2.domain.Index;
 import nl.knaw.huygens.timbuctoo.server.endpoints.v2.domain.SingleEntity;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Path("/domain/")
-public class Redirections {
+/**
+ * This class redirects from API endpoints that are no longer supported, but whose urls are still cached by google
+ * or the handle server to their current counterparts.
+ */
+public class LegacyApiRedirects {
 
   private final UriHelper uriHelper;
 
-  public Redirections(UriHelper uriHelper) {
+  public LegacyApiRedirects(UriHelper uriHelper) {
     this.uriHelper = uriHelper;
   }
 
-  @Path("{collection}/{id}")
+  @Path("/domain/{collection}/{id}")
+  @GET
   public Response singleEntity(@PathParam("collection") String collectionName, @PathParam("id") UUIDParam id,
                                @QueryParam("rev") Integer rev) {
     return Response.status(301)
@@ -27,7 +32,8 @@ public class Redirections {
                    .build();
   }
 
-  @Path("{collection}")
+  @Path("/domain/{collection}")
+  @GET
   public Response index(@PathParam("collection") String collectionName) {
     return Response.status(301)
                    .location(uriHelper.fromResourceUri(Index.makeUrl(collectionName)))
