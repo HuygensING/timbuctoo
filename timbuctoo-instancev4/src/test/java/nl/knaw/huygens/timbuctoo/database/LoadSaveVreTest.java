fd8b0395397ade2a2b6ff64924c2815386adcf11
Jauco Noordzij <code@jauco.nl>
2016-09-19 09:04:26 +0200

    refactor unittests to use graphbuilder
    Makes them a bit more readable

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/database/LoadSaveVreTest.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/database/LoadSaveVreTest.java
@@ -6,17 +6,20 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import nl.knaw.huygens.timbuctoo.database.dto.dataset.Collection;
 import nl.knaw.huygens.timbuctoo.model.vre.Vre;
+import nl.knaw.huygens.timbuctoo.util.TestGraphBuilder;
+import org.apache.tinkerpop.gremlin.neo4j.process.traversal.LabelP;
 import org.apache.tinkerpop.gremlin.structure.Direction;
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
-import static nl.knaw.huygens.timbuctoo.model.vre.Vre.DATABASE_LABEL;
+import static nl.knaw.huygens.timbuctoo.database.dto.dataset.Collection.HAS_ARCHETYPE_RELATION_NAME;
 import static nl.knaw.huygens.timbuctoo.model.vre.Vre.HAS_COLLECTION_RELATION_NAME;
 import static nl.knaw.huygens.timbuctoo.model.vre.Vre.KEYWORD_TYPES_PROPERTY_NAME;
 import static nl.knaw.huygens.timbuctoo.model.vre.Vre.VRE_NAME_PROPERTY_NAME;
@@ -28,16 +31,22 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.core.Is.is;
 
 public class LoadSaveVreTest {
-  private Graph graph;
 
-  @Before
-  public void setUp() {
-    graph = newGraph().build();
+  private Graph initGraph() {
+    return initGraph(c -> {
+    });
   }
 
-  private Vertex save(Vre vre) {
+  private Graph initGraph(Consumer<TestGraphBuilder> init) {
+    TestGraphBuilder testGraphBuilder = newGraph();
+    init.accept(testGraphBuilder);
+    return testGraphBuilder.build();
+  }
+
+  private Vertex save(Vre vre, Graph graph) {
     return vre.save(graph);
   }
 
@@ -45,27 +54,39 @@ public class LoadSaveVreTest {
     return Vre.load(vertex);
   }
 
+  private Vertex makeVreVertex(Consumer<TestGraphBuilder> testGraphBuilderConsumer) {
+    return initGraph(testGraphBuilderConsumer)
+      .traversal().V()
+      .has(T.label, LabelP.of(Vre.DATABASE_LABEL))
+      .next();
+  }
+
   @Test
   public void saveCreatesAVertexForTheVre() {
     final Vre vre = new Vre("VreName");
 
-    final Vertex result = save(vre);
+    final Vertex result = save(vre, initGraph());
 
     assertThat(result, likeVertex()
-      .withLabel(DATABASE_LABEL)
+      .withLabel(Vre.DATABASE_LABEL)
       .withProperty(VRE_NAME_PROPERTY_NAME, "VreName")
     );
   }
 
   @Test
   public void saveReplacesAnExistingVertexForTheVre() {
-    final Vertex existingVertex = graph.addVertex(DATABASE_LABEL);
-    existingVertex.property(VRE_NAME_PROPERTY_NAME, "VreName");
+    Graph graph = initGraph(b -> b.withVertex(
+      v -> v
+        .withLabel(Vre.DATABASE_LABEL)
+        .withProperty(VRE_NAME_PROPERTY_NAME, "VreName")
+    ));
     final Vre vre = new Vre("VreName");
 
-    final Vertex result = save(vre);
+    final Vertex result = save(vre, graph);
 
-    assertThat(result, equalTo(existingVertex));
+    assertThat(result.label(), is(Vre.DATABASE_LABEL));
+    assertThat(result.value(VRE_NAME_PROPERTY_NAME), is("VreName"));
+    assertThat(graph.traversal().V().count().next(), is(1L));
   }
 
   @Test
@@ -75,7 +96,7 @@ public class LoadSaveVreTest {
     keyWordTypes.put("typeB", "valueB");
     final Vre vre = new Vre("VreName", keyWordTypes);
 
-    final Vertex result = save(vre);
+    final Vertex result = save(vre, initGraph());
 
     assertThat(result.property(KEYWORD_TYPES_PROPERTY_NAME).value(),
       equalTo(new ObjectMapper().writeValueAsString(keyWordTypes))
@@ -89,7 +110,7 @@ public class LoadSaveVreTest {
       .withCollection("prefixdocuments")
       .build();
 
-    final Vertex savedVertex = save(vre);
+    final Vertex savedVertex = save(vre, initGraph());
     final List<Vertex> result = Lists.newArrayList(savedVertex.vertices(Direction.OUT, HAS_COLLECTION_RELATION_NAME));
 
     assertThat(result, containsInAnyOrder(
@@ -104,13 +125,17 @@ public class LoadSaveVreTest {
 
   @Test
   public void loadLoadsAVreFromAVertex() throws JsonProcessingException {
-    final Vertex vertex = graph.addVertex("VRE");
     final Map<String, String> keyWordTypes = new HashMap<>();
-
     keyWordTypes.put("keyword", "type");
-    vertex.property(VRE_NAME_PROPERTY_NAME, "VreName");
-    vertex.property(KEYWORD_TYPES_PROPERTY_NAME, new ObjectMapper().writeValueAsString(keyWordTypes));
-    final Vre instance = load(vertex);
+    String stringifiedKeyWordTypes = new ObjectMapper().writeValueAsString(keyWordTypes);
+
+    Vertex source = makeVreVertex(g ->
+      g.withVertex(v ->
+        v.withLabel(Vre.DATABASE_LABEL)
+          .withProperty(VRE_NAME_PROPERTY_NAME, "VreName")
+          .withProperty(KEYWORD_TYPES_PROPERTY_NAME, stringifiedKeyWordTypes)
+      ));
+    final Vre instance = load(source);
 
     assertThat(instance.getVreName(), equalTo("VreName"));
     assertThat(instance.getKeywordTypes().get("keyword"), equalTo("type"));
@@ -122,16 +147,24 @@ public class LoadSaveVreTest {
     final String collectionName = "persons";
     final HashMap<String, String> keywordTypes = new HashMap<>();
     keywordTypes.put("keyword", "type");
-    final Vertex vertex = graph.addVertex("VRE");
-    final Vertex collectionVertex = graph.addVertex(entityTypeName);
-    vertex.property(VRE_NAME_PROPERTY_NAME, "VreName");
-    vertex.property(KEYWORD_TYPES_PROPERTY_NAME, new ObjectMapper().writeValueAsString(keywordTypes));
-    collectionVertex.property(Collection.COLLECTION_NAME_PROPERTY_NAME, collectionName);
-    collectionVertex.property(Collection.ENTITY_TYPE_NAME_PROPERTY_NAME, entityTypeName);
-    collectionVertex.property(Collection.IS_RELATION_COLLECTION_PROPERTY_NAME, false);
-    vertex.addEdge(HAS_COLLECTION_RELATION_NAME, collectionVertex);
+    String stringifiedKeyWordTypes = new ObjectMapper().writeValueAsString(keywordTypes);
 
-    final Vre instance = load(vertex);
+    Vertex source = makeVreVertex(g -> g
+      .withVertex("vre", v -> v
+        .withLabel(Vre.DATABASE_LABEL)
+        .withProperty(VRE_NAME_PROPERTY_NAME, "VreName")
+        .withProperty(KEYWORD_TYPES_PROPERTY_NAME, stringifiedKeyWordTypes)
+      )
+      .withVertex(v -> v
+        .withLabel(entityTypeName)
+        .withProperty(Collection.COLLECTION_NAME_PROPERTY_NAME, collectionName)
+        .withProperty(Collection.ENTITY_TYPE_NAME_PROPERTY_NAME, entityTypeName)
+        .withProperty(Collection.IS_RELATION_COLLECTION_PROPERTY_NAME, false)
+        .withIncomingRelation(HAS_COLLECTION_RELATION_NAME, "vre")
+      ));
+
+
+    final Vre instance = load(source);
     final Collection collectionByName = instance.getCollectionForCollectionName(collectionName).get();
 
     assertThat(instance.getKeywordTypes().get("keyword"), equalTo("type"));
@@ -147,21 +180,29 @@ public class LoadSaveVreTest {
 
   @Test
   public void loadLoadsInheritingCollections() {
-    final Vertex vertex = graph.addVertex("VRE");
     final String entityTypeName = "wwperson";
     final String collectionName = "wwpersons";
     final String archetypeName = "person";
     final String archetypeCollectionName = "persons";
-    final Vertex archetypeVertex = graph.addVertex("collection");
-    final Vertex collectionVertex = graph.addVertex("collection");
-    vertex.property(VRE_NAME_PROPERTY_NAME, "VreName");
-    collectionVertex.property(Collection.COLLECTION_NAME_PROPERTY_NAME, collectionName);
-    collectionVertex.property(Collection.ENTITY_TYPE_NAME_PROPERTY_NAME, entityTypeName);
-    collectionVertex.property(Collection.IS_RELATION_COLLECTION_PROPERTY_NAME, false);
-    archetypeVertex.property(Collection.COLLECTION_NAME_PROPERTY_NAME, archetypeCollectionName);
-    archetypeVertex.property(Collection.ENTITY_TYPE_NAME_PROPERTY_NAME, archetypeName);
-    vertex.addEdge(HAS_COLLECTION_RELATION_NAME, collectionVertex);
-    collectionVertex.addEdge(Collection.HAS_ARCHETYPE_RELATION_NAME, archetypeVertex);
+
+    Vertex vertex = makeVreVertex(g -> g
+      .withVertex("vre", v -> v
+        .withLabel(Vre.DATABASE_LABEL)
+        .withProperty(VRE_NAME_PROPERTY_NAME, "VreName")
+      )
+      .withVertex("collection", v -> v
+        .withLabel(Collection.DATABASE_LABEL)
+        .withProperty(Collection.COLLECTION_NAME_PROPERTY_NAME, collectionName)
+        .withProperty(Collection.ENTITY_TYPE_NAME_PROPERTY_NAME, entityTypeName)
+        .withProperty(Collection.IS_RELATION_COLLECTION_PROPERTY_NAME, false)
+        .withIncomingRelation(HAS_COLLECTION_RELATION_NAME, "vre")
+      )
+      .withVertex("archetype", v -> v
+        .withLabel(Collection.DATABASE_LABEL)
+        .withProperty(Collection.COLLECTION_NAME_PROPERTY_NAME, archetypeCollectionName)
+        .withProperty(Collection.ENTITY_TYPE_NAME_PROPERTY_NAME, archetypeName)
+        .withIncomingRelation(HAS_ARCHETYPE_RELATION_NAME, "collection")
+      ));
 
     final Vre instance = load(vertex);
 
@@ -170,19 +211,27 @@ public class LoadSaveVreTest {
 
   @Test
   public void loadLoadsRelationCollections() {
-    final Vertex vertex = graph.addVertex("VRE");
     final String entityTypeName = "relation";
     final String collectionName = "relations";
-    final Vertex collectionVertex = graph.addVertex(entityTypeName);
-    vertex.property(VRE_NAME_PROPERTY_NAME, "VreName");
-    collectionVertex.property(Collection.COLLECTION_NAME_PROPERTY_NAME, collectionName);
-    collectionVertex.property(Collection.ENTITY_TYPE_NAME_PROPERTY_NAME, entityTypeName);
-    collectionVertex.property(Collection.IS_RELATION_COLLECTION_PROPERTY_NAME, true);
-    vertex.addEdge(HAS_COLLECTION_RELATION_NAME, collectionVertex);
+
+    Vertex vertex = makeVreVertex(g -> g
+      .withVertex("vre", v -> v
+        .withLabel(Vre.DATABASE_LABEL)
+        .withProperty(VRE_NAME_PROPERTY_NAME, "VreName")
+      )
+      .withVertex("collection", v -> v
+        .withLabel(entityTypeName)
+        .withProperty(Collection.COLLECTION_NAME_PROPERTY_NAME, collectionName)
+        .withProperty(Collection.ENTITY_TYPE_NAME_PROPERTY_NAME, entityTypeName)
+        .withProperty(Collection.IS_RELATION_COLLECTION_PROPERTY_NAME, true)
+        .withIncomingRelation(HAS_COLLECTION_RELATION_NAME, "vre")
+      ));
+
 
     final Vre instance = load(vertex);
     final Collection collectionByName = instance.getCollectionForCollectionName(collectionName).get();
 
     assertThat(instance.getRelationCollection().get(), equalTo(collectionByName));
   }
+
 }
