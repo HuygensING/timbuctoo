41da0d3ca698ab2af9efb602abeb915ec8288f45
Jauco Noordzij <code@jauco.nl>
2016-09-20 12:57:23 +0200

    Close transactions when a new TestGraph is built

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/util/TestGraphBuilder.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/util/TestGraphBuilder.java
@@ -3,9 +3,9 @@ package nl.knaw.huygens.timbuctoo.util;
 import nl.knaw.huygens.timbuctoo.server.TestableTinkerpopGraphManager;
 import nl.knaw.huygens.timbuctoo.server.TinkerpopGraphManager;
 import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
-import org.apache.tinkerpop.gremlin.process.traversal.strategy.decoration.SubgraphStrategy;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.test.TestGraphDatabaseFactory;
 import org.neo4j.tinkerpop.api.impl.Neo4jGraphAPIImpl;
 
@@ -16,20 +16,14 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 import static nl.knaw.huygens.timbuctoo.util.Neo4jHelper.cleanDb;
-import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.has;
 
+//FIXME should become a rule
 public class TestGraphBuilder {
-
-  private static final SubgraphStrategy LATEST_ELEMENTS =
-    SubgraphStrategy.build().edgeCriterion(has("isLatest", true)).vertexCriterion(has("isLatest", true)).create();
 
   private static GraphDatabaseService neo4jDb = new TestGraphDatabaseFactory().newImpermanentDatabase();
   private final List<VertexBuilder> vertexBuilders = new ArrayList<>();
   private final Map<String, VertexBuilder> identifiableVertexBuilders = new HashMap<>();
-
-  {
-    neo4jDb.beginTx();
-  }
+  private static Transaction trans;
 
   // FIXME do use the label to map the relations
   private TestGraphBuilder() {
@@ -48,35 +42,45 @@ public class TestGraphBuilder {
     //second to 3 minutes on a machine with an SSD) so instead we now re-use
     //the same neo4j database that we clear when the function to create a new
     //one is called.
+    if (trans != null) {
+      trans.close();
+    }
     cleanDb(neo4jDb);
+
     Neo4jGraphAPIImpl neo4jGraphApi = new Neo4jGraphAPIImpl(neo4jDb);
     Neo4jGraph neo4jGraph = Neo4jGraph.open(neo4jGraphApi);
-    Map<String, Vertex> identifiableVertices = new HashMap<>();
-    //Create all identifiable vertices
-    identifiableVertexBuilders.forEach(
-      (key, builder) -> identifiableVertices.put(key, builder.build(neo4jGraph.addVertex()))
-    );
-    //now we can add the links
-    identifiableVertexBuilders.forEach(
-      (key, builder) -> builder.setRelations(identifiableVertices.get(key), identifiableVertices)
-    );
-    //finally add all the non-identifiable vertices
-    vertexBuilders.forEach(
-      (builder) -> {
 
-        Vertex vertex;
-        if (builder.getLabels().size() == 1) {
-          // If there is exactly one label, it is still a valid tinkerpop vertex and needs to be passed to the
-          // addVertex method
-          vertex = neo4jGraph.addVertex(builder.getLabels().get(0));
-        } else {
-          vertex = neo4jGraph.addVertex();
+    try (org.apache.tinkerpop.gremlin.structure.Transaction tx = neo4jGraph.tx()) {
+      final Map<String, Vertex> identifiableVertices = new HashMap<>();
+      //Create all identifiable vertices
+      identifiableVertexBuilders.forEach(
+        (key, builder) -> identifiableVertices.put(key, builder.build(neo4jGraph.addVertex()))
+      );
+      //now we can add the links
+      identifiableVertexBuilders.forEach(
+        (key, builder) -> builder.setRelations(identifiableVertices.get(key), identifiableVertices)
+      );
+      //finally add all the non-identifiable vertices
+      vertexBuilders.forEach(
+        (builder) -> {
+
+          Vertex vertex;
+          if (builder.getLabels().size() == 1) {
+            // If there is exactly one label, it is still a valid tinkerpop vertex and needs to be passed to the
+            // addVertex method
+            vertex = neo4jGraph.addVertex(builder.getLabels().get(0));
+          } else {
+            vertex = neo4jGraph.addVertex();
+          }
+
+          builder.build(vertex);
+          builder.setRelations(vertex, identifiableVertices);
         }
+      );
+      tx.commit();
+    }
 
-        builder.build(vertex);
-        builder.setRelations(vertex, identifiableVertices);
-      }
-    );
+    trans = neo4jDb.beginTx();
     return neo4jGraph;
   }
 
