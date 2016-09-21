c1f441d2b2211bd2c38c04d5794949274bad551f
Jauco Noordzij <code@jauco.nl>
2016-09-21 13:38:56 +0200

    be able to write a testgraph to a formatted json file

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/TestableTinkerpopGraphManager.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/TestableTinkerpopGraphManager.java
@@ -1,6 +1,9 @@
 package nl.knaw.huygens.timbuctoo.server;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
+import org.apache.tinkerpop.gremlin.structure.io.IoCore;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.slf4j.Logger;
 
@@ -36,4 +39,31 @@ public class TestableTinkerpopGraphManager extends TinkerpopGraphManager {
   @Override
   protected void initGraphDatabaseService() {
   }
+
+  public String writeGraph() {
+    java.io.OutputStream output = new java.io.OutputStream() {
+      private StringBuilder stringBuilder = new StringBuilder();
+      @Override
+      public void write(int chr) throws java.io.IOException {
+        stringBuilder.append((char) chr );
+      }
+
+      @Override
+      public String toString() {
+        return stringBuilder.toString();
+      }
+    };
+    try {
+      graph.io(IoCore.graphson()).writer().create().writeGraph(output, graph);
+    } catch (java.io.IOException e) {
+      e.printStackTrace();
+    }
+
+    String wellformedJson = "[" + String.join(",\n", (CharSequence[]) output.toString().split("\n")) + "]";
+    com.google.gson.Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    com.google.gson.JsonParser jp = new JsonParser();
+    com.google.gson.JsonElement je = jp.parse(wellformedJson);
+    return gson.toJson(je);
+  }
+
 }
