335a42a8238082a3634dd64c8c332d26fb0084b7
Jauco Noordzij <code@jauco.nl>
2016-09-27 20:08:50 +0200

    docker clean build script improvements

--- timbuctoo-instancev4/docker/build.sh
+++ timbuctoo-instancev4/docker/build.sh
@@ -16,16 +16,30 @@ done
 
 maventarget=${2:-test}
 
+if [ ${maventarget} = verify ]; then
+  if [ -e ../timbuctoo-db.zip ]; then
+     verifyparam="-v"
+     verifydb="$PWD/../timbuctoo-db.zip"
+     verifytarget=":/root/timbuctoo-db.zip"
+   else
+     echo "The verify will fail because you have no prod database available in ../timbuctoo-db.zip"
+     exit 1
+  fi
+fi
 if git rev-parse --verify "$1" 2> /dev/null; then
-  local_ref="$1"
+  local_ref=`git rev-parse --abbrev-ref "$1"`
   docker run \
     --rm \
     -it \
     ${mavencache} \
     -v "$(git rev-parse --show-toplevel)":/root/timbuctoo \
+    $verifyparam "$verifydb"$verifytarget \
     maven:3.3-jdk-8 \
-    bash -c "git clone -b ${local_ref} /root/timbuctoo /root/build && cd /root/build && mvn ${maventarget}" \
-  || exit 1
+    bash -c "git clone -b ${local_ref} /root/timbuctoo /root/build && \
+    cd /root/build/timbuctoo-instancev4/src/spec/resources && \
+    [ -e /root/timbuctoo-db.zip ] && unzip /root/timbuctoo-db.zip; \
+    cd /root/build/ && mvn ${maventarget}; \
+    cp -R /root/build/timbuctoo-instancev4/target/concordion /root/timbuctoo/timbuctoo-instancev4/target/concordion" \ || exit 1
 else
   echo "you should specify a branchname";
   exit 2
