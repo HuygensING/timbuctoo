d41672c1dc4d18cddb214ba8021de8cc88ab5029
Jauco Noordzij <code@jauco.nl>
2016-09-27 11:55:08 +0200

    prepare for writing more integration tests

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/rml/RmlIntegrationTests.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/rml/RmlIntegrationTests.java
@@ -6,7 +6,6 @@ import nl.knaw.huygens.timbuctoo.bulkupload.savers.TinkerpopSaver;
 import nl.knaw.huygens.timbuctoo.crud.HandleAdder;
 import nl.knaw.huygens.timbuctoo.database.ChangeListener;
 import nl.knaw.huygens.timbuctoo.database.DataAccess;
-import nl.knaw.huygens.timbuctoo.model.vre.Vres;
 import nl.knaw.huygens.timbuctoo.model.vre.vres.DatabaseConfiguredVres;
 import nl.knaw.huygens.timbuctoo.rml.jena.JenaBasedReader;
 import nl.knaw.huygens.timbuctoo.server.TinkerpopGraphManager;
@@ -18,6 +17,7 @@ import nl.knaw.huygens.timbuctoo.server.security.UserPermissionChecker;
 import org.apache.jena.ext.com.google.common.collect.ImmutableList;
 import org.apache.jena.ext.com.google.common.collect.ImmutableMap;
 import org.apache.tinkerpop.gremlin.neo4j.process.traversal.LabelP;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Direction;
 import org.apache.tinkerpop.gremlin.structure.T;
@@ -39,44 +39,38 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 
-public class LinkingToExternalCollectionsTest {
+public class RmlIntegrationTests {
+
+  public static final ObjectNode RML_JSON_LD_CONTEXT = jsnO(
+    tuple("@vocab", jsn("http://www.w3.org/ns/r2rml#")),
+    tuple("rml", jsn("http://semweb.mmlab.be/ns/rml#")),
+    tuple("tim", jsn("http://timbuctoo.com/mapping#")),
+    tuple("http://www.w3.org/2000/01/rdf-schema#subClassOf", jsnO(
+      "@type", jsn("@id")
+    )),
+    tuple("predicate", jsnO(
+      "@type", jsn("@id")
+    )),
+    tuple("termType", jsnO(
+      "@type", jsn("@id")
+    )),
+    tuple("parentTriplesMap", jsnO(
+      "@type", jsn("@id")
+    )),
+    tuple("class", jsnO(
+      "@type", jsn("@id")
+    ))
+  );
 
   @Test
   public void canLinkToExternalCollections() throws Exception {
-    TinkerpopGraphManager graphManager = newGraph()
-      .wrap();
+    IntegrationTester tester = new IntegrationTester();
 
-    ObjectNode rmlJsonLdContext = jsnO(
-      tuple("@vocab", jsn("http://www.w3.org/ns/r2rml#")),
-      tuple("rml", jsn("http://semweb.mmlab.be/ns/rml#")),
-      tuple("tim", jsn("http://timbuctoo.com/mapping#")),
-      tuple("http://www.w3.org/2000/01/rdf-schema#subClassOf", jsnO(
-        "@type", jsn("@id")
-      )),
-      tuple("predicate", jsnO(
-        "@type", jsn("@id")
-      )),
-      tuple("termType", jsnO(
-        "@type", jsn("@id")
-      )),
-      tuple("parentTriplesMap", jsnO(
-        "@type", jsn("@id")
-      )),
-      tuple("class", jsnO(
-        "@type", jsn("@id")
-      ))
-    );
-
-    DataAccess dataAccess =
-      new DataAccess(graphManager, null, null, mock(ChangeListener.class), mock(HandleAdder.class));
-    new ScaffoldMigrator(dataAccess).execute();
-    DatabaseConfiguredVres vres = new DatabaseConfiguredVres(dataAccess);
-
-    executeRawUpload("locaties", "locaties", vres, graphManager, ImmutableList.of(
+    tester.executeRawUpload("locaties", "locaties", ImmutableList.of(
       ImmutableMap.of("naam", "Vlissingen")
     ));
-    executeRml(dataAccess, vres, graphManager, jsnO(
-      "@context", rmlJsonLdContext,
+    Response response = tester.executeRml("locaties", jsnO(
+      "@context", RML_JSON_LD_CONTEXT,
       "@graph", jsnA(
         jsnO(
           "@id", jsn("http://timbuctoo.com/mapping/locaties/locaties"),
@@ -101,11 +95,11 @@ public class LinkingToExternalCollectionsTest {
           )
         )
       )
-    ), "locaties");
+    ));
+    assertThat(response.getStatus(), is(200));
 
     //one location should have been created
-    GraphTraversalSource traversalSource = graphManager.getGraph().traversal();
-    List<Vertex> locaties = traversalSource.V().has(T.label, LabelP.of("locaties")).toList();
+    List<Vertex> locaties = tester.traversalSource.V().has(T.label, LabelP.of("locaties")).toList();
     assertThat(locaties.size(), is(1));
     Vertex locatie = locaties.get(0);
 
@@ -117,14 +111,14 @@ public class LinkingToExternalCollectionsTest {
     String locationName = locatie.value("location_name");
     assertThat(locatiesName, is(locationName));
 
-    executeRawUpload("migraties", "migranten", vres, graphManager, ImmutableList.of(
+    tester.executeRawUpload("migraties", "migranten", ImmutableList.of(
       ImmutableMap.of(
         "naam", "Karel",
         "geboorteplaats", rdfUri
       )
     ));
-    Response response = executeRml(dataAccess, vres, graphManager, jsnO(
-      "@context", rmlJsonLdContext,
+    response = tester.executeRml("migraties", jsnO(
+      "@context", RML_JSON_LD_CONTEXT,
       "@graph", jsnA(
         jsnO(
           "@id", jsn("http://timbuctoo.com/mapping/migraties/migranten"),
@@ -151,10 +145,11 @@ public class LinkingToExternalCollectionsTest {
           )
         )
       )
-    ), "migraties");
-    //fixme assert status code
+    ));
+    assertThat(response.getStatus(), is(200));
+
     //one migrant should have been created
-    List<Vertex> migrants = traversalSource.V().has(T.label, LabelP.of("migratiesmigranten")).toList();
+    List<Vertex> migrants = tester.traversalSource.V().has(T.label, LabelP.of("migratiesmigranten")).toList();
     assertThat(migrants.size(), is(1));
     Vertex migrant = migrants.get(0);
 
@@ -162,43 +157,59 @@ public class LinkingToExternalCollectionsTest {
     assertThat(migrant.vertices(Direction.OUT, "hasBirthPlace").next().id(), is(locatie.id()));
   }
 
-  private void executeRawUpload(String vreName, String collectionName, Vres vres, TinkerpopGraphManager graphManager,
-                                List<Map<String, String>> data) {
-    try (TinkerpopSaver rawSaver = new TinkerpopSaver(vres, graphManager, vreName, 100)) {
-      Vertex rawCollection = rawSaver.addCollection(collectionName);
-      ImportPropertyDescriptions importPropertyDescriptions = new ImportPropertyDescriptions();
-      int count = 0;
-      for (String key : data.get(0).keySet()) {
-        importPropertyDescriptions.getOrCreate(count++).setPropertyName(key);
-      }
-      rawSaver.addPropertyDescriptions(rawCollection, importPropertyDescriptions);
-      for (Map<String, String> map : data) {
-        rawSaver.addEntity(rawCollection, map);
+
+
+
+  public class IntegrationTester {
+    private final TinkerpopGraphManager graphManager;
+    private final DataAccess dataAccess;
+    private final DatabaseConfiguredVres vres;
+    public final GraphTraversalSource traversalSource;
+
+    public IntegrationTester() {
+      graphManager = newGraph().wrap();
+      traversalSource = graphManager.getGraph().traversal();
+      dataAccess = new DataAccess(graphManager, null, null, mock(ChangeListener.class), mock(HandleAdder.class));
+      new ScaffoldMigrator(dataAccess).execute();
+
+      vres = new DatabaseConfiguredVres(dataAccess);
+    }
+
+    public void executeRawUpload(String vreName, String collectionName, List<Map<String, String>> data) {
+      try (TinkerpopSaver rawSaver = new TinkerpopSaver(vres, graphManager, vreName, 100)) {
+        Vertex rawCollection = rawSaver.addCollection(collectionName);
+        ImportPropertyDescriptions importPropertyDescriptions = new ImportPropertyDescriptions();
+        int count = 0;
+        for (String key : data.get(0).keySet()) {
+          importPropertyDescriptions.getOrCreate(count++).setPropertyName(key);
+        }
+        rawSaver.addPropertyDescriptions(rawCollection, importPropertyDescriptions);
+        for (Map<String, String> map : data) {
+          rawSaver.addEntity(rawCollection, map);
+        }
       }
     }
+
+    public Response executeRml(String vreName, ObjectNode mapping) {
+      UserPermissionChecker alwaysAllowed = mock(UserPermissionChecker.class);
+      given(alwaysAllowed.check(any(), any())).willReturn(UserPermissionChecker.UserPermission.ALLOWED_TO_WRITE);
+
+      ExecuteRml executeRml = new ExecuteRml(
+        new UriHelper("http://example.com"),
+        graphManager,
+        vres,
+        new JenaBasedReader(),
+        alwaysAllowed,
+        new DataSourceFactory(graphManager),
+        dataAccess
+      );
+
+      return executeRml.post(
+        mapping.toString(),
+        vreName,
+        "blaat"
+      );
+    }
+
   }
-
-  private Response executeRml(DataAccess dataAccess, Vres vres, TinkerpopGraphManager graphManager, ObjectNode mapping,
-                              String vreName) {
-    UserPermissionChecker alwaysAllowed = mock(UserPermissionChecker.class);
-    given(alwaysAllowed.check(any(), any())).willReturn(UserPermissionChecker.UserPermission.ALLOWED_TO_WRITE);
-
-    ExecuteRml executeRml = new ExecuteRml(
-      new UriHelper("http://example.com"),
-      graphManager,
-      vres,
-      new JenaBasedReader(),
-      alwaysAllowed,
-      new DataSourceFactory(graphManager),
-      dataAccess
-    );
-
-    return executeRml.post(
-      mapping.toString(),
-      vreName,
-      "blaat"
-    );
-  }
-
-
 }
