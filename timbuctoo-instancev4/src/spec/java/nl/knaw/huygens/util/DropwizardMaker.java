1f2428d544017d89f8c1ab05f489429d2243aeb4
Jauco Noordzij <code@jauco.nl>
2016-09-30 08:59:24 +0200

    remove run state
    These files are created by timbuctoo while running. They should not be
    in source control, but launching timbuctoo without them won't work so I
    also made the run script autogenerate them.
    
    I have also renamed the run script to make it clear that it is not
    really for production runs (it launches in debugger mode by default,
    and redefines the data dirs)

--- timbuctoo-instancev4/src/spec/java/nl/knaw/huygens/util/DropwizardMaker.java
+++ timbuctoo-instancev4/src/spec/java/nl/knaw/huygens/util/DropwizardMaker.java
@@ -1,5 +1,6 @@
 package nl.knaw.huygens.util;
 
+import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import nl.knaw.huygens.timbuctoo.server.TimbuctooConfiguration;
@@ -9,7 +10,11 @@ public class DropwizardMaker {
   public static DropwizardAppRule<TimbuctooConfiguration> makeTimbuctoo() {
     return new DropwizardAppRule<>(
       TimbuctooV4.class,
-      ResourceHelpers.resourceFilePath("acceptance_test_config.yaml")
+      ResourceHelpers.resourceFilePath("specrunstate/acceptance_test_config.yaml"),
+      ConfigOverride.config("databasePath", ResourceHelpers.resourceFilePath("specrunstate/database")),
+      ConfigOverride.config("authorizationsPath", ResourceHelpers.resourceFilePath("specrunstate/authorizations")),
+      ConfigOverride.config("usersFilePath", ResourceHelpers.resourceFilePath("specrunstate/users.json")),
+      ConfigOverride.config("loginsFilePath", ResourceHelpers.resourceFilePath("specrunstate/logins.json"))
     );
   }
 }
