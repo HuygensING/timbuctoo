a04dd156e25df8bf5e30cf21628d97e9f114b147
Jauco Noordzij <code@jauco.nl>
2016-10-03 13:37:48 +0200

    Name the excel column in the error message

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/bulkupload/loaders/ResultHandler.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/bulkupload/loaders/ResultHandler.java
@@ -27,7 +27,7 @@ public class ResultHandler {
   }
 
   public void endRow(HashMap<Integer, Result> extraResults) {
-    extraResults.forEach((column, result) -> result.handle(msg -> statusUpdate.accept("failure: " + msg)));
+    extraResults.forEach((column, result) -> result.handle(msg -> log(column, msg)));
   }
 
   public void endSheet() {
@@ -37,7 +37,27 @@ public class ResultHandler {
   }
 
   public void handle(int column, String value, Result result) {
-    result.handle(msg -> statusUpdate.accept("failure: " + msg));
+
+    result.handle(msg -> log(column, msg));
+  }
+
+  private void log(int columnNumber, String message) {
+    String columnName = getExcelColumnName(columnNumber);
+    statusUpdate.accept("failure: at " + columnName + " " + message);
+  }
+
+  private String getExcelColumnName(int columnNumber) {
+    int dividend = columnNumber;
+    String columnName = "";
+    int modulo;
+
+    while (dividend > 0) {
+      modulo = (dividend - 1) % 26;
+      columnName = ((char) (65 + modulo)) + columnName;
+      dividend = ((dividend - modulo) / 26);
+    }
+
+    return columnName;
   }
 
 }
