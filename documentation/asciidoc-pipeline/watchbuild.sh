291988c9af79fd2c46f1687c9eeb4b4e363db3d8
Jauco Noordzij <code@jauco.nl>
2016-11-01 11:19:23 +0100

    make watchbuild work inside a vanilla container

--- documentation/asciidoc-pipeline/watchbuild.sh
+++ documentation/asciidoc-pipeline/watchbuild.sh
@@ -2,6 +2,18 @@
 
 cd -P -- "$(dirname -- "$0")" #go to dir of script even if it was called as a symbolic link
 
+if ! which inotifywait; then
+  if [ -f /.dockerenv ]; then
+      echo "inotifywait not available, we're running inside a container so we'll install it.";
+      dnf install -y inotify-tools
+      if ! which inotifywait; then
+        echo "inotifywait installation failed"
+        exit 1
+      fi
+  else
+      echo "inotifywait not available. Please install it.";
+  fi
+fi
 ./build.sh
 while inotifywait -r ../../timbuctoo-instancev4/src ../; 
   do ./build.sh
