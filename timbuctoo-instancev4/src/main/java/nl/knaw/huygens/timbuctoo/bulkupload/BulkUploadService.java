557a9fc14d88bad6e315bd6c178adfeae35a644f
Jauco Noordzij <code@jauco.nl>
2016-11-07 11:29:17 +0100

    Implement dataperfect importer

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/bulkupload/BulkUploadService.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/bulkupload/BulkUploadService.java
@@ -1,38 +1,35 @@
 package nl.knaw.huygens.timbuctoo.bulkupload;
 
 import nl.knaw.huygens.timbuctoo.bulkupload.loaders.BulkLoader;
+import nl.knaw.huygens.timbuctoo.bulkupload.loaders.dataperfect.DataPerfectLoader;
 import nl.knaw.huygens.timbuctoo.bulkupload.loaders.excel.allsheetloader.AllSheetLoader;
 import nl.knaw.huygens.timbuctoo.bulkupload.parsingstatemachine.Importer;
 import nl.knaw.huygens.timbuctoo.bulkupload.savers.TinkerpopSaver;
 import nl.knaw.huygens.timbuctoo.model.vre.Vres;
-import nl.knaw.huygens.timbuctoo.security.AuthorizationException;
-import nl.knaw.huygens.timbuctoo.security.AuthorizationUnavailableException;
-import nl.knaw.huygens.timbuctoo.security.Authorizer;
 import nl.knaw.huygens.timbuctoo.server.TinkerpopGraphManager;
 
+import java.io.IOException;
 import java.util.function.Consumer;
 
 public class BulkUploadService {
 
   private final Vres vres;
   private final TinkerpopGraphManager graphwrapper;
-  private final Authorizer authorizer;
 
   public BulkUploadService(Vres vres, TinkerpopGraphManager graphwrapper/*, Authorizer authorizer*/) {
     this.vres = vres;
     this.graphwrapper = graphwrapper;
-    this.authorizer = null;//authorizer;
   }
 
   public void saveToDb(String vreName, byte[] file, String fileName, Consumer<String> statusUpdate)
-    throws AuthorizationUnavailableException, AuthorizationException, InvalidFileException {
+    throws InvalidFileException, IOException {
 
     try (TinkerpopSaver saver = new TinkerpopSaver(vres, graphwrapper, vreName, 50_000)) {
       BulkLoader loader;
       if (fileName.endsWith(".xlsx")) {
         loader = new AllSheetLoader();
       } else {
-        throw new InvalidFileException("File type not found");
+        loader = new DataPerfectLoader();
       }
       loader.loadData(file, new Importer(saver), statusUpdate);
     }
--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/bulkupload/loaders/BulkLoader.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/bulkupload/loaders/BulkLoader.java
@@ -3,8 +3,10 @@ package nl.knaw.huygens.timbuctoo.bulkupload.loaders;
 import nl.knaw.huygens.timbuctoo.bulkupload.InvalidFileException;
 import nl.knaw.huygens.timbuctoo.bulkupload.parsingstatemachine.Importer;
 
+import java.io.IOException;
 import java.util.function.Consumer;
 
 public interface BulkLoader {
-  void loadData(byte[] source, Importer importer, Consumer<String> statusUpdate) throws InvalidFileException;
+  void loadData(byte[] source, Importer importer, Consumer<String> statusUpdate) throws InvalidFileException,
+    IOException;
 }
