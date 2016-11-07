373246e30bffdd8ff8fd236cb5800b2067e09eb2
renevanderark <r.van.der.ark@gmail.com>
2016-11-07 10:36:36 +0100

    converter configs per property type.

--- timbuctoo-instancev4/src/main/scripts/index_scripts/federated-indexer/federated_indexer.rb
+++ timbuctoo-instancev4/src/main/scripts/index_scripts/federated-indexer/federated_indexer.rb
@@ -5,55 +5,36 @@ require '../lib/timbuctoo_solr/default_mapper'
 require '../lib/timbuctoo_solr/timbuctoo_io'
 require '../lib/timbuctoo_solr/solr_io'
 
-=begin
-class WwDocumentConfig
-  def WwDocumentConfig.get
-    {
-        :properties => [
-            { :name => '_id',  :converted_name => 'id'},
-            { :name => 'title', :converted_name => 'displayName_s'},
-            { :name => 'title', :converted_name => 'title_t'},
-            { :name => 'documentType', :converted_name => 'documentType_s'},
-            { :name => 'englishTitle', :converted_name  => '^englishTitle'},
-            { :name => 'date', :converted_name => 'date_i', :type => 'year'},
-            { :name => 'notes', :converted_name => 'notes_t'},
-            { :name => ['^modified', 'timeStamp'], :converted_name => 'modified_l' },
-            { :name => ['^modified', 'userId'], :converted_name => 'modifiedBy_s' },
-            { :name => ['^modified', 'username'], :converted_name => 'modifiedBy_s' },
-        ], :relations => [
-        { :relation_name => 'hasPublishLocation', :property_name => 'displayName', :converted_name => 'publishLocation_ss'},
-        { :relation_name => 'hasWorkLanguage', :property_name => 'displayName', :converted_name => 'language_ss'},
-        { :relation_name => 'hasGenre', :property_name => 'displayName', :converted_name => 'genre_ss'},
-        { :relation_name => 'hasDocumentSource', :property_name => 'displayName', :converted_name => 'source_ss'},
-
-        # Locates creators
-        { :relation_name => 'isCreatedBy', :property_name => 'id', :converted_name => '@authorIds'}
-    ]
-    }
-  end
-end
-
-{:properties=>[{:raw_type=>"text"}],
-{:properties=>[{:raw_type=>"text"}, {:raw_type=>"text"}],
-   {:raw_type=>"datable"},
-   {:raw_type=>"links"}],
-   {:raw_type=>"multiselect"},
-  [{:raw_type=>"names"},
-   {:raw_type=>"select"},
-   {:raw_type=>"text"},
-  [{:raw_type=>"text"},
-  [{:raw_type=>"text"}, {:raw_type=>"select"}, {:raw_type=>"links"}],
-
-=end
-
 class ArchetypeConfig
   attr_reader :name
 
-  ArchetypeConfig.postfixes = {
-      :text => "_s",
-      :datable => "_i",
-
-  }
+  def ArchetypeConfig.conversion_configs
+    {
+      :text => [{
+        :postfix => "_t"
+      }],
+      :select => [{
+        :postfix => "_s"
+      }],
+      :multiselect => [{
+        :postfix => "_ss",
+      }],
+      :names => [{
+        :postfix => "_s",
+        :converter_type => "names"
+      }],
+      :datable => [{
+        :postfix => "_i",
+        :converter_type => "year"
+       }, {
+        :postfix => "_s"
+      }],
+      :links => [{
+        :postfix => "_s",
+        :converter_type => "links"
+      }]
+    }
+  end
 
   def initialize(name, properties)
     @name = name
@@ -61,19 +42,32 @@ class ArchetypeConfig
   end
 
   def resolve_type_mapping (prop)
-    { :raw_type => prop[:type]}
+    { :raw_type => prop[:type] }
+  end
+
+  def resolve_property_configs
+    property_configs = [
+      { :name => '@displayName', :converted_name => 'displayName_s' },
+      { :name => '@displayName', :converted_name => 'displayName_t' },
+      { :name => '_id', :converted_name => 'tim_id_s' },
+      { :name => "^rdfUri", :converted_name => 'rdfUri_s' }
+    ]
+    @properties.reject {|prop| prop[:type].eql?("relation") }.each do |prop|
+      ArchetypeConfig.conversion_configs[prop[:type].to_sym].each do |conf|
+        property_configs << {
+            :name => prop[:name],
+            :converted_name => "#{prop[:name]}#{conf[:postfix]}",
+            :type => conf.key?(:converter_type) ? conf[:converter_type] : nil
+        }
+      end
+    end
+
+    property_configs
   end
 
   def get
     {
-        :properties => @properties
-           .reject {|prop| prop[:type].eql?("relation") }
-           .map {|prop| resolve_type_mapping(prop)
-           } <<
-            { :name => '@displayName', :converted_name => 'displayName_s' }  <<
-            { :name => '@displayName', :converted_name => 'displayName_t' } <<
-            { :name => '_id', :converted_name => 'tim_id_s' } <<
-            { :name => "^rdfUri", :converted_name => 'rdfUri_s' },
+        :properties => resolve_property_configs,
         :relations => @properties
           .select {|prop| prop[:type].eql?("relation") }
           .map {|rel| {
@@ -111,7 +105,7 @@ class FederatedIndexer
         puts "#{collection[:collectionName]} < #{collection[:archetypeName]}"
         pp archetype_config
 
-#        @timbuctoo_io.scrape_collection(collection[:collectionName], :debug_sample => true)
+        @timbuctoo_io.scrape_collection(collection[:collectionName], :debug_sample => true)
       end
     end
   end
