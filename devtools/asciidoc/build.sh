5301863aa88209c57e095c6beef5e706d500ae14
Jauco Noordzij <code@jauco.nl>
2016-12-07 13:15:09 +0100

    fix asciidoc build script

--- devtools/asciidoc/build.sh
+++ devtools/asciidoc/build.sh
@@ -2,15 +2,17 @@
 
 cd -P -- "$(dirname -- "$0")" #go to dir of script even if it was called as a symbolic link
 
-cd ../../documentation
+rootsrcdir="$(git rev-parse --show-toplevel)"
+
+mkdir -p "$rootsrcdir"/documentation/output
 echo generating documentation as HTML >&2
-asciidoctor \
+#docker run -it --rm -v "$PWD":/documents/ -v "$devtoolsdir":/devtools/ asciidoctor/docker-asciidoctor bash
+docker run -it --rm -v "$rootsrcdir":/documents/ -v "$PWD":/devtools/ asciidoctor/docker-asciidoctor asciidoctor \
   -a stylesheet=readthedocs.css \
   -r asciidoctor-diagram \
-  -r ./asciidoc-pipeline/macros/featureMacro.rb \
-  -D ./asciidoc-pipeline/output \
-  index.adoc
+  -r /devtools/macros/featureMacro.rb \
+  -D documentation/output \
+  documentation/index.adoc
 
-mkdir -p ./output
-
+cd "$rootsrcdir"/documentation
 cp readthedocs.css concordion.css api.html ./output
