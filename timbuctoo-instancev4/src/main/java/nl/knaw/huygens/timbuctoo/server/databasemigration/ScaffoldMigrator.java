4c2dbb9c8419711d33b72901fcca0446420d75d9
Jauco Noordzij <code@jauco.nl>
2016-12-12 16:04:57 +0100

    Close TinkerpopOperations transaction when done

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/databasemigration/ScaffoldMigrator.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/databasemigration/ScaffoldMigrator.java
@@ -30,86 +30,98 @@ public class ScaffoldMigrator {
   public void execute() {
     //The migrations are executed first, so those vertices _will_ be present, even on a new empty database
     //The code below will add vertices, so a second launch will not run this code
-    TinkerPopOperations db = forInitDb(graphManager);
+    try (TinkerPopOperations db = forInitDb(graphManager)) {
 
-    if (db.databaseIsEmptyExceptForMigrations()) {
-      LOG.info("Setting up a new scaffold for empty database");
-      Vres mappings = new VresBuilder()
-        .withVre("Admin", "", vre ->
-          vre
-            .withCollection("persons", collection ->
-              collection
-                .withDisplayName(localProperty("person_names", defaultFullPersonNameConverter))
-                .withProperty("names", localProperty("names", personNames))
-                .withProperty("gender", localProperty("person_gender"))
-                .withProperty("birthDate", localProperty("person_birthDate", datable))
-                .withProperty("deathDate", localProperty("person_deathDate", datable))
-            )
-            .withCollection("locations", collection ->
-              collection
-                .withDisplayName(localProperty("location_name"))
-                .withProperty("name", localProperty("location_name"))
-                .withProperty("country", localProperty("location_country"))
-                .withProperty("altLabel", localProperty("location_altLabel", arrayToEncodedArray))
-            )
-            .withCollection("collectives", collection ->
-              collection
-                .withDisplayName(localProperty("collective_name"))
-                .withProperty("name", localProperty("collective_name"))
-                .withProperty("altLabel", localProperty("collective_altLabel", arrayToEncodedArray))
-            )
-            .withCollection("documents", collection ->
-              collection
-                .withDisplayName(localProperty("document_title"))
-                .withProperty("title", localProperty("document_title"))
-                .withProperty("documentType", localProperty("document_documentType"))
-                .withProperty("date", localProperty("document_date", datable))
+      if (db.databaseIsEmptyExceptForMigrations()) {
+        LOG.info("Setting up a new scaffold for empty database");
+        Vres mappings = new VresBuilder()
+          .withVre("Admin", "", vre ->
+            vre
+              .withCollection("persons", collection ->
+                collection
+                  .withDisplayName(localProperty("person_names", defaultFullPersonNameConverter))
+                  .withProperty("names", localProperty("names", personNames))
+                  .withProperty("gender", localProperty("person_gender"))
+                  .withProperty("birthDate", localProperty("person_birthDate", datable))
+                  .withProperty("deathDate", localProperty("person_deathDate", datable))
+              )
+              .withCollection("locations", collection ->
+                collection
+                  .withDisplayName(localProperty("location_name"))
+                  .withProperty("name", localProperty("location_name"))
+                  .withProperty("country", localProperty("location_country"))
+                  .withProperty("altLabel", localProperty("location_altLabel", arrayToEncodedArray))
+              )
+              .withCollection("collectives", collection ->
+                collection
+                  .withDisplayName(localProperty("collective_name"))
+                  .withProperty("name", localProperty("collective_name"))
+                  .withProperty("altLabel", localProperty("collective_altLabel", arrayToEncodedArray))
+              )
+              .withCollection("documents", collection ->
+                collection
+                  .withDisplayName(localProperty("document_title"))
+                  .withProperty("title", localProperty("document_title"))
+                  .withProperty("documentType", localProperty("document_documentType"))
+                  .withProperty("date", localProperty("document_date", datable))
 
-            )
-            .withCollection("concepts", collection ->
-              collection
-                .withDisplayName(localProperty("label"))
-            )
-            .withCollection("relations", CollectionBuilder::isRelationCollection))
-        .build();
-      db.initDb(
-        mappings,
-        relationType("person", "hasBirthPlace", "location", "isBirthPlaceOf", false, false, false, UUID.randomUUID()),
-        relationType("person", "hasDeathPlace", "location", "isDeathPlaceOf", false, false, false, UUID.randomUUID()),
-        relationType("collective", "hasMember", "person", "isMemberOf", false, false, false, UUID.randomUUID()),
-        relationType("collective", "locatedAt", "location", "isHomeOf", false, false, false, UUID.randomUUID()),
-        relationType("document", "isCreatedBy", "person", "isCreatorOf", false, false, false, UUID.randomUUID()),
+              )
+              .withCollection("concepts", collection ->
+                collection
+                  .withDisplayName(localProperty("label"))
+              )
+              .withCollection("relations", CollectionBuilder::isRelationCollection))
+          .build();
+        db.initDb(
+          mappings,
+          relationType("person", "hasBirthPlace", "location", "isBirthPlaceOf", false, false, false, UUID.randomUUID()),
+          relationType("person", "hasDeathPlace", "location", "isDeathPlaceOf", false, false, false, UUID.randomUUID()),
+          relationType("collective", "hasMember", "person", "isMemberOf", false, false, false, UUID.randomUUID()),
+          relationType("collective", "locatedAt", "location", "isHomeOf", false, false, false, UUID.randomUUID()),
+          relationType("document", "isCreatedBy", "person", "isCreatorOf", false, false, false, UUID.randomUUID()),
 
-        // TODO+FIXME, these BIA relationTypes should be made VRE specific and editable before mapping
-        // person to person relations
-        relationType("concept", "hasFirstPerson", "person", "isFirstPersonInRelation",
-          false, false, false, UUID.randomUUID()),
-        relationType("concept", "hasSecondPerson", "person", "isSecondPersonInRelation",
-          false, false, false, UUID.randomUUID()),
-        relationType("concept", "hasPersonToPersonRelationType", "concept", "isPersonToPersonRelationTypeOf",
-          false, false, false, UUID.randomUUID()),
+          // TODO+FIXME, these BIA relationTypes should be made VRE specific and editable before mapping
+          // person to person relations
+          relationType("concept", "hasFirstPerson", "person", "isFirstPersonInRelation",
+            false, false, false, UUID.randomUUID()
+          ),
+          relationType("concept", "hasSecondPerson", "person", "isSecondPersonInRelation",
+            false, false, false, UUID.randomUUID()
+          ),
+          relationType("concept", "hasPersonToPersonRelationType", "concept", "isPersonToPersonRelationTypeOf",
+            false, false, false, UUID.randomUUID()
+          ),
 
-        // states of persons
-        relationType("concept", "hasStateType", "concept", "isStateTypeOf", false, false, false, UUID.randomUUID()),
-        relationType("concept", "isStateOfPerson", "person", "hasPersonState",
-          false, false, false, UUID.randomUUID()),
-        relationType("concept", "isStateLinkedToInstitute", "collective", "isInstituteLinkedToState",
-          false, false, false, UUID.randomUUID()),
-        relationType("concept", "isStateLinkedToLocation", "location", "isLocationLinkedToState",
-          false, false, false, UUID.randomUUID()),
+          // states of persons
+          relationType("concept", "hasStateType", "concept", "isStateTypeOf", false, false, false, UUID.randomUUID()),
+          relationType("concept", "isStateOfPerson", "person", "hasPersonState",
+            false, false, false, UUID.randomUUID()
+          ),
+          relationType("concept", "isStateLinkedToInstitute", "collective", "isInstituteLinkedToState",
+            false, false, false, UUID.randomUUID()
+          ),
+          relationType("concept", "isStateLinkedToLocation", "location", "isLocationLinkedToState",
+            false, false, false, UUID.randomUUID()
+          ),
 
-        // data lines for persons
-        relationType("concept", "hasDataLineType", "concept", "isDataLineTypeOf",
-          false, false, false, UUID.randomUUID()),
-        relationType("concept", "isDataLineForPerson", "person", "hasDataLine",
-          false, false, false, UUID.randomUUID()),
+          // data lines for persons
+          relationType("concept", "hasDataLineType", "concept", "isDataLineTypeOf",
+            false, false, false, UUID.randomUUID()
+          ),
+          relationType("concept", "isDataLineForPerson", "person", "hasDataLine",
+            false, false, false, UUID.randomUUID()
+          ),
 
-        // scientist_bios for persons
-        relationType("concept", "hasFieldOfInterest", "concept", "isFieldOfInterestOf",
-          false, false, false, UUID.randomUUID()),
-        relationType("concept", "isScientistBioOf", "person", "hasScientistBio",
-          false, false, false, UUID.randomUUID())
-      );
+          // scientist_bios for persons
+          relationType("concept", "hasFieldOfInterest", "concept", "isFieldOfInterestOf",
+            false, false, false, UUID.randomUUID()
+          ),
+          relationType("concept", "isScientistBioOf", "person", "hasScientistBio",
+            false, false, false, UUID.randomUUID()
+          )
+        );
+      }
+      db.success();
     }
   }
 }
