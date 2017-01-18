4947e2661f8e2959298c26027aec10222b5ddd6c
LB <lhuygi@users.noreply.github.com>
2017-01-18 16:35:51 +0100

    Add javadocs for Importer and Loader

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/bulkupload/loaders/Loader.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/bulkupload/loaders/Loader.java
@@ -6,6 +6,11 @@ import nl.knaw.huygens.timbuctoo.bulkupload.parsingstatemachine.Importer;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * A Loader applies an Importer to the contents of a File.
+ * Specific Loader implementations know how to parse a file format and call the
+ * required Importer methods.
+ */
 public interface Loader {
   void loadData(File file, Importer importer) throws InvalidFileException, IOException;
 }
