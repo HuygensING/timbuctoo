54a30a6f24e18f9227cf0fb355931fc20051c496
Jauco Noordzij <code@jauco.nl>
2017-01-23 11:11:19 +0100

    clean up quicksearch index code

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/database/tinkerpop/changelistener/FulltextIndexChangeListener.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/database/tinkerpop/changelistener/FulltextIndexChangeListener.java
@@ -6,15 +6,15 @@ import javaslang.control.Try;
 import nl.knaw.huygens.timbuctoo.core.dto.dataset.Collection;
 import nl.knaw.huygens.timbuctoo.database.tinkerpop.IndexHandler;
 import nl.knaw.huygens.timbuctoo.server.GraphWrapper;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.Traverser;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static nl.knaw.huygens.timbuctoo.logging.Logmarkers.databaseInvariant;
 import static nl.knaw.huygens.timbuctoo.util.JsonBuilder.jsn;
@@ -62,33 +62,50 @@ public class FulltextIndexChangeListener implements ChangeListener {
   }
 
   private void handleChange(Collection collection, Vertex vertex, Vertex oldVertex) {
-    GraphTraversal<Vertex, Vertex> traversal = graphWrapper.getGraph().traversal().V(vertex.id());
-    final GraphTraversal<Vertex, Try<JsonNode>> displayNameT = traversal.asAdmin().clone()
-                                                                 .union(collection.getDisplayName().traversalJson());
-    String docCaption = displayNameT.hasNext() ?
-      displayNameT.next().getOrElse(jsn("")).asText() :
-      null;
+    final String displayName;
+    long vertexId = (long) vertex.id();
+    GraphTraversalSource traversalSource = graphWrapper.getGraph().traversal();
 
     if (collection.getEntityTypeName().equals("wwdocument")) {
-      Collection wwpersons = collection.getVre().getCollectionForCollectionName("wwpersons").get();
-      List<String> authors = traversal.out("isCreatedBy")
-                                      .union(wwpersons.getDisplayName().traversalJson())
-                                      .map(x -> x.get().getOrElse(jsn("")).asText())
-                                      .toList();
-      Collections.sort(authors);
-      String authorCaption = String.join(
-        "; ",
-        authors
-      );
-      docCaption = authorCaption + " " + docCaption;
-    }
-    if (docCaption == null) {
-      LOG.warn(databaseInvariant,
-        "Displayname traversal resulted in no results vertexId={} collection={} propertyType={}",
-        vertex.id(), collection.getCollectionName(), collection.getDisplayName().getUniqueTypeId());
+      Collection wwPersonsCollection = collection.getVre().getCollectionForTypeName("wwperson");
+      displayName = getWwDocumentsQuickSearchValue(collection, wwPersonsCollection, vertexId, traversalSource);
     } else {
-      indexHandler.upsertIntoQuickSearchIndex(collection, docCaption, vertex, oldVertex);
+      displayName = getGenericQuickSearchValue(collection, vertexId, traversalSource);
     }
+
+    indexHandler.upsertIntoQuickSearchIndex(collection, displayName, vertex, oldVertex);
+  }
+
+  private String getWwDocumentsQuickSearchValue(Collection wwDocumentsCollection, Collection wwPersonsCollection,
+                                                long nodeId, GraphTraversalSource traversalS) {
+    String docCaption = getGenericQuickSearchValue(wwDocumentsCollection, nodeId, traversalS);
+
+    String authors = traversalS.V(nodeId).out("isCreatedBy")
+      .toStream()
+      .map(v -> getGenericQuickSearchValue(wwPersonsCollection, (long) v.id(), traversalS))
+      .sorted()
+      .collect(Collectors.joining("; "));
+    return authors + " " + docCaption;
+  }
+
+  private String getGenericQuickSearchValue(Collection collection, long nodeId, GraphTraversalSource traversalS) {
+    return traversalS.V(nodeId)
+      .union(collection.getDisplayName().traversalJson())
+      .map(nodeOrException -> unwrapError(nodeId, nodeOrException))
+      .toStream().findAny()
+      .orElseGet(() -> {
+        LOG.warn(databaseInvariant,
+          "Displayname traversal resulted in no results vertexId={} collection={} propertyType={}",
+          nodeId, collection.getCollectionName(), collection.getDisplayName().getUniqueTypeId());
+        return "(empty)";
+      });
+  }
+
+  private String unwrapError(long nodeId, Traverser<Try<JsonNode>> nodeOrException) {
+    return nodeOrException.get().getOrElseGet(e -> {
+      LOG.error("An error occurred while generating the displayName for " + nodeId);
+      return jsn("#Error#");
+    }).asText("");
   }
 
   private void handleRemove(Collection collection, Vertex vertex) {
