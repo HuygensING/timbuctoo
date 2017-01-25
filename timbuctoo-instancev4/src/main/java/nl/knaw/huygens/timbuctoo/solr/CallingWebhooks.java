dff163aa11a26176f694d33d8ba02dc418ac712f
Jauco Noordzij <code@jauco.nl>
2017-01-25 09:28:35 +0100

    make request more like webhooks in the wild
    The client url is usually fixed, and the payload contains the variation.
    Your consumer is more likely to have constraints on the url then on the
    data.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/solr/CallingWebhooks.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/solr/CallingWebhooks.java
@@ -2,11 +2,16 @@ package nl.knaw.huygens.timbuctoo.solr;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.SocketTimeoutException;
+
+import static nl.knaw.huygens.timbuctoo.util.JsonBuilder.jsn;
+import static nl.knaw.huygens.timbuctoo.util.JsonBuilder.jsnO;
 
 public class CallingWebhooks implements Webhooks {
   public static final Logger LOG = LoggerFactory.getLogger(CallingWebhooks.class);
@@ -22,8 +27,13 @@ public class CallingWebhooks implements Webhooks {
   @Override
   public void startIndexingForVre(String vreName) throws IOException {
     try {
-      final String url = String.format("%s/%s", solrIndexingUrl, vreName);
-      httpClient.execute(new HttpPost(url));
+      HttpPost request = new HttpPost(solrIndexingUrl);
+      //FIXME sending only the datasetName requires the client to construct the urls by hand
+      //it would be better to send the full vre metadata, but then the metadata needs to contain proper urls first
+      request.setEntity(new StringEntity(jsnO(
+        "datasetName", jsn(vreName)
+      ).toString(), ContentType.APPLICATION_JSON));
+      httpClient.execute(request);
     } catch (SocketTimeoutException e) {
       // Setting a low read timeout will terminate the connection prematurely, causing a SocketTimeoutException:
       // This is expected, because writing to an HTTP connection without waiting for a response does not conform
