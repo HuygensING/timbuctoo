3cb7454ff200a08eaa0981d90c1720afcafa94ae
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-01-26 17:05:48 +0100

    Add missing comments on this unusual construction

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/UriBearingPersonNames.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/UriBearingPersonNames.java
@@ -7,8 +7,12 @@ import nl.knaw.huygens.timbuctoo.model.PersonName;
 import java.util.List;
 import java.util.Map;
 
+// FIXME: add uri property to PersonName
+// According to René it is blocked by a failing (de)serialization in the JsonCrudService at this moment.
 public final class UriBearingPersonNames {
   public List<PersonName> list;
+  // a map that contains the uri strings of of the PersonNames in list. The value corresponds with the position of
+  // the PersonName in the list.
   public Map<String, Integer> nameUris;
 
   public UriBearingPersonNames() {
