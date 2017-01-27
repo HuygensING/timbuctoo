c2d941644565e0aee8abc222ed718a523a7464e1
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-01-27 11:27:39 +0100

    Encapsulate the logic of UriBearingPersonNames

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/UriBearingPersonNames.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/UriBearingPersonNames.java
@@ -3,6 +3,7 @@ package nl.knaw.huygens.timbuctoo.rdf;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import nl.knaw.huygens.timbuctoo.model.PersonName;
+import nl.knaw.huygens.timbuctoo.model.PersonNameComponent;
 
 import java.util.List;
 import java.util.Map;
@@ -10,9 +11,20 @@ import java.util.Map;
 // FIXME: add uri property to PersonName
 // According to René it is blocked by a failing (de)serialization in the JsonCrudService at this moment.
 public final class UriBearingPersonNames {
+  private static final List<PersonNameComponent.Type> NATURAL_ORDER = Lists.newArrayList(
+    PersonNameComponent.Type.ROLE_NAME,
+    PersonNameComponent.Type.FORENAME,
+    PersonNameComponent.Type.NAME_LINK,
+    PersonNameComponent.Type.SURNAME,
+    PersonNameComponent.Type.GEN_NAME,
+    PersonNameComponent.Type.ADD_NAME
+  );
+
+  // TODO make private
   public List<PersonName> list;
   // a map that contains the uri strings of of the PersonNames in list. The value corresponds with the position of
   // the PersonName in the list.
+  // TODO make private
   public Map<String, Integer> nameUris;
 
   public UriBearingPersonNames() {
@@ -26,5 +38,42 @@ public final class UriBearingPersonNames {
       "list=" + list +
       ", nameUris=" + nameUris +
       '}';
+  }
+
+  public UriBearingPersonNames addNameComponent(String nameUri, PersonNameComponent.Type nameType, String value) {
+    PersonName personName;
+    if (nameUris.containsKey(nameUri)) {
+      personName = list.get(nameUris.get(nameUri));
+    } else {
+      personName = new PersonName();
+      list.add(personName);
+      nameUris.put(nameUri, list.indexOf(personName));
+    }
+
+    insertNameComponentAtNaturalPosition(nameType, value, personName);
+
+    return this;
+  }
+
+  private void insertNameComponentAtNaturalPosition(PersonNameComponent.Type nameType, String value,
+                                                    PersonName currentPersonName) {
+    final List<PersonNameComponent> currentPersonNameComponents = currentPersonName.getComponents();
+    int currentIndex = getNaturalComponentPos(nameType, currentPersonNameComponents);
+    final PersonNameComponent newNameComponent = new PersonNameComponent(nameType, value);
+    currentPersonNameComponents.add(currentIndex, newNameComponent);
+  }
+
+  private int getNaturalComponentPos(PersonNameComponent.Type nameType,
+                                     List<PersonNameComponent> currentPersonNameComponents) {
+
+    for (int  currentIndex = 0; currentIndex < currentPersonNameComponents.size(); currentIndex++) {
+      final int currentOrderIndex = NATURAL_ORDER.indexOf(currentPersonNameComponents.get(currentIndex).getType());
+      final int newOrderIndex = NATURAL_ORDER.indexOf(nameType);
+      if (currentOrderIndex > newOrderIndex) {
+        return currentIndex;
+      }
+    }
+
+    return currentPersonNameComponents.size();
   }
 }
--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/tripleprocessor/PersonNamesTripleProcessor.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rdf/tripleprocessor/PersonNamesTripleProcessor.java
@@ -2,8 +2,6 @@ package nl.knaw.huygens.timbuctoo.rdf.tripleprocessor;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Lists;
-import nl.knaw.huygens.timbuctoo.model.PersonName;
 import nl.knaw.huygens.timbuctoo.model.PersonNameComponent;
 import nl.knaw.huygens.timbuctoo.model.properties.converters.PersonNamesConverter;
 import nl.knaw.huygens.timbuctoo.rdf.Database;
@@ -14,7 +12,6 @@ import org.apache.jena.graph.Triple;
 import org.slf4j.Logger;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -26,16 +23,6 @@ class PersonNamesTripleProcessor implements TripleProcessor {
 
   private final Database database;
   private ObjectMapper objectMapper = new ObjectMapper();
-
-  private final List<PersonNameComponent.Type> naturalOrder = Lists.newArrayList(
-    PersonNameComponent.Type.ROLE_NAME,
-    PersonNameComponent.Type.FORENAME,
-    PersonNameComponent.Type.NAME_LINK,
-    PersonNameComponent.Type.SURNAME,
-    PersonNameComponent.Type.GEN_NAME,
-    PersonNameComponent.Type.ADD_NAME
-  );
-
 
   PersonNamesTripleProcessor(Database database) {
     this.database = database;
@@ -66,62 +53,15 @@ class PersonNamesTripleProcessor implements TripleProcessor {
     final PersonNameComponent.Type nameType = PersonNameComponent.Type.getInstance(nameTypePredicate);
     final Optional<String> currentRawValue = entity.getPropertyValue("names");
 
+    UriBearingPersonNames names;
     if (currentRawValue.isPresent()) {
-      final UriBearingPersonNames current = objectMapper.readValue(currentRawValue.get(), UriBearingPersonNames.class);
-      addNamesProperty(entity, appendToPersonNames(value, nameType, current, subjectUri));
+      names = objectMapper.readValue(currentRawValue.get(), UriBearingPersonNames.class);
     } else {
-      addNamesProperty(entity, makeNewPersonNames(value, nameType, subjectUri));
-    }
-  }
-
-  private UriBearingPersonNames makeNewPersonNames(String value, PersonNameComponent.Type nameType, String subjectUri) {
-    final UriBearingPersonNames personNames = new UriBearingPersonNames();
-    final PersonName personName = new PersonName();
-    personName.addNameComponent(nameType, value);
-    personNames.list.add(personName);
-    personNames.nameUris.put(subjectUri, 0);
-    return personNames;
-  }
-
-  private UriBearingPersonNames appendToPersonNames(String value, PersonNameComponent.Type nameType,
-                                                    UriBearingPersonNames current, String subjectUri) {
-
-    final Integer nameIndex = current.nameUris.getOrDefault(subjectUri, -1);
-    final PersonName newOrUpdatedPersonName = nameIndex > -1 && current.list.size() > nameIndex ?
-      current.list.get(nameIndex) : new PersonName();
-
-
-    insertNameComponentAtNaturalPosition(nameType, value, newOrUpdatedPersonName);
-
-    if (nameIndex < 0) {
-      current.list.add(newOrUpdatedPersonName);
-      current.nameUris.put(subjectUri, current.list.size() - 1);
+      names = new UriBearingPersonNames();
     }
 
-    return current;
-  }
-
-
-  private void insertNameComponentAtNaturalPosition(PersonNameComponent.Type nameType, String value,
-                                                    PersonName currentPersonName) {
-    final List<PersonNameComponent> currentPersonNameComponents = currentPersonName.getComponents();
-    int currentIndex = getNaturalComponentPos(nameType, currentPersonNameComponents);
-    final PersonNameComponent newNameComponent = new PersonNameComponent(nameType, value);
-    currentPersonNameComponents.add(currentIndex, newNameComponent);
-  }
-
-  private int getNaturalComponentPos(PersonNameComponent.Type nameType,
-                                     List<PersonNameComponent> currentPersonNameComponents) {
-
-    for (int  currentIndex = 0; currentIndex < currentPersonNameComponents.size(); currentIndex++) {
-      final int currentOrderIndex = naturalOrder.indexOf(currentPersonNameComponents.get(currentIndex).getType());
-      final int newOrderIndex = naturalOrder.indexOf(nameType);
-      if (currentOrderIndex > newOrderIndex) {
-        return currentIndex;
-      }
-    }
-
-    return currentPersonNameComponents.size();
+    names.addNameComponent(subjectUri, nameType, value);
+    addNamesProperty(entity, names);
   }
 
   private void addNamesProperty(Entity entity, UriBearingPersonNames personNames) throws JsonProcessingException {
