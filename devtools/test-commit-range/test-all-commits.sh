d14e5773ba5469e870b5f44f85553ab645206f75
LB <lhuygi@users.noreply.github.com>
2017-02-13 15:40:39 +0100

    Simplify test-all-commits.sh.
    * git log --reverse replaces tac
    * git log --pretty replaces awk

--- devtools/test-commit-range/test-all-commits.sh
+++ devtools/test-commit-range/test-all-commits.sh
@@ -5,7 +5,7 @@ cd -P -- "$(dirname -- "$0")" #go to dir of script even if it was called as a sy
 if [ $1 = 'clean' ]; then
   git tag --delete $(git tag --list | grep '^fail')
 else
-  for sha in $(git log --pretty=format:'%H' --no-merges $1 | awk '{ print $0 }' | tac); do
+  for sha in $(git log --reverse --pretty=format:'%H' --no-merges $1); do
     git branch -f testbranch $sha && ../verify/cleanverify.sh testbranch test || git tag fail-$sha $sha
   done
 fi
