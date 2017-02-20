44c9906780a623a66643579c828dea25dce47653
Jauco Noordzij <code@jauco.nl>
2017-02-20 10:21:43 +0100

    fix docker-compose
    Make it work with the new docker-container split

--- devtools/run-with-services/run.sh
+++ devtools/run-with-services/run.sh
@@ -2,4 +2,8 @@
 
 cd -P -- "$(dirname -- "$0")" #go to dir of script even if it was called as a symbolic link
 
-docker-compose up
+docker-compose pull && \
+docker-compose up -d && \
+docker-compose logs -f
+
+echo "Services are still running in the background"
