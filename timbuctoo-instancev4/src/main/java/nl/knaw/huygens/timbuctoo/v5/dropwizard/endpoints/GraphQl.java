4f71978cfe35accec52391a77dd708c74626eb51
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-06-11 15:51:06 +0200

    Add GET for GraphQL endpoint

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/dropwizard/endpoints/GraphQl.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/dropwizard/endpoints/GraphQl.java
@@ -8,9 +8,11 @@ import nl.knaw.huygens.timbuctoo.v5.graphql.GraphQlService;
 import nl.knaw.huygens.timbuctoo.v5.graphql.exceptions.GraphQlFailedException;
 import nl.knaw.huygens.timbuctoo.v5.graphql.exceptions.GraphQlProcessingException;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
@@ -36,7 +38,21 @@ public class GraphQl {
   }
 
   @POST
-  public Response get(String query, @PathParam("userId") String userId, @PathParam("dataSet") String dataSet) {
+  public Response post(String query, @PathParam("userId") String userId, @PathParam("dataSet") String dataSet) {
+    return executeGraphQlQuery(query, userId, dataSet);
+  }
+
+
+  @GET
+  public Response get(@QueryParam("query") String query,
+                      @PathParam("userId") String userId,
+                      @PathParam("dataSet") String dataSet
+  ) {
+    return executeGraphQlQuery(query, userId, dataSet);
+  }
+
+  private Response executeGraphQlQuery(String query, @PathParam("userId") String userId,
+                                       @PathParam("dataSet") String dataSet) {
     try {
       return ok(graphQlService.executeQuery(userId, dataSet, query)).build();
     } catch (GraphQlProcessingException | GraphQlFailedException e) {
@@ -44,5 +60,4 @@ public class GraphQl {
       return Response.status(500).entity(e.getMessage()).build();
     }
   }
-
 }
