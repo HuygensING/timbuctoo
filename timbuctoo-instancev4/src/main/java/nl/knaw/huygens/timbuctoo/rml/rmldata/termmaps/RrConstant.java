c193f8090d342da9d28820774880c91ab06b9bd4
Jauco Noordzij <code@jauco.nl>
2017-06-21 12:49:15 +0200

    make constant rml mapping work

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rml/rmldata/termmaps/RrConstant.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/rml/rmldata/termmaps/RrConstant.java
@@ -1,6 +1,7 @@
 package nl.knaw.huygens.timbuctoo.rml.rmldata.termmaps;
 
 import nl.knaw.huygens.timbuctoo.rml.Row;
+import org.apache.jena.datatypes.RDFDatatype;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 
@@ -11,6 +12,14 @@ public class RrConstant implements RrTermMap {
 
   public RrConstant(String value) {
     this.value = NodeFactory.createURI(value);
+  }
+
+  public RrConstant(String value, TermType termType, RDFDatatype dataType) {
+    if (termType == TermType.Literal) {
+      this.value = NodeFactory.createLiteral(value, dataType);
+    } else {
+      this.value = NodeFactory.createURI(value);
+    }
   }
 
   @Override
