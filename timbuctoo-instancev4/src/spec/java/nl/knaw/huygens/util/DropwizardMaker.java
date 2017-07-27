658580eadcdcf08c5e28e9c835a64dfb5722ec97
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-07-27 15:59:02 +0200

    Fix the legacy acceptance tests
    To make sure the legacy api stil works after the refactoring.

--- timbuctoo-instancev4/src/spec/java/nl/knaw/huygens/util/DropwizardMaker.java
+++ timbuctoo-instancev4/src/spec/java/nl/knaw/huygens/util/DropwizardMaker.java
@@ -1,20 +1,24 @@
 package nl.knaw.huygens.util;
 
-import io.dropwizard.testing.ConfigOverride;
-import io.dropwizard.testing.ResourceHelpers;
+import com.google.common.collect.ImmutableMap;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import nl.knaw.huygens.timbuctoo.server.TimbuctooConfiguration;
 import nl.knaw.huygens.timbuctoo.server.TimbuctooV4;
+import nl.knaw.huygens.timbuctoo.util.EvilEnvironmentVariableHacker;
 
 public class DropwizardMaker {
+  static {
+    EvilEnvironmentVariableHacker.setEnv(ImmutableMap.of(
+      "timbuctoo_dataPath", "src/spec/resources/specrunstate",
+      "timbuctoo_port", "8089",
+      "timbuctoo_adminPort", "8088"
+    ));
+  }
+
   public static DropwizardAppRule<TimbuctooConfiguration> makeTimbuctoo() {
     return new DropwizardAppRule<>(
       TimbuctooV4.class,
-      ResourceHelpers.resourceFilePath("specrunstate/acceptance_test_config.yaml"),
-      ConfigOverride.config("databasePath", ResourceHelpers.resourceFilePath("specrunstate/database")),
-      ConfigOverride.config("authorizationsPath", ResourceHelpers.resourceFilePath("specrunstate/authorizations")),
-      ConfigOverride.config("usersFilePath", ResourceHelpers.resourceFilePath("specrunstate/users.json")),
-      ConfigOverride.config("loginsFilePath", ResourceHelpers.resourceFilePath("specrunstate/logins.json"))
+      "example_config.yaml"
     );
   }
 }
