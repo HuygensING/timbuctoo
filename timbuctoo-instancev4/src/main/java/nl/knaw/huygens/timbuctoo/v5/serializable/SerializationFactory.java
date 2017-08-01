ba285bb55b4f780d2c7efb82373e58325013817a
Jauco Noordzij <code@jauco.nl>
2017-08-01 13:26:47 +0200

    remove serializations
    We'll re-use the code but they will break in the meantime.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/serializable/SerializationFactory.java
+++
@@ -1,8 +0,0 @@
-package nl.knaw.huygens.timbuctoo.v5.serializable;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-public interface SerializationFactory {
-  Serialization create(OutputStream outputStream) throws IOException;
-}
--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/serializable/TocGenerator.java
+++
@@ -1,5 +0,0 @@
-package nl.knaw.huygens.timbuctoo.v5.serializable;
-
-public interface TocGenerator {
-  ResultToC generateToC();
-}
