19a9d24be88aab8cb3166c96a4b65e150a66dcba
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-08-15 13:33:28 +0200

    Use the path of hte resource sync endpoint
    Use the same path for the resourcesync endpoint as for one written in the resourcelist.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/datastores/resourcesync/ResourceSyncUriHelper.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/datastores/resourcesync/ResourceSyncUriHelper.java
@@ -1,13 +1,15 @@
 package nl.knaw.huygens.timbuctoo.v5.datastores.resourcesync;
 
+import nl.knaw.huygens.timbuctoo.v5.dropwizard.endpoints.ResourceSyncEndpoint;
 import nl.knaw.huygens.timbuctoo.v5.filestorage.implementations.filesystem.FileHelper;
 
 import java.io.File;
 
+import static javax.ws.rs.core.UriBuilder.fromResource;
 import static nl.knaw.huygens.timbuctoo.v5.datastores.resourcesync.ResourceSync.BASE_URI_PLACE_HOLDER;
 
 class ResourceSyncUriHelper {
-  private static final String RESOURCE_SYNC_PATH = "resourcesync";
+  private static final String RESOURCE_SYNC_PATH = fromResource(ResourceSyncEndpoint.class).build().getPath();
   private final FileHelper fileHelper;
 
   ResourceSyncUriHelper(FileHelper fileHelper) {
@@ -15,6 +17,7 @@ class ResourceSyncUriHelper {
   }
 
   String uriForFile(File file) {
+
     return String.format("%s/%s/%s", BASE_URI_PLACE_HOLDER, RESOURCE_SYNC_PATH, fileHelper.getRelativePath(file));
   }
 }
