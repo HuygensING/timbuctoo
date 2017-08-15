a081d92192d9f53b3dc9d50f89695a61e96a60f3
Meindert Kroese <meindert.kroese@huygens.knaw.nl>
2017-08-15 13:00:30 +0200

    changed extends to CollectionsofEntitiesSerialization

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/serializable/serializations/SqlSerialization.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/serializable/serializations/SqlSerialization.java
@@ -1,50 +1,41 @@
 package nl.knaw.huygens.timbuctoo.v5.serializable.serializations;
 
 import nl.knaw.huygens.timbuctoo.v5.serializable.dto.Value;
-import nl.knaw.huygens.timbuctoo.v5.serializable.serializations.base.FlatTableSerialization;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
+import nl.knaw.huygens.timbuctoo.v5.serializable.serializations.base.CollectionsOfEntitiesSerialization;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.util.List;
 
-public class SqlSerialization extends FlatTableSerialization {
+public class SqlSerialization extends CollectionsOfEntitiesSerialization {
 
-  private final CSVPrinter csvPrinter;
+  //  private final CSVPrinter csvPrinter;
   private String tableName;
   private String columnHeaders;
   private String createTable;
 
   public SqlSerialization(OutputStream outputStream) throws IOException {
-    csvPrinter = new CSVPrinter(new PrintWriter(outputStream), CSVFormat.EXCEL);
+    //    csvPrinter = new CSVPrinter(new PrintWriter(outputStream), CSVFormat.EXCEL);
   }
 
-  @Override
   protected void initialize(List<String> columnHeaders) throws IOException {
-    //    for (String columnHeader : columnHeaders) {
-    //      csvPrinter.print(columnHeader);
-    //    }
     this.columnHeaders = "";
     createTable = "CREATE TABLE tableName (";
     for (String columnHeader : columnHeaders) {
       this.columnHeaders += ", " + columnHeader;
-      createTable += columnHeader + " text,";
+      createTable += columnHeader + " text, ";
     }
     this.columnHeaders = this.columnHeaders.substring(2);
-    setTableName("");
-    createTable += createTable.subSequence(0, createTable.length() - 1) + ")";
+    createTable = createTable.subSequence(0, createTable.length() - 2) + ");";
   }
   
   protected void setTableName(String tableName) {
     this.tableName = tableName;
-    createTable.replace("tableName", tableName);
+    createTable = createTable.replace("tableName", tableName);
     System.out.println("DROP TABLE " + tableName + ";");
     System.out.println(createTable);
   }
 
-  @Override
   protected void writeRow(List<Value> values) throws IOException {
     String columnValues = "";
     for (Value value : values) {
@@ -54,12 +45,8 @@ public class SqlSerialization extends FlatTableSerialization {
         columnValues += ", '" + value.getValue().toString() + "'";
       }
     }
-    System.out.println("INSERT INTO " + tableName + "(" + columnHeaders +
+    System.out.println("INSERT INTO " + this.tableName + "(" + columnHeaders +
             ") VALUES (" + columnValues.substring(2) + ");");
-  }
-
-  @Override
-  protected void finish() throws IOException {
   }
 
 }
