84c66342906a0a0d6d465497cfdb9b43ef59b7aa
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-08-16 10:39:19 +0200

    Import supported rdf and copy other files

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/remote/rs/Import.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/server/endpoints/v2/remote/rs/Import.java
@@ -1,10 +1,12 @@
 package nl.knaw.huygens.timbuctoo.server.endpoints.v2.remote.rs;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Charsets;
+import com.google.common.collect.Sets;
 import nl.knaw.huygens.timbuctoo.remote.rs.download.RemoteFile;
 import nl.knaw.huygens.timbuctoo.remote.rs.download.ResourceSyncFileLoader;
-import nl.knaw.huygens.timbuctoo.v5.dataset.ImportManager;
 import nl.knaw.huygens.timbuctoo.v5.dataset.DataSetFactory;
+import nl.knaw.huygens.timbuctoo.v5.dataset.ImportManager;
 import nl.knaw.huygens.timbuctoo.v5.datastores.exceptions.DataStoreCreationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,11 +14,13 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.Iterator;
 import java.util.Optional;
+import java.util.Set;
 
 @Path("/v2.1/remote/rs/import")
 public class Import {
@@ -31,6 +35,7 @@ public class Import {
   }
 
   @POST
+  @Produces("application/json")
   public Response importData(@HeaderParam("Authorization") String authorization, ImportData importData)
     throws DataStoreCreationException {
     ImportManager importManager = dataSetFactory.createImportManager(importData.userId, importData.dataSetId);
@@ -39,6 +44,8 @@ public class Import {
       Iterator<RemoteFile> files =
         resourceSyncFileLoader.loadFiles(importData.source.toString()).iterator();
       LOG.info("Found files '{}'", files.hasNext());
+      ResourceSyncResport resourceSyncResport = new ResourceSyncResport();
+
       while (files.hasNext()) {
         RemoteFile file = files.next();
         MediaType parsedMediatype = null;
@@ -47,17 +54,34 @@ public class Import {
         } catch (IllegalArgumentException e) {
           LOG.error("Failed to get mediatype", e);
         }
-        importManager.addLog(
-          URI.create(file.getUrl()),
-          file.getData(),
-          Optional.of(Charsets.UTF_8),
-          Optional.ofNullable(parsedMediatype)
-        );
+        if (importManager.isRdfTypeSupported(parsedMediatype)) {
+          resourceSyncResport.importedFiles.add(file.getUrl());
+          importManager.addLog(
+            URI.create(file.getUrl()),
+            file.getData(),
+            Optional.of(Charsets.UTF_8),
+            Optional.ofNullable(parsedMediatype)
+          );
+        } else {
+          resourceSyncResport.ignoredFiles.add(file.getUrl());
+          importManager.addFile(
+            file.getData(),
+            file.getUrl(),
+            Optional.ofNullable(parsedMediatype)
+          );
+        }
       }
+
+      return Response.ok(resourceSyncResport).build();
     } catch (Exception e) {
       LOG.error("Could not read files to import", e);
+      return Response.serverError().build();
     }
-    return Response.ok().build();
+  }
+
+  public static class ResourceSyncResport {
+    public final Set<String> importedFiles = Sets.newTreeSet();
+    public final Set<String> ignoredFiles = Sets.newTreeSet();
   }
 
 
