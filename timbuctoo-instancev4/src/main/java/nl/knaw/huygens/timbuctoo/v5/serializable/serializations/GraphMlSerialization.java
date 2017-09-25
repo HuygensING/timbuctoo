d838a6c6d13a83d2bea8bb42a5fa3badff4ea736
Meindert Kroese <meindert.kroese@huygens.knaw.nl>
2017-09-25 10:55:20 +0200

    Added writeHeader, writeFooter

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/serializable/serializations/GraphMlSerialization.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/serializable/serializations/GraphMlSerialization.java
@@ -11,11 +11,50 @@ import nl.knaw.huygens.timbuctoo.v5.serializable.serializations.base.Collections
 public class GraphMlSerialization extends CollectionsOfEntitiesSerialization {
 
   //  private final CSVPrinter csvPrinter;
+  private String tableName;
+  private String columnHeaders;
+  private List<String> columns;
 
   public GraphMlSerialization(OutputStream outputStream) throws IOException {
     //    csvPrinter = new CSVPrinter(new PrintWriter(outputStream), CSVFormat.EXCEL);
   }
 
-  protected void initialize() throws IOException {
+  protected void initialize(List<String> columnHeaders) throws IOException {
+    writeHeader();
+    this.columnHeaders = "";
+    columns = new ArrayList<String>();
+    for (String columnHeader : columnHeaders) {
+      columns.add(columnHeader);
+      this.columnHeaders += ", " + columnHeader;
+      this.columnHeaders += ", " + columnHeader + "_type";
+    }
+    this.columnHeaders = this.columnHeaders.substring(2);
+  }
+  
+  protected void setTableName(String tableName) {
+    this.tableName = tableName;
+  }
+
+  protected void writeHeader() {
+    String header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"\n" +  
+      "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+      "xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns\n" +
+      "http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">";
+    System.out.println(header);
+  }
+
+  protected void writeFooter() {
+    String footer = "</graphml>";
+    System.out.println(footer);
+  }
+
+  protected void writeRow(List<Value> values) throws IOException {
+    //<graph id="G" edgedefault="undirected">
+    //<node id="n0"/>
+    //<node id="n1"/>
+    //<edge id="e1" source="n0" target="n1"/>
+    //</graph>
+    //</graphml>
   }
 }
