2415293dd5d96f5349e097f08ea0440734de2676
Meindert Kroese <meindert.kroese@huygens.knaw.nl>
2017-10-23 16:40:42 +0200

    An attempt the build the return value as a json object

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/dropwizard/endpoints/OpenRefineReconciliationEndpoint.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/dropwizard/endpoints/OpenRefineReconciliationEndpoint.java
@@ -20,6 +20,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 import nl.knaw.huygens.timbuctoo.util.UriHelper;
 import nl.knaw.huygens.timbuctoo.v5.datastores.resourcesync.ResourceSync;
@@ -87,11 +88,12 @@ public class OpenRefineReconciliationEndpoint {
     System.err.println(jsonObject);
     Set<Entry<String, JsonElement>> jsonSet = jsonObject.entrySet();
     String result = "{";
+    JsonObject jsonResult = new JsonObject();
     for (Entry<String, JsonElement> jsonItem : jsonSet) {
       String key = jsonItem.getKey();
       JsonObject value = (JsonObject) jsonItem.getValue();
       System.err.println("query: " + value.getAsJsonPrimitive("query"));
-      System.err.println("limit: " + value.getAsJsonPrimitive("limit"));
+      // System.err.println("limit: " + value.getAsJsonPrimitive("limit"));
 
       result += "\"" + key + "\": {";
       result += "\"result\" : [ { \"id\" : " + key.substring(1) + ",";
@@ -100,9 +102,26 @@ public class OpenRefineReconciliationEndpoint {
       result += "\"score\" : 1.0 ,";
       result += "\"match\" : true";
       result += " } ] } ,";
+      // building the result as a json object should be more easy, but...
+      JsonObject itemResult = new JsonObject();
+      itemResult.addProperty("id", key.substring(1));
+      itemResult.add("name", value.getAsJsonPrimitive("query"));
+      JsonArray types = new JsonArray();
+      types.add(new JsonPrimitive("String"));
+      itemResult.add("type", types);
+      itemResult.addProperty("score", 1.0);
+      itemResult.addProperty("match", true);
+      JsonArray itemArray = new JsonArray();
+      itemArray.add(itemResult);
+      JsonObject thisResult = new JsonObject();
+      thisResult.add("result",itemArray);
+      jsonResult.add(key, thisResult);
     }
     result = result.substring(0, result.length() - 2) + " }";
     System.err.println(result);
-    return result;
+    System.err.println(jsonResult.getClass());
+    System.err.println(jsonResult.toString());
+    
+    return jsonResult.toString();
   }
 }
