4851cff2f8c16dc5e1d0d3ff3f4893bbb8042fd6
meindertk <meindert.kroese@huygens.knaw.nl>
2017-10-24 14:35:55 +0200

    Started splitting Query

---
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/openrefine/Query.java
@@ -0,0 +1,11 @@
+package nl.knaw.huygens.timbuctoo.v5.openrefine;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Query {
+  public String query;
+  public int limit;
+  //  String type;
+  //  @JsonProperty("type_strict")
+  //  String typeStrict;
+}
