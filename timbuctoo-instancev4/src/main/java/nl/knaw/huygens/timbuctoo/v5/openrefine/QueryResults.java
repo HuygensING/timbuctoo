c1bd39b58b991a3aae8d778e2077104758eea60c
Meindert Kroese <meindert.kroese@huygens.knaw.nl>
2017-10-25 09:57:11 +0200

    Added classes for queryResults

---
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/openrefine/QueryResults.java
@@ -0,0 +1,7 @@
+package nl.knaw.huygens.timbuctoo.v5.openrefine;
+
+import java.util.Map;
+
+public class QueryResults {
+  public Map<String,QueryResult[]> queryResults;
+}
---
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/openrefine/RequestResult.java
@@ -0,0 +1,8 @@
+package nl.knaw.huygens.timbuctoo.v5.openrefine;
+
+import java.util.Map;
+
+public class RequestResult {
+  Map<String,QueryResults> results;
+
+}
