63a20f683913cf235a2ad74b8e9d450111217b33
meindertk <meindert.kroese@huygens.knaw.nl>
2017-11-03 14:00:50 +0100

    I commit this (allthough it isn't working), to get it on my local machine

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/dropwizard/endpoints/OpenRefineReconciliationEndpoint.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/dropwizard/endpoints/OpenRefineReconciliationEndpoint.java
@@ -3,15 +3,18 @@ package nl.knaw.huygens.timbuctoo.v5.dropwizard.endpoints;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.knaw.huygens.timbuctoo.v5.datastores.resourcesync.ResourceSyncException;
 import nl.knaw.huygens.timbuctoo.v5.openrefine.Query;
 import nl.knaw.huygens.timbuctoo.v5.openrefine.QueryResults;
 import nl.knaw.huygens.timbuctoo.v5.openrefine.ReconciliationQueryExecuter;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -53,4 +56,24 @@ public class OpenRefineReconciliationEndpoint {
 
     return Response.serverError().build();
   }
+
+  @GET
+  public Response query(@QueryParam("queries") String queries,
+                        @QueryParam("callback") String callback) throws ResourceSyncException {
+
+    String result = "{ \"queries\" : \"" + queries + "\" }";
+    System.err.println(result);
+    if (queries == null) {
+      result = "{ \"name\": \"Timbuctoo\", \"view\" : {\"url\": \"http://localhost:8080/v5/openrefinereconciliation/{{id}}\"}, \"defaultTypes\" : [{\"id\":\"/getname\",\"name\":\"Person\"}], \"identifierSpace\": \"http://rdf.freebase.com/ns/authority.netflix.movie\",\"schemaSpace\": \"http://rdf.freebase.com/ns/type.object.id\" }";
+    }
+    if (callback != null) {
+      result = "/**/" + callback + "(" + result + ");";
+      System.err.println(result);
+      return Response.ok(result, MediaType.TEXT_PLAIN).build();
+
+    }
+    System.err.println(result);
+    return Response.ok(result, MediaType.APPLICATION_JSON_TYPE).build();
+  }
+
 }
