f1c70864538a5f2d64cf9f1b9cc9d944aa23e440
Martijn Maas <martijn.maas@huygens.knaw.nl>
2018-03-22 16:40:40 +0100

    Add DefaultSummaryProps configuration

---
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/graphql/defaultconfiguration/DefaultSummaryProps.java
@@ -0,0 +1,34 @@
+package nl.knaw.huygens.timbuctoo.v5.graphql.defaultconfiguration;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Lists;
+import org.immutables.value.Value;
+
+import java.util.List;
+
+@Value.Immutable
+public interface DefaultSummaryProps {
+  @JsonCreator
+  static DefaultSummaryProps create(
+    @JsonProperty("defaultTitles") List<SummaryProp> defaultTitles,
+    @JsonProperty("defaultDescriptions") List<SummaryProp> defaultDescriptions,
+    @JsonProperty("defaultImages") List<SummaryProp> defaultImages
+  ) {
+
+    defaultTitles = defaultTitles == null ? Lists.newArrayList() : defaultTitles;
+    defaultDescriptions = defaultDescriptions == null ? Lists.newArrayList() : defaultDescriptions;
+    defaultImages = defaultImages == null ? Lists.newArrayList() : defaultImages;
+
+    return ImmutableDefaultSummaryProps.builder()
+                                       .defaultTitles(defaultTitles)
+                                       .defaultDescriptions(defaultDescriptions)
+                                       .defaultImages(defaultImages).build();
+  }
+
+  List<SummaryProp> getDefaultTitles();
+
+  List<SummaryProp> getDefaultDescriptions();
+
+  List<SummaryProp> getDefaultImages();
+}
