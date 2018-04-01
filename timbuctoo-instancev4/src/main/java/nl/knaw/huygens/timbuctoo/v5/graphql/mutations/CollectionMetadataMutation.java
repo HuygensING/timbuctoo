7da375d1ed1c2f9d4bdd060b324b69c1b936624c
Jauco Noordzij <code@jauco.nl>
2018-04-01 13:17:12 +0200

    add collection description mutation

---
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/graphql/mutations/CollectionMetadataMutation.java
@@ -0,0 +1,55 @@
+package nl.knaw.huygens.timbuctoo.v5.graphql.mutations;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import nl.knaw.huygens.timbuctoo.v5.dataset.DataSetRepository;
+import nl.knaw.huygens.timbuctoo.v5.dataset.dto.DataSet;
+import nl.knaw.huygens.timbuctoo.v5.filestorage.exceptions.LogStorageFailedException;
+import nl.knaw.huygens.timbuctoo.v5.graphql.datafetchers.berkeleydb.dto.LazyTypeSubjectReference;
+import nl.knaw.huygens.timbuctoo.v5.graphql.mutations.dto.PredicateMutation;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import static nl.knaw.huygens.timbuctoo.v5.graphql.mutations.dto.PredicateMutation.replace;
+import static nl.knaw.huygens.timbuctoo.v5.graphql.mutations.dto.PredicateMutation.subject;
+import static nl.knaw.huygens.timbuctoo.v5.graphql.mutations.dto.PredicateMutation.value;
+import static nl.knaw.huygens.timbuctoo.v5.util.RdfConstants.RDFS_LABEL;
+
+public class CollectionMetadataMutation implements DataFetcher {
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().registerModule(new GuavaModule());
+  private final DataSetRepository dataSetRepository;
+
+  public CollectionMetadataMutation(DataSetRepository dataSetRepository) {
+    this.dataSetRepository = dataSetRepository;
+  }
+
+  @Override
+  public Object get(DataFetchingEnvironment env) {
+    DataSet dataSet = MutationHelpers.getDataSet(env, dataSetRepository::getDataSet);
+    MutationHelpers.checkPermissions(env, dataSet.getMetadata());
+    try {
+      String collectionUri = env.getArgument("collectionUri");
+      Map data = env.getArgument("metadata");
+      final PredicateMutation mutation = new PredicateMutation();
+      mutation.entity(
+        collectionUri,
+        getValue(data, "title").map(v -> replace(RDFS_LABEL, value(v))).orElse(null),
+        getValue(data, "archeType").map(v -> replace("http://www.w3.org/2000/01/rdf-schema#subClassOf", subject(v))).orElse(null)
+      );
+
+      MutationHelpers.addMutation(dataSet, mutation);
+      return new LazyTypeSubjectReference(collectionUri, dataSet);
+    } catch (LogStorageFailedException | InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Optional<String> getValue(Map viewConfig, String valueName) {
+    return Optional.ofNullable((String) viewConfig.get(valueName));
+  }
+
+}
--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/graphql/rootquery/RootQuery.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/graphql/rootquery/RootQuery.java
@@ -30,6 +30,7 @@ import nl.knaw.huygens.timbuctoo.v5.graphql.datafetchers.dto.DataSetWithDatabase
 import nl.knaw.huygens.timbuctoo.v5.graphql.datafetchers.dto.RootData;
 import nl.knaw.huygens.timbuctoo.v5.graphql.datafetchers.dto.SubjectReference;
 import nl.knaw.huygens.timbuctoo.v5.graphql.derivedschema.DerivedSchemaTypeGenerator;
+import nl.knaw.huygens.timbuctoo.v5.graphql.mutations.CollectionMetadataMutation;
 import nl.knaw.huygens.timbuctoo.v5.graphql.mutations.CreateDataSetMutation;
 import nl.knaw.huygens.timbuctoo.v5.graphql.mutations.DataSetDescriptionMutation;
 import nl.knaw.huygens.timbuctoo.v5.graphql.mutations.DeleteDataSetMutation;
@@ -310,6 +311,7 @@ public class RootQuery implements Supplier<GraphQLSchema> {
       .dataFetcher("publish", new MakePublicMutation(dataSetRepository))
       .dataFetcher("extendSchema", new ExtendSchemaMutation(dataSetRepository))
       .dataFetcher("setDataSetMetadata", new DataSetDescriptionMutation(dataSetRepository))
+      .dataFetcher("setCollectionMetadata", new CollectionMetadataMutation(dataSetRepository))
     );
 
     wiring.wiringFactory(wiringFactory);
