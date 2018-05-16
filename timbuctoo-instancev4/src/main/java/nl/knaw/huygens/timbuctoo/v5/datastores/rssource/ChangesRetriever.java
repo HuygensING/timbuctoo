ad23eed5829c4a6515d2ff092479a09e0e954b65
Pratham Joshi <pratham.raj.joshi@gmail.com>
2018-05-16 15:56:57 +0200

    test case for getChanges

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/datastores/rssource/ChangesRetriever.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/datastores/rssource/ChangesRetriever.java
@@ -8,6 +8,7 @@ import nl.knaw.huygens.timbuctoo.v5.datastores.quadstore.dto.CursorQuad;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ChangesRetriever {
@@ -24,10 +25,11 @@ public class ChangesRetriever {
 
   public List<String> retrieveChangeFileNames(Supplier<List<Integer>> versions) {
     List<String> changeFileNames = new ArrayList<>();
-
-    versions.get().forEach(version -> {
-      changeFileNames.add("changes" + version.toString() + ".nqud");
-    });
+    //filter hack to ignore isClosed values. needs to be fixed in DataRetriever.
+    versions.get().stream().filter(x -> x != Integer.MAX_VALUE).collect(Collectors.toList())
+      .forEach(version -> {
+        changeFileNames.add("changes" + version.toString() + ".nqud");
+      });
 
     return changeFileNames;
   }
