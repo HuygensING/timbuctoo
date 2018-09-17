b7b55a2738247562862a075a61cbc9db80428e29
Pratham Joshi <pratham.raj.joshi@gmail.com>
2018-09-17 15:12:58 +0200

    refactored HandleService

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/redirectionservice/HandleService.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/redirectionservice/HandleService.java
@@ -1,108 +1,52 @@
 package nl.knaw.huygens.timbuctoo.v5.redirectionservice;
 
 import com.kjetland.dropwizard.activemq.ActiveMQBundle;
-import com.kjetland.dropwizard.activemq.ActiveMQSender;
 import nl.knaw.huygens.persistence.PersistenceException;
 import nl.knaw.huygens.persistence.PersistenceManager;
 import nl.knaw.huygens.timbuctoo.core.NotFoundException;
-import nl.knaw.huygens.timbuctoo.core.PersistentUrlCreator;
 import nl.knaw.huygens.timbuctoo.core.TransactionEnforcer;
 import nl.knaw.huygens.timbuctoo.core.TransactionState;
 import nl.knaw.huygens.timbuctoo.core.dto.EntityLookup;
-import nl.knaw.huygens.timbuctoo.logging.Logmarkers;
-import nl.knaw.huygens.timbuctoo.queued.ActiveMqQueueCreator;
-import nl.knaw.huygens.timbuctoo.v5.redirectionservice.HandleServiceFactory;
-import nl.knaw.huygens.timbuctoo.v5.redirectionservice.RedirectionService;
 import org.slf4j.Logger;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class HandleService implements RedirectionService {
+public class HandleService extends RedirectionService {
   private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(HandleService.class);
   public static final String HANDLE_QUEUE = "pids";
 
   private final PersistenceManager manager;
-  private final ActiveMQSender sender;
   private TransactionEnforcer transactionEnforcer;
 
   public HandleService(PersistenceManager manager, ActiveMQBundle activeMqBundle) {
+    super(activeMqBundle, HANDLE_QUEUE);
     this.manager = manager;
     this.transactionEnforcer = null;
-    ActiveMqQueueCreator<HandleAdderParameters> queueCreator = new ActiveMqQueueCreator<>(
-      HandleAdderParameters.class,
-      HANDLE_QUEUE,
-      activeMqBundle
-    );
-    queueCreator.registerReceiver(this::actualExecution);
-    this.sender = queueCreator.createSender();
   }
 
-  public void init(TransactionEnforcer transactionEnforcer) {
-    this.transactionEnforcer = transactionEnforcer;
-  }
+  protected void savePid(HandleAdderParameters params) throws PersistenceException, URISyntaxException {
+    URI uri = params.getUrlToRedirectTo();
+    LOG.info(String.format("Retrieving persistent url for '%s'", uri));
+    String persistentId = (manager.persistURL(uri.toString()));
+    URI persistentUrl = new URI(manager.getPersistentURL(persistentId));
 
-  private void actualExecution(HandleAdderParameters params) {
-    try {
-      URI uri = params.getUrlToRedirectTo();
-      LOG.info(String.format("Retrieving persistent url for '%s'", uri));
-      String persistentId = (manager.persistURL(uri.toString()));
-      URI persistentUrl = new URI(manager.getPersistentURL(persistentId));
-
-      transactionEnforcer.execute(timbuctooActions -> {
+    transactionEnforcer.execute(timbuctooActions -> {
+        try {
+          timbuctooActions.addPid(persistentUrl, params.getEntityLookup());
+          LOG.info("committed pid");
+          return TransactionState.commit();
+        } catch (NotFoundException e) {
+          LOG.warn("Entity for entityLookup '{}' cannot be found", params.getEntityLookup());
           try {
-            timbuctooActions.addPid(persistentUrl, params.getEntityLookup());
-            LOG.info("committed pid");
-            return TransactionState.commit();
-          } catch (NotFoundException e) {
-            LOG.warn("Entity for entityLookup '{}' cannot be found", params.getEntityLookup());
-            try {
-              manager.deletePersistentId(persistentId);
-            } catch (PersistenceException e1) {
-              LOG.error("Cannot remove handle with id '{}'", persistentId);
-            }
-            return TransactionState.rollback();
+            manager.deletePersistentId(persistentId);
+          } catch (PersistenceException e1) {
+            LOG.error("Cannot remove handle with id '{}'", persistentId);
           }
+          return TransactionState.rollback();
         }
-      );
-    } catch (PersistenceException | URISyntaxException e) {
-      LOG.error(Logmarkers.serviceUnavailable, "Could not create handle", e);
-      if (params.getRetries() < 5) {
-        LOG.warn(String.format("Re-adding %s%s job to the queue for '%s' '%s'",
-          params.getRetries() + 1, getOrdinalSuffix(params.getRetries() + 1),
-          params.getEntityLookup(),
-          params.getUrlToRedirectTo()
-        ));
-        this.sender.send(params.nextTry());
       }
-    }
+    );
   }
 
-  @Override
-  public void add(URI uriToRedirectTo, EntityLookup entityLookup) {
-    if (transactionEnforcer == null) {
-      throw new IllegalStateException("init() must be called before you can add items");
-    }
-    this.sender.send(new HandleAdderParameters(uriToRedirectTo, entityLookup));
-  }
-
-  // gogo gadgetstackoverflow
-  private String getOrdinalSuffix(int value) {
-    int hunRem = value % 100;
-    int tenRem = value % 10;
-
-    if (hunRem - tenRem == 10) {
-      return "th";
-    }
-    switch (tenRem) {
-      case 1:
-        return "st";
-      case 2:
-        return "nd";
-      case 3:
-        return "rd";
-      default:
-        return "th";
-    }
-  }
 }
--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/redirectionservice/RedirectionService.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/redirectionservice/RedirectionService.java
@@ -1,12 +1,79 @@
 package nl.knaw.huygens.timbuctoo.v5.redirectionservice;
 
+import com.kjetland.dropwizard.activemq.ActiveMQBundle;
+import com.kjetland.dropwizard.activemq.ActiveMQSender;
+import nl.knaw.huygens.persistence.PersistenceException;
 import nl.knaw.huygens.timbuctoo.core.TransactionEnforcer;
 import nl.knaw.huygens.timbuctoo.core.dto.EntityLookup;
+import nl.knaw.huygens.timbuctoo.logging.Logmarkers;
+import nl.knaw.huygens.timbuctoo.queued.ActiveMqQueueCreator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
-public interface RedirectionService {
-  void add(URI uriToRedirectTo, EntityLookup entityLookup);
+public abstract class RedirectionService {
+  private static final Logger LOG = LoggerFactory.getLogger(RedirectionService.class);
+  private final ActiveMQSender sender;
+  protected TransactionEnforcer transactionEnforcer;
 
-  void init(TransactionEnforcer transactionEnforcer);
+  public RedirectionService(ActiveMQBundle activeMqBundle, String queueName) {
+    ActiveMqQueueCreator<HandleAdderParameters> queueCreator = new ActiveMqQueueCreator<>(
+      HandleAdderParameters.class,
+      queueName,
+      activeMqBundle
+    );
+    queueCreator.registerReceiver(this::actualExecution);
+    this.sender = queueCreator.createSender();
+  }
+
+  private void actualExecution(HandleAdderParameters params) {
+    try {
+      savePid(params);
+    } catch (PersistenceException | URISyntaxException e) {
+      LOG.error(Logmarkers.serviceUnavailable, "Could not create handle", e);
+      if (params.getRetries() < 5) {
+        LOG.warn(String.format("Re-adding %s%s job to the queue for '%s' '%s'",
+          params.getRetries() + 1, getOrdinalSuffix(params.getRetries() + 1),
+          params.getEntityLookup(),
+          params.getUrlToRedirectTo()
+        ));
+        this.sender.send(params.nextTry());
+      }
+    }
+  }
+
+  // gogo gadgetstackoverflow
+  private String getOrdinalSuffix(int value) {
+    int hunRem = value % 100;
+    int tenRem = value % 10;
+
+    if (hunRem - tenRem == 10) {
+      return "th";
+    }
+    switch (tenRem) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  }
+
+  protected abstract void savePid(HandleAdderParameters params) throws PersistenceException, URISyntaxException;
+
+  public final void add(URI uriToRedirectTo, EntityLookup entityLookup) {
+    if (transactionEnforcer == null) {
+      throw new IllegalStateException("init() must be called before you can add items");
+    }
+    this.sender.send(new HandleAdderParameters(uriToRedirectTo, entityLookup));
+  }
+
+  public final void init(TransactionEnforcer transactionEnforcer) {
+    this.transactionEnforcer = transactionEnforcer;
+  }
 }
