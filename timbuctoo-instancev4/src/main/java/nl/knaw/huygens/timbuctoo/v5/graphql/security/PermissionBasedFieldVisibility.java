1ac7b99b3f02522010c4ea8960112eae953e327e
Martijn Maas <martijn.maas@huygens.knaw.nl>
2019-01-23 16:40:48 +0100

    Make PermissionBasedFieldVisibility load data once
    A new instance of this class is created each GraphQL request.
    The permissions on data sets will not be changed during the loading of the GraphQL schema.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/graphql/security/PermissionBasedFieldVisibility.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/graphql/security/PermissionBasedFieldVisibility.java
@@ -4,11 +4,9 @@ import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLFieldsContainer;
 import graphql.schema.visibility.GraphqlFieldVisibility;
 import nl.knaw.huygens.timbuctoo.v5.dataset.DataSetRepository;
-import nl.knaw.huygens.timbuctoo.v5.dataset.dto.DataSet;
 import nl.knaw.huygens.timbuctoo.v5.security.dto.Permission;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -17,24 +15,25 @@ import java.util.stream.Collectors;
 public class PermissionBasedFieldVisibility implements GraphqlFieldVisibility {
   private final UserPermissionCheck userPermissionCheck;
   private final DataSetRepository dataSetRepository;
+  private Set<String> dataSetNamesWithOutReadPermission;
 
   public PermissionBasedFieldVisibility(UserPermissionCheck userPermissionCheck, DataSetRepository dataSetRepository) {
     this.userPermissionCheck = userPermissionCheck;
     this.dataSetRepository = dataSetRepository;
+    dataSetNamesWithOutReadPermission = dataSetRepository
+        .getDataSets()
+        .stream()
+        .filter(dataSet -> !userPermissionCheck.hasPermission(dataSet.getMetadata(), Permission.READ))
+        .map(dataSet -> dataSet.getMetadata().getCombinedId())
+        .collect(Collectors.toSet());
   }
 
   @Override
   public List<GraphQLFieldDefinition> getFieldDefinitions(GraphQLFieldsContainer fieldsContainer) {
     List<GraphQLFieldDefinition> graphQlFieldDefinitions = new ArrayList<>();
 
-    Collection<DataSet> dataSets = dataSetRepository.getDataSets();
-
-    Set<String> dataSetNamesWithOutReadPermission = dataSets.stream()
-      .filter(dataSet -> !userPermissionCheck.hasPermission(dataSet.getMetadata(), Permission.READ))
-      .map(dataSet -> dataSet.getMetadata().getCombinedId())
-      .collect(Collectors.toSet());
-
-    Iterator<GraphQLFieldDefinition> graphQlFieldDefinitionIterator = fieldsContainer.getFieldDefinitions().iterator();
+    Iterator<GraphQLFieldDefinition> graphQlFieldDefinitionIterator =
+        fieldsContainer.getFieldDefinitions().iterator();
     while (graphQlFieldDefinitionIterator.hasNext()) {
       GraphQLFieldDefinition graphQlFieldDefinition = graphQlFieldDefinitionIterator.next();
       if (!dataSetNamesWithOutReadPermission.contains(graphQlFieldDefinition.getName())) {
@@ -47,24 +46,22 @@ public class PermissionBasedFieldVisibility implements GraphqlFieldVisibility {
 
   @Override
   public GraphQLFieldDefinition getFieldDefinition(GraphQLFieldsContainer fieldsContainer, String fieldName) {
-    Collection<DataSet> dataSets = dataSetRepository.getDataSets();
-
-    Set<String> dataSetNamesWithOutReadPermission = dataSets.stream()
-      .filter(dataSet -> !userPermissionCheck.hasPermission(dataSet.getMetadata(), Permission.READ))
-      .map(dataSet -> dataSet.getMetadata().getCombinedId())
-      .collect(Collectors.toSet());
-
-    Iterator<GraphQLFieldDefinition> graphQlFieldDefinitionIterator = fieldsContainer.getFieldDefinitions().iterator();
+    Iterator<GraphQLFieldDefinition> graphQlFieldDefinitionIterator =
+        fieldsContainer.getFieldDefinitions().iterator();
 
     while (graphQlFieldDefinitionIterator.hasNext()) {
       GraphQLFieldDefinition graphQlFieldDefinition = graphQlFieldDefinitionIterator.next();
 
       if (!dataSetNamesWithOutReadPermission.contains(graphQlFieldDefinition.getName()) &&
-        graphQlFieldDefinition.getName().equals(fieldName)) {
+          graphQlFieldDefinition.getName().equals(fieldName)) {
         return graphQlFieldDefinition;
       }
 
     }
     return null;
+
+
   }
+
+
 }
