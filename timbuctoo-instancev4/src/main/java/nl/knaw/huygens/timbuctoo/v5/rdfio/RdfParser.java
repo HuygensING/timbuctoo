eb456450010d46661c2df8b527b0e166ad7c519f
Martijn Maas <martijn.maas@huygens.knaw.nl>
2019-01-25 16:07:13 +0100

    Add context to Rdf parse problem
    Communicate the problem with the user and gif the user enought context to solve the error.

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/rdfio/RdfParser.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/rdfio/RdfParser.java
@@ -1,10 +1,11 @@
 package nl.knaw.huygens.timbuctoo.v5.rdfio;
 
+import nl.knaw.huygens.timbuctoo.v5.dataset.exceptions.RdfProcessingParseException;
 import nl.knaw.huygens.timbuctoo.v5.filestorage.dto.CachedLog;
 import nl.knaw.huygens.timbuctoo.v5.dataset.RdfProcessor;
 import nl.knaw.huygens.timbuctoo.v5.dataset.exceptions.RdfProcessingFailedException;
 
 public interface RdfParser {
   void importRdf(CachedLog input, String baseUri, String defaultGraph,
-                 RdfProcessor rdfProcessor) throws RdfProcessingFailedException;
+                 RdfProcessor rdfProcessor) throws RdfProcessingFailedException, RdfProcessingParseException;
 }
