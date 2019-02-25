25820f8869d91a981cc7c225d409e0c75ff54c72
kerim1 <kerim1@users.noreply.github.com>
2019-02-25 11:09:56 +0100

    Use a non persistent database, rather than mocking BdbTruePatchStore

--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/v5/datastores/rssource/ChangeListBuilderTest.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/v5/datastores/rssource/ChangeListBuilderTest.java
@@ -1,24 +1,22 @@
 package nl.knaw.huygens.timbuctoo.v5.datastores.rssource;
 
 import com.google.common.collect.Lists;
+import com.sleepycat.bind.tuple.TupleBinding;
+import nl.knaw.huygens.timbuctoo.v5.berkeleydb.isclean.StringStringIsCleanHandler;
 import nl.knaw.huygens.timbuctoo.v5.dataset.ChangesRetriever;
 import nl.knaw.huygens.timbuctoo.v5.datastores.implementations.bdb.BdbTruePatchStore;
-import nl.knaw.huygens.timbuctoo.v5.datastores.quadstore.dto.ChangeType;
-import nl.knaw.huygens.timbuctoo.v5.datastores.quadstore.dto.CursorQuad;
 import nl.knaw.huygens.timbuctoo.v5.datastores.quadstore.dto.Direction;
+import nl.knaw.huygens.timbuctoo.v5.dropwizard.BdbNonPersistentEnvironmentCreator;
 import nl.knaw.huygens.timbuctoo.v5.util.RdfConstants;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 public class ChangeListBuilderTest {
 
@@ -34,69 +32,32 @@ public class ChangeListBuilderTest {
   }
 
   @Test
-  public void retrieveChangesReturnsQuadsForGivenVersionAndSubjects() {
-    BdbTruePatchStore bdbTruePatchStore = mock(BdbTruePatchStore.class);
+  public void retrieveChangesReturnsQuadsForGivenVersionAndSubjects() throws Exception {
+    BdbNonPersistentEnvironmentCreator dataStoreFactory = new BdbNonPersistentEnvironmentCreator();
+    BdbTruePatchStore bdbTruePatchStore = new BdbTruePatchStore(dataStoreFactory.getDatabase(
+      "user",
+      "dataSet",
+      "schema",
+      false,
+      TupleBinding.getPrimitiveBinding(String.class),
+      TupleBinding.getPrimitiveBinding(String.class),
+      new StringStringIsCleanHandler()
+    ));
 
-    CursorQuad cursorQuad1 = mock(CursorQuad.class);
-    given(cursorQuad1.getSubject()).willReturn("s1");
-    given(cursorQuad1.getPredicate()).willReturn("p1");
-    given(cursorQuad1.getChangeType()).willReturn(ChangeType.ASSERTED);
-    given(cursorQuad1.getObject()).willReturn("o1");
-    given(cursorQuad1.getDirection()).willReturn(Direction.OUT);
-
-    CursorQuad cursorQuad2 = mock(CursorQuad.class);
-    given(cursorQuad2.getSubject()).willReturn("s2");
-    given(cursorQuad2.getPredicate()).willReturn("p2");
-    given(cursorQuad2.getChangeType()).willReturn(ChangeType.RETRACTED);
-    given(cursorQuad2.getObject()).willReturn("o2");
-    given(cursorQuad2.getDirection()).willReturn(Direction.OUT);
-
-    CursorQuad cursorQuad3 = mock(CursorQuad.class);
-    given(cursorQuad3.getSubject()).willReturn("s3");
-    given(cursorQuad3.getPredicate()).willReturn("p3");
-    given(cursorQuad3.getChangeType()).willReturn(ChangeType.ASSERTED);
-    given(cursorQuad3.getObject()).willReturn("o3");
-    given(cursorQuad3.getDirection()).willReturn(Direction.OUT);
-    given(cursorQuad3.getValuetype()).willReturn(Optional.of(RdfConstants.STRING));
-
-    CursorQuad cursorQuad4 = mock(CursorQuad.class);
-    given(cursorQuad4.getSubject()).willReturn("s4");
-    given(cursorQuad4.getPredicate()).willReturn("p4");
-    given(cursorQuad4.getChangeType()).willReturn(ChangeType.RETRACTED);
-    given(cursorQuad4.getObject()).willReturn("o4");
-    given(cursorQuad4.getDirection()).willReturn(Direction.OUT);
-    given(cursorQuad4.getValuetype()).willReturn(Optional.of(RdfConstants.STRING));
-
-    CursorQuad cursorQuad5 = mock(CursorQuad.class);
-    given(cursorQuad5.getSubject()).willReturn("s5");
-    given(cursorQuad5.getPredicate()).willReturn("p5");
-    given(cursorQuad5.getChangeType()).willReturn(ChangeType.ASSERTED);
-    given(cursorQuad5.getObject()).willReturn("o5");
-    given(cursorQuad5.getDirection()).willReturn(Direction.OUT);
-    given(cursorQuad5.getValuetype()).willReturn(Optional.of(RdfConstants.LANGSTRING));
-    given(cursorQuad5.getLanguage()).willReturn(Optional.of("en"));
-
-    CursorQuad cursorQuad6 = mock(CursorQuad.class);
-    given(cursorQuad6.getSubject()).willReturn("s6");
-    given(cursorQuad6.getPredicate()).willReturn("p6");
-    given(cursorQuad6.getChangeType()).willReturn(ChangeType.RETRACTED);
-    given(cursorQuad6.getObject()).willReturn("o6");
-    given(cursorQuad6.getDirection()).willReturn(Direction.OUT);
-    given(cursorQuad6.getValuetype()).willReturn(Optional.of(RdfConstants.LANGSTRING));
-    given(cursorQuad6.getLanguage()).willReturn(Optional.of("en"));
-
-    given(bdbTruePatchStore.getChangesOfVersion(1, true))
-      .willReturn(Stream.of(cursorQuad1, cursorQuad2, cursorQuad3, cursorQuad4, cursorQuad5, cursorQuad6));
+    int version = 1;
+    bdbTruePatchStore.put("s1", version, "p1", Direction.OUT, true, "o1", null, null);
+    bdbTruePatchStore.put("s2", version, "p2", Direction.OUT, false, "o2", null, null);
+    bdbTruePatchStore.put("s3", version, "p3", Direction.OUT, true, "o3", RdfConstants.STRING, null);
+    bdbTruePatchStore.put("s4", version, "p4", Direction.OUT, false, "o4", RdfConstants.STRING, null);
+    bdbTruePatchStore.put("s5", version, "p5", Direction.OUT, true, "o5", RdfConstants.LANGSTRING, "en");
+    bdbTruePatchStore.put("s6", version, "p6", Direction.OUT, false, "o6", RdfConstants.LANGSTRING, "en");
 
     ChangeListBuilder changeListBuilder = new ChangeListBuilder("graph");
-
-    Integer version = 1;
-
     ChangesRetriever changesRetriever = new ChangesRetriever(bdbTruePatchStore, null);
 
     List<String> changes = changeListBuilder.retrieveChanges(changesRetriever, version).collect(Collectors.toList());
 
-    assertThat(changes, contains(
+    assertThat(changes, containsInAnyOrder(
       "+<s1> <p1> <o1> <graph> .\n",
       "-<s2> <p2> <o2> <graph> .\n",
       "+<s3> <p3> \"o3\"^^<http://www.w3.org/2001/XMLSchema#string> <graph> .\n",
