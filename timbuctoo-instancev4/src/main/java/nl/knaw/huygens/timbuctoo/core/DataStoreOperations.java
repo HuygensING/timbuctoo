4aa318670deaeb6d0115fbe4ef3db417e4882c96
Martijn Maas <martijn.maas@huygens.knaw.nl>
2019-03-04 16:03:16 +0100

    Remove unused methods in DataStoreOperations

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/core/DataStoreOperations.java
+++ timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/core/DataStoreOperations.java
@@ -1,6 +1,5 @@
 package nl.knaw.huygens.timbuctoo.core;
 
-import nl.knaw.huygens.timbuctoo.core.dto.CreateCollection;
 import nl.knaw.huygens.timbuctoo.core.dto.CreateEntity;
 import nl.knaw.huygens.timbuctoo.core.dto.CreateRelation;
 import nl.knaw.huygens.timbuctoo.core.dto.DataStream;
@@ -11,22 +10,15 @@ import nl.knaw.huygens.timbuctoo.core.dto.RelationType;
 import nl.knaw.huygens.timbuctoo.core.dto.UpdateEntity;
 import nl.knaw.huygens.timbuctoo.core.dto.UpdateRelation;
 import nl.knaw.huygens.timbuctoo.core.dto.dataset.Collection;
-import nl.knaw.huygens.timbuctoo.core.dto.rdf.CreateProperty;
-import nl.knaw.huygens.timbuctoo.core.dto.rdf.PredicateInUse;
-import nl.knaw.huygens.timbuctoo.core.dto.rdf.RdfProperty;
-import nl.knaw.huygens.timbuctoo.core.dto.rdf.RdfReadProperty;
 import nl.knaw.huygens.timbuctoo.database.tinkerpop.CustomEntityProperties;
 import nl.knaw.huygens.timbuctoo.database.tinkerpop.CustomRelationProperties;
 import nl.knaw.huygens.timbuctoo.model.Change;
 import nl.knaw.huygens.timbuctoo.model.vre.Vre;
-import nl.knaw.huygens.timbuctoo.model.vre.VreMetadata;
 import nl.knaw.huygens.timbuctoo.model.vre.Vres;
 
-import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -83,9 +75,6 @@ public interface DataStoreOperations extends AutoCloseable {
 
   void saveVre(Vre vre);
 
-  Vre ensureVreExists(String vreName);
-
-  void deleteVre(String vreName);
 
   byte[] getVreImageBlob(String vreName);
 
@@ -98,39 +87,5 @@ public interface DataStoreOperations extends AutoCloseable {
 
   List<RelationType> getRelationTypes();
 
-  /**
-   * Only adds the collection to the VRE when the VRE does not contain a collection with the same entity type name.
-   */
-  void addCollectionToVre(Vre vre, CreateCollection createCollection);
 
-  void addPredicateValueTypeVertexToVre(Vre vre);
-
-  void assertProperty(Vre vre, String entityRdfUri, RdfProperty property);
-
-  void retractProperty(Vre vre, String entityRdfUri, RdfProperty property);
-
-  /**
-   * Tries to retrieve a property value.
-   */
-  Optional<RdfReadProperty> retrieveProperty(Vre vre, String entityRdfUri, String propertyUri);
-
-  List<PredicateInUse> getPredicatesFor(Collection defaultCollection);
-
-  /**
-   * @return a list with the rdf uri's of the entities without type
-   */
-  List<String> getEntitiesWithUnknownType(Vre vre);
-
-  /**
-   * Adds the mandatory administrative fields (like id, created, modified, rev) to the entities imported by rdf.
-   */
-  void finishEntities(Vre vre, EntityFinisherHelper entityFinisherHelper);
-
-  /**
-   * @param collection the collection to add the properties to
-   * @param createProperties the properties to add to the collection
-   */
-  void addPropertiesToCollection(Collection collection, List<CreateProperty> createProperties);
-  
-  void setAdminCollection(Collection collection, Collection adminCollection);
 }
--- timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/model/vre/VreStubs.java
+++ timbuctoo-instancev4/src/test/java/nl/knaw/huygens/timbuctoo/model/vre/VreStubs.java
@@ -1,34 +1,10 @@
 package nl.knaw.huygens.timbuctoo.model.vre;
 
 import com.google.common.collect.Maps;
-import nl.knaw.huygens.timbuctoo.core.CollectionNameHelper;
-import nl.knaw.huygens.timbuctoo.core.DataStoreOperations;
-import nl.knaw.huygens.timbuctoo.core.dto.CreateCollection;
-import nl.knaw.huygens.timbuctoo.core.dto.dataset.Collection;
 
 public class VreStubs {
   public static Vre withName(String vreName) {
     return new Vre(vreName, Maps.newHashMap());
   }
 
-  public static Vre minimalCorrectVre(DataStoreOperations dataStoreOperations, String name) {
-    Vre admin = dataStoreOperations.ensureVreExists("Admin");
-    dataStoreOperations.addCollectionToVre(admin, CreateCollection.defaultCollection("Admin"));
-
-
-    Vre vre = dataStoreOperations.ensureVreExists(name);
-    dataStoreOperations.addCollectionToVre(vre, CreateCollection.defaultCollection(name));
-    dataStoreOperations.addPredicateValueTypeVertexToVre(vre);
-    Vres vres = dataStoreOperations.loadVres();
-
-    Collection collection =
-      vres.getVre(name).getCollectionForCollectionName(CollectionNameHelper.defaultCollectionName(name)).get();
-
-    Collection adminCollection =
-      vres.getVre("Admin").getCollectionForCollectionName(CollectionNameHelper.defaultCollectionName(admin)).get();
-
-    dataStoreOperations.setAdminCollection(collection, adminCollection);
-
-    return dataStoreOperations.loadVres().getVre(name);
-  }
 }
