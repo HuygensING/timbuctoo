48de8c0437d53012cb793eec3a811c347fe36c3e
kerim1 <kerim.meijer@di.huc.knaw.nl>
2023-04-24 13:24:00 +0200

    Remove old GraphML code

--- timbuctoo-instancev4/src/main/java/nl/knaw/huygens/timbuctoo/v5/xml/graphml/Gml.java
+++
@@ -1,9 +0,0 @@
-package nl.knaw.huygens.timbuctoo.v5.xml.graphml;
-
-/**
- * Created on 2017-06-06 13:58.
- */
-public interface Gml {
-  String NAMESPACE = "http://graphml.graphdrawing.org/xmlns";
-  String LOCAL_NAME = "graphml";
-}
